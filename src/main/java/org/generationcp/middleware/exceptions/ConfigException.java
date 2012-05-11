68ac56e713a989657c5fb224ea7af0e578d0e357
glenn <glenn@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2012-05-11 13:49:56 +0000

    changed ConfigException into an unchecked exception
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPMiddleware@61 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/middleware/exceptions/ConfigException.java
+++ src/main/java/org/generationcp/middleware/exceptions/ConfigException.java
@@ -1,6 +1,6 @@
 package org.generationcp.middleware.exceptions;
 
-public class ConfigException extends Exception 
+public class ConfigException extends RuntimeException 
 {
 	private static final long serialVersionUID = 1L;
 	
