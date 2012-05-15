fbd015b9063c143c0a3f118b545aa54608833b61
kevin <kevin@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2012-05-15 13:17:01 +0000

    made some changes to tests to avoid unexpected NullPointerException due to data errors
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPMiddleware@77 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/test/java/org/generationcp/middleware/manager/test/TestGermplasmPedigreeQueries.java
+++ src/test/java/org/generationcp/middleware/manager/test/TestGermplasmPedigreeQueries.java
@@ -144,7 +144,8 @@ public class TestGermplasmPedigreeQueries
 		System.out.println("RESULTS:");
 		for(Germplasm g : neighbors)
 		{
-			System.out.println(g.getGid() + " : " + g.getPreferredName().getNval());
+			String name = g.getPreferredName() != null ? g.getPreferredName().getNval() : null;
+			System.out.println(g.getGid() + " : " + name);
 		}
 	}
 	
@@ -158,7 +159,8 @@ public class TestGermplasmPedigreeQueries
 		System.out.println("RESULTS:");
 		for(Germplasm g : neighbors)
 		{
-			System.out.println(g.getGid() + " : " + g.getPreferredName().getNval());
+			String name = g.getPreferredName() != null ? g.getPreferredName().getNval() : null;
+			System.out.println(g.getGid() + " : " + name);
 		}
 	}
 	
@@ -172,7 +174,8 @@ public class TestGermplasmPedigreeQueries
 		System.out.println("RESULTS:");
 		for(Germplasm g : results)
 		{
-			System.out.println(g.getGid() + " : " + g.getPreferredName().getNval());
+			String name = g.getPreferredName() != null ? g.getPreferredName().getNval() : null;
+			System.out.println(g.getGid() + " : " + name);
 		}
 	}
 	
