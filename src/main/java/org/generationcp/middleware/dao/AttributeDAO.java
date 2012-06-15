a43c01f2e7d4f1f3083e57691dcb6a1438fca2ee
joyce <joyce@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2012-06-15 08:38:22 +0000

    Added file header
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPMiddleware@190 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/middleware/dao/AttributeDAO.java
+++ src/main/java/org/generationcp/middleware/dao/AttributeDAO.java
@@ -1,3 +1,15 @@
+/***************************************************************
+ * Copyright (c) 2012, All Rights Reserved.
+ * 
+ * Generation Challenge Programme (GCP)
+ * 
+ * 
+ * This software is licensed for use under the terms of the 
+ * GNU General Public License (http://bit.ly/8Ztv8M) and the 
+ * provisions of Part F of the Generation Challenge Programme 
+ * Amended Consortium Agreement (http://bit.ly/KQX1nL)
+ * 
+ **************************************************************/
 package org.generationcp.middleware.dao;
 
 import java.util.List;
