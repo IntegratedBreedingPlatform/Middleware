2d384818694e71dfffcb086ccd95235620216aff
glenn <glenn@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2012-10-16 05:08:31 +0000

    added javadocs
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPMiddleware@1103 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/middleware/util/SessionFactoryUtil.java
+++ src/main/java/org/generationcp/middleware/util/SessionFactoryUtil.java
@@ -10,18 +10,57 @@ import org.hibernate.cfg.AnnotationConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * <p>
+ * A utility class providing methods for creating {@link SessionFactory}
+ * instances.
+ * </p>
+ * 
+ * @author Glenn Marintes
+ */
 public abstract class SessionFactoryUtil {
     private final static Logger LOG = LoggerFactory.getLogger(SessionFactoryUtil.class);
     
+    /**
+     * The default Hibernate configuration filename.
+     */
     private static final String MIDDLEWARE_INTERNAL_HIBERNATE_CFG = "ibpmidware_hib.cfg.xml";
     
+    /**
+     * Open a {@link SessionFactory} using the default Middleware Hibernate
+     * configuration file.
+     * 
+     * @param params
+     * @return
+     * @throws FileNotFoundException
+     */
     public static SessionFactory openSessionFactory(DatabaseConnectionParameters params) throws FileNotFoundException {
+        return openSessionFactory(MIDDLEWARE_INTERNAL_HIBERNATE_CFG, params);
+    }
+    
+    /**
+     * Open a {@link SessionFactory} using the specified Hibernate configuration
+     * file.
+     * 
+     * @param hibernateConfigurationFile
+     *            the hibernate configuration filename. If null, this method
+     *            will use the default Middleware Hibernate configuration
+     *            filename
+     * @param params
+     * @return
+     * @throws FileNotFoundException
+     */
+    public static SessionFactory openSessionFactory(String hibernateConfigurationFile, DatabaseConnectionParameters params) throws FileNotFoundException {
+        if (hibernateConfigurationFile == null) {
+            hibernateConfigurationFile = MIDDLEWARE_INTERNAL_HIBERNATE_CFG;
+        }
+        
         String connectionUrl = String.format("jdbc:mysql://%s:%s/%s", params.getHost()
                                              , params.getPort()
                                              , params.getDbName()
                                              );
 
-        URL urlOfCfgFile = ResourceFinder.locateFile(MIDDLEWARE_INTERNAL_HIBERNATE_CFG);
+        URL urlOfCfgFile = ResourceFinder.locateFile(hibernateConfigurationFile);
 
         AnnotationConfiguration cfg = new AnnotationConfiguration().configure(urlOfCfgFile);
         cfg.setProperty("hibernate.connection.url", connectionUrl);
