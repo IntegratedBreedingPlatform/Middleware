125effaa3804813cc11ddae1e837800cc2501cfd
Macky <Macky@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-02-06 01:31:20 +0000

    add study browser and list browser to ToolName enum
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPMiddleware@2212 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/middleware/pojos/workbench/ToolName.java
+++ src/main/java/org/generationcp/middleware/pojos/workbench/ToolName.java
@@ -2,6 +2,8 @@ package org.generationcp.middleware.pojos.workbench;
 
 public enum ToolName {
      germplasm_browser
+    ,study_browser
+    ,germplasm_list_browser
     ,germplasm_phenotypic
     ,gdms
     ,fieldbook
