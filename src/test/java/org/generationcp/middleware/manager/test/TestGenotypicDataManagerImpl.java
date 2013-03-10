ffabc49dadeecfb5e93d1ce1042da06ffc6fd5e9
joyce <joyce@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-03-10 23:33:35 +0000

    Update to GCP-816 Test
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPMiddleware@2321 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/test/java/org/generationcp/middleware/manager/test/TestGenotypicDataManagerImpl.java
+++ src/test/java/org/generationcp/middleware/manager/test/TestGenotypicDataManagerImpl.java
@@ -946,7 +946,7 @@ public class TestGenotypicDataManagerImpl{
         Integer markerId = 1037; 
         String charValue = "CV"; 
       
-        CharValues charValues = new CharValues(acId, datasetId, gId, markerId, charValue);
+        CharValues charValues = new CharValues(acId, datasetId, markerId, gId, charValue);
       
         Integer idAdded = manager.addCharValues(charValues);
         System.out.println("testAddCharValues() Added: " + (idAdded != null ? charValues : null));
