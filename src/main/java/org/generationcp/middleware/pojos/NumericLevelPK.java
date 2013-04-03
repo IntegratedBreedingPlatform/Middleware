1f5f643de141b77fd2a05edc7f1196355a26c3f7
tiffany <tiffany@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-04-03 01:21:04 +0000

    GCP-937 & GCP-938
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPMiddleware@2462 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/middleware/pojos/NumericLevelPK.java
+++ src/main/java/org/generationcp/middleware/pojos/NumericLevelPK.java
@@ -14,28 +14,17 @@ package org.generationcp.middleware.pojos;
 
 import java.io.Serializable;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-@Embeddable
 public class NumericLevelPK implements Serializable{
 
     private static final long serialVersionUID = -8070038562792694453L;
 
-    @Basic(optional = false)
-    @Column(name = "labelid")
     private Integer labelId;
 
-    @Basic(optional = false)
-    @Column(name = "factorid")
     private Integer factorId;
 
-    @Basic(optional = false)
-    @Column(name = "levelno")
     private Integer levelNumber;
 
     public NumericLevelPK() {
