99ae9701373dc1fe68537a925dbd5b9cd76683b3
tiffany <tiffany@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-04-08 16:28:23 +0000

    GCP-949 & GCP-983
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPMiddleware@2548 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/middleware/helper/dms/ProjectPropertiesHelper.java
+++ src/main/java/org/generationcp/middleware/helper/dms/ProjectPropertiesHelper.java
@@ -1,84 +1,103 @@
 package org.generationcp.middleware.helper.dms;
 
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.generationcp.middleware.pojos.Factor;
 import org.generationcp.middleware.pojos.dms.CVTermId;
 import org.generationcp.middleware.pojos.dms.ProjectProperty;
 
 public class ProjectPropertiesHelper {
 
-	private Map<Long, String> propertyMap;
-
-	private static final Comparator<ProjectProperty> propertiesComparator = new Comparator<ProjectProperty>() {
-
-		@Override
-		public int compare(ProjectProperty o1, ProjectProperty o2) {
+	private static enum VariableType {STUDY, FACTOR, VARIATE}
 	
-			//1st sort order: by rank
-			if (o1.getRank().equals(o2.getRank())) {
-			
-				//2nd sort order: put cvtermId of "standard variable" on top
-				if (CVTermId.STANDARD_VARIABLE.getId().equals(o1.getTypeId())) {
-					return -1;
-					
-				} else if (CVTermId.STANDARD_VARIABLE.getId().equals(o2.getTypeId())) {
-					return 1;
-					
-				} else {
-					return 0;
-				}
-				
-			} else {
-				return o1.getRank().compareTo(o2.getRank());
-			}
-			
-		}
-		
-	};
+	private static final List<Long> FACTOR_TYPES = Arrays.asList(
+			CVTermId.STUDY_INFORMATION.getId(), CVTermId.GERMPLASM_ENTRY.getId(), CVTermId.TRIAL_DESIGN_INFO.getId(), 
+			CVTermId.TRIAL_ENVIRONMENT_INFO.getId()
+	);
 	
+	private List<Factor> factors;
+	private Map<Long, String> propertyMap = new HashMap<Long, String>();
+	
+
 	public ProjectPropertiesHelper(List<ProjectProperty> properties) {
-				
-		//sort the properties by rank with the "Standard Variable" type on top for each rank
-		Collections.sort(properties, propertiesComparator);
-		
-		this.propertyMap = translateProperties(properties);
+		List<Variable> variableList = createVariables(properties);
+		processVariables(variableList);
 	}
 	
-	private Map<Long, String> translateProperties(List<ProjectProperty> properties) {
-		long rankValue;
-		long valueTerm;
-		long typeTerm;
+	private List<Variable> createVariables(List<ProjectProperty> properties) {
+		Long rankValue, prevRankValue = null;
 		ProjectProperty property;
-		Map<Long, String> map = new HashMap<Long, String>();
- 
+		Variable variable = null;
+		List<Variable> variableList = null;
+		
+		//a list of properties with the same rank constitutes a Variable object
 		for (int i = 0; i < properties.size(); i++) {
 			property = properties.get(i);
-			
-			if (CVTermId.STANDARD_VARIABLE.getId().equals(property.getTypeId())) {
-				rankValue = property.getRank();
-				typeTerm = Long.valueOf(property.getValue());
-				//valueTerm is the CVTerm Id of the property that holds the actual value
-				//in the future, this might change to a "Value" cvterm, 
-				valueTerm = Long.valueOf(property.getValue());
-				
-				//find the value using the valueTermId within the same rank
-				while (rankValue == property.getRank() && i < properties.size()-1) {
-					i++;
-					property = properties.get(i);
-					if (valueTerm == property.getTypeId()) {
-						map.put(typeTerm, property.getValue());
-						i--;
-						break;
-					}
+			rankValue = property.getRank();
+
+			if (rankValue.equals(prevRankValue)) {
+				setPropertyToVariable(property, variable);
+								
+			} else {
+				prevRankValue = rankValue;
+				if (variableList != null) {
+					variableList.add(variable);
+				} else {
+					variableList = new ArrayList<Variable>();
 				}
-				
+				variable = new Variable();
+				setPropertyToVariable(property, variable);
 			}
 		}
-		return map;
+		
+		//last element
+		if (variable != null && variableList != null) {
+			variableList.add(variable);
+		}
+
+		return variableList;
+	}
+	
+	private void setPropertyToVariable(ProjectProperty property, Variable variable) {
+		if (CVTermId.STUDY_INFORMATION.getId().equals(property.getTypeId())) {
+			variable.setType(VariableType.STUDY);
+		} else if (FACTOR_TYPES.contains(property.getTypeId()))	{
+			variable.setType(VariableType.FACTOR);
+		} else if (CVTermId.STANDARD_VARIABLE.getId().equals(property.getTypeId())) {
+			variable.setVarId(Long.valueOf(property.getValue()));
+		} else if (CVTermId.VARIABLE_DESCRIPTION.getId().equals(property.getTypeId())) {
+			variable.setDescription(property.getValue());
+		} else {
+			variable.getUncategorized().put(property.getTypeId(), property.getValue());
+		}
+	}
+	
+	private void processVariables(List<Variable> variableList) {
+		if (variableList != null && variableList.size() > 0) {
+			for (Variable variable : variableList) {
+				if (variable.getType() == VariableType.STUDY) {
+					processStudy(variable);
+				} else if (variable.getType() == VariableType.FACTOR) {
+					processFactor(variable);
+				} else if (variable.getType() == VariableType.VARIATE) {
+					
+				}
+			}
+		}
+	}
+	
+	private void processStudy(Variable variable) {
+		propertyMap.put(variable.getVarId(), variable.getUncategorized().get(variable.getVarId()));
+	}
+	
+	private void processFactor(Variable variable) {
+		//TODO
+		//set the label id or factor id with the variable.varId
+		//set the factor.fname with cvterm.name
 	}
 	
 	public String getString(CVTermId type) {
@@ -88,5 +107,52 @@ public class ProjectPropertiesHelper {
 	public Integer getInteger(CVTermId type) {
 		String value = getString(type);
 		return value != null ? Integer.valueOf(value) : null;
+	}
+	
+	public List<Factor> getFactors() {
+		return this.factors;
+	}
+	
+	class Variable {
+		private VariableType type;
+		private Long varId;
+		private String description;
+		Map<Long, String> uncategorized;
+		
+		Variable() {
+			this.uncategorized = new HashMap<Long, String>();
+		}
+		
+		VariableType getType() {
+			return type;
+		}
+
+		void setType(VariableType type) {
+			this.type = type;
+		}
+
+		Long getVarId() {
+			return varId;
+		}
+
+		void setVarId(Long varId) {
+			this.varId = varId;
+		}
+
+		String getDescription() {
+			return description;
+		}
+
+		void setDescription(String description) {
+			this.description = description;
+		}
+
+		Map<Long, String> getUncategorized() {
+			return uncategorized;
+		}
+
+		void setUncategorized(Map<Long, String> uncategorized) {
+			this.uncategorized = uncategorized;
+		}
 	}
 }
