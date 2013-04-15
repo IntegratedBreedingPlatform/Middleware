9e4b4362cf21f66e42aa1f14be3b46e79cb1b314
joyce <joyce@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-04-15 09:20:39 +0000

    v2 StudyDataManagerImpl
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPMiddleware@2627 4ebb2e00-0138-4608-bc6e-edf3948d4863

---
+++ src/main/java/org/generationcp/middleware/v2/manager/StudyDataManagerImpl.java
@@ -0,0 +1,8 @@
+package org.generationcp.middleware.v2.manager;
+
+import org.generationcp.middleware.manager.DataManager;
+import org.generationcp.middleware.v2.manager.api.StudyDataManager;
+
+public class StudyDataManagerImpl extends DataManager  implements StudyDataManager {
+
+}
