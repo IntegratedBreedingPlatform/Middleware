095d03c81eae4fa7cf8ccafac5a048a6abaad13b
tiffany <tiffany@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-04-19 09:33:48 +0000

    GCP-1015 & GCP-1016
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPMiddleware@2716 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/middleware/v2/pojos/VariableDetails.java
+++ src/main/java/org/generationcp/middleware/v2/pojos/VariableDetails.java
@@ -60,5 +60,35 @@ public abstract class VariableDetails {
 	public void setStudyId(Integer studyId) {
 		this.studyId = studyId;
 	}
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		VariableDetails other = (VariableDetails) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "VariableDetails [id=" + id + ", name=" + name
+				+ ", description=" + description + ", property=" + property
+				+ ", method=" + method + ", scale=" + scale + ", dataType="
+				+ dataType + ", studyId=" + studyId + "]";
+	}
+
 }
