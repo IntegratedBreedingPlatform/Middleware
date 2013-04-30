09084feb5c613bbc141e916e92a2e052662a1466
tiffany <tiffany@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-04-30 06:42:23 +0000

    added rank
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPMiddleware@2798 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/middleware/v2/domain/StudyPropertyConstants.java
+++ src/main/java/org/generationcp/middleware/v2/domain/StudyPropertyConstants.java
@@ -3,19 +3,21 @@ package org.generationcp.middleware.v2.domain;
 
 public enum StudyPropertyConstants {
 
-	USER_ID (CVTermId.STUDY_UID.getId(), "USERID", "User ID")
-	, STUDY_TYPE (CVTermId.STUDY_TYPE.getId(), "TYPE", "Study type")
-	, START_DATE (CVTermId.START_DATE.getId(), "START", "Start date")
+	USER_ID (CVTermId.STUDY_UID.getId(), "USERID", "User ID", 1)
+	, STUDY_TYPE (CVTermId.STUDY_TYPE.getId(), "TYPE", "Study type", 2)
+	, START_DATE (CVTermId.START_DATE.getId(), "START", "Start date", 3)
 	;
 	
 	private Integer cvTermId;
 	private String name;
 	private String description;
+	private Integer rank;
 	
-	StudyPropertyConstants(Integer cvTermId, String name, String description) {
+	StudyPropertyConstants(Integer cvTermId, String name, String description, Integer rank) {
 		this.cvTermId = cvTermId;
 		this.name = name;
 		this.description = description;
+		this.rank = rank;
 	}
 	
 	public Integer getCvTermId() {
@@ -28,5 +30,9 @@ public enum StudyPropertyConstants {
 	
 	public String getDescription() {
 		return this.description;
+	}
+	
+	public Integer getRank() {
+		return this.rank;
 	}
 }
