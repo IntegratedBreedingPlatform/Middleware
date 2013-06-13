e440457fa53eb534fe6c33e8dd4ef487f644551e
aldrin <aldrin@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-06-13 10:09:59 +0000

    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPMiddleware@3974 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/middleware/v2/manager/StudyDataManagerImpl.java
+++ src/main/java/org/generationcp/middleware/v2/manager/StudyDataManagerImpl.java
@@ -363,7 +363,7 @@ public class StudyDataManagerImpl extends DataManager implements StudyDataManage
 			String sql = "select DISTINCT pp.value " + 
 					"from projectprop pp " +
 					"inner join projectprop pp2 on pp.rank = pp2.rank and pp.type_id = 1041 " +
-					"where pp.project_id = :projectId and pp2.value = :standardVariableId";
+					"where pp.project_id = :projectId and pp2.value = :standardVariableId LIMIT 0,1";
 
 			
 			Query query = session.createSQLQuery(sql);
