4e8ec79fe2da37bc9a0d7a328eeb7bd4a54a1d82
tippsgo <tiffany@efficio.us.com>
2013-09-09 14:56:57 +0800

    GCP-5600

--- src/main/java/org/generationcp/middleware/operation/transformer/etl/VariableTypeListTransformer.java
+++ src/main/java/org/generationcp/middleware/operation/transformer/etl/VariableTypeListTransformer.java
@@ -1,21 +1,37 @@
 package org.generationcp.middleware.operation.transformer.etl;
 
+import java.util.List;
+
+import org.generationcp.middleware.domain.dms.StandardVariable;
+import org.generationcp.middleware.domain.dms.VariableType;
 import org.generationcp.middleware.domain.dms.VariableTypeList;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 
-import java.util.List;
-
 public class VariableTypeListTransformer extends Transformer {
-	public VariableTypeListTransformer(HibernateSessionProvider sessionProviderForLocal,
+	
+    public VariableTypeListTransformer(HibernateSessionProvider sessionProviderForLocal,
             HibernateSessionProvider sessionProviderForCentral) {
 				super(sessionProviderForLocal, sessionProviderForCentral);
 	}
 	
-	public VariableTypeList transform(List<MeasurementVariable> mvList) throws MiddlewareQueryException {
-		VariableTypeList variableTypeList = null;
-		
-		return variableTypeList ;
+	public VariableTypeList transform(List<MeasurementVariable> measurementVariables) throws MiddlewareQueryException {
+		VariableTypeList variableTypeList = new VariableTypeList();
+		int rank = 1;
+		for (MeasurementVariable measurementVariable : measurementVariables) {
+			StandardVariable standardVariable = getOntologyDataManager().findStandardVariableByTraitScaleMethodNames(
+						measurementVariable.getProperty(), 
+						measurementVariable.getScale(), 
+						measurementVariable.getMethod());
+			
+			VariableType variableType = new VariableType(
+						measurementVariable.getName(), 
+						measurementVariable.getDescription(), 
+						standardVariable, rank++);
+			
+			variableTypeList.add(variableType);
+		}
+		return variableTypeList;
 	}
 }
