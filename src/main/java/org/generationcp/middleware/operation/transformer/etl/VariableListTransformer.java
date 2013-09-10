3b2bff6d838edbf6fca34a3e9323ffb262abcf49
Crisjaira Villarino <crisjaira@efficio.us.com>
2013-09-10 12:37:21 +0800

    GCP-5604

--- src/main/java/org/generationcp/middleware/operation/transformer/etl/VariableListTransformer.java
+++ src/main/java/org/generationcp/middleware/operation/transformer/etl/VariableListTransformer.java
@@ -1,5 +1,8 @@
 package org.generationcp.middleware.operation.transformer.etl;
 
+import java.util.List;
+
+import org.generationcp.middleware.domain.dms.ExperimentValues;
 import org.generationcp.middleware.domain.dms.FactorType;
 import org.generationcp.middleware.domain.dms.Variable;
 import org.generationcp.middleware.domain.dms.VariableList;
@@ -31,9 +34,25 @@ public class VariableListTransformer {
 		return variableList;
 	}
 	
-	public VariableList transformTrialEnvironment(MeasurementRow mRow) throws MiddlewareQueryException {
-		VariableList variableList = null;
+	public VariableList transformTrialEnvironment(MeasurementRow mRow, VariableTypeList variableTypeList) throws MiddlewareQueryException {
+		VariableList variableList = new VariableList() ;
+		
+		if (mRow != null && mRow.getDataList() != null && variableTypeList != null && variableTypeList.getVariableTypes() != null) {
+			if (mRow.getDataList().size() == variableTypeList.getVariableTypes().size()) {
+				int i = 0;
+				for (VariableType variableType : variableTypeList.getVariableTypes()) {
+					if (variableType.getStandardVariable().getFactorType() == FactorType.TRIAL_ENVIRONMENT) {
+						variableList.add(new Variable(variableType, mRow.getDataList().get(i).getValue()));
+					}
+					i++;
+				}
+				
+			} else {//else invalid data
+				throw new MiddlewareQueryException("Variables did not match the Measurements Row.");
+			}
+		}
 		
 		return variableList;
 	}
+	
 }
