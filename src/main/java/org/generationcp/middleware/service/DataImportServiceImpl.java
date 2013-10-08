6feff310e4c17049622da262ba519acd12bfb50d
Aldrin Batac <aldrin@efficio.us.com>
2013-10-08 17:24:15 +0800

    GCP-5800

--- src/main/java/org/generationcp/middleware/service/DataImportServiceImpl.java
+++ src/main/java/org/generationcp/middleware/service/DataImportServiceImpl.java
@@ -15,11 +15,15 @@ import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.generationcp.middleware.domain.dms.StandardVariable;
+import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.domain.etl.Workbook;
 import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 import org.generationcp.middleware.manager.Database;
+import org.generationcp.middleware.manager.OntologyDataManagerImpl;
+import org.generationcp.middleware.manager.api.OntologyDataManager;
 import org.generationcp.middleware.operation.parser.WorkbookParser;
 import org.generationcp.middleware.operation.parser.WorkbookParserException;
 import org.generationcp.middleware.service.api.DataImportService;
@@ -84,6 +88,10 @@ public class DataImportServiceImpl extends Service implements DataImportService 
         if (studyId != null) {
             messages.add(new Message("error.duplicate.study.name"));
         }
+        
+        if (!isEntryExists(workbook.getFactors())){
+        	messages.add(new Message("error.entry.doesnt.exist"));
+        }
 
         if (messages.size() > 0) {
             throw new WorkbookParserException(messages);
@@ -111,4 +119,25 @@ public class DataImportServiceImpl extends Service implements DataImportService 
         }
         return id;
     }
+    
+    private Boolean isEntryExists(java.util.List<MeasurementVariable> list){
+    	OntologyDataManagerImpl ontology = new OntologyDataManagerImpl(getSessionProviderForLocal(), getSessionProviderForCentral());
+		for (MeasurementVariable mvar : list){
+			try {
+				StandardVariable svar = ontology.findStandardVariableByTraitScaleMethodNames(mvar.getProperty(), mvar.getScale(), mvar.getMethod());
+				if (svar != null){
+					if (svar.getStoredIn() != null){
+						if (svar.getStoredIn().getId() == 1041){
+							return true;
+						}
+					}
+				}
+			} catch (MiddlewareQueryException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+    
 }
