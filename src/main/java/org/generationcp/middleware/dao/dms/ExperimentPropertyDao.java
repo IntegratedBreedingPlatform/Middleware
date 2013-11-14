faadd02fe60baf6b4560056bb652de02c6290e0d
Joyce Avestro <joyce.avestro@gmail.com>
2013-11-14 13:43:58 +0800

    GCP-6073

--- src/main/java/org/generationcp/middleware/dao/dms/ExperimentPropertyDao.java
+++ src/main/java/org/generationcp/middleware/dao/dms/ExperimentPropertyDao.java
@@ -47,87 +47,6 @@ public class ExperimentPropertyDao extends GenericDAO<ExperimentProperty, Intege
 		return new ArrayList<Integer>();
 	}
 	
-//	
-//	@SuppressWarnings("unchecked")
-//    public List<Integer> getRepsOfProject(int projectId) throws MiddlewareQueryException{
-//	    
-//        List<Integer> reps = new ArrayList<Integer>();
-//
-//        /*
-//             SELECT eprop.value 
-//             FROM nd_experiment_project eprojREP 
-//                INNER JOIN nd_experimentprop eprop ON eprojREP.nd_experiment_id = eprop.nd_experiment_id
-//                        AND eprop.type_id = 8210 AND eprojREP.project_id = @project_id
-//                        AND eprop.value IS NOT NULL  AND eprop.value <> ''
-//         */
-//        try {
-//            
-//            StringBuilder sql = new StringBuilder()
-//                    .append("SELECT eprop.value ")
-//                    .append("FROM nd_experiment_project eprojREP ")
-//                    .append("    INNER JOIN nd_experimentprop eprop ON eprojREP.nd_experiment_id = eprop.nd_experiment_id ")
-//                    .append("            AND eprop.type_id = " + TermId.REP_NO.getId()  + " AND eprojREP.project_id = :projectId ") // 8210
-//                    .append("            AND eprop.value IS NOT NULL  AND eprop.value <> '' ")
-//                    ;
-//
-//            Query query = getSession().createSQLQuery(sql.toString());
-//            query.setParameter("projectId", projectId);
-//
-//            List<String> repsString = query.list();
-//            
-//            for (String repString : repsString){
-//                reps.add(Integer.parseInt(repString));
-//            }
-//            
-//        } catch(HibernateException e) {
-//            logAndThrowException("Error at getReps(projectId=" + projectId + ") at ExperimentPropertyDao: " + e.getMessage(), e);
-//        }
-//        
-//        return reps;
-//	    
-//	}
-//	
-//    public long getPlotCount(int projectId) throws MiddlewareQueryException{
-//        long count = 0;
-//
-//        /*  
-//            SELECT CASE WHEN type_id = 8200 THEN MAX(CAST(eprop.value AS UNSIGNED))
-//                    WHEN type_id = 8380  THEN MAX(CAST(eprop.value AS SIGNED)) - 100
-//                    END AS PLOT_COUNT
-//            FROM nd_experiment_project eproj 
-//                INNER JOIN nd_experimentprop eprop ON eproj.nd_experiment_id = eprop.nd_experiment_id
-//                        AND eprop.type_id IN (8200, 8380) AND eproj.project_id = @projectId
-//                        AND eprop.value IS NOT NULL  AND eprop.value <> ''
-//        */
-//        try {
-//            StringBuilder sql = new StringBuilder()
-//                    .append("SELECT CASE WHEN type_id = " + TermId.PLOT_NO.getId() + " THEN MAX(CAST(eprop.value AS UNSIGNED)) ")  // PLOT_NO (8200), PLOT_NNO (8380)
-//                    .append("        WHEN type_id = " + TermId.PLOT_NNO.getId() + "  THEN MAX(CAST(eprop.value AS SIGNED)) - 100 ")
-//                    .append("        END AS PLOT_COUNT ")
-//                    .append("FROM nd_experiment_project eproj ")
-//                    .append("    INNER JOIN nd_experimentprop eprop ON eproj.nd_experiment_id = eprop.nd_experiment_id ")
-//                    .append("            AND eprop.type_id IN ("+ TermId.PLOT_NO.getId() + ", "+ TermId.PLOT_NNO.getId() +") AND eproj.project_id = :projectId ") // PLOT_NO (8200), PLOT_NNO (8380)
-//                    .append("            AND eprop.value IS NOT NULL  AND eprop.value <> '' ")
-//                    ;
-//
-//            Query query = getSession().createSQLQuery(sql.toString());
-//            query.setParameter("projectId", projectId);
-//
-//            Object result = query.uniqueResult();
-//            if (result != null){
-//            	count = ((BigInteger) result).longValue();
-//            }
-//            
-//        } catch(HibernateException e) {
-//            logAndThrowException("Error at getPlotCount(projectId=" + projectId + ") at ExperimentPropertyDao: " + e.getMessage(), e);
-//        }
-//        
-//        return count;
-//        
-//    }
-//	
-//    
-
     @SuppressWarnings("unchecked")
 	public List<FieldMapLabel> getFieldMapLabels(int projectId) throws MiddlewareQueryException{
     	List<FieldMapLabel> labels = new ArrayList<FieldMapLabel>();
@@ -187,8 +106,11 @@ public class ExperimentPropertyDao extends GenericDAO<ExperimentProperty, Intege
                     String rep = (String) row[3];
                     String plotNo = (String) row[4];
 
-                    labels.add(new FieldMapLabel(experimentId, Integer.parseInt(entryNumber), 
-                    		germplasmName, Integer.parseInt(rep), Integer.parseInt(plotNo)));
+                    labels.add(new FieldMapLabel(experimentId 
+                    			, (entryNumber == null ? null : Integer.parseInt(entryNumber))
+                    			, germplasmName
+                    			, (rep == null ? 1 : Integer.parseInt(rep))
+                    			, (plotNo == null ? 0 : Integer.parseInt(plotNo))));
                 }
             }
             
