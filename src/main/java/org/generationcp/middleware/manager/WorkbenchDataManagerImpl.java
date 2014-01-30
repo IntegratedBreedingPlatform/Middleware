IntegratedBreedingPlatform/Middleware

commit b94e78b83bb686030b174722d4b9c2f170bfce71

added installationDirectory property

src/main/java/org/generationcp/middleware/manager/WorkbenchDataManagerImpl.java
@@ -64,6 +64,8 @@ public class WorkbenchDataManagerImpl implements WorkbenchDataManager {
     private WorkbenchSidebarCategoryDAO workbenchSidebarCategoryDAO;
     private WorkbenchSidebarCategoryLinkDAO workbenchSidebarCategoryLinkDAO;
 
+    private String installationDirectory;
+    
     public WorkbenchDataManagerImpl(HibernateSessionProvider sessionProvider) {
         this.sessionProvider = sessionProvider;
     }
@@ -1403,7 +1405,11 @@ public class WorkbenchDataManagerImpl implements WorkbenchDataManager {
     public WorkbenchSetting getWorkbenchSetting() throws MiddlewareQueryException {
         try {
             List<WorkbenchSetting> list = getWorkbenchSettingDao().getAll();
-            return list.isEmpty() ? null : list.get(0);
+            WorkbenchSetting setting = list.isEmpty() ? null : list.get(0);
+            
+            if (setting != null && installationDirectory != null) {
+                setting.setInstallationDirectory(installationDirectory);
+            }
         } catch (Exception e) {
             logAndThrowException("Error encountered while getting workbench setting: " + e.getMessage(), e);
         }
@@ -1649,4 +1655,11 @@ public class WorkbenchDataManagerImpl implements WorkbenchDataManager {
         return workbenchSidebarCategoryLinkDAO;
     }
 
+    public String getInstallationDirectory() {
+        return installationDirectory;
+    }
+
+    public void setInstallationDirectory(String installationDirectory) {
+        this.installationDirectory = installationDirectory;
+    }
 }

commit 1f4e9c9a5e16ddae066026f58363435a178d42a6

fix for workbench setting not found bug introduced in b94e78b83bb686030b174722d4b9c2f170bfce71

src/main/java/org/generationcp/middleware/manager/WorkbenchDataManagerImpl.java
@@ -1410,6 +1410,8 @@ public class WorkbenchDataManagerImpl implements WorkbenchDataManager {
             if (setting != null && installationDirectory != null) {
                 setting.setInstallationDirectory(installationDirectory);
             }
+            
+            return setting;
         } catch (Exception e) {
             logAndThrowException("Error encountered while getting workbench setting: " + e.getMessage(), e);
         }