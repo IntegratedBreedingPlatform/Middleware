c954aba6c03ead1017fe06c57c37861d7ecaae46
chezka <chezka@efficio.us.com>
2014-01-31 09:22:20 +0800

    GCP-7319

--- src/main/java/org/generationcp/middleware/operation/builder/WorkbookBuilder.java
+++ src/main/java/org/generationcp/middleware/operation/builder/WorkbookBuilder.java
@@ -15,9 +15,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.generationcp.middleware.domain.dms.DataSetType;
+import org.generationcp.middleware.domain.dms.DatasetReference;
 import org.generationcp.middleware.domain.dms.Experiment;
+import org.generationcp.middleware.domain.dms.PhenotypicType;
 import org.generationcp.middleware.domain.dms.Variable;
 import org.generationcp.middleware.domain.dms.VariableList;
+import org.generationcp.middleware.domain.dms.VariableTypeList;
 import org.generationcp.middleware.domain.etl.MeasurementData;
 import org.generationcp.middleware.domain.etl.MeasurementRow;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
@@ -33,36 +36,49 @@ public class WorkbookBuilder extends Builder {
 	public WorkbookBuilder(HibernateSessionProvider sessionProviderForLocal,
 			                   HibernateSessionProvider sessionProviderForCentral) {
 		super(sessionProviderForLocal, sessionProviderForCentral);
-	}
+	} 
 	
 	public Workbook create(int id) throws MiddlewareQueryException {
 		Workbook workbook = new Workbook();
 		
 		/*
-                 * 1. findOneDataSetByType get datasetid only
-                 * 2. countExperiments
+                 * 1. Get the dataset id
+                 * 2. Count total no. of experiments of the dataset
                  * 3. getExperiments
-                 * 4. per experiment, transform it to MeasurementRow
-                 *  a. MeasurementRow stockid, locationid, list of MeasurementData
-                 *  b. MeasurementData label (???), 
+                 * 4. Per experiment, transform it to MeasurementRow
+                 *  a. MeasurementRow (list of MeasurementData) 
+                 *  b. MeasurementData label (Experiment > VariableList > Variable > localName), 
                  *  value (Experiment > VariableList > Variable), 
                  *  datatype (Experiment > VariableList > Variable > VariableType > StandardVariable), 
                  *  iseditable (true for variates, else, false)
                  *  
-                 *  to do: add storedin and phenotypicId
                  * */
 		
 		StudyDetails studyDetails = getStudyDataManager().getStudyDetails(Database.LOCAL, StudyType.N, id);
+		int dataSetId = 0;
 		
-		//to do: change datasettype
-		int dataSetId = getStudyDataManager().findOneDataSetByType(id, DataSetType.PLOT_DATA).getId();
+		//get observation dataset
+		List<DatasetReference> datasetRefList = getStudyDataManager().getDatasetReferences(id);
+		if (datasetRefList != null) {
+		    for (DatasetReference datasetRef : datasetRefList) {
+		        if (datasetRef.getName().equals("MEASUREMENT_EFEC_" + studyDetails.getStudyName())) {
+		            dataSetId = datasetRef.getId();
+		        }
+		    }
+		}
+		
+		//if not found in the list using the name, get dataset with Plot Data type
+		if (dataSetId == 0) {
+		    dataSetId = getStudyDataManager().findOneDataSetByType(id, DataSetType.PLOT_DATA).getId();
+		}
+		
 		long expCount = getStudyDataManager().countExperiments(dataSetId);
 		
 		List<Experiment> experiments = getStudyDataManager().getExperiments(dataSetId, 1, (int)expCount);
 		
-		List<MeasurementRow> observations = buildObservations(experiments);
 		List<MeasurementVariable> factors = buildFactors(experiments);		
 		List<MeasurementVariable> variates = buildVariates(experiments);
+		List<MeasurementRow> observations = buildObservations(experiments);
 		
 		workbook.setStudyDetails(studyDetails);
 		workbook.setFactors(factors);
@@ -84,15 +100,17 @@ public class WorkbookBuilder extends Builder {
 	        List<MeasurementData> measurementDataList = new ArrayList<MeasurementData>();
 	        
 	        for (Variable variable : factors.getVariables()) {
-	            MeasurementData measurementData = new MeasurementData(variable.getVariableType().getLocalName(), 
-	                    variable.getValue(), false, 
-	                    variable.getVariableType().getStandardVariable().getDataType().getName());
-	            measurementDataList.add(measurementData);
+	            if (!PhenotypicType.TRIAL_ENVIRONMENT.getLabelList().contains(getLabelOfStoredIn(variable.getVariableType().getStandardVariable().getStoredIn().getId()))) {
+                        MeasurementData measurementData = new MeasurementData(variable.getVariableType().getLocalName(), 
+                                variable.getValue(), false, 
+                                variable.getVariableType().getStandardVariable().getDataType().getName());
+                        measurementDataList.add(measurementData);
+                    }
 	        }
 	        
 	        for (Variable variable : variates.getVariables()) {
                     MeasurementData measurementData = new MeasurementData(variable.getVariableType().getLocalName(), 
-                            variable.getValue(), false, 
+                            variable.getValue(), true,  
                             variable.getVariableType().getStandardVariable().getDataType().getName());
                     measurementData.setPhenotypeId(variable.getPhenotypeId());
                     measurementDataList.add(measurementData);
@@ -107,10 +125,20 @@ public class WorkbookBuilder extends Builder {
 	    return observations;
 	}
 	
+	private String getLabelOfStoredIn(int storedIn) {
+            return PhenotypicType.getPhenotypicTypeById(storedIn).getLabelList().get(0);
+        }
+	
 	private List<MeasurementVariable> buildFactors(List<Experiment> experiments) {
 	    List<MeasurementVariable> factors = new ArrayList<MeasurementVariable>();
-	    for (Experiment experiment : experiments) {                
-                factors = getMeasurementVariableTransformer().transform(experiment.getFactors().getVariableTypes(), true);
+	    VariableTypeList factorList = new VariableTypeList();
+            for (Experiment experiment : experiments) {    
+                for (Variable variable : experiment.getFactors().getVariables()) {
+                    if (!PhenotypicType.TRIAL_ENVIRONMENT.getLabelList().contains(getLabelOfStoredIn(variable.getVariableType().getStandardVariable().getStoredIn().getId()))) {
+                        factorList.add(variable.getVariableType());
+                    }
+                }
+                factors = getMeasurementVariableTransformer().transform(factorList, true);
                 break;
             }
 	    return factors;
--- src/main/java/org/generationcp/middleware/operation/transformer/etl/MeasurementVariableTransformer.java
+++ src/main/java/org/generationcp/middleware/operation/transformer/etl/MeasurementVariableTransformer.java
@@ -8,6 +8,7 @@ import org.generationcp.middleware.domain.dms.VariableType;
 import org.generationcp.middleware.domain.dms.VariableTypeList;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
+import org.generationcp.middleware.domain.dms.PhenotypicType;
 
 public class MeasurementVariableTransformer extends Transformer {
 	
@@ -23,10 +24,12 @@ public class MeasurementVariableTransformer extends Transformer {
 	    if (variableTypeList != null && variableTypeList.size() > 0) {
 	        for (VariableType variableType : variableTypeList.getVariableTypes()) {
 	            StandardVariable stdVariable = variableType.getStandardVariable();
+	            String label = getLabelOfStoredIn(stdVariable.getStoredIn().getId());
+	            
 	            MeasurementVariable measurementVariable = new MeasurementVariable(stdVariable.getId(), stdVariable.getName(), 
 	                    stdVariable.getDescription(), stdVariable.getScale().getName(), stdVariable.getMethod().getName(),
 	                    stdVariable.getProperty().getName(), stdVariable.getDataType().getName(), "", 
-	                    variableType.getLocalName());
+	                    label);
 	            measurementVariable.setStoredIn(stdVariable.getStoredIn().getName());
 	            measurementVariable.setFactor(isFactor);
 	            measurementVariables.add(measurementVariable);
@@ -35,4 +38,8 @@ public class MeasurementVariableTransformer extends Transformer {
 	    
 	    return measurementVariables;
 	}
+	
+	private String getLabelOfStoredIn(int storedIn) {
+            return PhenotypicType.getPhenotypicTypeById(storedIn).getLabelList().get(0);
+        }
 }
