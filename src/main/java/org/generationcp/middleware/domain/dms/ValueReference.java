39931f8ce20f89af758c641555abb9a88f1b88b0
tiffanygo <tiffany@efficio.us.com>
2014-02-13 14:33:59 +0800

    use reference instead of datalistelement

---
+++ src/main/java/org/generationcp/middleware/domain/dms/ValueReference.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2013, All Rights Reserved.
+ * 
+ * Generation Challenge Programme (GCP)
+ * 
+ * 
+ * This software is licensed for use under the terms of the GNU General Public
+ * License (http://bit.ly/8Ztv8M) and the provisions of Part F of the Generation
+ * Challenge Programme Amended Consortium Agreement (http://bit.ly/KQX1nL)
+ * 
+ *******************************************************************************/
+package org.generationcp.middleware.domain.dms;
+
+public class ValueReference extends Reference {
+
+}
