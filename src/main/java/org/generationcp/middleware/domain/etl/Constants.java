070e327a6b908b3b1bfcdc945faeec923db96cae
Mae Isabelle Turiana <mae@efficio.us.com>
2014-03-06 17:46:22 +0800

    GCP-7647 and GCP-7645

--- src/main/java/org/generationcp/middleware/domain/etl/Constants.java
+++ src/main/java/org/generationcp/middleware/domain/etl/Constants.java
@@ -2,4 +2,5 @@ package org.generationcp.middleware.domain.etl;
 
 public class Constants {
 	public static final String GLOBAL = "GLOBAL";
+	public static final String MESSAGE_KEY_GENERIC_ERROR = "error.generic.unhandled.exception";
 }
