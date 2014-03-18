28edf7e13c7d7864027f3f6562df05f8ef7e9e4c
tiffanygo <tiffany@efficio.us.com>
2014-03-18 14:55:23 +0800

    GCP-7829

--- src/main/java/org/generationcp/middleware/pojos/LocdesType.java
+++ src/main/java/org/generationcp/middleware/pojos/LocdesType.java
@@ -2,26 +2,26 @@ package org.generationcp.middleware.pojos;
 
 public enum LocdesType {
 	
-	COLUMNS_IN_BLOCK(10002)
-	, RANGES_IN_BLOCK(10003)
-	, PLANTING_ORDER(10005)
-	, ROWS_IN_PLOT(10004)
-	, MACHINE_ROW_CAPACITY(10006)
-	, DELETED_PLOTS(10007)
+	COLUMNS_IN_BLOCK("COL_IN_BLK")
+	, RANGES_IN_BLOCK("RANGE_IN_BLK")
+	, PLANTING_ORDER("PLOT_LYOUT")
+	, ROWS_IN_PLOT("ROWS_IN_PLOT")
+	, MACHINE_ROW_CAPACITY("MACHINE_CAP")
+	, DELETED_PLOTS("DELETED_PLOT")
 	;
 	
-	private int id;
+	private String code;
 	
-	private LocdesType(int id) {
-		this.id = id;
+	private LocdesType(String code) {
+		this.code = code;
 	}
 
-	public int getId() {
-		return id;
+	public String getCode() {
+		return code;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setCode(String code) {
+		this.code = code;
 	}
 
 }
