1a5e7214d81400261418351328d9cc4077409dd6
Aldrin Batac <aldrin@efficio.us.com>
2014-03-20 11:15:20 +0800

    GCP-7861 , save the result of the formula, not the formula itself

--- src/main/java/org/generationcp/middleware/util/PoiUtil.java
+++ src/main/java/org/generationcp/middleware/util/PoiUtil.java
@@ -78,7 +78,18 @@ public class PoiUtil {
             		return doubleVal;
             	}
             case Cell.CELL_TYPE_FORMULA:
-                return cell.getCellFormula();
+          
+            	 switch(cell.getCachedFormulaResultType()) {
+            	 
+                 case Cell.CELL_TYPE_NUMERIC:
+                     return cell.getNumericCellValue();
+                 case Cell.CELL_TYPE_STRING:
+                     return cell.getRichStringCellValue();
+                 default: return cell.getCellFormula();    
+                    
+            	 }
+            	
+               
             default:
                 return null;
         }
