7a873a5c3053a80a969cfbbb16ae8ad30942de65
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-09-29 14:07:01 +0800

    Cleanup: Remove hardcoded values in test

--- src/test/java/org/generationcp/middleware/dao/ProjectUserInfoDAOTest.java
+++ src/test/java/org/generationcp/middleware/dao/ProjectUserInfoDAOTest.java
@@ -13,33 +13,55 @@
 package org.generationcp.middleware.dao;
 
 import org.generationcp.middleware.MiddlewareIntegrationTest;
+import org.generationcp.middleware.pojos.User;
 import org.generationcp.middleware.pojos.workbench.ProjectUserInfo;
 import org.generationcp.middleware.util.Debug;
+import org.hibernate.Session;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 public class ProjectUserInfoDAOTest extends MiddlewareIntegrationTest {
 
     private ProjectUserInfoDAO dao;
+    private UserDAO userDao;
+    private ProjectUserRoleDAO projectUserRoleDao;
 
     @Before
     public void setUp() throws Exception {
+        Session currentSession = workbenchSessionUtil.getCurrentSession();
+
+
         dao = new ProjectUserInfoDAO();
-        dao.setSession(workbenchSessionUtil.getCurrentSession());
+        dao.setSession(currentSession);
+
+        userDao = new UserDAO();
+        projectUserRoleDao = new ProjectUserRoleDAO();
+
+        userDao.setSession(currentSession);
+        projectUserRoleDao.setSession(currentSession);
+
     }
 
     @Test
     public void testGetByProjectIdAndUserId() throws Exception {
-           	
-        ProjectUserInfo result = dao.getByProjectIdAndUserId(2, 1);
+        List<User> usersList = userDao.getAll();
+
+        int userId = usersList.get(0).getUserid();
+        int projectId = projectUserRoleDao.getProjectsByUser(usersList.get(0)).get(0).getProjectId().intValue();
+
+        ProjectUserInfo result = dao.getByProjectIdAndUserId(projectId,userId);
+
+        Assert.assertNotNull(result);
+
         if (result == null){
-        	Debug.println(0, "testGetByProjectIdAndUserId RESULTS: no result");
+            Debug.println(0, "testGetByProjectIdAndUserId RESULTS: no result");
         }else{
             Debug.println(0, "testGetByProjectIdAndUserId RESULTS:" + result.getProjectId());
             Debug.println(0, result.toString());
         }
-        
-        
     }
 }
 
