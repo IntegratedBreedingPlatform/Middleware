fb94eb6d185efcdff81814989bb62c3d2ec1f8ed
Naymesh <naymesh@leafnode.io>
2014-12-12 14:03:05 +1300

    Cleanup: Create all DAOs in same way (local flyweight objects v/s instance variables).
    Issue: BMS-176

--- src/main/java/org/generationcp/middleware/util/DatabaseBroker.java
+++ src/main/java/org/generationcp/middleware/util/DatabaseBroker.java
@@ -93,66 +93,13 @@ import org.hibernate.Transaction;
 
 public class DatabaseBroker {
 
-    public final static String NO_LOCAL_INSTANCE_MSG = "There is no connection to a local instance.";
-    public final static String NO_CENTRAL_INSTANCE_MSG = "There is no connection to a central instance.";
-
     protected HibernateSessionProvider sessionProviderForLocal;
     protected String localDatabaseName;
 
     protected static final int JDBC_BATCH_SIZE = 50;
   
-    // GDMS DAOs
-    private NameDAO nameDao;
-    private AccMetadataSetDAO accMetadataSetDao;
-    private AlleleValuesDAO alleleValuesDao;
-    private CharValuesDAO charValuesDao;
-    private DartValuesDAO dartValuesDao;
-    private DatasetDAO datasetDao;
-    private DatasetUsersDAO datasetUsersDao;
-    private MapDAO mapDao;
-    private MappingDataDAO mappingDataDao;
-    private MappingPopDAO mappingPopDao;
-    private MappingPopValuesDAO mappingPopValuesDao;
-    private MarkerAliasDAO markerAliasDao;
-    private MarkerDAO markerDao;
-    private MarkerDetailsDAO markerDetailsDao;
-    private MarkerInfoDAO markerInfoDao;
-    private ExtendedMarkerInfoDAO extendedMarkerInfoDAO;
-    private MarkerMetadataSetDAO markerMetadataSetDao;
-    private MarkerOnMapDAO markerOnMapDao;
-    private MarkerUserInfoDAO markerUserInfoDao;
-    private MarkerUserInfoDetailsDAO markerUserInfoDetailsDao;
-    private QtlDAO qtlDao;
-    private QtlDetailsDAO qtlDetailsDao;
-    private MtaDAO mtaDao;
-    private MtaMetadataDAO mtaMetadataDao;
-    private TrackDataDAO trackDataDao;
-    private TrackMarkerDAO trackMarkerDao;
-
-    
-    // GermplasmDataManager DAOs
-    private AttributeDAO attributeDao;
-    private BibrefDAO bibrefDao;
-    private GermplasmDAO germplasmDao;
-    private LocationDAO locationDao;
-    private LocdesDAO locdesDao;
-    private MethodDAO methodDao;
-    private ProgenitorDAO progenitorDao;
-    private UserDefinedFieldDAO userDefinedFieldDao;
-    private ProgramFavoriteDAO programFavoriteDao;
-
-    // GermplasmListDataManager DAOs
-    private GermplasmListDAO germplasmListDao;
-	private GermplasmListDataDAO germplasmListDataDao;
-	private ListDataPropertyDAO listDataPropertyDao;
-	private ListDataProjectDAO listDataProjectDao;
-	
-	// InventoryDataManager DAOs
-    private LotDAO lotDao;
-    private PersonDAO personDao;
-    private TransactionDAO transactionDao;
-
     protected DatabaseBroker() {
+ 
     }
 
     protected DatabaseBroker(HibernateSessionProvider sessionProviderForLocal) {
@@ -194,8 +141,6 @@ public class DatabaseBroker {
         }
     }
 
-
-    //================================  StudyDataManagerv2 DAO Methods =============================
 
     protected final DmsProjectDao getDmsProjectDao() {
 	    DmsProjectDao dmsProjectDao = new DmsProjectDao();
@@ -343,373 +288,274 @@ public class DatabaseBroker {
     	return cvTermSynonymDao;
     }
     
-    //================================  GDMS DAO Methods =============================
-    
     protected final NameDAO getNameDao() {
-        if (nameDao == null) {
-            nameDao = new NameDAO();
-        }
+    	NameDAO nameDao = new NameDAO();
         nameDao.setSession(getActiveSession());
         return nameDao;
     }
 
     protected final AccMetadataSetDAO getAccMetadataSetDao() {
-        if (accMetadataSetDao == null) {
-            accMetadataSetDao = new AccMetadataSetDAO();
-        }
+    	AccMetadataSetDAO accMetadataSetDao = new AccMetadataSetDAO();
         accMetadataSetDao.setSession(getActiveSession());
         return accMetadataSetDao;
     }
 
     protected final AlleleValuesDAO getAlleleValuesDao() {
-        if (alleleValuesDao == null) {
-            alleleValuesDao = new AlleleValuesDAO();
-        }
+    	AlleleValuesDAO alleleValuesDao = new AlleleValuesDAO();
         alleleValuesDao.setSession(getActiveSession());
         return alleleValuesDao;
     }
 
     protected final CharValuesDAO getCharValuesDao() {
-        if (charValuesDao == null) {
-            charValuesDao = new CharValuesDAO();
-        }
+    	CharValuesDAO  charValuesDao = new CharValuesDAO();
         charValuesDao.setSession(getActiveSession());
         return charValuesDao;
     }
 
     protected final DartValuesDAO getDartValuesDao() {
-        if (dartValuesDao == null) {
-            dartValuesDao = new DartValuesDAO();
-        }
+    	DartValuesDAO dartValuesDao = new DartValuesDAO();
         dartValuesDao.setSession(getActiveSession());
         return dartValuesDao;
     }
 
     protected final DatasetDAO getDatasetDao() {
-        if (datasetDao == null) {
-            datasetDao = new DatasetDAO();
-        }
+    	DatasetDAO datasetDao = new DatasetDAO();
         datasetDao.setSession(getActiveSession());
         return datasetDao;
     }
 
     protected final DatasetUsersDAO getDatasetUsersDao() {
-        if (datasetUsersDao == null) {
-            datasetUsersDao = new DatasetUsersDAO();
-        }
+    	DatasetUsersDAO    datasetUsersDao = new DatasetUsersDAO();
         datasetUsersDao.setSession(getActiveSession());
         return datasetUsersDao;
     }
 
     protected final MapDAO getMapDao() {
-        if (mapDao == null) {
-            mapDao = new MapDAO();
-        }
+    	MapDAO mapDao = new MapDAO();
         mapDao.setSession(getActiveSession());
         return mapDao;
     }
 
     protected final MappingDataDAO getMappingDataDao() {
-        if (mappingDataDao == null) {
-            mappingDataDao = new MappingDataDAO();
-        }
+    	MappingDataDAO mappingDataDao = new MappingDataDAO();
         mappingDataDao.setSession(getActiveSession());
         return mappingDataDao;
     }
 
     protected final MappingPopDAO getMappingPopDao() {
-        if (mappingPopDao == null) {
-            mappingPopDao = new MappingPopDAO();
-        }
+    	MappingPopDAO mappingPopDao = new MappingPopDAO();
         mappingPopDao.setSession(getActiveSession());
         return mappingPopDao;
     }
 
     protected final MappingPopValuesDAO getMappingPopValuesDao() {
-        if (mappingPopValuesDao == null) {
-            mappingPopValuesDao = new MappingPopValuesDAO();
-        }
+    	MappingPopValuesDAO mappingPopValuesDao = new MappingPopValuesDAO();
         mappingPopValuesDao.setSession(getActiveSession());
         return mappingPopValuesDao;
     }
 
     protected final MarkerAliasDAO getMarkerAliasDao() {
-        if (markerAliasDao == null) {
-            markerAliasDao = new MarkerAliasDAO();
-        }
+    	MarkerAliasDAO markerAliasDao = new MarkerAliasDAO();
         markerAliasDao.setSession(getActiveSession());
         return markerAliasDao;
     }
 
     protected final MarkerDAO getMarkerDao() {
-        if (markerDao == null) {
-            markerDao = new MarkerDAO();
-        }
+    	MarkerDAO markerDao = new MarkerDAO();
         markerDao.setSession(getActiveSession());
         return markerDao;
     }
 
     protected final MarkerDetailsDAO getMarkerDetailsDao() {
-        if (markerDetailsDao == null) {
-            markerDetailsDao = new MarkerDetailsDAO();
-        }
+    	MarkerDetailsDAO markerDetailsDao = new MarkerDetailsDAO();
         markerDetailsDao.setSession(getActiveSession());
         return markerDetailsDao;
     }
 
     protected final MarkerInfoDAO getMarkerInfoDao() {
-        if (markerInfoDao == null) {
-            markerInfoDao = new MarkerInfoDAO();
-        }
+    	MarkerInfoDAO markerInfoDao = new MarkerInfoDAO();
         markerInfoDao.setSession(getActiveSession());
         return markerInfoDao;
     }
 
     protected final ExtendedMarkerInfoDAO getExtendedMarkerInfoDao() {
-        if (extendedMarkerInfoDAO == null) {
-            extendedMarkerInfoDAO = new ExtendedMarkerInfoDAO();
-        }
-
+    	ExtendedMarkerInfoDAO extendedMarkerInfoDAO = new ExtendedMarkerInfoDAO();
         extendedMarkerInfoDAO.setSession(getActiveSession());
         return extendedMarkerInfoDAO;
     }
 
     protected final MarkerMetadataSetDAO getMarkerMetadataSetDao() {
-        if (markerMetadataSetDao == null) {
-            markerMetadataSetDao = new MarkerMetadataSetDAO();
-        }
+    	MarkerMetadataSetDAO markerMetadataSetDao = new MarkerMetadataSetDAO();
         markerMetadataSetDao.setSession(getActiveSession());
         return markerMetadataSetDao;
     }
 
     protected final MarkerOnMapDAO getMarkerOnMapDao() {
-        if (markerOnMapDao == null) {
-            markerOnMapDao = new MarkerOnMapDAO();
-        }
+    	MarkerOnMapDAO markerOnMapDao = new MarkerOnMapDAO();
         markerOnMapDao.setSession(getActiveSession());
         return markerOnMapDao;
     }
 
     protected final MarkerUserInfoDAO getMarkerUserInfoDao() {
-        if (markerUserInfoDao == null) {
-            markerUserInfoDao = new MarkerUserInfoDAO();
-        }
+    	MarkerUserInfoDAO markerUserInfoDao = new MarkerUserInfoDAO();
         markerUserInfoDao.setSession(getActiveSession());
         return markerUserInfoDao;
     }
     
     protected final MarkerUserInfoDetailsDAO getMarkerUserInfoDetailsDao() {
-        if (markerUserInfoDetailsDao == null) {
-        	markerUserInfoDetailsDao = new MarkerUserInfoDetailsDAO();
-        }
+    	MarkerUserInfoDetailsDAO markerUserInfoDetailsDao = new MarkerUserInfoDetailsDAO();
         markerUserInfoDetailsDao.setSession(getActiveSession());
         return markerUserInfoDetailsDao;
     }
 
     protected final QtlDAO getQtlDao() {
-        if (qtlDao == null) {
-            qtlDao = new QtlDAO();
-        }
+    	QtlDAO qtlDao = new QtlDAO();
         qtlDao.setSession(getActiveSession());
         return qtlDao;
     }
 
     protected final QtlDetailsDAO getQtlDetailsDao() {
-        if (qtlDetailsDao == null) {
-            qtlDetailsDao = new QtlDetailsDAO();
-        }
+    	QtlDetailsDAO qtlDetailsDao = new QtlDetailsDAO();
         qtlDetailsDao.setSession(getActiveSession());
         return qtlDetailsDao;
     }
     
     protected final MtaDAO getMtaDao() {
-        if (mtaDao == null) {
-            mtaDao = new MtaDAO();
-        }
+    	MtaDAO mtaDao = new MtaDAO();
         mtaDao.setSession(getActiveSession());
         return mtaDao;
     }
 
     protected final MtaMetadataDAO getMtaMetadataDao() {
-        if (mtaMetadataDao == null) {
-            mtaMetadataDao = new MtaMetadataDAO();
-        }
+    	MtaMetadataDAO  mtaMetadataDao = new MtaMetadataDAO();
         mtaMetadataDao.setSession(getActiveSession());
         return mtaMetadataDao;
     }
 
     protected final TrackDataDAO getTrackDataDao() {
-        if (trackDataDao == null) {
-        	trackDataDao = new TrackDataDAO();
-        }
+    	TrackDataDAO trackDataDao = new TrackDataDAO();
         trackDataDao.setSession(getActiveSession());
         return trackDataDao;
     }
 
     protected final TrackMarkerDAO getTrackMarkerDao() {
-        if (trackMarkerDao == null) {
-        	trackMarkerDao = new TrackMarkerDAO();
-        }
+    	TrackMarkerDAO trackMarkerDao = new TrackMarkerDAO();
         trackMarkerDao.setSession(getActiveSession());
         return trackMarkerDao;
     }
 
-
-
-
-    //================================ GermplasmDataManager DAO Methods =============================
-    
-    
     protected final AttributeDAO getAttributeDao() {
-        if (attributeDao == null) {
-            attributeDao = new AttributeDAO();
-        }
+    	AttributeDAO attributeDao = new AttributeDAO();
         attributeDao.setSession(getActiveSession());
         return attributeDao;
     }
 
     protected final BibrefDAO getBibrefDao() {
-        if (bibrefDao == null) {
-            bibrefDao = new BibrefDAO();
-        }
+    	BibrefDAO bibrefDao = new BibrefDAO();
         bibrefDao.setSession(getActiveSession());
         return bibrefDao;
     }
 
     protected final GermplasmDAO getGermplasmDao() {
-        if (germplasmDao == null) {
-            germplasmDao = new GermplasmDAO();
-        }
+    	GermplasmDAO germplasmDao = new GermplasmDAO();
         germplasmDao.setSession(getActiveSession());
         return germplasmDao;
     }
 
     protected final LocationDAO getLocationDao() {
-        if (locationDao == null) {
-            locationDao = new LocationDAO();
-        }
+    	LocationDAO  locationDao = new LocationDAO();
         locationDao.setSession(getActiveSession());
         return locationDao;
     }
 
     protected final LocdesDAO getLocDesDao() {
-        if (locdesDao == null) {
-            locdesDao = new LocdesDAO();
-        }
+    	LocdesDAO locdesDao = new LocdesDAO();
         locdesDao.setSession(getActiveSession());
         return locdesDao;
     }
 
     protected final MethodDAO getMethodDao() {
-        if (methodDao == null) {
-            methodDao = new MethodDAO();
-        }
+    	MethodDAO methodDao = new MethodDAO();
         methodDao.setSession(getActiveSession());
         return methodDao;
     }
 
     protected final ProgenitorDAO getProgenitorDao() {
-        if (progenitorDao == null) {
-            progenitorDao = new ProgenitorDAO();
-        }
+    	ProgenitorDAO progenitorDao = new ProgenitorDAO();
         progenitorDao.setSession(getActiveSession());
         return progenitorDao;
     }
 
     protected final UserDefinedFieldDAO getUserDefinedFieldDao() {
-        if (userDefinedFieldDao == null) {
-            userDefinedFieldDao = new UserDefinedFieldDAO();
-        }
+    	UserDefinedFieldDAO userDefinedFieldDao = new UserDefinedFieldDAO();
         userDefinedFieldDao.setSession(getActiveSession());
         return userDefinedFieldDao;
     }
 
     protected final LocationDAO getLocationDAO() {
-		if (locationDao == null) {
-			locationDao = new LocationDAO();
-		}
+    	LocationDAO locationDao = new LocationDAO();
 		locationDao.setSession(getActiveSession());
 		return locationDao;
 	}    
     
     protected final LocdesDAO getLocdesDao() {
-        if (locdesDao == null) {
-            locdesDao = new LocdesDAO();
-        }
+    	LocdesDAO locdesDao = new LocdesDAO();
         locdesDao.setSession(getActiveSession());
         return locdesDao;
     }
     
-    public ProgramFavoriteDAO getProgramFavoriteDao() {
-		 if (programFavoriteDao == null) {
-			 	programFavoriteDao = new ProgramFavoriteDAO();
-	        }
-		 programFavoriteDao.setSession(getActiveSession());
-	        return programFavoriteDao;
+	public ProgramFavoriteDAO getProgramFavoriteDao() {
+		ProgramFavoriteDAO programFavoriteDao = new ProgramFavoriteDAO();
+		programFavoriteDao.setSession(getActiveSession());
+		return programFavoriteDao;
 	}
 
-    //================================ GermplasmListDataManager DAO Methods =============================
-    
     protected final GermplasmListDAO getGermplasmListDAO() {
-		if (germplasmListDao == null) {
-			germplasmListDao = new GermplasmListDAO();
-		}
+    	GermplasmListDAO germplasmListDao = new GermplasmListDAO();
 		germplasmListDao.setSession(getActiveSession());
 		return germplasmListDao;
 	}
 	
 	protected final GermplasmListDataDAO getGermplasmListDataDAO() {
-		if (germplasmListDataDao == null) {
-			germplasmListDataDao = new GermplasmListDataDAO();
-		}
+		GermplasmListDataDAO germplasmListDataDao = new GermplasmListDataDAO();
 		germplasmListDataDao.setSession(getActiveSession());
 		return germplasmListDataDao;
 	}
 	
     
     protected final ListDataPropertyDAO getListDataPropertyDAO() {
-		if (listDataPropertyDao == null) {
-			listDataPropertyDao = new ListDataPropertyDAO();
-		}
+    	ListDataPropertyDAO listDataPropertyDao = new ListDataPropertyDAO();
 		listDataPropertyDao.setSession(getActiveSession());
 		return listDataPropertyDao;
 	}
     
     protected final ListDataProjectDAO getListDataProjectDAO() {
-    	if (listDataProjectDao == null) {
-    		listDataProjectDao = new ListDataProjectDAO(); 
-    	}
+    	ListDataProjectDAO listDataProjectDao = new ListDataProjectDAO(); 
     	listDataProjectDao.setSession(getActiveSession());
     	return listDataProjectDao;
     }
 
-    //================================  InventoryDataManager DAO Methods =============================
-	
     protected final LotDAO getLotDao() {
-        if (lotDao == null) {
-            lotDao = new LotDAO();
-        }
+    	LotDAO lotDao = new LotDAO();
         lotDao.setSession(getActiveSession());
         return lotDao;
     }
 
     protected final PersonDAO getPersonDao() {
-        if (personDao == null) {
-            personDao = new PersonDAO();
-        }
+    	PersonDAO personDao = new PersonDAO();
         personDao.setSession(getActiveSession());
         return personDao;
     }
 
     protected final TransactionDAO getTransactionDao() {
-        if (transactionDao == null) {
-            transactionDao = new TransactionDAO();
-        }
+    	TransactionDAO transactionDao = new TransactionDAO();
         transactionDao.setSession(getActiveSession());
         return transactionDao;
     }
-
-    //===========================================================================================
+    
+    protected final TermPropertyBuilder getTermPropertyBuilder() {
+        return new TermPropertyBuilder(sessionProviderForLocal);
+    }
     
     protected final void clearSessions() {
     	if (sessionProviderForLocal != null) {
@@ -722,9 +568,4 @@ public class DatabaseBroker {
     		this.sessionProviderForLocal.getSession().flush();
     	}
     }
-    
-    protected final TermPropertyBuilder getTermPropertyBuilder() {
-        return new TermPropertyBuilder(sessionProviderForLocal);
-    }
-	
 }
