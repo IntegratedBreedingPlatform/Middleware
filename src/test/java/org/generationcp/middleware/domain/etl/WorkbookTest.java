da24a7c84aa19a8bdf9ae5471a526bae0e61689b
Daniel Jao <daniel@efficio.us.com>
2015-02-11 14:01:28 +0800

    Added JUnit testing for the new function that arranges the measurement variables
    BMS-171
    
    Reviewer: Mae Turiana

--- src/test/java/org/generationcp/middleware/domain/etl/WorkbookTest.java
+++ src/test/java/org/generationcp/middleware/domain/etl/WorkbookTest.java
@@ -16,6 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import junit.framework.Assert;
+
 import org.generationcp.middleware.domain.etl.MeasurementData;
 import org.generationcp.middleware.domain.etl.MeasurementRow;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
@@ -25,6 +27,7 @@ import org.generationcp.middleware.domain.oms.StudyType;
 import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.manager.Operation;
 import org.generationcp.middleware.pojos.Location;
+import org.junit.Test;
 
 public class WorkbookTest {
 	
@@ -505,5 +508,28 @@ public class WorkbookTest {
 			}
 		}
 	}
-
+	
+	private MeasurementVariable createMeasurementVariable(Integer termId){
+		MeasurementVariable var = new MeasurementVariable();
+		var.setTermId(termId);
+		return var;
+	}
+	@Test
+	public void testArrangeMeasurementVariables(){
+		Workbook workbook = new Workbook();
+		List<MeasurementVariable> varList = new ArrayList<MeasurementVariable>();
+		varList.add(createMeasurementVariable(10));
+		varList.add(createMeasurementVariable(20));
+		varList.add(createMeasurementVariable(30));
+		List<Integer> columnOrderedList = new ArrayList<Integer>();
+		columnOrderedList.add(new Integer(20));
+		columnOrderedList.add(new Integer(30));
+		columnOrderedList.add(new Integer(10));
+		workbook.setColumnOrderedLists(columnOrderedList);
+		varList = workbook.arrangeMeasurementVariables(varList);
+		
+		Assert.assertEquals("1st element should have term id 20", 20, varList.get(0).getTermId());
+		Assert.assertEquals("2nd element should have term id 30", 30, varList.get(1).getTermId());
+		Assert.assertEquals("3rd element should have term id 10", 10, varList.get(2).getTermId());
+	}
 }
