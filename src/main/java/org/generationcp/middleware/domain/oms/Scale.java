d9d323644118acaf46c8879b78c66d868aea3610
srajwadi <sunny@leafnode.io>
2015-03-09 17:20:10 +0530

    Update Scale.java as per APIARY.

--- src/main/java/org/generationcp/middleware/domain/oms/Scale.java
+++ src/main/java/org/generationcp/middleware/domain/oms/Scale.java
@@ -11,7 +11,11 @@
  *******************************************************************************/
 package org.generationcp.middleware.domain.oms;
 
+import org.generationcp.middleware.domain.dms.Enumeration;
+import org.generationcp.middleware.domain.dms.VariableConstraints;
 import org.generationcp.middleware.util.Debug;
+
+import java.util.List;
 
 public class Scale {
     
@@ -24,13 +28,11 @@ public class Scale {
     public Scale(Term term) {
         this.term = term;
     }
-    
 
     public Term getTerm() {
         return term;
     }
 
-    
     public void setTerm(Term term) {
         this.term = term;
     }
@@ -42,7 +44,6 @@ public class Scale {
     public void setId(int id) {
         term.setId(id);
     }
-    
 
     public String getName() {
         return term.getName();
@@ -59,9 +60,85 @@ public class Scale {
     public void setDefinition(String definition) {
         term.setDefinition(definition);
     }
-    
 
-	@Override
+    private Term dataType;
+    
+    public Term getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(Term dataType) {
+        this.dataType = dataType;
+    }    
+    
+    private VariableConstraints constraints;
+
+    public VariableConstraints getConstraints() { return constraints; }
+
+    public void setConstraints(VariableConstraints constraints) {
+        this.constraints = constraints;
+    }
+
+    private List<Enumeration> enumerations;
+    
+    public List<Enumeration> getEnumerations() {
+        return enumerations;
+    }
+
+    public Enumeration getEnumeration(Integer id){
+        if (enumerations == null){
+            return null;
+        }
+        for (Enumeration enumeration : enumerations){
+            if (enumeration.getId().equals(id)){
+                return enumeration;
+            }
+        }
+        return null;
+    }
+
+    public Enumeration getEnumeration(String name, String description){
+        if (enumerations == null){
+            return null;
+        }
+        for (Enumeration enumeration : enumerations){
+            if (enumeration.getName().equalsIgnoreCase(name) && enumeration.getDescription().equalsIgnoreCase(description)){
+                return enumeration;
+            }
+        }
+        return null;
+    }
+
+    public Enumeration getEnumerationByName(String name){
+        if (enumerations == null){
+            return null;
+        }
+        for (Enumeration enumeration : enumerations){
+            if (enumeration.getName().equalsIgnoreCase(name) ){
+                return enumeration;
+            }
+        }
+        return null;
+    }
+
+    public Enumeration getEnumerationByDescription(String description){
+        if (enumerations == null){
+            return null;
+        }
+        for (Enumeration enumeration : enumerations){
+            if (enumeration.getDescription().equalsIgnoreCase(description)){
+                return enumeration;
+            }
+        }
+        return null;
+    }
+
+
+    public void setEnumerations(List<Enumeration> enumerations) {
+        this.enumerations = enumerations;
+    }
+
+    @Override
 	public String toString() {
 	    
 	    if (term == null){
