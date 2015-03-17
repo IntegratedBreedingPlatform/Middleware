90282092d7c3591482e44f1df26d56bd811a371b
srajwadi <sunny@leafnode.io>
2015-03-17 12:02:58 +0530

    Adding missing TermRelationship enum
    Issue: BMS-354 BMS-369 BMS-370 BMS-371 BMS-372
    
    Reviewer: Naymesh

---
+++ src/main/java/org/generationcp/middleware/domain/oms/TermRelationship.java
@@ -0,0 +1,33 @@
+package org.generationcp.middleware.domain.oms;
+
+public enum TermRelationship {
+
+    //CV Term Relationship
+    HAS_METHOD(1210)
+    , HAS_PROPERTY(1200)
+    , HAS_SCALE(1220)
+    , HAS_TYPE(1105)
+    , HAS_VALUE(1190)
+    , IS_A(1225)
+    , STORED_IN(1044),
+    ;
+
+    private final int id;
+
+    private TermRelationship(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public static TermRelationship getById(int id) {
+        for (TermRelationship term : values()) {
+            if (term.getId() == id) {
+                return term;
+            }
+        }
+        return null;
+    }
+}
