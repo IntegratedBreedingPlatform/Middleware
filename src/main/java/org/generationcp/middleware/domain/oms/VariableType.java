374d46d27e2031e038dcc55e49c79395c77c5a82
srajwadi <sunny@leafnode.io>
2015-03-17 15:14:29 +0530

    Adding variable type schema
    Issue: BMS-304
    
    Reviewer: Naymesh

---
+++ src/main/java/org/generationcp/middleware/domain/oms/VariableType.java
@@ -0,0 +1,16 @@
+package org.generationcp.middleware.domain.oms;
+
+/**
+ * Variable Type of a Variable. Ontology variable will have multiple variable types associated to it.
+ */
+public enum VariableType {
+    ANALYSIS,
+    TRIAL_CONDITION,
+    NURSERY_CONDITION,
+    GERMPLASM_DESCRIPTOR,
+    STUDY_DETAIL,
+    ENVIRONMENT_DETAIL,
+    SELECTION_METHOD,
+    TRAIT,
+    TREATMENT_FACTOR
+}
