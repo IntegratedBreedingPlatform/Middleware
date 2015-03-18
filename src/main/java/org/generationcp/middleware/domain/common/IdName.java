2ac3376d4b3d2a89895fd1a681abe206648db492
srajwadi <sunny@leafnode.io>
2015-03-18 11:04:00 +0530

    Adding IdName pair in common. This class will be useful where we need only id and name value.
    Reviewer: Naymesh

---
+++ src/main/java/org/generationcp/middleware/domain/common/IdName.java
@@ -0,0 +1,36 @@
+package org.generationcp.middleware.domain.common;
+
+/**
+ * This class will be used where we need id and name pair
+ */
+
+public class IdName {
+
+    private Integer id;
+    private String name;
+
+    public IdName() {
+
+    }
+
+    public IdName(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
