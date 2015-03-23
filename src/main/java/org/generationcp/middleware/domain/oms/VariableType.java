37b4b0a944e9a94215481bf6e92c35fec452083c
srajwadi <sunny@leafnode.io>
2015-03-23 09:15:15 +0530

    Updating variable type enum to provide Id,Name pair.
    Issue: BMS-304
    
    Reviewer: Naymesh

--- src/main/java/org/generationcp/middleware/domain/oms/VariableType.java
+++ src/main/java/org/generationcp/middleware/domain/oms/VariableType.java
@@ -1,16 +1,71 @@
 package org.generationcp.middleware.domain.oms;
 
+import org.generationcp.middleware.domain.common.IdName;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Variable Type of a Variable. Ontology variable can have zero or more variable types associated to it.
  */
 public enum VariableType {
-    ANALYSIS,
-    TRIAL_CONDITION,
-    NURSERY_CONDITION,
-    GERMPLASM_DESCRIPTOR,
-    STUDY_DETAIL,
-    ENVIRONMENT_DETAIL,
-    SELECTION_METHOD,
-    TRAIT,
-    TREATMENT_FACTOR
+    ANALYSIS(1, "Analysis"),
+    TRIAL_CONDITION(2, "Trial Condition"),
+    NURSERY_CONDITION(3, "Nursery Condition"),
+    GERMPLASM_DESCRIPTOR(4, "Germplasm Descriptor"),
+    STUDY_DETAIL(5, "Study Detail"),
+    ENVIRONMENT_DETAIL(6, "Environment Detail"),
+    SELECTION_METHOD(7, "Selection Method"),
+    TRAIT(8, "Trait"),
+    TREATMENT_FACTOR(9, "Treatment Factor")
+    ;
+
+    private Integer id;
+    private String name;
+
+
+    private VariableType(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    private static final Map<Integer, DataType> byId = new HashMap<>();
+    private static final Map<String, DataType> byName = new HashMap<>();
+
+    static {
+        for (DataType e : DataType.values()) {
+            if (byId.put(e.getId(), e) != null) {
+                throw new IllegalArgumentException("duplicate id: " + e.getId());
+            }
+
+            if (byName.put(e.getName(), e) != null) {
+                throw new IllegalArgumentException("duplicate name: " + e.getName());
+            }
+        }
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getName() { return this.name; }
+
+    public static DataType getById(Integer id) {
+        return byId.get(id);
+    }
+
+    public static DataType getByName(String name) { return byName.get(name); }
+
+    public IdName toIdName(){
+        return new IdName(getId(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "DataType{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
