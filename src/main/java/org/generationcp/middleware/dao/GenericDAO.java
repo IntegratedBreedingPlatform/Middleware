b0b65a139a0a27869819c7333554820c608b1912
srajwadi <sunny@leafnode.io>
2015-04-16 10:34:02 +0530

    Unused imports.

--- src/main/java/org/generationcp/middleware/dao/GenericDAO.java
+++ src/main/java/org/generationcp/middleware/dao/GenericDAO.java
@@ -22,7 +22,6 @@ import org.slf4j.LoggerFactory;
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
