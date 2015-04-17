5ba2c3129509c835a61a2de8b6909105dc6d91c3
Aldrin Batac <aldrin@leafnode.io>
2015-04-17 14:01:55 +0800

    Enable searching of partial GID in Germplasm Search
    BMS-593
    
    Reviewer: Mae Turiana

--- src/main/java/org/generationcp/middleware/dao/GermplasmDAO.java
+++ src/main/java/org/generationcp/middleware/dao/GermplasmDAO.java
@@ -769,15 +769,21 @@ public class GermplasmDAO extends GenericDAO<Germplasm, Integer>{
             
             //First priority, germplasms with GID=q
             
-            if(q.matches("\\d+") || q.matches("-\\d+")) {
+            if(q.matches("(\\d+)(%|_)?") || q.matches("(-\\d+)(%|_)?")) {
             	SQLQuery p1Query;
-	            if((q.contains("%") || q.contains("_")) && o.equals(Operation.LIKE)){
+	            if(o.equals(Operation.LIKE) || q.endsWith("%")){
 	            	p1Query = getSession().createSQLQuery(Germplasm.SEARCH_GERMPLASM_BY_GID_LIKE);
+	            	if(q.contains("%") || q.contains("_")){
+	            		p1Query.setParameter("gid", q);
+	            	}else{
+	            		p1Query.setParameter("gid", q+"%");
+	            	}
 	            } else {
 	            	p1Query = getSession().createSQLQuery(Germplasm.SEARCH_GERMPLASM_BY_GID);
 	            	p1Query.setParameter("gidLength", q.length());
+	            	p1Query.setParameter("gid", q);
 	            }
-	            p1Query.setParameter("gid", q);
+	            
 	            p1Query.addEntity("germplsm", Germplasm.class);
 	
 	            result.addAll(p1Query.list());
