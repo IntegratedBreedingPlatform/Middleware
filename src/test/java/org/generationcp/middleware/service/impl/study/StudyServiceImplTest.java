704ddad913ffcfc0123a4f58c7275057ad10836b
Naymesh <naymesh@leafnode.io>
2015-04-24 10:16:49 +1200

    Support for loading program specific studies in the listAllStudies() service when the optional programUniqueId parameter is supplied. Added unit test for the service and JavaDoc.
    Issue: BMS-591
    Reviewer: AkhilM

--- src/test/java/org/generationcp/middleware/service/impl/study/StudyServiceImplTest.java
+++ src/test/java/org/generationcp/middleware/service/impl/study/StudyServiceImplTest.java
@@ -1,27 +1,23 @@
 package org.generationcp.middleware.service.impl.study;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.generationcp.middleware.hibernate.HibernateSessionPerRequestProvider;
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 import org.generationcp.middleware.service.api.study.Measurement;
 import org.generationcp.middleware.service.api.study.StudySummary;
 import org.generationcp.middleware.service.api.study.Trait;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 import org.mockito.Mockito;
-
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import org.tmatesoft.sqljet.core.internal.lang.SqlParser.when_expr_return;
-
-import static org.junit.Assert.*;
 
 /**
  * The class <code>StudyServiceImplTest</code> contains tests for the class
@@ -53,5 +49,39 @@ public class StudyServiceImplTest {
 
 		final List<Measurement> allMeasurements = mockTrailMeasurements.getAllMeasurements(1234, projectTraits);
 		assertEquals(allMeasurements,testMeasurements);
+	}
+	
+	@Test
+	public void testlistAllStudies() throws MiddlewareQueryException {
+		Session mockSession = Mockito.mock(Session.class);
+		SQLQuery mockSqlQuery = Mockito.mock(SQLQuery.class);
+		
+		HibernateSessionProvider mockSessionProvider = Mockito.mock(HibernateSessionProvider.class);
+		Mockito.when(mockSessionProvider.getSession()).thenReturn(mockSession);
+		Mockito.when(mockSession.createSQLQuery(Mockito.anyString())).thenReturn(mockSqlQuery);
+		Mockito.when(mockSqlQuery.addScalar(Mockito.anyString())).thenReturn(mockSqlQuery);
+		
+		final Object[] testDBRow =
+				{2007, "Wheat Trial 1", "Wheat Trial 1 Title", "c996de54-3ebb-41ca-8fed-160a33ffffd4", "10010", "Wheat Trial 1 Objective", "20150417", "20150422"};
+		final List<Object[]> testResult = Arrays.<Object[]>asList(testDBRow);
+		
+		Mockito.when(mockSqlQuery.list()).thenReturn(testResult);
+		
+		StudyServiceImpl studyServiceImpl = new StudyServiceImpl(mockSessionProvider);
+		List<StudySummary> studySummaries = studyServiceImpl.listAllStudies("c996de54-3ebb-41ca-8fed-160a33ffffd4");
+		Assert.assertNotNull(studySummaries);
+		Assert.assertEquals(1, studySummaries.size());
+		
+		StudySummary studySummary = studySummaries.get(0);
+		
+		Assert.assertEquals(testDBRow[0], studySummary.getId());
+		Assert.assertEquals(testDBRow[1], studySummary.getName());
+		Assert.assertEquals(testDBRow[2], studySummary.getTitle());
+		Assert.assertEquals(testDBRow[3], studySummary.getProgramUUID());
+		Assert.assertEquals(testDBRow[4], String.valueOf(studySummary.getType().getId()));
+		Assert.assertEquals(testDBRow[5], studySummary.getObjective());
+		Assert.assertEquals(testDBRow[6], studySummary.getStartDate());
+		Assert.assertEquals(testDBRow[7], studySummary.getEndDate());
+		
 	}
 }
