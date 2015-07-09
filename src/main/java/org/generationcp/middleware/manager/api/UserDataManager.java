9c6b32329a7aa336525985fe44f397f609c75226
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2015-07-09 12:36:54 +0800

    Code Clean-up: - Apply leafnode formatting style

--- src/main/java/org/generationcp/middleware/manager/api/UserDataManager.java
+++ src/main/java/org/generationcp/middleware/manager/api/UserDataManager.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2012, All Rights Reserved.
- *
+ * 
  * Generation Challenge Programme (GCP)
- *
- *
+ * 
+ * 
  * This software is licensed for use under the terms of the GNU General Public License (http://bit.ly/8Ztv8M) and the provisions of Part F
  * of the Generation Challenge Programme Amended Consortium Agreement (http://bit.ly/KQX1nL)
- *
+ * 
  *******************************************************************************/
 
 package org.generationcp.middleware.manager.api;
@@ -24,21 +24,21 @@ public interface UserDataManager {
 
 	/**
 	 * Returns all Persons.
-	 *
+	 * 
 	 * @return gets all Persons
 	 */
 	List<User> getAllUsers() throws MiddlewareQueryException;
 
 	/**
 	 * Returns number of all Users.
-	 *
+	 * 
 	 * @return the number of all Users
 	 */
 	long countAllUsers() throws MiddlewareQueryException;
 
 	/**
 	 * Adds the user.
-	 *
+	 * 
 	 * @param user the user
 	 * @return Returns the id of the {@code User} record added
 	 * @throws MiddlewareQueryException the MiddlewareQueryException
@@ -47,7 +47,7 @@ public interface UserDataManager {
 
 	/**
 	 * Updates the user
-	 *
+	 * 
 	 * @param user the user to update
 	 * @return Returns the id of the {@code User} record updated
 	 * @throws MiddlewareQueryException the MiddlewareQueryException
@@ -56,7 +56,7 @@ public interface UserDataManager {
 
 	/**
 	 * Gets the user by id.
-	 *
+	 * 
 	 * @param id the id
 	 * @return the user by id
 	 */
@@ -64,7 +64,7 @@ public interface UserDataManager {
 
 	/**
 	 * Delete user.
-	 *
+	 * 
 	 * @param user the user
 	 * @throws MiddlewareQueryException the MiddlewareQueryException
 	 */
@@ -72,28 +72,28 @@ public interface UserDataManager {
 
 	/**
 	 * Returns all Persons.
-	 *
+	 * 
 	 * @return gets all Persons
 	 */
 	List<Person> getAllPersons() throws MiddlewareQueryException;
 
 	/**
 	 * Returns all Persons from local sorted by first-middle-last followed by all persons from local sorted by first-middle-last
-	 *
+	 * 
 	 * @return gets all Persons
 	 */
 	List<Person> getAllPersonsOrderedByLocalCentral() throws MiddlewareQueryException;
 
 	/**
 	 * Returns number of all Persons.
-	 *
+	 * 
 	 * @return the number of all Persons
 	 */
 	long countAllPersons() throws MiddlewareQueryException;
 
 	/**
 	 * Adds the person.
-	 *
+	 * 
 	 * @param person the person
 	 * @return Returns the id of the {@code Person} record added
 	 * @throws MiddlewareQueryException the MiddlewareQueryException
@@ -102,7 +102,7 @@ public interface UserDataManager {
 
 	/**
 	 * Gets the person by id.
-	 *
+	 * 
 	 * @param id the id
 	 * @return the person by id
 	 */
@@ -110,7 +110,7 @@ public interface UserDataManager {
 
 	/**
 	 * Delete person.
-	 *
+	 * 
 	 * @param person the person
 	 * @throws MiddlewareQueryException the MiddlewareQueryException
 	 */
@@ -118,7 +118,7 @@ public interface UserDataManager {
 
 	/**
 	 * Checks if is valid user login.
-	 *
+	 * 
 	 * @param username the username
 	 * @param password the password
 	 * @return true, if is valid user login
@@ -128,7 +128,7 @@ public interface UserDataManager {
 
 	/**
 	 * Checks if is person exists.
-	 *
+	 * 
 	 * @param firstName the first name
 	 * @param lastName the last name
 	 * @return true, if is person exists
@@ -138,7 +138,7 @@ public interface UserDataManager {
 
 	/**
 	 * Checks if is username exists.
-	 *
+	 * 
 	 * @param userName the user name
 	 * @return true, if is username exists
 	 * @throws MiddlewareQueryException the MiddlewareQueryException
