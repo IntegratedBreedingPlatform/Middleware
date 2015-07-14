f3a5bfe56ddc249d736bacf1f76d6507214d6348
Daniel Jao <daniel@efficio.us.com>
2015-07-14 14:51:35 +0800

    Modified the map key to use camelCase notation
    BMS-980
    
    Reviewer: Crisjaira Villarino

--- src/main/java/org/generationcp/middleware/reports/AbstractWheatTrialReporter.java
+++ src/main/java/org/generationcp/middleware/reports/AbstractWheatTrialReporter.java
@@ -44,10 +44,10 @@ public abstract class AbstractWheatTrialReporter extends AbstractReporter {
 					params.put("program", var.getValue());
 					break;
 				case "STUDY_NAME":
-					params.put("trial_abbr", var.getValue());
+					params.put("trialAbbr", var.getValue());
 					break;
 				case "STUDY_TITLE":
-					params.put("trial_name", var.getValue());
+					params.put("trialName", var.getValue());
 					break;
 				case "CROP_SEASON":
 					params.put("cycle", var.getValue());
@@ -70,8 +70,8 @@ public abstract class AbstractWheatTrialReporter extends AbstractReporter {
 					params.put("organization", var.getValue());
 					break;
 				default:
-					params.put("dms_ip", "???");
-					params.put("gms_ip", "???");
+					params.put("dmsIp", "???");
+					params.put("gmsIp", "???");
 					params.put("version", "v-1");
 					break;
 			}
