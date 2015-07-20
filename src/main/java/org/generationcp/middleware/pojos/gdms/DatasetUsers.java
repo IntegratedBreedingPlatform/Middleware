afd4476ed8900c491402540db2b0348e55949411
Iryna Dziuban <iryna@leafnode.io>
2015-07-21 10:48:58 +1200

    Revert "Fix id generation for gdms_dataset_users id."
    This reverts commit 845738ac938367efc572e7d336ea644be13cc63c.

--- src/main/java/org/generationcp/middleware/pojos/gdms/DatasetUsers.java
+++ src/main/java/org/generationcp/middleware/pojos/gdms/DatasetUsers.java
@@ -16,8 +16,6 @@ import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -36,7 +34,6 @@ public class DatasetUsers implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Basic(optional = false)
 	@Column(name = "dataset_id")
 	private Integer datasetId;
