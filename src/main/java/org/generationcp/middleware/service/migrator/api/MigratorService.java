0d83a403d614a887751bd2baa67e2f464a051b23
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2015-07-24 18:01:31 +0800

    Remove deprecated classes for migrator
    BMS-538
    
    Reviewer: Daniel Villafuerte

--- src/main/java/org/generationcp/middleware/service/migrator/api/MigratorService.java
+++
@@ -1,9 +0,0 @@
-package org.generationcp.middleware.service.migrator.api;
-
-import org.generationcp.middleware.exceptions.MiddlewareQueryException;
-
-public interface MigratorService {
-
-	void resetLocalReferenceId(String tableName, String columnName)
-			throws MiddlewareQueryException;
-}
