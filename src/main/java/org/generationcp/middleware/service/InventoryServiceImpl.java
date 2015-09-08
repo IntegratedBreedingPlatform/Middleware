a256834802b09e528398aae2eef7058c630e7817
Mae Isabelle Turiana <maeisabelle@gmail.com>
2015-09-08 14:21:22 +0800

    Increase test coverage for InventoryServiceImpl.getCurrentNotationNumberForBreederIdentifier to 100%
    BMS-1546
    
    Reviewer:

--- src/main/java/org/generationcp/middleware/service/InventoryServiceImpl.java
+++ src/main/java/org/generationcp/middleware/service/InventoryServiceImpl.java
@@ -16,10 +16,15 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.generationcp.middleware.dao.ims.StockTransactionDAO;
+import org.generationcp.middleware.dao.ims.TransactionDAO;
 import org.generationcp.middleware.domain.gms.GermplasmListType;
 import org.generationcp.middleware.domain.inventory.InventoryDetails;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
+import org.generationcp.middleware.manager.api.InventoryDataManager;
+import org.generationcp.middleware.operation.builder.LotBuilder;
+import org.generationcp.middleware.operation.builder.TransactionBuilder;
 import org.generationcp.middleware.pojos.GermplasmListData;
 import org.generationcp.middleware.pojos.ListDataProject;
 import org.generationcp.middleware.pojos.ims.EntityType;
@@ -37,38 +42,65 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class InventoryServiceImpl extends Service implements InventoryService {
 
+	private TransactionDAO transactionDAO;
+	private InventoryDataManager inventoryDataManager;
+	private StockTransactionDAO stockTransactionDAO;
+	private LotBuilder lotBuilder;
+	private TransactionBuilder transactionBuilder;
+
 	public InventoryServiceImpl() {
 		super();
+		this.transactionDAO = this.getTransactionDao();
+		this.inventoryDataManager = this.getInventoryDataManager();
+		this.stockTransactionDAO = this.getStockTransactionDAO();
+		this.lotBuilder = this.getLotBuilder();
+		this.transactionBuilder = this.getTransactionBuilder();
 	}
-	
+
 	public InventoryServiceImpl(HibernateSessionProvider sessionProvider, String localDatabaseName) {
 		super(sessionProvider, localDatabaseName);
+		this.transactionDAO = this.getTransactionDao();
+		this.inventoryDataManager = this.getInventoryDataManager();
+		this.stockTransactionDAO = this.getStockTransactionDAO();
+		this.lotBuilder = this.getLotBuilder();
+		this.transactionBuilder = this.getTransactionBuilder();
+	}
+
+	public InventoryServiceImpl(HibernateSessionProvider sessionProvider, TransactionDAO transactionDAO,
+			InventoryDataManager inventoryDataManager, StockTransactionDAO stockTransactionDAO, LotBuilder lotBuilder,
+			TransactionBuilder transactionBuilder) {
+		super(sessionProvider);
+		this.transactionDAO = transactionDAO;
+		this.inventoryDataManager = inventoryDataManager;
+		this.stockTransactionDAO = stockTransactionDAO;
+		this.lotBuilder = lotBuilder;
+		this.transactionBuilder = transactionBuilder;
 	}
 
 	@Override
 	public List<InventoryDetails> getInventoryDetailsByGermplasmList(Integer listId) throws MiddlewareQueryException {
-		return this.getInventoryDataManager().getInventoryDetailsByGermplasmList(listId);
+		return this.inventoryDataManager.getInventoryDetailsByGermplasmList(listId);
 	}
 
 	@Override
 	public List<InventoryDetails> getInventoryDetailsByGids(List<Integer> gids) throws MiddlewareQueryException {
-		return this.getInventoryDataManager().getInventoryDetailsByGids(gids);
+		return this.inventoryDataManager.getInventoryDetailsByGids(gids);
 	}
 
 	@Override
 	public List<InventoryDetails> getInventoryDetailsByStudy(Integer studyId) throws MiddlewareQueryException {
-		return this.getInventoryDataManager().getInventoryDetailsByGermplasmList(studyId);
+		return this.inventoryDataManager.getInventoryDetailsByGermplasmList(studyId);
 	}
 
 	@Override
 	public List<InventoryDetails> getInventoryDetailsByGermplasmList(Integer listId, String germplasmListType)
 			throws MiddlewareQueryException {
-		return this.getInventoryDataManager().getInventoryDetailsByGermplasmList(listId, germplasmListType);
+		return this.inventoryDataManager.getInventoryDetailsByGermplasmList(listId, germplasmListType);
 	}
 
 	@Override
 	public Integer getCurrentNotationNumberForBreederIdentifier(String breederIdentifier) throws MiddlewareQueryException {
-		List<String> inventoryIDs = this.getTransactionDao().getInventoryIDsWithBreederIdentifier(breederIdentifier);
+		List<String> inventoryIDs = this.transactionDAO.getInventoryIDsWithBreederIdentifier(breederIdentifier);
 
 		if (inventoryIDs == null || inventoryIDs.isEmpty()) {
 			return 0;
@@ -86,12 +118,10 @@ public class InventoryServiceImpl extends Service implements InventoryService {
 
 		for (String inventoryID : inventoryIDs) {
 			Matcher matcher = pattern.matcher(inventoryID);
-			if (matcher.find()) {
-				// Matcher.group(1) is needed because group(0) includes the identifier in the match
-				// Matcher.group(1) only captures the value inside the parenthesis
-				currentMax = Math.max(currentMax, Integer.valueOf(matcher.group(1)));
-			}
-
+			matcher.find();
+			// Matcher.group(1) is needed because group(0) includes the identifier in the match
+			// Matcher.group(1) only captures the value inside the parenthesis
+			currentMax = Math.max(currentMax, Integer.valueOf(matcher.group(1)));
 		}
 
 		return currentMax;
@@ -101,43 +131,41 @@ public class InventoryServiceImpl extends Service implements InventoryService {
 	public void addLotAndTransaction(InventoryDetails details, GermplasmListData listData, ListDataProject listDataProject)
 			throws MiddlewareQueryException {
 		Lot existingLot =
-				this.getInventoryDataManager().getLotByEntityTypeAndEntityIdAndLocationIdAndScaleId(EntityType.GERMPLSM.name(),
+				this.inventoryDataManager.getLotByEntityTypeAndEntityIdAndLocationIdAndScaleId(EntityType.GERMPLSM.name(),
 						details.getGid(), details.getLocationId(), details.getScaleId());
 
 		if (existingLot != null) {
 			throw new MiddlewareQueryException("A lot with the same entity id, location id, and scale id already exists");
 		}
 
-		Lot lot =
-				this.getLotBuilder().createLotForAdd(details.getGid(), details.getLocationId(), details.getScaleId(), details.getComment(),
-						details.getUserId());
-		this.getInventoryDataManager().addLot(lot);
+		Lot lot = this.lotBuilder.createLotForAdd(details.getGid(), details.getLocationId(), details.getScaleId(), details.getComment(),
+				details.getUserId());
+		this.inventoryDataManager.addLot(lot);
 
-		Transaction transaction =
-				this.getTransactionBuilder().buildForAdd(lot, listData == null ? 0 : listData.getId(), details.getAmount(),
-						details.getUserId(), details.getComment(), details.getSourceId(), details.getInventoryID(), details.getBulkWith(),
-						details.getBulkCompl());
-		this.getInventoryDataManager().addTransaction(transaction);
+		Transaction transaction = this.transactionBuilder.buildForAdd(lot, listData == null ? 0 : listData.getId(), details.getAmount(),
+				details.getUserId(), details.getComment(), details.getSourceId(), details.getInventoryID(), details.getBulkWith(),
+				details.getBulkCompl());
+		this.inventoryDataManager.addTransaction(transaction);
 
 		StockTransaction stockTransaction = new StockTransaction(null, listDataProject, transaction);
 		stockTransaction.setSourceRecordId(transaction.getSourceRecordId());
-		this.getInventoryDataManager().addStockTransaction(stockTransaction);
+		this.inventoryDataManager.addStockTransaction(stockTransaction);
 	}
 
 	@Override
 	public List<InventoryDetails> getInventoryListByListDataProjectListId(Integer listDataProjectListId, GermplasmListType type)
 			throws MiddlewareQueryException {
-		return this.getStockTransactionDAO().retrieveInventoryDetailsForListDataProjectListId(listDataProjectListId, type);
+		return this.stockTransactionDAO.retrieveInventoryDetailsForListDataProjectListId(listDataProjectListId, type);
 	}
 
 	@Override
 	public List<InventoryDetails> getSummedInventoryListByListDataProjectListId(Integer listDataProjectListId, GermplasmListType type)
 			throws MiddlewareQueryException {
-		return this.getStockTransactionDAO().retrieveSummedInventoryDetailsForListDataProjectListId(listDataProjectListId, type);
+		return this.stockTransactionDAO.retrieveSummedInventoryDetailsForListDataProjectListId(listDataProjectListId, type);
 	}
 
 	@Override
 	public boolean stockHasCompletedBulking(Integer listId) throws MiddlewareQueryException {
-		return this.getStockTransactionDAO().stockHasCompletedBulking(listId);
+		return this.stockTransactionDAO.stockHasCompletedBulking(listId);
 	}
 }
--- src/test/java/org/generationcp/middleware/service/InventoryServiceImplTest.java
+++ src/test/java/org/generationcp/middleware/service/InventoryServiceImplTest.java
@@ -5,31 +5,68 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.generationcp.middleware.IntegrationTestBase;
+import org.generationcp.middleware.dao.ims.StockTransactionDAO;
 import org.generationcp.middleware.dao.ims.TransactionDAO;
+import org.generationcp.middleware.domain.gms.GermplasmListType;
+import org.generationcp.middleware.domain.inventory.InventoryDetails;
 import org.generationcp.middleware.exceptions.MiddlewareException;
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
-import org.generationcp.middleware.service.api.InventoryService;
+import org.generationcp.middleware.manager.api.InventoryDataManager;
+import org.generationcp.middleware.operation.builder.LotBuilder;
+import org.generationcp.middleware.operation.builder.TransactionBuilder;
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Matchers;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class InventoryServiceImplTest extends IntegrationTestBase {
-
-	@Autowired
-	private InventoryService inventoryService;
 
 	public static final String TEST_INVENTORY_ID = "TR1-123";
 
+	@Mock
+	private HibernateSessionProvider sessionProvider;
+
+	@Mock
+	private TransactionDAO transactionDAO;
+
+	@Mock
+	private InventoryDataManager inventoryDataManager;
+
+	@Mock
+	private StockTransactionDAO stockTransactionDAO;
+
+	@Mock
+	private LotBuilder lotBuilder;
+
+	@Mock
+	private TransactionBuilder transactionBuilder;
+
+	@InjectMocks
+	private InventoryServiceImpl inventoryServiceImpl;
+
 	@Test
-	public void testGetCurrentNotificationNumber() throws MiddlewareException {
-		Integer currentNotificationNumber = this.inventoryService.getCurrentNotationNumberForBreederIdentifier("TR");
-		Assert.assertEquals(2, currentNotificationNumber.intValue());
+	public void testGetCurrentNotificationNumber_NullInventoryIds() throws MiddlewareException {
+		String breederIdentifier = "TR";
+		Mockito.doReturn(null).when(this.transactionDAO).getInventoryIDsWithBreederIdentifier(breederIdentifier);
+		Integer currentNotificationNumber = this.inventoryServiceImpl.getCurrentNotationNumberForBreederIdentifier(breederIdentifier);
+		Assert.assertEquals(0, currentNotificationNumber.intValue());
 	}
 
 	@Test
-	public void testMockedGetCurrentNotationNumberForBreederIdentifier() throws MiddlewareException {
+	public void testGetCurrentNotificationNumber_EmptyInventoryIds() throws MiddlewareException {
+		String breederIdentifier = "TR";
+		Mockito.doReturn(new ArrayList<String>()).when(this.transactionDAO).getInventoryIDsWithBreederIdentifier(breederIdentifier);
+		Integer currentNotificationNumber = this.inventoryServiceImpl.getCurrentNotationNumberForBreederIdentifier(breederIdentifier);
+		Assert.assertEquals(0, currentNotificationNumber.intValue());
+	}
+
+	@Test
+	public void testGetCurrentNotationNumberForBreederIdentifier_WithExisting() throws MiddlewareException {
 		List<String> inventoryIDs = new ArrayList<>();
 		inventoryIDs.add("PRE1-12");
 		inventoryIDs.add("PRE1-13");
@@ -38,13 +75,24 @@ public class InventoryServiceImplTest extends IntegrationTestBase {
 		inventoryIDs.add("PRE3-1");
 		inventoryIDs.add("PRE35-1");
 
-		TransactionDAO dao = Mockito.mock(TransactionDAO.class);
-		InventoryServiceImpl dut = Mockito.spy(new InventoryServiceImpl(Mockito.mock(HibernateSessionProvider.class), ""));
-		Mockito.doReturn(dao).when(dut).getTransactionDao();
-		Mockito.when(dao.getInventoryIDsWithBreederIdentifier(Matchers.anyString())).thenReturn(inventoryIDs);
-
-		Integer currentNotationNumber = dut.getCurrentNotationNumberForBreederIdentifier("PRE");
+		String breederIdentifier = "PRE";
+		Mockito.doReturn(inventoryIDs).when(this.transactionDAO).getInventoryIDsWithBreederIdentifier(breederIdentifier);
+		Integer currentNotationNumber = this.inventoryServiceImpl.getCurrentNotationNumberForBreederIdentifier(breederIdentifier);
 		Assert.assertEquals(35, currentNotationNumber.intValue());
 
+	}
+
+	@Test
+	public void testStockHasCompletedBulking() throws MiddlewareQueryException {
+		Integer listId = 17;
+		List<InventoryDetails> inventoryDetailsList =
+				this.inventoryServiceImpl.getInventoryListByListDataProjectListId(listId, GermplasmListType.CROSSES);
+		boolean hasCompletedBulking = false;
+		for (InventoryDetails inventoryDetails : inventoryDetailsList) {
+			if (InventoryDetails.BULK_COMPL_COMPLETED.equals(inventoryDetails.getBulkCompl())) {
+				hasCompletedBulking = true;
+			}
+		}
+		Assert.assertEquals(hasCompletedBulking, this.inventoryServiceImpl.stockHasCompletedBulking(listId));
 	}
 }
