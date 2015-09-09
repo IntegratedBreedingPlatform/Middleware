d5c598a0bc5adb246d1d117770df4bb197b6a44e
Akhil Mehra <akhil@leafnode.io>
2015-09-10 11:03:06 +1200

    The XADatasource creation is now unit tested
    Creation of unit tests for the XA datasource resulted in a major refactor and thus resulted in breaking up the XADatasource class into smaller chunks.
    
    Additionally javadoc and logging was added to the new classes.
    
    issue: BMS-1369
    reviewer: MatthewB

---
+++ src/test/java/org/generationcp/middleware/hibernate/XABeanDefinitionTest.java
@@ -0,0 +1,100 @@
+
+package org.generationcp.middleware.hibernate;
+
+import java.util.Map;
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
+
+import com.atomikos.jdbc.AtomikosDataSourceBean;
+import com.google.common.collect.ImmutableList;
+
+public class XABeanDefinitionTest {
+
+	private final XADataSourceProperties xaDataSourceProperties = XATestUtility.mockProperties();
+	private final String cropDatabaseName = "TestCrop";
+
+	@Test
+	public void testGetDatabaseConnectionProperties() throws Exception {
+
+		final XABeanDefinition xaBeanDefinition = new XABeanDefinition();
+		final Properties databaseConnectionProperties =
+				xaBeanDefinition.getDatabaseConnectionProperties(this.cropDatabaseName, this.xaDataSourceProperties);
+
+		Assert.assertEquals(XATestUtility.DB_USERNAME, databaseConnectionProperties.getProperty(XABeanDefinition.USER));
+		Assert.assertEquals(XATestUtility.DB_PASSWORD, databaseConnectionProperties.getProperty(XABeanDefinition.PASSWORD_PROPERTY));
+		Assert.assertEquals("jdbc:mysql://" + this.xaDataSourceProperties.getHost() + ":" + this.xaDataSourceProperties.getPort() + "/"
+				+ this.cropDatabaseName, databaseConnectionProperties.getProperty(XABeanDefinition.URL));
+		Assert.assertEquals("true", databaseConnectionProperties.getProperty(XABeanDefinition.PIN_GLOBAL_TX_TO_PHYSICAL_CONNECTION));
+
+	}
+
+	@Test
+	public void testGetDataSourceBeanDefinitionProperties() throws Exception {
+		final XABeanDefinition xaBeanDefinition = new XABeanDefinition();
+		final Map<String, Object> dataSourceBeanDefinitionProperties =
+				xaBeanDefinition.getDataSourceBeanDefinitionProperties(this.cropDatabaseName, this.xaDataSourceProperties);
+
+		Assert.assertTrue(((String) dataSourceBeanDefinitionProperties.get(XABeanDefinition.UNIQUE_RESOURCE_NAME))
+				.startsWith(XABeanDefinition.XA_PREFIX + this.cropDatabaseName.toUpperCase()));
+
+		Assert.assertEquals(XATestUtility.CONNECTIONPOOL_MAINTENANCE_INTERVAL,
+				dataSourceBeanDefinitionProperties.get(XABeanDefinition.MAINTENANCE_INTERVAL));
+
+		Assert.assertEquals(XATestUtility.CONNECTIONPOOL_MAINTENANCE_INTERVAL,
+				dataSourceBeanDefinitionProperties.get(XABeanDefinition.MAINTENANCE_INTERVAL));
+		Assert.assertEquals(XATestUtility.CONNECTIONPOOL_MAX_IDLE_TIME,
+				dataSourceBeanDefinitionProperties.get(XABeanDefinition.MAX_IDLE_TIME));
+		Assert.assertEquals(XATestUtility.CONNECTIONPOOL_MAX_POOL_SIZE,
+				dataSourceBeanDefinitionProperties.get(XABeanDefinition.MAX_POOL_SIZE));
+		Assert.assertEquals(XATestUtility.CONNECTIONPOOL_MIN_POOL_SIZE,
+				dataSourceBeanDefinitionProperties.get(XABeanDefinition.MIN_POOL_SIZE));
+		Assert.assertEquals(XATestUtility.CONNECTIONPOOL_TEST_QUERY, dataSourceBeanDefinitionProperties.get(XABeanDefinition.TEST_QUERY));
+		Assert.assertEquals(XATestUtility.CONNECTIONPOOL_BORROW_CONNECTION_TIMEOUT,
+				dataSourceBeanDefinitionProperties.get(XABeanDefinition.BORROW_CONNECTION_TIMEOUT));
+
+		Assert.assertEquals(XATestUtility.CONNECTIONPOOL_XADRIVER_NAME,
+				dataSourceBeanDefinitionProperties.get(XABeanDefinition.XA_DATA_SOURCE_CLASS_NAME));
+		Assert.assertEquals(XATestUtility.CONNECTIONPOOL_BORROW_CONNECTION_TIMEOUT,
+				dataSourceBeanDefinitionProperties.get(XABeanDefinition.BORROW_CONNECTION_TIMEOUT));
+		Assert.assertTrue(dataSourceBeanDefinitionProperties.get(XABeanDefinition.XA_PROPERTIES).getClass().equals(Properties.class));
+	}
+
+	@Test
+	public void testCreateAllXADataSources() throws Exception {
+
+		final XADatasourceUtilities mockXaDatasourceUtilities = Mockito.mock(XADatasourceUtilities.class);
+		final XABeanDefinition xaBeanDefinition = new XABeanDefinition(mockXaDatasourceUtilities);
+		final BeanDefinitionRegistry mockBeanDefinitionRegistry = Mockito.mock(BeanDefinitionRegistry.class);
+		final SingleConnectionDataSource mockSingleConnectionDataSource = Mockito.mock(SingleConnectionDataSource.class);
+
+		Mockito.when(mockXaDatasourceUtilities.retrieveCropDatabases((SingleConnectionDataSource) Matchers.anyObject())).thenReturn(
+				ImmutableList.<String>of("Test_Merged"));
+		final RootBeanDefinition mockRootBeanDefinition = Mockito.mock(RootBeanDefinition.class);
+		Mockito.when(
+				mockXaDatasourceUtilities.createRootBeanDefinition(Matchers.any(Class.class), Matchers.any(Map.class),
+						Matchers.any(Map.class))).thenReturn(mockRootBeanDefinition);
+
+		xaBeanDefinition.createAllXARelatedBeans(mockSingleConnectionDataSource, mockBeanDefinitionRegistry,
+				this.xaDataSourceProperties);
+
+		Mockito.verify(mockXaDatasourceUtilities, Mockito.times(2)).createRootBeanDefinition(Matchers.eq(AtomikosDataSourceBean.class),
+				Matchers.any(Map.class), Matchers.any(Map.class));
+
+		Mockito.verify(mockXaDatasourceUtilities, Mockito.times(2)).createRootBeanDefinition(
+				Matchers.eq(AnnotationSessionFactoryBean.class), Matchers.any(Map.class), Matchers.any(Map.class));
+
+		Mockito.verify(mockBeanDefinitionRegistry, Mockito.times(1)).registerBeanDefinition(
+				"Test_Merged".toUpperCase() + XABeanDefinition.DATA_SOURCE, mockRootBeanDefinition);
+		Mockito.verify(mockBeanDefinitionRegistry, Mockito.times(1)).registerBeanDefinition(
+				this.xaDataSourceProperties.getWorkbenchDbName().toUpperCase() + XABeanDefinition.DATA_SOURCE, mockRootBeanDefinition);
+
+	}
+}
---
+++ src/test/java/org/generationcp/middleware/hibernate/XADataSourcesTest.java
@@ -0,0 +1,26 @@
+
+package org.generationcp.middleware.hibernate;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+public class XADataSourcesTest {
+
+	@Test
+	public void testPostProcessBeanDefinitionRegistry() throws Exception {
+		final XABeanDefinition mockXABeanDefinition = Mockito.mock(XABeanDefinition.class);
+		final XADatasourceUtilities mockXaDataSourceUtilities = Mockito.mock(XADatasourceUtilities.class);
+		final XADataSourceProperties xaDataSourceProperties = Mockito.mock(XADataSourceProperties.class);
+		final XADataSources xaDataSources = new XADataSources(mockXaDataSourceUtilities, mockXABeanDefinition, xaDataSourceProperties);
+		final SingleConnectionDataSource mockSingleConnectionDataSource = Mockito.mock(SingleConnectionDataSource.class);
+		Mockito.when(mockXaDataSourceUtilities.getSingleConnectionDataSource(xaDataSourceProperties)).thenReturn(
+				mockSingleConnectionDataSource);
+		final BeanDefinitionRegistry mockBeanDefinitionReqistry = Mockito.mock(BeanDefinitionRegistry.class);
+		xaDataSources.postProcessBeanDefinitionRegistry(mockBeanDefinitionReqistry);
+
+		Mockito.verify(mockXABeanDefinition).createAllXARelatedBeans(mockSingleConnectionDataSource, mockBeanDefinitionReqistry,
+				xaDataSourceProperties);
+	}
+}
