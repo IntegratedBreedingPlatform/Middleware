8f284a35c06720789b508b1c977e225a3fa0fa8b
Daniel Villafuerte <dannyvonline@gmail.com>
2015-09-23 13:51:07 +0800

    Modified the get study ID code such that it also checks  for the value within the study details object contained in the Workbook object
    BMS-1327
    Reviewer : Cyrus Venn Casada

--- src/main/java/org/generationcp/middleware/domain/etl/Workbook.java
+++ src/main/java/org/generationcp/middleware/domain/etl/Workbook.java
@@ -772,7 +772,11 @@ public class Workbook {
 	}
 
 	public Integer getStudyId() {
-		return this.studyId;
+		if (this.studyId == null && this.getStudyDetails().getId() != null) {
+			return this.getStudyDetails().getId();
+		} else {
+			return studyId;
+		}
 	}
 
 	public void setStudyId(Integer studyId) {
