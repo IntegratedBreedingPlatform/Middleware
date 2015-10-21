7e201a1a02230873eb879dc649521aaf6d565963
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2015-10-21 10:45:21 +0800

    Added StandardVariableInitializer Class
    BMS-1659
    
    Reviewer: Crisjaira Villarino

---
+++ src/test/java/org/generationcp/middleware/data/initializer/StandardVariableInitializer.java
@@ -0,0 +1,31 @@
+package org.generationcp.middleware.data.initializer;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.generationcp.middleware.domain.dms.Enumeration;
+import org.generationcp.middleware.domain.dms.StandardVariable;
+import org.generationcp.middleware.domain.dms.VariableConstraints;
+import org.generationcp.middleware.domain.oms.Term;
+
+public class StandardVariableInitializer {
+	
+	public static StandardVariable createStdVariable(){
+		StandardVariable stdVariable = new StandardVariable();
+		stdVariable.setName("variable name " + new Random().nextInt(10000));
+		stdVariable.setDescription("variable description");
+		stdVariable.setProperty(new Term(2002, "User", "Database user"));
+		stdVariable.setMethod(new Term(4030, "Assigned", "Term, name or id assigned"));
+		stdVariable.setScale(new Term(61220, "DBCV", "Controlled vocabulary from a database"));
+		stdVariable.setDataType(new Term(1120, "Character variable", "variable with char values"));
+		stdVariable.setIsA(new Term(1050, "Study condition", "Study condition class"));
+		stdVariable.setEnumerations(new ArrayList<Enumeration>());
+		stdVariable.getEnumerations().add(new Enumeration(10000, "N", "Nursery", 1));
+		stdVariable.getEnumerations().add(new Enumeration(10001, "HB", "Hybridization nursery", 2));
+		stdVariable.getEnumerations().add(new Enumeration(10002, "PN", "Pedigree nursery", 3));
+		stdVariable.setConstraints(new VariableConstraints(100.0, 999.0));
+		stdVariable.setCropOntologyId("CROP-TEST");
+		
+		return stdVariable;
+	}
+}
--- src/test/java/org/generationcp/middleware/operation/saver/StandardVariableSaverTest.java
+++ src/test/java/org/generationcp/middleware/operation/saver/StandardVariableSaverTest.java
@@ -1,15 +1,10 @@
 package org.generationcp.middleware.operation.saver;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 import org.generationcp.middleware.IntegrationTestBase;
 import org.generationcp.middleware.dao.oms.CVTermDao;
+import org.generationcp.middleware.data.initializer.StandardVariableInitializer;
 import org.generationcp.middleware.domain.dms.Enumeration;
 import org.generationcp.middleware.domain.dms.StandardVariable;
-import org.generationcp.middleware.domain.dms.VariableConstraints;
-import org.generationcp.middleware.domain.oms.Term;
-import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.pojos.oms.CVTerm;
 import org.junit.Assert;
 import org.junit.Before;
@@ -31,7 +26,7 @@ public class StandardVariableSaverTest extends IntegrationTestBase{
 	
 	@Test
 	public void testSaveCheckType(){
-		StandardVariable stdVar = this.createStdVariable();
+		StandardVariable stdVar = StandardVariableInitializer.createStdVariable();
 		Enumeration testEnum = new Enumeration(null, this.TESTNAME, this.TESTDEFINITION, 0);
 		this.stdVarSaver.saveEnumeration(stdVar, testEnum, CVID);
 		
@@ -39,24 +34,5 @@ public class StandardVariableSaverTest extends IntegrationTestBase{
 		Assert.assertNotNull("The newly created CvTerm should exist in the DB", cvTerm);
 		Assert.assertEquals("The newly created CvTerm's name should be " + this.TESTNAME, this.TESTNAME, cvTerm.getName());
 		Assert.assertEquals("The newly created CvTerm's description should be " + this.TESTDEFINITION, this.TESTDEFINITION, cvTerm.getDefinition());
-	}
-	
-	public StandardVariable createStdVariable(){
-		StandardVariable stdVariable = new StandardVariable();
-		stdVariable.setName("variable name " + new Random().nextInt(10000));
-		stdVariable.setDescription("variable description");
-		stdVariable.setProperty(new Term(2002, "User", "Database user"));
-		stdVariable.setMethod(new Term(4030, "Assigned", "Term, name or id assigned"));
-		stdVariable.setScale(new Term(61220, "DBCV", "Controlled vocabulary from a database"));
-		stdVariable.setDataType(new Term(1120, "Character variable", "variable with char values"));
-		stdVariable.setIsA(new Term(1050, "Study condition", "Study condition class"));
-		stdVariable.setEnumerations(new ArrayList<Enumeration>());
-		stdVariable.getEnumerations().add(new Enumeration(10000, "N", "Nursery", 1));
-		stdVariable.getEnumerations().add(new Enumeration(10001, "HB", "Hybridization nursery", 2));
-		stdVariable.getEnumerations().add(new Enumeration(10002, "PN", "Pedigree nursery", 3));
-		stdVariable.setConstraints(new VariableConstraints(100.0, 999.0));
-		stdVariable.setCropOntologyId("CROP-TEST");
-		
-		return stdVariable;
 	}
 }
