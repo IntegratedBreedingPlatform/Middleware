0f16acb03b43831dc2435078308dbe0694530381
srajwadi <sunny@leafnode.io>
2015-10-30 17:17:23 +0530

    Adding Unit Tests for Property: - testUpdatePropertyShouldVerifyDaoCallsAndSetUpdatedDate - testUpdatePropertyShouldFailIfTermIsNotProperty - testDeletePropertyShouldVerifyDaoCalls - testDeletePropertyShouldNotAllowIfTermIsReferred - testDeletePropertyShouldFailIfTermDoesNotExists - testValidateParentClass - testIfPropertyIsReferred
    Issue: BMS-1598
    Reviewer: Keyur

--- src/test/java/org/generationcp/middleware/manager/ontology/OntologyPropertyDataManagerImplUnitTest.java
+++ src/test/java/org/generationcp/middleware/manager/ontology/OntologyPropertyDataManagerImplUnitTest.java
@@ -277,4 +277,195 @@ public class OntologyPropertyDataManagerImplUnitTest extends UnitTestBase{
 		//Verify save cvterm
 		Mockito.verify(this.cvTermDao).save(property.getName(), property.getDefinition(), CvId.PROPERTIES);
 	}
+
+	@Test
+	public void testUpdatePropertyShouldVerifyDaoCallsAndSetUpdatedDate() throws Exception {
+		CVTerm propertyTerm = TestDataHelper.getTestCvTerm(CvId.PROPERTIES);
+
+		//Updating property via manager
+		Property property = new Property();
+		property.setId(propertyTerm.getCvTermId());
+		property.setName("New Property Name");
+		property.setDefinition("New Property Definition");
+		property.setCropOntologyId("CO:1234");
+
+		Mockito.when(this.cvTermDao.getById(propertyTerm.getCvTermId())).thenReturn(propertyTerm);
+		Mockito.when(this.cvTermDao.merge(propertyTerm)).thenReturn(propertyTerm);
+
+		Date updatedDate = this.constructDate(2015, Calendar.MAY, 20);
+		this.stubCurrentDate(updatedDate);
+
+		this.propertyDataManager.updateProperty(property);
+
+		// Make sure each property data inserted properly, assert them and display proper message if not inserted properly
+		String message = "The %s for property '" + propertyTerm.getCvTermId() + "' was not updated correctly.";
+		Assert.assertNotNull(String.format(message, "Id"), property.getId());
+
+		Mockito.verify(this.cvTermDao).getById(propertyTerm.getCvTermId());
+		Mockito.verify(this.cvTermDao).merge(propertyTerm);
+
+		// TODO: Mockito.verify needs to be fixed
+        // Mockito.verify(this.cvTermPropertyDao, Mockito.times(2)).save(property.getId(), Matchers.any(Integer.class), Matchers.any(String.class), 0);
+	}
+
+	/**
+	 * This test should check if Term is not property
+	 */
+	@Test(expected = MiddlewareException.class)
+	public void testUpdatePropertyShouldFailIfTermIsNotProperty() throws Exception {
+		CVTerm methodTerm = TestDataHelper.getTestCvTerm(CvId.METHODS);
+
+		//Updating property via manager
+		Property property = new Property();
+		property.setId(methodTerm.getCvTermId());
+		property.setName("New Property Name");
+		property.setDefinition("New Property Definition");
+
+		Mockito.when(this.cvTermDao.getById(methodTerm.getCvTermId())).thenReturn(methodTerm);
+		this.propertyDataManager.updateProperty(property);
+	}
+
+	/**
+	 * This test should check delete property should delete
+	 * 1. Term
+	 * 2. Properties associated to term
+	 */
+	@Test
+	public void testDeletePropertyShouldVerifyDaoCalls() throws Exception {
+		// Save Property Term
+		CVTerm propertyTerm = TestDataHelper.getTestCvTerm(CvId.PROPERTIES);
+
+		final Integer isATermCount = 1;
+		List<CVTerm> isATerms = TestDataHelper.generateNewIsATerms(isATermCount, this.cvTermDao);
+
+		List<CVTermRelationship> cvTermRelationships = new ArrayList<>();
+		CVTermRelationship cvTermRelationship = new CVTermRelationship();
+		cvTermRelationship.setSubjectId(propertyTerm.getCvTermId());
+		cvTermRelationship.setTypeId(TermId.IS_A.getId());
+		cvTermRelationship.setObjectId(isATerms.get(0).getCvTermId());
+		cvTermRelationships.add(cvTermRelationship);
+
+		// Fill Test Created Date Property
+		Date testCreatedDate = this.constructDate(2015, Calendar.JANUARY, 1);
+		List<CVTermProperty> createdDateProperties = new ArrayList<>();
+		TestDataHelper.fillTestCreatedDateProperties(Collections.singletonList(propertyTerm), createdDateProperties, testCreatedDate);
+
+		// Fill Test Updated Date Property
+		Date testUpdatedDate = this.constructDate(2015, Calendar.MAY, 20);
+		List<CVTermProperty> updatedDateProperties = new ArrayList<>();
+		TestDataHelper.fillTestUpdatedDateProperties(Collections.singletonList(propertyTerm), updatedDateProperties, testUpdatedDate);
+
+		List<CVTermProperty> combinedProperties = new ArrayList<>(createdDateProperties);
+		combinedProperties.addAll(updatedDateProperties);
+
+		Mockito.when(this.cvTermDao.getById(isATerms.get(0).getCvTermId())).thenReturn(isATerms.get(0));
+		Mockito.when(this.cvTermPropertyDao.getByCvTermId(propertyTerm.getCvTermId())).thenReturn(combinedProperties);
+		Mockito.doNothing().when(this.cvTermDao).makeTransient(propertyTerm);
+		Mockito.doNothing().when(this.cvTermPropertyDao).makeTransient(Mockito.any(CVTermProperty.class));
+		Mockito.when(this.cvTermDao.getById(propertyTerm.getCvTermId())).thenReturn(propertyTerm);
+		Mockito.when(this.cvTermRelationshipDao.getByObjectId(propertyTerm.getCvTermId())).thenReturn(new ArrayList<CVTermRelationship>());
+		Mockito.when(this.cvTermRelationshipDao.getBySubject(propertyTerm.getCvTermId())).thenReturn(cvTermRelationships);
+
+		this.propertyDataManager.deleteProperty(propertyTerm.getCvTermId());
+
+		Mockito.verify(this.cvTermDao, Mockito.times(1)).makeTransient(propertyTerm);
+		Mockito.verify(this.cvTermPropertyDao, Mockito.times(2)).makeTransient(Mockito.any(CVTermProperty.class));
+		Mockito.verify(this.cvTermRelationshipDao, Mockito.times(1)).makeTransient(Mockito.any(CVTermRelationship.class));
+	}
+
+	/**
+	 * This test should fail if term is referred
+	 */
+	@Test(expected = MiddlewareException.class)
+	public void testDeletePropertyShouldNotAllowIfTermIsReferred() throws Exception {
+		// Save Property Term using dao
+		CVTerm propertyTerm = TestDataHelper.getTestCvTerm(CvId.PROPERTIES);
+		Mockito.when(this.cvTermDao.getById(propertyTerm.getCvTermId())).thenReturn(propertyTerm);
+		Mockito.doReturn(true).when(this.cvTermRelationshipDao).isTermReferred(propertyTerm.getCvTermId());
+		this.propertyDataManager.deleteProperty(propertyTerm.getCvTermId());
+	}
+
+	/**
+	 * This test should check term exists
+	 */
+	@Test(expected = MiddlewareException.class)
+	public void testDeletePropertyShouldFailIfTermDoesNotExists() throws Exception {
+		Mockito.when(this.cvTermDao.getById(0)).thenReturn(null);
+		this.propertyDataManager.deleteProperty(0);
+	}
+
+	/**
+	 * Test to validate Parent Class
+	 * @throws Exception
+	 */
+	@Test(expected = MiddlewareException.class)
+	public void testValidateParentClass() throws Exception {
+		// Save Property Term
+		CVTerm propertyTerm = TestDataHelper.getTestCvTerm(CvId.PROPERTIES);
+
+		final Integer isATermCount = 1;
+		List<CVTerm> isATerms = TestDataHelper.generateNewIsATerms(isATermCount, this.cvTermDao);
+
+		List<CVTermRelationship> cvTermRelationships = new ArrayList<>();
+		CVTermRelationship cvTermRelationship = new CVTermRelationship();
+		cvTermRelationship.setSubjectId(propertyTerm.getCvTermId());
+		cvTermRelationship.setTypeId(TermId.IS_A.getId());
+		cvTermRelationship.setObjectId(isATerms.get(0).getCvTermId());
+		cvTermRelationships.add(cvTermRelationship);
+
+		// Fill Test Created Date Property
+		Date testCreatedDate = this.constructDate(2015, Calendar.JANUARY, 1);
+		List<CVTermProperty> createdDateProperties = new ArrayList<>();
+		TestDataHelper.fillTestCreatedDateProperties(Collections.singletonList(propertyTerm), createdDateProperties, testCreatedDate);
+
+		// Fill Test Updated Date Property
+		Date testUpdatedDate = this.constructDate(2015, Calendar.MAY, 20);
+		List<CVTermProperty> updatedDateProperties = new ArrayList<>();
+		TestDataHelper.fillTestUpdatedDateProperties(Collections.singletonList(propertyTerm), updatedDateProperties, testUpdatedDate);
+
+		List<CVTermProperty> combinedProperties = new ArrayList<>(createdDateProperties);
+		combinedProperties.addAll(updatedDateProperties);
+
+		Mockito.when(this.cvTermDao.getById(isATerms.get(0).getCvTermId())).thenReturn(propertyTerm);
+		Mockito.when(this.cvTermPropertyDao.getByCvTermId(propertyTerm.getCvTermId())).thenReturn(combinedProperties);
+		Mockito.doNothing().when(this.cvTermDao).makeTransient(propertyTerm);
+		Mockito.doNothing().when(this.cvTermPropertyDao).makeTransient(Mockito.any(CVTermProperty.class));
+		Mockito.when(this.cvTermDao.getById(propertyTerm.getCvTermId())).thenReturn(propertyTerm);
+		Mockito.when(this.cvTermRelationshipDao.getByObjectId(propertyTerm.getCvTermId())).thenReturn(new ArrayList<CVTermRelationship>());
+		Mockito.when(this.cvTermRelationshipDao.getBySubject(propertyTerm.getCvTermId())).thenReturn(cvTermRelationships);
+		this.propertyDataManager.deleteProperty(propertyTerm.getCvTermId());
+	}
+
+	@Test(expected = MiddlewareException.class)
+	public void testIfPropertyIsReferred() throws Exception {
+		CVTerm propertyTerm = TestDataHelper.getTestCvTerm(CvId.PROPERTIES);
+
+		final Integer isATermCount = 1;
+		List<CVTerm> isATerms = TestDataHelper.generateNewIsATerms(isATermCount, this.cvTermDao);
+
+		List<CVTermRelationship> cvTermRelationships = new ArrayList<>();
+		CVTermRelationship cvTermRelationship = new CVTermRelationship();
+		cvTermRelationship.setSubjectId(propertyTerm.getCvTermId());
+		cvTermRelationship.setTypeId(TermId.IS_A.getId());
+		cvTermRelationship.setObjectId(isATerms.get(0).getCvTermId());
+		cvTermRelationships.add(cvTermRelationship);
+
+		// Fill Test Created Date Property
+		Date testCreatedDate = this.constructDate(2015, Calendar.JANUARY, 1);
+		List<CVTermProperty> createdDateProperties = new ArrayList<>();
+		TestDataHelper.fillTestCreatedDateProperties(Collections.singletonList(propertyTerm), createdDateProperties, testCreatedDate);
+
+		// Fill Test Updated Date Property
+		Date testUpdatedDate = this.constructDate(2015, Calendar.MAY, 20);
+		List<CVTermProperty> updatedDateProperties = new ArrayList<>();
+		TestDataHelper.fillTestUpdatedDateProperties(Collections.singletonList(propertyTerm), updatedDateProperties, testUpdatedDate);
+
+		List<CVTermProperty> combinedProperties = new ArrayList<>(createdDateProperties);
+		combinedProperties.addAll(updatedDateProperties);
+
+		Mockito.when(this.cvTermDao.getById(propertyTerm.getCvTermId())).thenReturn(propertyTerm);
+		Mockito.when(this.cvTermRelationshipDao.isTermReferred(propertyTerm.getCvTermId())).thenReturn(true);
+
+		this.propertyDataManager.deleteProperty(propertyTerm.getCvTermId());
+	}
 }
