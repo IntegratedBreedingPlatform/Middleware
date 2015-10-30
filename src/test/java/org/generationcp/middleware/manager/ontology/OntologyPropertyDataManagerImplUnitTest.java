c35a13ad1263463cc29be9256ea7205d36cffe2a
srajwadi <sunny@leafnode.io>
2015-10-30 10:21:31 +0530

    Added unit tests for get property by id that return valid property and test for that will get no property while fetching property by id.
    Issue: BMS-1598
    Reviewer: Keyur

--- src/test/java/org/generationcp/middleware/manager/ontology/OntologyPropertyDataManagerImplUnitTest.java
+++ src/test/java/org/generationcp/middleware/manager/ontology/OntologyPropertyDataManagerImplUnitTest.java
@@ -2,10 +2,12 @@ package org.generationcp.middleware.manager.ontology;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.google.common.base.Function;
 import com.google.common.collect.Ordering;
@@ -83,16 +85,70 @@ public class OntologyPropertyDataManagerImplUnitTest extends UnitTestBase{
 		List<Property> properties = this.propertyDataManager.getAllProperties();
 
 		List<String> propertyNames = Util.convertAll(properties, new Function<Property, String>() {
-				@Override
-				public String apply(Property x) {
-					return x.getName();
-				}
-			});
+
+			@Override
+			public String apply(Property x) {
+				return x.getName();
+			}
+		});
 
 		final boolean propertyNameIsOrdered = Ordering.natural().isOrdered(propertyNames);
 
 		Assert.assertEquals(propertyMap.size(), properties.size());
 		Assert.assertTrue(propertyNameIsOrdered);
 
+	}
+
+	@Test
+	public void testGetPropertyByIdShouldFetchAndVerify() throws Exception {
+		Map<Integer, Property> propertyMap = new HashMap<>();
+		TestDataHelper.fillTestProperties(propertyMap, 1);
+
+		// Fill Test Created Date Property
+		Date testCreatedDate = this.constructDate(2015, Calendar.JANUARY, 1);
+		List<CVTermProperty> createdDateProperties = new ArrayList<>();
+		TestDataHelper.fillTestCreatedDatePropertyProps(new ArrayList<>(propertyMap.values()), createdDateProperties, testCreatedDate);
+
+		// Fill Test Updated Date Property
+		Date testUpdatedDate = this.constructDate(2015, Calendar.JUNE, 30);
+		List<CVTermProperty> updatedDateProperties = new ArrayList<>();
+		TestDataHelper.fillTestUpdatedDatePropertyProps(new ArrayList<>(propertyMap.values()), updatedDateProperties, testUpdatedDate);
+
+		List<CVTermProperty> combinedProperties = new ArrayList<>(createdDateProperties);
+		combinedProperties.addAll(updatedDateProperties);
+
+		Property existingProperty = propertyMap.get(propertyMap.keySet().iterator().next());
+
+		Mockito.when(this.cvTermPropertyDao.getByCvTermIds(new ArrayList<>(Collections.singletonList(existingProperty.getId())))).thenReturn(combinedProperties);
+		Mockito.when(this.ontologyCommonDAO.getPropertiesWithCropOntologyAndTraits(false, new ArrayList<>(Collections.singletonList(existingProperty.getId())), true)).thenReturn(propertyMap);
+
+		final Property property = this.propertyDataManager.getProperty(existingProperty.getId(), true);
+
+		// Make sure our property exists and is inserted properly and display proper message if it is not inserted properly
+		String message = "The %s for property '" + existingProperty.getId() + "' was not added correctly.";
+		Assert.assertEquals(String.format(message, "Name"), existingProperty.getName(), property.getName());
+		Assert.assertEquals(String.format(message, "Definition"), existingProperty.getDefinition(), property.getDefinition());
+		Assert.assertEquals(String.format(message, "CreatedDate"),existingProperty.getDateCreated(), property.getDateCreated());
+		Assert.assertEquals(String.format(message, "UpdatedDate"), existingProperty.getDateLastModified(), property.getDateLastModified());
+		Assert.assertEquals(String.format(message, "CropOntologyId"), existingProperty.getCropOntologyId(), property.getCropOntologyId());
+		final Set<String> classNames = existingProperty.getClasses();
+
+		for(String className : classNames) {
+			Assert.assertTrue(String.format(message, "ClassName"), property.getClasses().contains(className));
+		}
+	}
+
+	@Test
+	public void testGetPropertyByIdShouldReturnEmptyList() throws Exception {
+		Map<Integer, Property> propertyMap = new HashMap<>();
+		TestDataHelper.fillTestProperties(propertyMap, 1);
+
+		Integer propertyId = propertyMap.keySet().iterator().next();
+		Mockito.when(this.ontologyCommonDAO.getPropertiesWithCropOntologyAndTraits(false, new ArrayList<>(Collections.singletonList(propertyMap.get(propertyId))), true)).thenReturn(
+				null);
+
+		final Property property = this.propertyDataManager.getProperty(propertyId, true);
+
+		Assert.assertNull(property);
 	}
 }
