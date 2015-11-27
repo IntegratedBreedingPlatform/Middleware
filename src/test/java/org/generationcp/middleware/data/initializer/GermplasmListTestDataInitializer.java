580c4ccdc226186d6d32d7410c42a9eaf97df9ab
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2015-11-27 10:43:28 -0800

    Added test data initializer in Middleware
    Issue: BMS-2025
    Reviewer: Cyrus Venn Casada

---
+++ src/test/java/org/generationcp/middleware/data/initializer/GermplasmListTestDataInitializer.java
@@ -0,0 +1,69 @@
+
+package org.generationcp.middleware.data.initializer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.generationcp.middleware.pojos.GermplasmList;
+import org.generationcp.middleware.pojos.GermplasmListData;
+
+public class GermplasmListTestDataInitializer {
+
+	public static GermplasmList createGermplasmList(final int id) {
+		final GermplasmList germplasmList = new GermplasmList();
+		germplasmList.setId(id);
+		germplasmList.setName("List " + id);
+		germplasmList.setDescription("List " + id + " Description");
+		germplasmList.setDate(20150101L);
+
+		return germplasmList;
+	}
+
+	public static GermplasmList createGermplasmListWithListData(final int id, final int noOfEntries) {
+		final GermplasmList germplasmList = createGermplasmList(id);
+		germplasmList.setListData(createGermplasmListData(noOfEntries));
+
+		return germplasmList;
+	}
+
+	public static List<GermplasmListData> createGermplasmListData(final Integer itemNo) {
+		final List<GermplasmListData> listEntries = new ArrayList<GermplasmListData>();
+		for (int i = 1; i <= itemNo; i++) {
+			final GermplasmListData listEntry = new GermplasmListData();
+			listEntry.setId(i);
+			listEntry.setDesignation("Designation " + i);
+			listEntry.setEntryCode("EntryCode " + i);
+			listEntry.setEntryId(i);
+			listEntry.setGroupName("GroupName " + i);
+			listEntry.setStatus(1);
+			listEntry.setSeedSource("SeedSource " + i);
+
+			listEntries.add(listEntry);
+		}
+
+		return listEntries;
+	}
+
+	public static List<GermplasmList> createGermplasmLists(final int numOfEntries) {
+		final List<GermplasmList> germplasmLists = new ArrayList<GermplasmList>();
+
+		for (int i = 0; i < numOfEntries; i++) {
+			final Integer id = i + 1;
+			germplasmLists.add(GermplasmListTestDataInitializer.createGermplasmListWithListData(id, numOfEntries));
+		}
+		return germplasmLists;
+	}
+
+	public static List<GermplasmList> createGermplasmListsWithType(final int numOfEntries) {
+		final List<GermplasmList> germplasmLists = new ArrayList<GermplasmList>();
+
+		for (int i = 0; i < numOfEntries; i++) {
+			final Integer id = i + 1;
+			final GermplasmList germplasmList = createGermplasmListWithListData(id, numOfEntries);
+			germplasmList.setType("test" + id);
+			germplasmLists.add(germplasmList);
+		}
+
+		return germplasmLists;
+	}
+}
