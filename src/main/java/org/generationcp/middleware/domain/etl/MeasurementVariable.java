b03c9db5bdce40487a25fdbee06a354d83147a1d
Daniel Villafuerte <dannyvonline@gmail.com>
2015-12-17 17:30:25 +0800

    Added a constant for the categorical variable type
    BMS-2064
    Reviewer : Cyrus  Venn Casada

--- src/main/java/org/generationcp/middleware/domain/etl/MeasurementVariable.java
+++ src/main/java/org/generationcp/middleware/domain/etl/MeasurementVariable.java
@@ -22,6 +22,8 @@ import org.generationcp.middleware.util.Debug;
 
 public class MeasurementVariable {
 
+    public static final String CATEGORICAL_VARIABLE_TYPE = "Categorical";
+
 	private int termId;
 
 	private String name;
