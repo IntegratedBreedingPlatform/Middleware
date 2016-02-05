f426dbbd4c643d464c26b604761e50a208261b82
Akhil Mehra <akhil@leafnode.io>
2016-02-05 16:01:11 +1300

    Moved the name type resolution into the cross builder util
    Moved the name type resolution into the cross builder util as this code is shared by the Single, GermplasmCross and Backcross node
    
    issue: BMS-2174
    reviewer: NaymeshM, MatthewB

--- src/main/java/org/generationcp/middleware/pojos/germplasm/CrossBuilderUtil.java
+++ src/main/java/org/generationcp/middleware/pojos/germplasm/CrossBuilderUtil.java
@@ -3,23 +3,33 @@ package org.generationcp.middleware.pojos.germplasm;
 
 import java.util.List;
 
+import org.generationcp.middleware.pojos.Germplasm;
 import org.generationcp.middleware.pojos.Name;
+import org.generationcp.middleware.service.pedigree.PedigreeDataManagerFactory;
 
 import com.google.common.base.Preconditions;
 
 public class CrossBuilderUtil {
 
-	static boolean nameTypeBasedResolution(StringBuilder toReturn, List<Integer> nameTypeOrder, List<Name> namesByGID) {
-		Preconditions.checkNotNull(toReturn);
-		Preconditions.checkNotNull(nameTypeOrder);
-		Preconditions.checkNotNull(namesByGID);
+	static boolean nameTypeBasedResolution(final StringBuilder toReturn, final PedigreeDataManagerFactory pedigreeDataManagerFactory,
+			final Germplasm germplasm, final List<Integer> nameTypeOrder) {
 
-		for (Integer integer : nameTypeOrder) {
-			if (namesByGID != null) {
-				for (Name name : namesByGID) {
-					if (integer.equals(name.getTypeId())) {
-						toReturn.append(name.getNval());
-						return true;
+		Preconditions.checkNotNull(toReturn);
+		Preconditions.checkNotNull(pedigreeDataManagerFactory);
+		Preconditions.checkNotNull(germplasm);
+		Preconditions.checkNotNull(nameTypeOrder);
+
+		if (germplasm != null) {
+			final List<Name> namesByGID =
+					pedigreeDataManagerFactory.getGermplasmDataManager().getByGIDWithListTypeFilters(germplasm.getGid(), null,
+							nameTypeOrder);
+			for (final Integer integer : nameTypeOrder) {
+				if (namesByGID != null) {
+					for (final Name name : namesByGID) {
+						if (integer.equals(name.getTypeId())) {
+							toReturn.append(name.getNval());
+							return true;
+						}
 					}
 				}
 			}
