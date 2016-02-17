6d3baf3cdbd3ab341839e4387c5871e439ac5f3c
Daniel Villafuerte <dannyvonline@gmail.com>
2016-02-17 10:05:18 +0800

    Created unit test for new logic required to support single parent information available for germplasm
    BMS-1747
    Reviewer : Cyrus Venn Casada

---
+++ src/test/java/org/generationcp/middleware/manager/GermplasmDataManagerTest.java
@@ -0,0 +1,73 @@
+
+package org.generationcp.middleware.manager;
+
+import org.generationcp.middleware.dao.GermplasmDAO;
+import org.generationcp.middleware.data.initializer.GermplasmDataManagerDataInitializer;
+import org.generationcp.middleware.pojos.Germplasm;
+import org.generationcp.middleware.pojos.GermplasmPedigreeTreeNode;
+import org.generationcp.middleware.pojos.Name;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
+import java.util.Map;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GermplasmDataManagerTest {
+
+	private static final Integer TEST_STUDY_ID = 1;
+
+	@Mock
+	private GermplasmDAO germplasmDAO;
+
+	@Test
+	public void testGetDirectParentsForStudyOneParentOnly() {
+
+		// we make use of partial mocking techniques so as to be able to inject the GermplasmDAO mock
+		final GermplasmDataManagerImpl germplasmDataManager = Mockito.mock(GermplasmDataManagerImpl.class);
+
+		Mockito.doReturn(germplasmDAO).when(germplasmDataManager).getGermplasmDao();
+		Mockito.doReturn(GermplasmDataManagerDataInitializer.createGermplasmParentNameMap()).when(this.germplasmDAO)
+				.getGermplasmParentNamesForStudy(TEST_STUDY_ID);
+
+		final List<Germplasm> germplasmList = GermplasmDataManagerDataInitializer.createGermplasmList();
+		// for testing purposes, we set the gpid1 of the germplasm to simulate a germplasm with only one parent
+		final Germplasm testGermplasm = germplasmList.get(0);
+		testGermplasm.setGpid1(0);
+
+		Mockito.doReturn(germplasmList).when(this.germplasmDAO).getGermplasmParentsForStudy(TEST_STUDY_ID);
+
+		// the doCallRealMethod allows us to execute the actual implementation instead of a test stub
+		Mockito.doCallRealMethod().when(germplasmDataManager).getDirectParentsForStudy(TEST_STUDY_ID);
+		Mockito.doCallRealMethod().when(germplasmDataManager)
+				.createGermplasmPedigreeTreeNode(Mockito.anyInt(), Mockito.anyMapOf(GermplasmNameType.class, Name.class));
+
+		final Map<Integer, GermplasmPedigreeTreeNode> output = germplasmDataManager.getDirectParentsForStudy(TEST_STUDY_ID);
+
+		Assert.assertTrue(
+				"Data manager should still be able to create a structure containing parent information even with only one parent",
+				output.containsKey(GermplasmDataManagerDataInitializer.TEST_GID));
+
+		Assert.assertNotNull(
+				"Data manager should still be able to create a structure containing parent information even with only one parent",
+				output.get(GermplasmDataManagerDataInitializer.TEST_GID));
+
+		final GermplasmPedigreeTreeNode node = output.get(GermplasmDataManagerDataInitializer.TEST_GID);
+
+		// we use 0 as the index for retrieving the expected null object here because previously, we set the germplasm's gpid1 to 0
+		// gpid1 represents the first parent, hence the first parent info should be null
+		Assert.assertNull("Non present parental information should be represented by a null in the linked nodes list", node
+				.getLinkedNodes().get(0));
+
+		// since gpid2 (2nd parent) information is available, we expect the 2nd entry in the list to have actual info
+		Assert.assertNotNull("Data manager unable to properly represent parent info when only one is present", node.getLinkedNodes().get(1));
+		Assert.assertEquals("Data manager unable to properly represent parent info when only one is present", testGermplasm.getGpid2(),
+				node.getLinkedNodes().get(1).getGermplasm().getGid());
+
+	}
+
+}
--- src/test/java/org/generationcp/middleware/service/ReportServiceImplTest.java
+++ src/test/java/org/generationcp/middleware/service/ReportServiceImplTest.java
@@ -1,19 +1,35 @@
 
 package org.generationcp.middleware.service;
 
+import org.generationcp.middleware.data.initializer.GermplasmDataManagerDataInitializer;
+import org.generationcp.middleware.domain.etl.MeasurementData;
+import org.generationcp.middleware.domain.etl.MeasurementRow;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.domain.oms.TermId;
+import org.generationcp.middleware.manager.api.GermplasmDataManager;
+import org.generationcp.middleware.pojos.GermplasmPedigreeTreeNode;
 import org.generationcp.middleware.reports.AbstractReporter;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ReportServiceImplTest {
+
+	@Mock
+	private GermplasmDataManager germplasmDataManager;
 
 	private final ReportServiceImpl reportService = new ReportServiceImpl();
 
 	public static final int TEST_GERMPLASM_LIST_ID = 2;
+	public static final int TEST_STUDY_ID = 1;
 
 	@Test
 	public void testRetrieveLocationIdFromConditionReturnsNullForEmptyString() {
@@ -36,10 +52,46 @@ public class ReportServiceImplTest {
 
 	@Test
 	public void testExtractGermplasmListData() {
-		Map<String, Object> data = reportService.extractGermplasmListData(TEST_GERMPLASM_LIST_ID);
+		final Map<String, Object> data = reportService.extractGermplasmListData(TEST_GERMPLASM_LIST_ID);
 		Assert.assertNotNull("Data extracter does not provide non null value for required report generation parameter",
 				data.get(AbstractReporter.STUDY_CONDITIONS_KEY));
-        Assert.assertNotNull("Data extracter does not provide non null value for required report generation parameter",
-                data.get(AbstractReporter.DATA_SOURCE_KEY));
+		Assert.assertNotNull("Data extracter does not provide non null value for required report generation parameter",
+				data.get(AbstractReporter.DATA_SOURCE_KEY));
+	}
+
+	@Test
+	public void testExtractParentDataSingleParent() {
+		// we use partial mocking on the ReportServiceImplementation so as to be able to mock the GermplasmDataManager object
+		final ReportServiceImpl unitUnderTest = Mockito.mock(ReportServiceImpl.class);
+		Mockito.doReturn(this.germplasmDataManager).when(unitUnderTest).getGermplasmDataManager();
+
+		final List<MeasurementRow> measurementRowList = createMeasurementList();
+		final Map<Integer, GermplasmPedigreeTreeNode> pedigreeTreeNodeMap = GermplasmDataManagerDataInitializer.createTreeNodeMap(true);
+
+		Mockito.doReturn(pedigreeTreeNodeMap).when(this.germplasmDataManager).getDirectParentsForStudy(TEST_STUDY_ID);
+		Mockito.doCallRealMethod().when(unitUnderTest).appendParentsInformation(TEST_STUDY_ID, measurementRowList);
+
+		unitUnderTest.appendParentsInformation(TEST_STUDY_ID, measurementRowList);
+
+		final MeasurementRow row = measurementRowList.get(0);
+		Assert.assertEquals("Female selection history should be blank if female parent information is not provided in database", "", row
+				.getMeasurementData(AbstractReporter.FEMALE_SELECTION_HISTORY_KEY).getValue());
+		Assert.assertEquals("Male selection history not properly filled in from provided information",
+				GermplasmDataManagerDataInitializer.MALE_SELECTION_HISTORY,
+				row.getMeasurementData(AbstractReporter.MALE_SELECTION_HISTORY_KEY).getValue());
+
+	}
+
+	protected List<MeasurementRow> createMeasurementList() {
+		final List<MeasurementRow> rowList = new ArrayList<>();
+		final MeasurementRow row = new MeasurementRow();
+		final List<MeasurementData> dataList = new ArrayList<>();
+		final MeasurementData data = new MeasurementData("GID", GermplasmDataManagerDataInitializer.TEST_GID.toString());
+		dataList.add(data);
+
+		row.setDataList(dataList);
+		rowList.add(row);
+
+		return rowList;
 	}
 }
