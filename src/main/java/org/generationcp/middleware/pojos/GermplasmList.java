4e69fc4e6028a6246da5deae8bc38f9fb01fdaec
Daniel Villafuerte <dannyvonline@gmail.com>
2016-03-08 11:58:28 +0800

    Added constants to represent the list type for advance lists and cross lists
    BMS-2322
    Reviewer : Aldrin Batac

--- src/main/java/org/generationcp/middleware/pojos/GermplasmList.java
+++ src/main/java/org/generationcp/middleware/pojos/GermplasmList.java
@@ -15,19 +15,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Basic;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OrderBy;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
@@ -48,6 +36,8 @@ public class GermplasmList implements Serializable {
 	private static final long serialVersionUID = 1L;
 	public static final String FOLDER_TYPE = "FOLDER";
 	public static final String LIST_TYPE = "LST";
+    public static final String ADVANCED_LIST_TYPE = "ADVANCED";
+    public static final String CROSS_LIST_TYPE = "CROSSES";
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
