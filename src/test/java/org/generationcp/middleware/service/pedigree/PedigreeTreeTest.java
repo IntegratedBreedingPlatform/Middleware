65146afbf6981c2858bb881671022c917d3ea84f
Akhil Mehra <akhil@leafnode.io>
2016-03-08 14:38:12 +1300

    Updating test to use new classes created
    issue: BMS-2174
    reviewer: MatthewB

--- src/test/java/org/generationcp/middleware/service/pedigree/PedigreeTreeTest.java
+++ src/test/java/org/generationcp/middleware/service/pedigree/PedigreeTreeTest.java
@@ -15,6 +15,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
@@ -24,11 +25,11 @@ public class PedigreeTreeTest {
 
 	private static final String MAIZE = "maize";
 
-	private LoadingCache<GermplasmKey, Optional<Germplasm>> germplasmCache;
-	private LoadingCache<MethodKey, Optional<Method>> methodCache;
+	private FunctionBasedGuavaCacheLoader<CropGermplasmKey, Germplasm> germplasmCache;
+	private FunctionBasedGuavaCacheLoader<CropMethodKey, Method> methodCache;
 
-	private Map<GermplasmKey, Germplasm> germplasmMap;
-	private Map<MethodKey, Method> methodsMap;
+	private Map<CropGermplasmKey, Germplasm> germplasmMap;
+	private Map<CropMethodKey, Method> methodsMap;
 
 	private Set<Integer> randomNumbers;
 
@@ -36,10 +37,10 @@ public class PedigreeTreeTest {
 	public void setUp() throws Exception {
 
 		// Keep our generated germplasm in this map
-		this.germplasmMap = new HashMap<GermplasmKey, Germplasm>();
+		this.germplasmMap = new HashMap<CropGermplasmKey, Germplasm>();
 
 		// Keep our generated methos in this map
-		this.methodsMap = new HashMap<MethodKey, Method>();
+		this.methodsMap = new HashMap<CropMethodKey, Method>();
 
 		this.randomNumbers = new LinkedHashSet<Integer>();
 
@@ -53,26 +54,22 @@ public class PedigreeTreeTest {
 
 	private void setUpMethodCache() {
 		this.methodCache =
-				CacheBuilder.newBuilder().maximumSize(100000).expireAfterWrite(100, TimeUnit.MINUTES)
-				.build(new CacheLoader<MethodKey, Optional<Method>>() {
-
+				new FunctionBasedGuavaCacheLoader<CropMethodKey, Method>(CacheBuilder.newBuilder().maximumSize(100000)
+						.expireAfterWrite(100, TimeUnit.MINUTES).<CropMethodKey, Method>build(), new Function<CropMethodKey, Method>() {
 					@Override
-					public Optional<Method> load(final MethodKey key) {
-						// Use our method map
-						return Optional.fromNullable(PedigreeTreeTest.this.methodsMap.get(key));
+					public Method apply(CropMethodKey key) {
+						return PedigreeTreeTest.this.methodsMap.get(key);
 					}
 				});
 	}
 
 	private void setUpGermplasmCache() {
 		this.germplasmCache =
-				CacheBuilder.newBuilder().maximumSize(100000).expireAfterWrite(100, TimeUnit.MINUTES)
-				.build(new CacheLoader<GermplasmKey, Optional<Germplasm>>() {
-
+				new FunctionBasedGuavaCacheLoader<CropGermplasmKey, Germplasm>(CacheBuilder.newBuilder().maximumSize(100000)
+						.expireAfterWrite(100, TimeUnit.MINUTES).<CropGermplasmKey, Germplasm>build(), new Function<CropGermplasmKey, Germplasm>() {
 					@Override
-					public Optional<Germplasm>  load(final GermplasmKey key) {
-						// Use our generated map
-						return Optional.fromNullable(PedigreeTreeTest.this.germplasmMap.get(key));
+					public Germplasm apply(CropGermplasmKey key) {
+						return PedigreeTreeTest.this.germplasmMap.get(key);
 					}
 				});
 	}
@@ -102,10 +99,10 @@ public class PedigreeTreeTest {
 			return;
 		}
 		Assert.assertEquals(generateRandomGermplasm.getGid(), resultNode.getGermplasm().getGid());
-		final GermplasmKey femaleGermplasmKey = new GermplasmKey(PedigreeTreeTest.MAIZE, generateRandomGermplasm.getGpid1());
+		final CropGermplasmKey femaleGermplasmKey = new CropGermplasmKey(PedigreeTreeTest.MAIZE, generateRandomGermplasm.getGpid1());
 		this.compareGeneratedNodes(this.germplasmMap.get(femaleGermplasmKey), resultNode.getFemaleParent());
 
-		final GermplasmKey maleGermplasmKey = new GermplasmKey(PedigreeTreeTest.MAIZE, generateRandomGermplasm.getGpid2());
+		final CropGermplasmKey maleGermplasmKey = new CropGermplasmKey(PedigreeTreeTest.MAIZE, generateRandomGermplasm.getGpid2());
 
 		this.compareGeneratedNodes(this.germplasmMap.get(maleGermplasmKey), resultNode.getMaleParent());
 
@@ -127,14 +124,14 @@ public class PedigreeTreeTest {
 		if (femaleSideNodes != 0) {
 			final Integer femaleGid = iterator.next();
 			final Germplasm generateTestGermplasm = this.generateTestGermplasm(femaleGid, 1);
-			generateTree(generateTestGermplasm, femaleSideNodes-1, maleSideNodes, iterator);
+			generateTree(generateTestGermplasm, femaleSideNodes - 1, maleSideNodes, iterator);
 			germplasm.setGpid1(femaleGid);
 		}
 
 		if (maleSideNodes != 0) {
 			final Integer maleGid = iterator.next();
 			final Germplasm generateTestGermplasm = this.generateTestGermplasm(maleGid, 1);
-			generateTree(generateTestGermplasm, femaleSideNodes, maleSideNodes-1, iterator);
+			generateTree(generateTestGermplasm, femaleSideNodes, maleSideNodes - 1, iterator);
 
 			germplasm.setGpid2(maleGid);
 		}
@@ -147,8 +144,8 @@ public class PedigreeTreeTest {
 		final Method method = new Method(methodId);
 		germplasm.setMethod(method);
 		germplasm.setMethodId(methodId);
-		this.germplasmMap.put(new GermplasmKey(PedigreeTreeTest.MAIZE, gid), germplasm);
-		this.methodsMap.put(new MethodKey(PedigreeTreeTest.MAIZE, methodId), method);
+		this.germplasmMap.put(new CropGermplasmKey(PedigreeTreeTest.MAIZE, gid), germplasm);
+		this.methodsMap.put(new CropMethodKey(PedigreeTreeTest.MAIZE, methodId), method);
 		return germplasm;
 	}
 
