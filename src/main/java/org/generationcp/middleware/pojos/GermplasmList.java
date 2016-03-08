ddaf7757bf252f2fbddf25bb717f7497f2f56c36
Daniel Villafuerte <dannyvonline@gmail.com>
2016-03-08 12:03:31 +0800

    Restored individual imports that were inadvertently collapsed by automated operation
    BMS-2322

--- src/main/java/org/generationcp/middleware/pojos/GermplasmList.java
+++ src/main/java/org/generationcp/middleware/pojos/GermplasmList.java
@@ -15,7 +15,19 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
