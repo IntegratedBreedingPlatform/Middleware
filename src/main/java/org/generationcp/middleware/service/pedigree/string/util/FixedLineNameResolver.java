1b2857e5c72a4a2af4c97738e4dc351edadb664d
Akhil Mehra <akhil@leafnode.io>
2016-03-10 09:44:36 +1300

    Removing unused imports
    issue: BMS-2174
    reviewer: MatthewB

--- src/main/java/org/generationcp/middleware/service/pedigree/string/util/FixedLineNameResolver.java
+++ src/main/java/org/generationcp/middleware/service/pedigree/string/util/FixedLineNameResolver.java
@@ -1,9 +1,7 @@
 
 package org.generationcp.middleware.service.pedigree.string.util;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.generationcp.middleware.pojos.Germplasm;
 import org.generationcp.middleware.pojos.Name;
