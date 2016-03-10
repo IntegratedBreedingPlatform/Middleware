a66cbbf303f8667d7148c9bfe76aeedf9ce4c151
Alejandro Mildiner <amildiner@droptek.com>
2016-03-10 14:43:39 -0300

    Ticket GCP-10163 Add Auditor Component. Implemet Auditable in Germplasm. Add fest-assertion dependency to the pom file
    Reviewer: Nicolas Lopez

---
+++ src/main/java/org/generationcp/middleware/auditory/AuditorManager.java
@@ -0,0 +1,10 @@
+package org.generationcp.middleware.auditory;
+
+import org.generationcp.middleware.manager.DataManager;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Transactional
+public class AuditorManager extends DataManager {
+
+}
---
+++ src/main/java/org/generationcp/middleware/auditory/Auditory.java
@@ -0,0 +1,14 @@
+package org.generationcp.middleware.auditory;
+
+
+public interface Auditory {
+
+	public int getId();
+
+	public String getFilename();
+
+	public String getUsername();
+
+	public int getDate();
+
+}
