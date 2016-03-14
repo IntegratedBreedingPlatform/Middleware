e8c262fcdbc0cd5695b336bf05c32628ecf4f939
Daniel Villafuerte <dannyvonline@gmail.com>
2016-03-14 17:22:47 +0800

    Cleanup : Removed unused import BMS-1747

--- src/test/java/org/generationcp/middleware/data/initializer/GermplasmDataManagerDataInitializer.java
+++ src/test/java/org/generationcp/middleware/data/initializer/GermplasmDataManagerDataInitializer.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.generationcp.middleware.IntegrationTestBase;
 import org.generationcp.middleware.manager.GermplasmNameType;
 import org.generationcp.middleware.pojos.Germplasm;
 import org.generationcp.middleware.pojos.GermplasmPedigreeTreeNode;
