dd5a60f0da1361a93745566b6cf4529d55437994
Alejandro Mildiner <amildiner@droptek.com>
2016-03-17 15:43:59 -0300

    add integration tests

--- src/main/java/org/generationcp/middleware/manager/AuditorDataManagerImpl.java
+++ src/main/java/org/generationcp/middleware/manager/AuditorDataManagerImpl.java
@@ -1,5 +1,6 @@
 package org.generationcp.middleware.manager;
 
+import org.generationcp.middleware.auditory.Auditory;
 import org.generationcp.middleware.auditory.AuditoryException;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 import org.generationcp.middleware.manager.api.AuditorDataManager;
@@ -11,6 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuditorDataManagerImpl extends DataManager implements AuditorDataManager {
 
 	public static final String INVALID_BIBLIOGRAFIC_REFERENCE = "Invalid bibliografic reference";
+	public static final String INVALID_BIBLIOGRAFIC_REFERENCE_TYPE = "Invalid bibliografic reference type";
+
 
 	public AuditorDataManagerImpl(HibernateSessionProvider sessionProvider) {
 		super(sessionProvider);
@@ -29,7 +32,22 @@ public class AuditorDataManagerImpl extends DataManager implements AuditorDataMa
 	}
 
 	@Override
-	public UserDefinedField getBibrefType() {
-		return this.getUserDefinedFieldDao().getByTableTypeAndCode("BIBREFS","PUBTYPE","BOOK");
+	public UserDefinedField getBibrefType() throws AuditoryException{
+		UserDefinedField bibrefType = getUserDefinedFieldDao().getByTableTypeAndCode("BIBREFS", "PUBTYPE", "BOOK");
+
+		if(bibrefType == null){
+			throw new AuditoryException(INVALID_BIBLIOGRAFIC_REFERENCE_TYPE);
+		}
+
+		return bibrefType;
+	}
+
+	@Override
+	public Auditory getAuditory(int id) throws AuditoryException{
+		Bibref auditory = this.getBibrefDao().getById(id);
+		if(auditory == null){
+			throw new AuditoryException(INVALID_BIBLIOGRAFIC_REFERENCE);
+		}
+		return auditory;
 	}
 }
--- src/main/java/org/generationcp/middleware/manager/api/AuditorDataManager.java
+++ src/main/java/org/generationcp/middleware/manager/api/AuditorDataManager.java
@@ -1,5 +1,6 @@
 package org.generationcp.middleware.manager.api;
 
+import org.generationcp.middleware.auditory.Auditory;
 import org.generationcp.middleware.auditory.AuditoryException;
 import org.generationcp.middleware.pojos.Bibref;
 import org.generationcp.middleware.pojos.UserDefinedField;
@@ -8,5 +9,7 @@ public interface AuditorDataManager {
 
 	Bibref save(Bibref ref) throws AuditoryException;
 
-	UserDefinedField getBibrefType();
+	UserDefinedField getBibrefType() throws AuditoryException;
+
+	Auditory getAuditory(int id) throws AuditoryException;
 }
