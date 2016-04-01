97ca53a7c4fc13c684303319f7a465734c631e00
Alejandro Mildiner <amildiner@droptek.com>
2016-04-01 15:39:00 -0300

    place String literals as contants

--- src/main/java/org/generationcp/middleware/manager/UserDefinedFieldsDataManagerImpl.java
+++ src/main/java/org/generationcp/middleware/manager/UserDefinedFieldsDataManagerImpl.java
@@ -10,7 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UserDefinedFieldsDataManagerImpl extends DataManager implements UserDefinedFieldsDataManager {
 
-
+	public static final String TABLE = "NAMES";
+	public static final String FTYPE = "NAME";
 
 	public UserDefinedFieldsDataManagerImpl(HibernateSessionProvider sessionProvider) {
 		super(sessionProvider);
@@ -22,7 +23,7 @@ public class UserDefinedFieldsDataManagerImpl extends DataManager implements Use
 
 	@Override
 	public List<UserDefinedField> getNotCodeNamesFactor(List<Integer> codedIds) {
-		List<UserDefinedField> namesFactors = getUserDefinedFieldDao().getByTableAndTypeWithoutList("NAMES", "NAME",codedIds);
+		List<UserDefinedField> namesFactors = getUserDefinedFieldDao().getByTableAndTypeWithoutList(TABLE, FTYPE,codedIds);
 
 		return namesFactors;
 	}
