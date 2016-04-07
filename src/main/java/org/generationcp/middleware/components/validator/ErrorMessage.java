f676eda1f981771177ae73077b3178c338ce6b21
Alejandro Mildiner <amildiner@droptek.com>
2016-04-07 14:38:54 -0300

    add new validation rule to validator. add new error message. add coded names filter by fcodes values

--- src/main/java/org/generationcp/middleware/components/validator/ErrorMessage.java
+++ src/main/java/org/generationcp/middleware/components/validator/ErrorMessage.java
@@ -8,22 +8,20 @@ import com.google.common.collect.Lists;
  * This class contains all the data related to an Internationalized error message.
  * As this type of errors are constant that can be parametrized, this class contains
  * the message key and the number of parameters needed to complete the placeholders in the message.
- *
+ * <p/>
  * NOTE: At this moment the errorHandler is only considering a predifined set of parameter to
  * parametrize the error message. With hope that in the future this may change this class will not be limited by this amount of parameter.
  */
 public class ErrorMessage {
 
-
-
 	private String key;
 
 	private List<String> parameters = Lists.newArrayList();
 
-
 	public ErrorMessage(String key) {
 		this.key = key;
 	}
+
 	public String getKey() {
 		return key;
 	}
@@ -36,7 +34,7 @@ public class ErrorMessage {
 		return parameters;
 	}
 
-	public void addParameters(String ... args) {
+	public void addParameters(String... args) {
 		for (String parameter : args) {
 			this.parameters.add(parameter);
 		}
