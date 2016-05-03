63291ff667f4fe6df89a2c4c6debd0227d747be9
Daniel Villafuerte <dannyvonline@gmail.com>
2016-05-03 08:46:01 +0800

    Changed field from type int to type Integer
    BMS-2684

--- src/main/java/org/generationcp/middleware/pojos/Methods.java
+++ src/main/java/org/generationcp/middleware/pojos/Methods.java
@@ -3,13 +3,13 @@ package org.generationcp.middleware.pojos;
 public enum Methods {
     SINGLE_CROSS(101), BACKCROSS(107), THREE_WAY_CROSS(102), COMPLEX_CROSS(106), DOUBLE_CROSS(103);
 
-    private final int methodID;
+    private final Integer methodID;
 
-    Methods(int methodID) {
+    Methods(Integer methodID) {
         this.methodID = methodID;
     }
 
-    public int getMethodID() {
+    public Integer getMethodID() {
         return methodID;
     }
 }
