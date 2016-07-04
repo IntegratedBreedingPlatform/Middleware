33e6e653c80e702ea6333a883ca3b58a64079cce
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2016-07-05 07:31:43 +0800

    Fixed dataset related test methods and added proper assertions. The test methods are testGetDatasetNodesByStudyId, testGetDataSet, testGetFactorsByProperty, testGetFactorsByPhenotypicType, testGetDataSetsByType.
    BMS-2866

--- src/test/java/org/generationcp/middleware/manager/StudyDataManagerImplTest.java
+++ src/test/java/org/generationcp/middleware/manager/StudyDataManagerImplTest.java
@@ -18,7 +18,11 @@ import org.generationcp.middleware.IntegrationTestBase;
 import org.generationcp.middleware.WorkbenchTestDataUtil;
 import org.generationcp.middleware.data.initializer.StudyTestDataInitializer;
 import org.generationcp.middleware.domain.dms.DMSVariableType;
+import org.generationcp.middleware.domain.dms.DataSet;
+import org.generationcp.middleware.domain.dms.DataSetType;
+import org.generationcp.middleware.domain.dms.DatasetReference;
 import org.generationcp.middleware.domain.dms.FolderReference;
+import org.generationcp.middleware.domain.dms.PhenotypicType;
 import org.generationcp.middleware.domain.dms.Reference;
 import org.generationcp.middleware.domain.dms.Study;
 import org.generationcp.middleware.domain.dms.StudyReference;
@@ -201,5 +205,55 @@ public class StudyDataManagerImplTest extends IntegrationTestBase {
 		// The test database might already have some pre-init and developer created folders too which we dont want the test to depend on
 		// because we do not reset the test database for each test run yet.
 		Assert.assertTrue("The number of all folders should be greater than or equal to 2", allFolders.size() >= 2);
+	}
+
+	@Test
+	public void testGetDatasetNodesByStudyId() throws Exception {
+		this.studyTDI.addTestDataset(this.studyReference.getId());
+		List<DatasetReference> datasetReferences = this.manager.getDatasetReferences(this.studyReference.getId());
+		Assert.assertNotNull(datasetReferences);
+		Assert.assertTrue(datasetReferences.size() > 0);
+		Assert.assertEquals("The dataset name should be " + StudyTestDataInitializer.DATASET_NAME, StudyTestDataInitializer.DATASET_NAME, datasetReferences.get(0).getName());
+	}
+
+	@Test
+	public void testGetDataSet() throws Exception {
+		DatasetReference datasetReference = this.studyTDI.addTestDataset(this.studyReference.getId());
+		DataSet dataset = this.manager.getDataSet(datasetReference.getId());
+		Assert.assertEquals("The dataset name should be " + StudyTestDataInitializer.DATASET_NAME, StudyTestDataInitializer.DATASET_NAME, dataset.getName());
+	}
+
+	@Test
+	public void testGetFactorsByProperty() throws Exception {
+		DatasetReference datasetReference = this.studyTDI.addTestDataset(this.studyReference.getId());
+		DataSet dataset = this.manager.getDataSet(datasetReference.getId());
+		int propertyId = 15009;
+		VariableTypeList factors = dataset.getFactorsByProperty(propertyId);
+		Assert.assertTrue("The size should be 1 since we added 1 factor, with property id = " +propertyId+ ", in the set up of the data set", factors.size()==1);
+	}
+
+	@Test
+	public void testGetFactorsByPhenotypicType() throws Exception {
+		DatasetReference datasetReference = this.studyTDI.addTestDataset(this.studyReference.getId());
+		DataSet dataset = this.manager.getDataSet(datasetReference.getId());
+		VariableTypeList factors = dataset.getFactorsByPhenotypicType(PhenotypicType.TRIAL_ENVIRONMENT);
+		Assert.assertTrue("The size should be 3 since we added 3 factors in the set up of the data set", factors.size()==3);
+	}
+
+	@Test
+	public void testGetDataSetsByType() throws Exception {
+		this.studyTDI.addTestDataset(this.studyReference.getId());
+		DataSetType dataSetType = DataSetType.MEANS_DATA;
+		List<DataSet> datasets = this.manager.getDataSetsByType(this.studyReference.getId(), dataSetType);
+		Assert.assertTrue("Datasets' size should be greter than 0", datasets.size() > 0);
+		Assert.assertTrue("The size should be greater than 0 since we are sure that it will return at least one data set", datasets.size()>0);
+	}
+
+	@Test
+	public void testFindOneDataSetByType() throws Exception {
+		this.studyTDI.addTestDataset(this.studyReference.getId());
+		DataSetType dataSetType = DataSetType.MEANS_DATA;
+		DataSet dataset = this.manager.findOneDataSetByType(this.studyReference.getId(), dataSetType);
+		Assert.assertEquals("Dataset's name should be " + StudyTestDataInitializer.DATASET_NAME, StudyTestDataInitializer.DATASET_NAME, dataset.getName());
 	}
 }
