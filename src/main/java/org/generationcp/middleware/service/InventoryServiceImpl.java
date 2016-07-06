a2b87695adea67f74edd1e324001935a38cf3175
chavdahard <hard@leafnode.io>
2016-07-06 09:17:40 +0530

    Organized single import.
    Issue : BMS-2832
    Reviewer : Abhishek

--- src/main/java/org/generationcp/middleware/service/InventoryServiceImpl.java
+++ src/main/java/org/generationcp/middleware/service/InventoryServiceImpl.java
@@ -12,7 +12,15 @@
 
 package org.generationcp.middleware.service;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
