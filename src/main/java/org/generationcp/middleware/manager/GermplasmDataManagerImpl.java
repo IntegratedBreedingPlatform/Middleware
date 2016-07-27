6a6a4aba7765d445c46e404d531db5377d7ca6a3
Akhil Mehra <akhil@leafnode.io>
2016-07-28 10:49:05 +1200

    Method to retrieve Germplasm and its ancestry
    Efficiently retrieve Germplasm and its ancestry in a performant manner.
    
    issue: BMS-2103
    reviewer: none

--- src/main/java/org/generationcp/middleware/manager/GermplasmDataManagerImpl.java
+++ src/main/java/org/generationcp/middleware/manager/GermplasmDataManagerImpl.java
@@ -13,8 +13,10 @@ package org.generationcp.middleware.manager;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
@@ -46,7 +48,14 @@ import org.generationcp.middleware.pojos.ProgenitorPK;
 import org.generationcp.middleware.pojos.UserDefinedField;
 import org.generationcp.middleware.pojos.dms.ProgramFavorite;
 import org.generationcp.middleware.pojos.dms.ProgramFavorite.FavoriteType;
+import org.hibernate.Criteria;
+import org.hibernate.SQLQuery;
+import org.hibernate.transform.DistinctResultTransformer;
+import org.hibernate.transform.Transformers;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Iterables;
 
 /**
  * Implementation of the GermplasmDataManager interface. To instantiate this class, a Hibernate Session must be passed to its constructor.
@@ -56,6 +65,8 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Transactional
 public class GermplasmDataManagerImpl extends DataManager implements GermplasmDataManager {
+
+	private static final String GID_SEPERATOR_FOR_STORED_PROCEDURE_CALL = ",";
 
 	public GermplasmDataManagerImpl() {
 	}
@@ -1383,4 +1394,47 @@ public class GermplasmDataManagerImpl extends DataManager implements GermplasmDa
 	public List<Method> getDerivativeAndMaintenanceMethods(final List<Integer> ids) {
 		return this.getMethodDao().getDerivativeAndMaintenanceMethods(ids);
 	}
+	
+
+	/** (non-Javadoc)
+	 * @see org.generationcp.middleware.manager.api.GermplasmDataManager#getGermplasmWithAllNamesAndAncestry(java.util.Set, int)
+	 */
+	@Override
+	public List<Germplasm> getGermplasmWithAllNamesAndAncestry(Set<Integer> gids, int numberOfLevelsToTraverse) {
+		final StringBuilder commaSeperatedListOfGids = getGidsAsCommaSeperatedList(gids);
+
+		final SQLQuery storedProcedure =
+				this.getActiveSession().createSQLQuery("CALL getGermplasmWithNamesAndAncestry(:gids, :numberOfLevelsToTraverse) ");
+		storedProcedure.setParameter("gids", commaSeperatedListOfGids.toString());
+		storedProcedure.setParameter("numberOfLevelsToTraverse", numberOfLevelsToTraverse);
+
+		storedProcedure.addEntity("g", Germplasm.class);
+		storedProcedure.addJoin("n", "g.names");
+		// Be very careful changing anything here.
+		// The entity has been added again because the distinct root entity works on the 
+		// Last added entity
+		storedProcedure.addEntity("g", Germplasm.class);
+		storedProcedure.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+		return storedProcedure.list();
+
+	}
+
+	private StringBuilder getGidsAsCommaSeperatedList(final Set<Integer> gids) {
+		final StringBuilder commaSeperatedListOfGids = new StringBuilder();
+	
+		for (final Integer input : gids) {
+			if(input != null) {
+				if(commaSeperatedListOfGids.length() == 0) {
+					commaSeperatedListOfGids.append(input.toString());
+				} else {
+					commaSeperatedListOfGids.append(GID_SEPERATOR_FOR_STORED_PROCEDURE_CALL);
+
+					commaSeperatedListOfGids.append(input.toString());
+				}
+			}
+		}
+		return commaSeperatedListOfGids;
+	}
+
+
 }
