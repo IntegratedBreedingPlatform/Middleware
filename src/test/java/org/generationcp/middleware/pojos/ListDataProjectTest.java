424ac59207adbc3481100e3927d00edc311f3a03
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2016-08-09 01:48:17 +0800

    Added before class for the initialization of the ListDataProject Object
    BMS-3141

--- src/test/java/org/generationcp/middleware/pojos/ListDataProjectTest.java
+++ src/test/java/org/generationcp/middleware/pojos/ListDataProjectTest.java
@@ -4,6 +4,7 @@ package org.generationcp.middleware.pojos;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ListDataProjectTest {
@@ -12,41 +13,65 @@ public class ListDataProjectTest {
 	public static final String TEST_PEDIGREE_RECIP_STRING = ListDataProject.PEDIGREE_RECIP + ": 8";
 	public static final String TEST_PLOT_DUPE_STRING = ListDataProject.PLOT_DUPE + ": 2,4";
 	public static final String TEST_PLOT_RECIP_STRING = ListDataProject.PLOT_RECIP + ": 6,11";
-
+	
+	private static ListDataProject ldp;
+	
+	@BeforeClass
+	public static void beforeClass(){
+		ldp = new ListDataProject();
+	}
+	
 	@Test
 	public void testParsePedigreeDupe() {
-		ListDataProject ldp = new ListDataProject();
 		ldp.setDuplicate(ListDataProjectTest.TEST_PEDIGREE_DUPE_STRING);
 
 		List<Integer> parsed = ldp.parsePedigreeDupeInformation();
-		Assert.assertEquals(2, parsed.size());
+		Assert.assertEquals("The size should be 2", 2, parsed.size());
 	}
 
 	@Test
 	public void testParsePedigreeRecip() {
-		ListDataProject ldp = new ListDataProject();
 		ldp.setDuplicate(ListDataProjectTest.TEST_PEDIGREE_RECIP_STRING);
 
 		List<Integer> parsed = ldp.parsePedigreeReciprocalInformation();
-		Assert.assertEquals(1, parsed.size());
+		Assert.assertEquals("The size should be 1", 1, parsed.size());
 	}
 
 	@Test
 	public void testParsePlotRecip() {
-		ListDataProject ldp = new ListDataProject();
 		ldp.setDuplicate(ListDataProjectTest.TEST_PLOT_RECIP_STRING);
 
 		List<Integer> parsed = ldp.parsePlotReciprocalInformation();
-		Assert.assertEquals(2, parsed.size());
+		Assert.assertEquals("The size should be 2", 2, parsed.size());
 	}
 
 	@Test
 	public void testParsePlotDuplicate() {
-		ListDataProject ldp = new ListDataProject();
 		ldp.setDuplicate(ListDataProjectTest.TEST_PLOT_DUPE_STRING);
 
 		List<Integer> parsed = ldp.parsePlotDupeInformation();
-		Assert.assertEquals(2, parsed.size());
+		Assert.assertEquals("The size should be 2", 2, parsed.size());
 	}
-
+	
+	@Test
+	public void testParseDuplicateString() {
+		String duplicate = ListDataProjectTest.TEST_PEDIGREE_DUPE_STRING + " | " + ListDataProjectTest.TEST_PEDIGREE_RECIP_STRING  + " | " + ListDataProjectTest.TEST_PLOT_DUPE_STRING  + " | " + ListDataProjectTest.TEST_PLOT_RECIP_STRING;
+		ldp.setDuplicate(duplicate);
+		
+		//Test for pedigree dupes
+		List<Integer> parsed = ldp.parseDuplicateString(ListDataProject.PEDIGREE_DUPE);
+		Assert.assertEquals("The size should be 2", 2, parsed.size());
+		
+		//Test for pedigree recips
+		parsed = ldp.parseDuplicateString(ListDataProject.PEDIGREE_RECIP);
+		Assert.assertEquals("The size should be 1", 1, parsed.size());
+		
+		//Test for plot dupes
+		parsed = ldp.parseDuplicateString(ListDataProject.PLOT_DUPE);
+		Assert.assertEquals("The size should be 2", 2, parsed.size());
+		
+		//Test for plot recips
+		parsed = ldp.parseDuplicateString(ListDataProject.PLOT_RECIP);
+		Assert.assertEquals("The size should be 2", 2, parsed.size());
+	}
 }
