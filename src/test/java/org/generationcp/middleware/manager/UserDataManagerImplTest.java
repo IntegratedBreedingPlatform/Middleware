4254d0aaceb63234e9f826c74ccb1e09b1dd9ec1
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2016-08-11 13:56:32 +0800

    Added assertions for better testing
    BMS-3136

--- src/test/java/org/generationcp/middleware/manager/UserDataManagerImplTest.java
+++ src/test/java/org/generationcp/middleware/manager/UserDataManagerImplTest.java
@@ -63,6 +63,12 @@ public class UserDataManagerImplTest extends IntegrationTestBase {
 		Assert.assertEquals("The user ids should be equal.", user.getUserid(), resultUser.getUserid());
 		Assert.assertEquals("The usernames should be equal.", user.getName(), resultUser.getName());
 		Assert.assertEquals("The Passwords should be equal.", user.getPassword(), resultUser.getPassword());
+		Assert.assertEquals("The install ids should be equal.", user.getInstalid(), resultUser.getInstalid());
+		Assert.assertEquals("The status values should be equal.", user.getStatus(), resultUser.getStatus());
+		Assert.assertEquals("The access values should be equal.", user.getAccess(), resultUser.getAccess());
+		Assert.assertEquals("The type values should be equal.", user.getType(), resultUser.getType());
+		Assert.assertEquals("The assign dates should be equal.", user.getAssignDate(), resultUser.getAssignDate());
+		Assert.assertEquals("The close dates should be equal.", user.getCloseDate(), resultUser.getCloseDate());
 	}
 
 	@Test
@@ -96,8 +102,19 @@ public class UserDataManagerImplTest extends IntegrationTestBase {
 		person = this.userDataManager.getPersonById(person.getId());
 		final Person resultPerson = this.userDataManager.getPersonById(person.getId());
 		Assert.assertEquals("The first names should be equal.", person.getFirstName(), resultPerson.getFirstName());
+		Assert.assertEquals("The middle names should be equal.", person.getMiddleName(), resultPerson.getMiddleName());
 		Assert.assertEquals("The last names should be equal.", person.getLastName(), resultPerson.getLastName());
 		Assert.assertEquals("The ids should be equal.", person.getId(), resultPerson.getId());
+		Assert.assertEquals("The institute ids should be equal.", person.getInstituteId(), resultPerson.getInstituteId());
+		Assert.assertEquals("The position names should be equal.", person.getPositionName(), resultPerson.getPositionName());
+		Assert.assertEquals("The titles should be equal.", person.getTitle(), resultPerson.getTitle());
+		Assert.assertEquals("The extension values should be equal.", person.getExtension(), resultPerson.getExtension());
+		Assert.assertEquals("The fax values should be equal.", person.getFax(), resultPerson.getFax());
+		Assert.assertEquals("The emails should be equal.", person.getEmail(), resultPerson.getEmail());
+		Assert.assertEquals("The notes should be equal.", person.getNotes(), resultPerson.getNotes());
+		Assert.assertEquals("The contact should be equal.", person.getContact(), resultPerson.getContact());
+		Assert.assertEquals("The language values should be equal.", person.getLanguage(), resultPerson.getLanguage());
+		Assert.assertEquals("The phone values should be equal.", person.getPhone(), resultPerson.getPhone());
 	}
 
 	@Test
@@ -131,8 +148,19 @@ public class UserDataManagerImplTest extends IntegrationTestBase {
 
 		final Person resultPerson = this.userDataManager.getPersonById(person.getId());
 		Assert.assertEquals("The first names should be equal.", person.getFirstName(), resultPerson.getFirstName());
+		Assert.assertEquals("The middle names should be equal.", person.getMiddleName(), resultPerson.getMiddleName());
 		Assert.assertEquals("The last names should be equal.", person.getLastName(), resultPerson.getLastName());
 		Assert.assertEquals("The ids should be equal.", person.getId(), resultPerson.getId());
+		Assert.assertEquals("The institute ids should be equal.", person.getInstituteId(), resultPerson.getInstituteId());
+		Assert.assertEquals("The position names should be equal.", person.getPositionName(), resultPerson.getPositionName());
+		Assert.assertEquals("The titles should be equal.", person.getTitle(), resultPerson.getTitle());
+		Assert.assertEquals("The extension values should be equal.", person.getExtension(), resultPerson.getExtension());
+		Assert.assertEquals("The fax values should be equal.", person.getFax(), resultPerson.getFax());
+		Assert.assertEquals("The emails should be equal.", person.getEmail(), resultPerson.getEmail());
+		Assert.assertEquals("The notes should be equal.", person.getNotes(), resultPerson.getNotes());
+		Assert.assertEquals("The contact should be equal.", person.getContact(), resultPerson.getContact());
+		Assert.assertEquals("The language values should be equal.", person.getLanguage(), resultPerson.getLanguage());
+		Assert.assertEquals("The phone values should be equal.", person.getPhone(), resultPerson.getPhone());
 	}
 
 	@Test
@@ -145,6 +173,12 @@ public class UserDataManagerImplTest extends IntegrationTestBase {
 		Assert.assertEquals("The user ids should be equal.", user.getUserid(), resultUser.getUserid());
 		Assert.assertEquals("The usernames should be equal.", user.getName(), resultUser.getName());
 		Assert.assertEquals("The Passwords should be equal.", user.getPassword(), resultUser.getPassword());
+		Assert.assertEquals("The install ids should be equal.", user.getInstalid(), resultUser.getInstalid());
+		Assert.assertEquals("The status values should be equal.", user.getStatus(), resultUser.getStatus());
+		Assert.assertEquals("The access values should be equal.", user.getAccess(), resultUser.getAccess());
+		Assert.assertEquals("The type values should be equal.", user.getType(), resultUser.getType());
+		Assert.assertEquals("The assign dates should be equal.", user.getAssignDate(), resultUser.getAssignDate());
+		Assert.assertEquals("The close dates should be equal.", user.getCloseDate(), resultUser.getCloseDate());
 	}
 
 	@Test
@@ -157,6 +191,12 @@ public class UserDataManagerImplTest extends IntegrationTestBase {
 		Assert.assertEquals("The user ids should be equal.", user.getUserid(), resultUser.getUserid());
 		Assert.assertEquals("The usernames should be equal.", user.getName(), resultUser.getName());
 		Assert.assertEquals("The Passwords should be equal.", user.getPassword(), resultUser.getPassword());
+		Assert.assertEquals("The install ids should be equal.", user.getInstalid(), resultUser.getInstalid());
+		Assert.assertEquals("The status values should be equal.", user.getStatus(), resultUser.getStatus());
+		Assert.assertEquals("The access values should be equal.", user.getAccess(), resultUser.getAccess());
+		Assert.assertEquals("The type values should be equal.", user.getType(), resultUser.getType());
+		Assert.assertEquals("The assign dates should be equal.", user.getAssignDate(), resultUser.getAssignDate());
+		Assert.assertEquals("The close dates should be equal.", user.getCloseDate(), resultUser.getCloseDate());
 	}
 	
 	@Test
@@ -175,5 +215,11 @@ public class UserDataManagerImplTest extends IntegrationTestBase {
 		Assert.assertEquals("The usernames should be equal.", user.getName(), resultUser.getName());
 		Assert.assertEquals("The Passwords should be equal.", user.getPassword(), resultUser.getPassword());
 		Assert.assertEquals("The personIds should be equal.", person.getId(), resultUser.getPersonid());
+		Assert.assertEquals("The install ids should be equal.", user.getInstalid(), resultUser.getInstalid());
+		Assert.assertEquals("The status values should be equal.", user.getStatus(), resultUser.getStatus());
+		Assert.assertEquals("The access values should be equal.", user.getAccess(), resultUser.getAccess());
+		Assert.assertEquals("The type values should be equal.", user.getType(), resultUser.getType());
+		Assert.assertEquals("The assign dates should be equal.", user.getAssignDate(), resultUser.getAssignDate());
+		Assert.assertEquals("The close dates should be equal.", user.getCloseDate(), resultUser.getCloseDate());
 	}
 }
