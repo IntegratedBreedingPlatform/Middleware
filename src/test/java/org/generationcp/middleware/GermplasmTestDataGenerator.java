713997aa424fcc021768c603db0cfc8eaa904014
darla-leafnode <darla@leafnode.io>
2016-08-23 16:53:37 +0800

    Apply standard formatting and cleanup
    Issue: BMS-3214 / BMS-3150

--- src/test/java/org/generationcp/middleware/GermplasmTestDataGenerator.java
+++ src/test/java/org/generationcp/middleware/GermplasmTestDataGenerator.java
@@ -10,13 +10,13 @@ import org.generationcp.middleware.pojos.Name;
 public class GermplasmTestDataGenerator {
 	public static final Integer TEST_METHOD_ID = 101;
 	public static final String TEST_METHOD_NAME = "Single cross";
-	
+
 	GermplasmDataManager germplasmDataManager;
 
-	public GermplasmTestDataGenerator(GermplasmDataManager manager) {
+	public GermplasmTestDataGenerator(final GermplasmDataManager manager) {
 		this.germplasmDataManager = manager;
 	}
-	
+
 	public Germplasm createGermplasmWithPreferredAndNonpreferredNames() {
 		final Germplasm germplasm = new GermplasmTestDataInitializer().createGermplasmWithPreferredName();
 		final Name preferredName = germplasm.getPreferredName();
@@ -26,42 +26,44 @@ public class GermplasmTestDataGenerator {
 		final Name otherName = GermplasmTestDataInitializer.createGermplasmName(germplasm.getGid(), "Other Name ");
 		otherName.setNstat(0);
 		this.germplasmDataManager.addGermplasmName(otherName);
-		
+
 		return germplasm;
 	}
-	
-	public Germplasm createChildGermplasm(final Germplasm parentGermplasm, String name) {
+
+	public Germplasm createChildGermplasm(final Germplasm parentGermplasm, final String name) {
 		final Germplasm germplasm = new GermplasmTestDataInitializer().createGermplasmWithPreferredName(name);
 		final Name preferredName = germplasm.getPreferredName();
 		preferredName.setGermplasmId(germplasm.getGid());
-		
+
 		germplasm.setGpid1(parentGermplasm.getGid());
 		germplasm.setGpid2(parentGermplasm.getGid());
-		germplasm.setMethodId(TEST_METHOD_ID);
-		
+		germplasm.setMethodId(GermplasmTestDataGenerator.TEST_METHOD_ID);
+
 		this.germplasmDataManager.addGermplasm(germplasm, preferredName);
 
 		return germplasm;
 	}
-	
-	public Integer[] createChildrenGermplasm(int numberOfChildGermplasm, String prefix, Germplasm parentGermplasm) throws MiddlewareQueryException {
-		Integer[] gids = new Integer[numberOfChildGermplasm];
+
+	public Integer[] createChildrenGermplasm(final int numberOfChildGermplasm, final String prefix,
+			final Germplasm parentGermplasm) throws MiddlewareQueryException {
+		final Integer[] gids = new Integer[numberOfChildGermplasm];
 		for (int i = 0; i < numberOfChildGermplasm; i++) {
-			String name = prefix+i;
-			final Germplasm germplasm = createChildGermplasm(parentGermplasm, name);
+			final String name = prefix + i;
+			final Germplasm germplasm = this.createChildGermplasm(parentGermplasm, name);
 			gids[i] = germplasm.getGid();
 		}
 		return gids;
 	}
 
-	public Integer[] createGermplasmRecords(int numberOfGermplasm, String prefix) throws MiddlewareQueryException {
-		Integer[] gids = new Integer[numberOfGermplasm];
+	public Integer[] createGermplasmRecords(final int numberOfGermplasm, final String prefix)
+			throws MiddlewareQueryException {
+		final Integer[] gids = new Integer[numberOfGermplasm];
 		for (int i = 0; i < numberOfGermplasm; i++) {
 			final Germplasm germplasm = new GermplasmTestDataInitializer().createGermplasmWithPreferredName(prefix + i);
 			final Name preferredName = germplasm.getPreferredName();
 			preferredName.setGermplasmId(germplasm.getGid());
 			this.germplasmDataManager.addGermplasm(germplasm, preferredName);
-			
+
 			gids[i] = germplasm.getGid();
 		}
 		return gids;
