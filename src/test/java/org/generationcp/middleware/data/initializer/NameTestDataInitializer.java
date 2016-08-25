788129464bb16e661e26b3b68605f0ea1050a83d
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2016-08-25 19:07:07 +0800

    Added test data initializer method
    BMS-3149

--- src/test/java/org/generationcp/middleware/data/initializer/NameTestDataInitializer.java
+++ src/test/java/org/generationcp/middleware/data/initializer/NameTestDataInitializer.java
@@ -8,6 +8,19 @@ import org.generationcp.middleware.pojos.Name;
 
 public class NameTestDataInitializer {
 
+	public Name createName(final Integer typeId, final Integer gid, final String nVal) {
+		final Name name = new Name();
+		name.setTypeId(typeId);
+		name.setNval(nVal);
+		name.setGermplasmId(gid);
+		name.setLocationId(0);
+		name.setNstat(1);
+		name.setNdate(20150707);
+		name.setReferenceId(0);
+		name.setUserId(1);
+		return name;
+	}
+	
 	public Name createName(final Integer typeId, final String nVal) {
 		final Name name = new Name();
 		name.setTypeId(typeId);
