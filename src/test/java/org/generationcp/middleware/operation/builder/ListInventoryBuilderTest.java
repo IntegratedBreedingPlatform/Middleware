39665a8f8d0509c8d7d3ebf216eb9d29a31d5735
abhishekptl <abhishek@leafnode.io>
2016-10-06 11:44:22 +0530

    Removing ignore and enabling failing test case.
    Germplasm was created using mgid=0 and assert statements match groupId=1 and so it was failing.
    
    issue: BMS-3347
    reviewer: Sunny

--- src/test/java/org/generationcp/middleware/operation/builder/ListInventoryBuilderTest.java
+++ src/test/java/org/generationcp/middleware/operation/builder/ListInventoryBuilderTest.java
@@ -13,15 +13,13 @@ import org.generationcp.middleware.pojos.GermplasmList;
 import org.generationcp.middleware.pojos.GermplasmListData;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Ignore("Historic failing test. Disabled temporarily. Developers working in this area please spend some time to fix and remove @Ignore.")
 public class ListInventoryBuilderTest extends IntegrationTestBase {
 
 	private static final int LIST_ID = 1;
-	private static final int GROUP_ID = 1;
+	private static final int GROUP_ID = 0;
 	private static final int NO_OF_ENTRIES = 5;
 	private static ListInventoryBuilder listInventoryBuilder;
 	private static List<Integer> gids;
