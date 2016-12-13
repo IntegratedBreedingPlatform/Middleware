6939592daf911feaaefccd1ad2fb740fab5e5e54
Iryna Dziuban <iryna@leafnode.io>
2016-12-13 14:32:06 +1300

    Fix saving the imported measurements.
    Issue: BMS-3175
    Reviewer: Naymesh

--- src/main/java/org/generationcp/middleware/service/FieldbookServiceImpl.java
+++ src/main/java/org/generationcp/middleware/service/FieldbookServiceImpl.java
@@ -215,7 +215,7 @@ public class FieldbookServiceImpl extends Service implements FieldbookService {
 
 	@Override
 	public void loadAllObservations(final Workbook workbook) {
-		if (workbook.getObservations() != null && workbook.getObservations().isEmpty() && workbook.getStudyDetails() != null && workbook
+		if (workbook.getObservations() == null || workbook.getObservations().isEmpty() && workbook.getStudyDetails() != null && workbook
 				.getStudyDetails().getId() != null) {
 			this.getWorkbookBuilder().loadAllObservations(workbook);
 		}
