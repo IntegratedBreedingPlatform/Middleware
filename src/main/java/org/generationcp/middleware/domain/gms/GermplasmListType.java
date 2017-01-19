08b7ff7c624d93723a11ec7ff9c1294400daede1
Nahuel Soldevilla <nsoldevilla@droptek.com>
2017-01-19 11:56:16 -0300

    Add Javadoc to new cross list types
    Issue: BMS-3788

--- src/main/java/org/generationcp/middleware/domain/gms/GermplasmListType.java
+++ src/main/java/org/generationcp/middleware/domain/gms/GermplasmListType.java
@@ -3,5 +3,17 @@ package org.generationcp.middleware.domain.gms;
 
 public enum GermplasmListType {
 
-	LST, NURSERY, CHECK, TRIAL, ADVANCED, CROSSES, STOCK, F1, F1IMP, F1CRT, PLCRT
+	LST, NURSERY, CHECK, TRIAL, ADVANCED, CROSSES, STOCK, F1,
+	/**
+	 * Imported F1 list
+	 */
+	F1IMP,
+	/**
+	 * Crossing tool F1 list
+	 */
+	F1CRT,
+	/**
+	 * Crossing tool parent list
+	 */
+	PLCRT
 }
