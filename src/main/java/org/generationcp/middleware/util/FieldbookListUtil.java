c373c93590168606bd34c4570911046ce84ca498
chavdahard <hard@leafnode.io>
2017-01-25 11:46:54 +0530

    Added two new Utility methods that will set stockIds.
    populateStockIdInListDataProject and populateStockIdInGermplasmListData will set stockids to ListDataProject and
    GermplasmListData respectively.
    
    issue: BMS-3774,BMS-3775
    reviewer: Abhishek

--- src/main/java/org/generationcp/middleware/util/FieldbookListUtil.java
+++ src/main/java/org/generationcp/middleware/util/FieldbookListUtil.java
@@ -1,10 +1,15 @@
 
 package org.generationcp.middleware.util;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
+import org.generationcp.middleware.manager.api.InventoryDataManager;
+import org.generationcp.middleware.pojos.GermplasmListData;
+import org.generationcp.middleware.pojos.ListDataProject;
 import org.generationcp.middleware.pojos.Method;
 
 public class FieldbookListUtil {
@@ -26,4 +31,59 @@ public class FieldbookListUtil {
 			}
 		});
 	}
+
+	public static void populateStockIdInListDataProject(final List<ListDataProject> data , InventoryDataManager inventoryDataManager) {
+		final List<Integer> gids = new ArrayList<>();
+		if (data != null && !data.isEmpty()) {
+			for (final ListDataProject listDataProject : data) {
+				gids.add(listDataProject.getGermplasmId());
+			}
+		}
+
+		Map<Integer, String> stockIds = inventoryDataManager.retrieveStockIds(gids);
+
+
+		if (data != null && !data.isEmpty()) {
+			for (final ListDataProject ListData : data) {
+				String stockIdValue = "";
+				if (stockIds != null) {
+					for (final Integer gid : stockIds.keySet()) {
+						if (ListData.getGermplasmId().equals(gid)) {
+							stockIdValue = stockIds.get(gid);
+							break;
+						}
+					}
+				}
+				ListData.setStockIDs(stockIdValue);
+			}
+		}
+	}
+
+	public static void populateStockIdInGermplasmListData(final List<GermplasmListData> data , InventoryDataManager inventoryDataManager) {
+		final List<Integer> gids = new ArrayList<>();
+		if (data != null && !data.isEmpty()) {
+			for (final GermplasmListData germplasmListData : data) {
+				gids.add(germplasmListData.getGid());
+			}
+		}
+
+		Map<Integer, String> stockIds = inventoryDataManager.retrieveStockIds(gids);
+
+		if (data != null && !data.isEmpty()) {
+			for (final GermplasmListData ListData : data) {
+				String stockIdValue = "";
+				if (stockIds != null) {
+					for (final Integer gid : stockIds.keySet()) {
+						if (ListData.getGid().equals(gid)) {
+							stockIdValue = stockIds.get(gid);
+							break;
+						}
+					}
+				}
+				ListData.setStockIDs(stockIdValue);
+			}
+		}
+	}
+
+
 }
