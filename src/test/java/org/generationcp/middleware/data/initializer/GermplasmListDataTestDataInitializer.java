fb781fa3a3e3129ca1371f05b555d8aebe05f851
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2017-02-28 10:36:44 +0800

    Added test data initializer method
    BMS-3933

--- src/test/java/org/generationcp/middleware/data/initializer/GermplasmListDataTestDataInitializer.java
+++ src/test/java/org/generationcp/middleware/data/initializer/GermplasmListDataTestDataInitializer.java
@@ -11,4 +11,11 @@ public class GermplasmListDataTestDataInitializer {
 				"Germplasm Name 5", "GroupName", 0, 99995);
 		return germplasmListData;
 	}
+	
+	public static GermplasmListData createGermplasmListData(final GermplasmList germplasmList, final Integer gid, final Integer entryId, final Integer groupId) {
+		final GermplasmListData germplasmListData = new GermplasmListData(null, germplasmList, gid, entryId, "EntryCode", "SeedSource",
+				"Germplasm Name 5", "GroupName", 0, 99995);
+		germplasmListData.setGroupId(groupId);
+		return germplasmListData;
+	}
 }
