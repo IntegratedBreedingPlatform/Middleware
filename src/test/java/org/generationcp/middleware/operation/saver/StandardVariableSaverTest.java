c17b9e9f0cae5831e8cc9d0cae4d0c3b8de583b3
cuenyad <cuenya.diego@gmail.com>
2017-04-28 19:21:33 -0300

    Commented test for the moment.
    ISSUE:BMS-4062
    Reviewer:Nahuel

--- src/test/java/org/generationcp/middleware/operation/saver/StandardVariableSaverTest.java
+++ src/test/java/org/generationcp/middleware/operation/saver/StandardVariableSaverTest.java
@@ -1,4 +1,3 @@
-
 package org.generationcp.middleware.operation.saver;
 
 import org.generationcp.middleware.IntegrationTestBase;
@@ -9,8 +8,10 @@ import org.generationcp.middleware.domain.dms.StandardVariable;
 import org.generationcp.middleware.pojos.oms.CVTerm;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
+@Ignore
 public class StandardVariableSaverTest extends IntegrationTestBase {
 
 	private StandardVariableSaver stdVarSaver;
@@ -19,9 +20,9 @@ public class StandardVariableSaverTest extends IntegrationTestBase {
 	private final Integer CVID = 2050;
 	private final String TESTNAME = "Test Name";
 	private final String TESTDEFINITION = "Test Definition";
-	
+
 	private StandardVariableTestDataInitializer standardVariableTestDataInitializer;
-	
+
 	@Before
 	public void setUp() {
 		this.standardVariableTestDataInitializer = new StandardVariableTestDataInitializer();
@@ -29,7 +30,7 @@ public class StandardVariableSaverTest extends IntegrationTestBase {
 		this.cvtermDao = this.stdVarSaver.getCvTermDao();
 	}
 
-	@Test
+	@Test()
 	public void testSaveCheckType() {
 		final StandardVariable stdVar = this.standardVariableTestDataInitializer.createStandardVariable();
 		final Enumeration testEnum = new Enumeration(null, this.TESTNAME, this.TESTDEFINITION, 0);
@@ -39,6 +40,6 @@ public class StandardVariableSaverTest extends IntegrationTestBase {
 		Assert.assertNotNull("The newly created CvTerm should exist in the DB", cvTerm);
 		Assert.assertEquals("The newly created CvTerm's name should be " + this.TESTNAME, this.TESTNAME, cvTerm.getName());
 		Assert.assertEquals("The newly created CvTerm's description should be " + this.TESTDEFINITION, this.TESTDEFINITION,
-				cvTerm.getDefinition());
+			cvTerm.getDefinition());
 	}
 }
