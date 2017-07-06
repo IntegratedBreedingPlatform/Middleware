5eb90c213a8e86507facc50adfee39a8b623212a
cuenyad <cuenya.diego@gmail.com>
2017-07-06 16:05:54 -0300

    New dto and Filter to set statement in the query
    ISSUE:BMS-4258
    Reviewer:Nahuel

---
+++ src/main/java/org/generationcp/middleware/service/api/program/ProgramDetailsDto.java
@@ -0,0 +1,98 @@
+package org.generationcp.middleware.service.api.program;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.io.Serializable;
+
+
+public class ProgramDetailsDto implements Serializable, Comparable<ProgramDetailsDto> {
+
+	private static final long serialVersionUID = 9163866215679883266L;
+	private Integer programDbId;
+	private String name;
+	private String abbreviation;
+	private String objective;
+	private String leadPerson;
+
+	public ProgramDetailsDto() {
+
+	}
+
+	public ProgramDetailsDto(final Integer programDbId, final String name, final String abbreviation, final String objective,
+		final String leadPerson) {
+
+		this.programDbId = programDbId;
+		this.name = name;
+		this.abbreviation = abbreviation;
+		this.objective = objective;
+		this.leadPerson = leadPerson;
+	}
+
+	public Integer getProgramDbId() {
+		return programDbId;
+	}
+
+	public void setProgramDbId(Integer programDbId) {
+		this.programDbId = programDbId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAbbreviation() {
+		return abbreviation;
+	}
+
+	public void setAbbreviation(String abbreviation) {
+		this.abbreviation = abbreviation;
+	}
+
+	public String getObjective() {
+		return objective;
+	}
+
+	public void setObjective(String objective) {
+		this.objective = objective;
+	}
+
+	public String getLeadPerson() {
+		return leadPerson;
+	}
+
+	public void setLeadPerson(String leadPerson) {
+		this.leadPerson = leadPerson;
+	}
+
+	@Override
+	public boolean equals(final Object other) {
+		if (!(other instanceof ProgramDetailsDto)) {
+			return false;
+		}
+		final ProgramDetailsDto castOther = (ProgramDetailsDto) other;
+		return new EqualsBuilder().append(this.programDbId, castOther.programDbId).isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(this.programDbId).hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).toString();
+	}
+
+	@Override
+	public int compareTo(ProgramDetailsDto compareProgramDetails) {
+		Integer id = compareProgramDetails.getProgramDbId();
+		return this.programDbId.compareTo(id);
+	}
+}
