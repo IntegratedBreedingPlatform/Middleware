53451a13c4a0001ff3d16390e708c957a7c4a66e
Aldrin Batac <aldrin@leafnode.io>
2017-07-27 15:48:40 +0800

    Improve data type information in exported Excel workbook.
    BMS-4307

--- src/main/java/org/generationcp/middleware/domain/ontology/DataType.java
+++ src/main/java/org/generationcp/middleware/domain/ontology/DataType.java
@@ -6,27 +6,29 @@ import java.util.Map;
 
 public enum DataType {
 
-	CATEGORICAL_VARIABLE(1130, "Categorical", false),
-	NUMERIC_VARIABLE(1110, "Numeric", false),
-	DATE_TIME_VARIABLE(1117, "Date", false),
-	CHARACTER_VARIABLE(1120, "Character", false),
+	CATEGORICAL_VARIABLE(1130, "Categorical", false, "C"),
+	NUMERIC_VARIABLE(1110, "Numeric", false, "N"),
+	DATE_TIME_VARIABLE(1117, "Date", false, "D"),
+	CHARACTER_VARIABLE(1120, "Character", false, "T"),
 
 	//Special Data types
-	PERSON(1131, "Person", true),
-	LOCATION(1132, "Location", true),
-	STUDY(1133, "Study", true),
-	DATASET(1134, "Dataset", true),
-	GERMPLASM_LIST(1135, "Germplasm List", true),
-	BREEDING_METHOD(1136, "Breeding Method", true);
+	PERSON(1131, "Person", true, "T"),
+	LOCATION(1132, "Location", true, "T"),
+	STUDY(1133, "Study", true, "T"),
+	DATASET(1134, "Dataset", true, "T"),
+	GERMPLASM_LIST(1135, "Germplasm List", true, "T"),
+	BREEDING_METHOD(1136, "Breeding Method", true, "T");
 
 	private Integer id;
 	private String name;
 	private boolean systemDataType;
+	private String dataTypeCode;
 
-	DataType(Integer id, String name, boolean systemDataType) {
+	DataType(Integer id, String name, boolean systemDataType, String dataTypeCode) {
 		this.id = id;
 		this.name = name;
 		this.systemDataType = systemDataType;
+		this.dataTypeCode = dataTypeCode;
 	}
 
 	private static final Map<Integer, DataType> byId = new HashMap<>();
@@ -55,6 +57,17 @@ public enum DataType {
 	public boolean isSystemDataType(){
 		return this.systemDataType;
 	}
+
+	/**
+	 * Gets the single character code that represents a data type.
+	 * "C" for Categorical
+	 * "N" for Numeric
+	 * "D" for Date
+	 * "T" for Character/Text
+	 * For Special Data Types, the default data type code is "T" (Character/Text).
+	 * @return
+	 */
+	public String getDataTypeCode() { return  this.dataTypeCode; }
 
 	public static DataType getById(Integer id) {
 		return DataType.byId.get(id);
