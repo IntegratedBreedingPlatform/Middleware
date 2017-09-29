b46db1e13760eca66e8aa4448f916b3f60f97de3
Aldrin Batac <aldrin@leafnode.io>
2017-09-29 15:01:47 +0800

    Revert "Merge pull request #358 from IntegratedBreedingPlatform/BMS-4414-TableOfEnvironmentsInAdvanceTrialNotUpdating"
    This reverts commit 884ede75c824375fc751610bd7f41a8dac19ed85, reversing
    changes made to 2d472f5462223452271afbfd0732dc7318b23aaf.

--- src/test/java/org/generationcp/middleware/data/initializer/LocationTestDataInitializer.java
+++ src/test/java/org/generationcp/middleware/data/initializer/LocationTestDataInitializer.java
@@ -11,27 +11,22 @@ public class LocationTestDataInitializer {
 	private static final int LOCATION_ID = 1;
 	private static final String LOCATION_NAME = "Sample Location";
 
-	public static Location createLocation() {
-		return LocationTestDataInitializer.createLocation(LocationTestDataInitializer.LOCATION_ID,
-				LocationTestDataInitializer.LOCATION_NAME);
+	public LocationTestDataInitializer() {
+		// do nothing
 	}
 
-	public static Location createLocation(final Integer locId, final String lname) {
+	public Location createLocation() {
+		return this.createLocation(LOCATION_ID, LOCATION_NAME);
+	}
+
+	public Location createLocation(final Integer locId, final String lname) {
 		final Location location = new Location();
 		location.setLocid(locId);
 		location.setLname(lname);
 		return location;
 	}
 	
-	public static Location createLocationWithLabbr(final Integer locId, final String lname, final String labbr) {
-		final Location location = new Location();
-		location.setLocid(locId);
-		location.setLname(lname);
-		location.setLabbr(labbr);
-		return location;
-	}
-
-	public static Location createLocation(final Integer locId, final String lname, final String uuid) {
+	public Location createLocation(final Integer locId, final String lname, final String uuid) {
 		final Location location = new Location();
 		location.setLocid(locId);
 		location.setLname(lname);
@@ -39,14 +34,14 @@ public class LocationTestDataInitializer {
 		return location;
 	}
 
-	public static Location createLocation(final Integer locId, final String lname, final Integer locationType,
+	public Location createLocation(final Integer locId, final String lname, final Integer locationType,
 			final String locationAbbreviation, final String programUUID) {
 		final Location location = new Location();
 		location.setLocid(locId);
 		location.setLname(lname);
 		location.setLtype(locationType);
 		location.setLabbr(locationAbbreviation);
-		if (programUUID != null) {
+		if (programUUID != null){
 			location.setUniqueID(programUUID);
 		}
 		location.setNllp(0);
@@ -58,12 +53,11 @@ public class LocationTestDataInitializer {
 		return location;
 	}
 
-	public static List<Location> createLocationList(final int noOfLocations) {
+	public List<Location> createLocationList(final int noOfLocations) {
 		final List<Location> locations = new ArrayList<Location>();
 
 		for (int i = 1; i <= noOfLocations; i++) {
-			locations.add(
-					LocationTestDataInitializer.createLocation(i, LocationTestDataInitializer.LOCATION_NAME + " " + i));
+			locations.add(this.createLocation(i, LOCATION_NAME + " " + i));
 		}
 
 		return locations;
