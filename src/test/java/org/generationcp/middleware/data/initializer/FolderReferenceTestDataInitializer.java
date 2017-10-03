e75d2ab2ce91c628f019a7421630b114f30e5b41
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2017-10-03 23:35:38 +0800

    Applied leafnode clean up
    BMS-2237

--- src/test/java/org/generationcp/middleware/data/initializer/FolderReferenceTestDataInitializer.java
+++ src/test/java/org/generationcp/middleware/data/initializer/FolderReferenceTestDataInitializer.java
@@ -4,8 +4,8 @@ import org.generationcp.middleware.domain.dms.FolderReference;
 import org.generationcp.middleware.domain.dms.Reference;
 
 public class FolderReferenceTestDataInitializer {
-	public static Reference createReference(final int id){
-		final Reference reference =  new FolderReference(id, "FOLDER");
+	public static Reference createReference(final int id) {
+		final Reference reference = new FolderReference(id, "FOLDER");
 		return reference;
 	}
 }
