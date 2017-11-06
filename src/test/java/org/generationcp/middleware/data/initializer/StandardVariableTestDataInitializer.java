a334aec365a0dc537647b828cf018a551109c9b6
Vanina Maletta <vmaletta@droptek.com>
2017-11-06 18:02:20 -0300

    Removed unused code
    Issue: BMS-4512
    
    Reviewer: Diego

--- src/test/java/org/generationcp/middleware/data/initializer/StandardVariableTestDataInitializer.java
+++ src/test/java/org/generationcp/middleware/data/initializer/StandardVariableTestDataInitializer.java
@@ -21,9 +21,6 @@ public class StandardVariableTestDataInitializer {
 		stdVariable.setScale(new Term(61220, "DBCV", "Controlled vocabulary from a database"));
 		stdVariable.setDataType(new Term(1120, "Character variable", "variable with char values"));
 		stdVariable.setIsA(new Term(1050, "Study condition", "Study condition class"));
-		stdVariable.setEnumerations(new ArrayList<Enumeration>());
-		stdVariable.getEnumerations().add(new Enumeration(10001, "HB", "Hybridization nursery", 2));
-		stdVariable.getEnumerations().add(new Enumeration(10002, "PN", "Pedigree nursery", 3));
 		stdVariable.setConstraints(new VariableConstraints(100.0, 999.0));
 		stdVariable.setCropOntologyId("CROP-TEST");
 
--- src/test/java/org/generationcp/middleware/manager/OntologyDataManagerImplIntegrationTest.java
+++ src/test/java/org/generationcp/middleware/manager/OntologyDataManagerImplIntegrationTest.java
@@ -223,9 +223,6 @@ public class OntologyDataManagerImplIntegrationTest extends IntegrationTestBase 
 		stdVariable.setScale(new Term(6000, "DBCV", "Controlled vocabulary from a database"));
 		stdVariable.setDataType(new Term(1120, "Character variable", "variable with char values"));
 		stdVariable.setIsA(new Term(1050, "Study condition", "Study condition class"));
-		stdVariable.setEnumerations(new ArrayList<Enumeration>());
-		stdVariable.getEnumerations().add(new Enumeration(10001, "HB", "Hybridization nursery", 2));
-		stdVariable.getEnumerations().add(new Enumeration(10002, "PN", "Pedigree nursery", 3));
 		stdVariable.setConstraints(new VariableConstraints(100.0, 999.0));
 		stdVariable.setCropOntologyId("CROP-TEST");
 
