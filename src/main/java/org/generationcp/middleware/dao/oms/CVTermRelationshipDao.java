58f680c7a7aeca7c47167f0d44fd4a1e7407886c
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2017-11-14 22:16:03 +0800

    Add closing parenthesis to fix query error.
    BMS-4536

--- src/main/java/org/generationcp/middleware/dao/oms/CVTermRelationshipDao.java
+++ src/main/java/org/generationcp/middleware/dao/oms/CVTermRelationshipDao.java
@@ -270,7 +270,7 @@ public class CVTermRelationshipDao extends GenericDAO<CVTermRelationship, Intege
 					+ " 	 FROM phenotype p " + " 	INNER JOIN nd_experiment_phenotype eph on eph.phenotype_id = p.phenotype_id "
 					+ "     INNER JOIN nd_experiment_project ep on ep.nd_experiment_id = eph.nd_experiment_id "
 					+ "     WHERE cvalue_id = v.cvterm_id "
-					+ "       AND ep.project_id not in (SELECT p.project_id FROM project p WHERE p.deleted = 1)");
+					+ "       AND ep.project_id not in (SELECT p.project_id FROM project p WHERE p.deleted = 1))");
 			query.setParameter("scaleId", scaleId);
 			query.addScalar("category", CVTermRelationshipDao.STRING);
 			return query.list();
