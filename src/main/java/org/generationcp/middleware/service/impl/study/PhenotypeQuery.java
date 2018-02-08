4e9db19be17e1c0ed56055f1801cdb65c6ad6e6f
Clarysabel Tovar <clarysabel@leafnode.io>
2018-02-08 14:49:58 +0100

    Replacing numbers by TermIds Issue: BMS-4691 Reviewer: none

--- src/main/java/org/generationcp/middleware/service/impl/study/PhenotypeQuery.java
+++ src/main/java/org/generationcp/middleware/service/impl/study/PhenotypeQuery.java
@@ -1,5 +1,7 @@
 package org.generationcp.middleware.service.impl.study;
 
+import org.generationcp.middleware.domain.oms.TermId;
+
 public class PhenotypeQuery {
 
 	public static final String PHENOTYPE_SEARCH = " SELECT " //
@@ -20,8 +22,8 @@ public class PhenotypeQuery {
 		+ "  (SELECT ndep.value FROM nd_experimentprop ndep INNER JOIN cvterm ispcvt ON ispcvt.cvterm_id = ndep.type_id WHERE ndep.nd_experiment_id = ep.nd_experiment_id AND ispcvt.name = 'REP_NO') AS replicate, " //
 		+ "  (SELECT ndep.value FROM nd_experimentprop ndep INNER JOIN cvterm ispcvt ON ispcvt.cvterm_id = ndep.type_id WHERE ndep.nd_experiment_id = ep.nd_experiment_id AND ispcvt.name = 'COL') AS COL, " //
 		+ "  (SELECT ndep.value FROM nd_experimentprop ndep INNER JOIN cvterm ispcvt ON ispcvt.cvterm_id = ndep.type_id WHERE ndep.nd_experiment_id = ep.nd_experiment_id AND ispcvt.name = 'ROW') AS ROW, " //
-		+ "  (SELECT l.locid FROM nd_geolocationprop gp INNER JOIN location l ON l.locid = gp.value WHERE gp.type_id = 8190 AND gp.nd_geolocation_id = gl.nd_geolocation_id) AS studyLocationDbId, " //
-		+ "  (SELECT l.lname FROM nd_geolocationprop gp INNER JOIN location l ON l.locid = gp.value WHERE gp.type_id = 8190 AND gp.nd_geolocation_id = gl.nd_geolocation_id) AS studyLocation, " //
+		+ "  (SELECT l.locid FROM nd_geolocationprop gp INNER JOIN location l ON l.locid = gp.value WHERE gp.type_id = " + TermId.LOCATION_ID.getId() + " AND gp.nd_geolocation_id = gl.nd_geolocation_id) AS studyLocationDbId, " //
+		+ "  (SELECT l.lname FROM nd_geolocationprop gp INNER JOIN location l ON l.locid = gp.value WHERE gp.type_id = " + TermId.LOCATION_ID.getId() + " AND gp.nd_geolocation_id = gl.nd_geolocation_id) AS studyLocation, " //
 		+ "  (SELECT iispcvt.definition FROM stockprop isp INNER JOIN cvterm ispcvt ON ispcvt.cvterm_id = isp.type_id INNER JOIN cvterm iispcvt ON iispcvt.cvterm_id = isp.value WHERE isp.stock_id = s.stock_id AND ispcvt.name = 'ENTRY_TYPE') AS entryType, " //
 		+ "  s.uniquename AS entryNumber " //
 		+ "FROM " //
@@ -48,9 +50,6 @@ public class PhenotypeQuery {
 		+ " WHERE ndeph.nd_experiment_id = nde.nd_experiment_id AND cvt.cvterm_id in (:cvTermIds))" //
 		;
 
-	public static final String PHENOTYPE_SEARCH_COUNT = " " //
-		;
-
 	public static final String PHENOTYPE_SEARCH_OBSERVATIONS = "SELECT " //
 		+ "  ndeph.nd_experiment_id as expid, " //
 		+ "  ndeph.nd_experiment_phenotype_id as nd_exp_phen_id, " //
@@ -62,7 +61,7 @@ public class PhenotypeQuery {
 		+ "  nd_experiment_phenotype ndeph" //
 		+ "  INNER JOIN phenotype ph ON ndeph.phenotype_id = ph.phenotype_id " //
 		+ "  INNER JOIN cvterm cvt ON ph.observable_id = cvt.cvterm_id " //
-		+ "  LEFT JOIN cvtermprop cvp on (cvp.cvterm_id = cvt.cvterm_id and cvp.type_id = 1226)"
+		+ "  LEFT JOIN cvtermprop cvp on (cvp.cvterm_id = cvt.cvterm_id and cvp.type_id = " + TermId.CROP_ONTOLOGY_ID.getId() + ")"
 		+ "WHERE ndeph.nd_experiment_id in (:ndExperimentIds)" //
 		;
 
