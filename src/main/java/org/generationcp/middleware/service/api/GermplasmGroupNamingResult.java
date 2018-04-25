0128fcc7f87d9712be20aac99d577f1df756b70d
Nahuel Soldevilla <nahuel@leafnode.io>
2018-04-25 18:52:38 -0300

    Split message types in germplasm code result
    Issue: BMS-3801
    Reviewer: None

--- src/main/java/org/generationcp/middleware/service/api/GermplasmGroupNamingResult.java
+++ src/main/java/org/generationcp/middleware/service/api/GermplasmGroupNamingResult.java
@@ -7,14 +7,42 @@ import java.util.List;
 // FIXME First cut, more structured DTO later.
 public class GermplasmGroupNamingResult {
 
-	private final List<String> messages = new ArrayList<>();
+	private final List<String> success = new ArrayList<>();
+	private final List<String> errors = new ArrayList<>();
 
 	public List<String> getMessages() {
+		List<String> messages = new ArrayList<>();
+		messages.addAll(errors);
+		messages.addAll(success);
 		return messages;
 	}
 
-	public void addMessage(String message) {
-		this.messages.add(message);
+	/**
+	 * Get error messages
+	 */
+	public List<String> getErrors() {
+		return errors;
+	}
+
+	/**
+	 * Get success messages
+	 */
+	public List<String> getSuccess() {
+		return success;
+	}
+
+	/**
+	 * Add error message
+	 */
+	public void addError(String error) {
+		this.errors.add(error);
+	}
+
+	/**
+	 * Add success message
+	 */
+	public void addSuccess(String success) {
+		this.success.add(success);
 	}
 
 }
