b9b739dc6d0098357d665e65573c7ae6fa5de3e1
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2018-05-10 16:44:54 +0800

    Applied leafnode clean up
    BMS-4898

--- src/test/java/org/generationcp/middleware/data/initializer/ProjectPropertyTestDataInitializer.java
+++ src/test/java/org/generationcp/middleware/data/initializer/ProjectPropertyTestDataInitializer.java
@@ -3,11 +3,11 @@ package org.generationcp.middleware.data.initializer;
 import org.generationcp.middleware.pojos.dms.ProjectProperty;
 
 public class ProjectPropertyTestDataInitializer {
-	public static ProjectProperty createProjectProperty(String alias, Integer variableId) {
-		ProjectProperty projectProperty = new ProjectProperty();
+	public static ProjectProperty createProjectProperty(final String alias, final Integer variableId) {
+		final ProjectProperty projectProperty = new ProjectProperty();
 		projectProperty.setAlias(alias);
 		projectProperty.setVariableId(variableId);
 		return projectProperty;
 	}
-	
+
 }
