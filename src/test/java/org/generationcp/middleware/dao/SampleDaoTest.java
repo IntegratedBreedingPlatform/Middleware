55b341de4ae36b981f35238aca010734341582b5
Aldrin Batac <aldrin.batac@leafnode.io>
2018-05-21 16:37:49 +0800

    Added unit tests for SampleDao.
    BMS-4882

---
+++ src/test/java/org/generationcp/middleware/dao/SampleDaoTest.java
@@ -0,0 +1,214 @@
+package org.generationcp.middleware.dao;
+
+import com.google.common.collect.Ordering;
+import org.generationcp.middleware.IntegrationTestBase;
+import org.generationcp.middleware.dao.dms.ExperimentDao;
+import org.generationcp.middleware.dao.dms.ExperimentStockDao;
+import org.generationcp.middleware.dao.dms.GeolocationDao;
+import org.generationcp.middleware.dao.dms.StockDao;
+import org.generationcp.middleware.data.initializer.PlantTestDataInitializer;
+import org.generationcp.middleware.data.initializer.SampleListTestDataInitializer;
+import org.generationcp.middleware.data.initializer.SampleTestDataInitializer;
+import org.generationcp.middleware.data.initializer.UserTestDataInitializer;
+import org.generationcp.middleware.domain.dms.Experiment;
+import org.generationcp.middleware.domain.oms.TermId;
+import org.generationcp.middleware.domain.sample.SampleDTO;
+import org.generationcp.middleware.operation.builder.StockModelBuilder;
+import org.generationcp.middleware.pojos.Plant;
+import org.generationcp.middleware.pojos.Sample;
+import org.generationcp.middleware.pojos.SampleList;
+import org.generationcp.middleware.pojos.User;
+import org.generationcp.middleware.pojos.dms.ExperimentModel;
+import org.generationcp.middleware.pojos.dms.ExperimentStock;
+import org.generationcp.middleware.pojos.dms.Geolocation;
+import org.generationcp.middleware.pojos.dms.StockModel;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class SampleDaoTest extends IntegrationTestBase {
+
+	public static final String ADMIN = "admin";
+	public static final Integer TEST_SAMPLE_RECORD_COUNT = 23;
+
+	private SampleListDao sampleListDao;
+	private UserDAO userDao;
+	private PlantDao plantDao;
+	private SampleDao sampleDao;
+	private ExperimentDao experimentDao;
+	private GeolocationDao geolocationDao;
+	private StockDao stockDao;
+	private ExperimentStockDao experimentStockDao;
+
+	private Integer listId;
+
+	@Before
+	public void setUp() throws Exception {
+		this.sampleListDao = new SampleListDao();
+		this.sampleListDao.setSession(this.sessionProvder.getSession());
+
+		this.userDao = new UserDAO();
+		this.userDao.setSession(this.sessionProvder.getSession());
+
+		this.plantDao = new PlantDao();
+		this.plantDao.setSession(this.sessionProvder.getSession());
+
+		this.sampleDao = new SampleDao();
+		this.sampleDao.setSession(this.sessionProvder.getSession());
+
+		this.experimentDao = new ExperimentDao();
+		this.experimentDao.setSession(this.sessionProvder.getSession());
+
+		this.geolocationDao = new GeolocationDao();
+		this.geolocationDao.setSession(this.sessionProvder.getSession());
+
+		this.experimentStockDao = new ExperimentStockDao();
+		this.experimentStockDao.setSession(this.sessionProvder.getSession());
+
+		this.stockDao = new StockDao();
+		this.stockDao.setSession(this.sessionProvder.getSession());
+
+		this.listId = this.createSampleListForFilter("TEST-LIST-1");
+	}
+
+	@Test
+	public void testCountFilter() {
+
+		final Long countAllSample = this.sampleDao.countFilter(null, this.listId);
+
+		Assert.assertEquals(TEST_SAMPLE_RECORD_COUNT.intValue(), countAllSample.intValue());
+
+	}
+
+	@Test
+	public void testFilterPagination() {
+
+		final Pageable pageable = Mockito.mock(Pageable.class);
+		Mockito.when(pageable.getPageSize()).thenReturn(10);
+
+		// Page 1
+		Mockito.when(pageable.getPageNumber()).thenReturn(0);
+		final List<SampleDTO> result1 = this.sampleDao.filter(null, this.listId, pageable);
+		Assert.assertEquals(10, result1.size());
+
+		// Page 2
+		Mockito.when(pageable.getPageNumber()).thenReturn(1);
+		final List<SampleDTO> result2 = this.sampleDao.filter(null, this.listId, pageable);
+		Assert.assertEquals(10, result2.size());
+
+		// Page 3
+		Mockito.when(pageable.getPageNumber()).thenReturn(2);
+		final List<SampleDTO> result3 = this.sampleDao.filter(null, this.listId, pageable);
+		Assert.assertEquals(3, result3.size());
+
+	}
+
+	@Test
+	public void testFilterSortAscending() {
+
+		final Pageable pageable = Mockito.mock(Pageable.class);
+		Mockito.when(pageable.getPageSize()).thenReturn(TEST_SAMPLE_RECORD_COUNT);
+		Mockito.when(pageable.getPageNumber()).thenReturn(0);
+		final Sort.Order order = new Sort.Order(Sort.Direction.ASC, "sampleName");
+		Mockito.when(pageable.getSort()).thenReturn(new Sort(order));
+
+		final List<SampleDTO> queryResult = this.sampleDao.filter(null, this.listId, pageable);
+
+		final List<String> result = new LinkedList<>();
+		for (final SampleDTO sampleDTO : queryResult) {
+			result.add(sampleDTO.getSampleName());
+		}
+
+		// Check if the sampleName is in ascending order
+		Assert.assertTrue(Ordering.natural().isOrdered(result));
+
+	}
+
+	@Test
+	public void testFilterSortDescending() {
+
+		final Pageable pageable = Mockito.mock(Pageable.class);
+		Mockito.when(pageable.getPageSize()).thenReturn(TEST_SAMPLE_RECORD_COUNT);
+		Mockito.when(pageable.getPageNumber()).thenReturn(0);
+		final Sort.Order order = new Sort.Order(Sort.Direction.DESC, "sampleName");
+		Mockito.when(pageable.getSort()).thenReturn(new Sort(order));
+
+		final List<SampleDTO> queryResult = this.sampleDao.filter(null, this.listId, pageable);
+
+		final List<String> result = new LinkedList<>();
+		for (final SampleDTO sampleDTO : queryResult) {
+			result.add(sampleDTO.getSampleName());
+		}
+
+		// Check if the sampleName is in descending order
+		Assert.assertTrue(Ordering.natural().reverse().isOrdered(result));
+
+	}
+
+	private Integer createSampleListForFilter(final String listName) {
+
+		User user = this.userDao.getUserByUserName(SampleListDaoTest.ADMIN);
+		if (user == null) {
+			// FIXME fresh db doesn't have admin user in crop. BMS-886
+			user = UserTestDataInitializer.createUser();
+			user.setName(ADMIN);
+			user.setUserid(1);
+			this.userDao.saveOrUpdate(user);
+		}
+
+		final Geolocation geolocation = new Geolocation();
+		geolocationDao.saveOrUpdate(geolocation);
+
+		final SampleList sampleList = SampleListTestDataInitializer.createSampleList(user);
+		sampleList.setListName(listName);
+		sampleList.setDescription("DESCRIPTION-" + listName);
+
+		this.sampleListDao.saveOrUpdate(sampleList);
+
+		for (int i = 1; i < TEST_SAMPLE_RECORD_COUNT + 1; i++) {
+
+			final ExperimentModel experimentModel = new ExperimentModel();
+			experimentModel.setGeoLocation(geolocation);
+			experimentModel.setTypeId(TermId.PLOT_EXPERIMENT.getId());
+			experimentModel.setPlotId("PLOT-ID" + i);
+			experimentDao.saveOrUpdate(experimentModel);
+
+			final StockModel stockModel = new StockModel();
+			stockModel.setName("Germplasm " + i);
+			stockModel.setIsObsolete(false);
+			stockModel.setTypeId(TermId.ENTRY_CODE.getId());
+			stockModel.setUniqueName(String.valueOf(i));
+			stockDao.saveOrUpdate(stockModel);
+
+			final ExperimentStock experimentStock = new ExperimentStock();
+			experimentStock.setExperiment(experimentModel);
+			experimentStock.setStock(stockModel);
+			experimentStock.setTypeId(TermId.IBDB_STRUCTURE.getId());
+			experimentStockDao.saveOrUpdate(experimentStock);
+
+			final Plant plant = PlantTestDataInitializer.createPlant();
+			plant.setExperiment(experimentModel);
+
+			final Sample sample = SampleTestDataInitializer.createSample(sampleList, plant, user);
+			sample.setCreatedBy(user);
+			sample.setSampleName("SAMPLE-" + listName + i);
+			sample.setSampleBusinessKey("BUSINESS-KEY-" + listName + i);
+			sample.setEntryNumber(i);
+			this.sampleDao.saveOrUpdate(sample);
+
+		}
+
+		return sampleList.getId();
+
+	}
+
+}
