2f23de5459dd60a088d76c2a067ba1e0f24cfb80
vmaletta <vmaletta@droptek.com>
2018-06-06 15:56:47 -0300

    GeolocationPropertyDao refactor and cleanup
    Issue: BMS-4927
    
    Reviewer: Clary

--- src/main/java/org/generationcp/middleware/dao/dms/GeolocationPropertyDao.java
+++ src/main/java/org/generationcp/middleware/dao/dms/GeolocationPropertyDao.java
@@ -40,16 +40,16 @@ public class GeolocationPropertyDao extends GenericDAO<GeolocationProperty, Inte
 	private static final Logger LOG = LoggerFactory.getLogger(GeolocationPropertyDao.class);
 
 	@SuppressWarnings("unchecked")
-	public List<Integer> getGeolocationIdsByPropertyTypeAndValue(Integer typeId, String value) throws MiddlewareQueryException {
+	public List<Integer> getGeolocationIdsByPropertyTypeAndValue(final Integer typeId, final String value) throws MiddlewareQueryException {
 		try {
-			Criteria criteria = this.getSession().createCriteria(this.getPersistentClass());
+			final Criteria criteria = this.getSession().createCriteria(this.getPersistentClass());
 			criteria.add(Restrictions.eq("typeId", typeId));
 			criteria.add(Restrictions.eq("value", value));
 			criteria.setProjection(Projections.property("geolocation.locationId"));
 
 			return criteria.list();
 
-		} catch (HibernateException e) {
+		} catch (final HibernateException e) {
 			this.logAndThrowException(
 					"Error at getIdsByPropertyTypeAndValue=" + typeId + ", " + value + " query on GeolocationDao: " + e.getMessage(), e);
 		}
@@ -57,38 +57,38 @@ public class GeolocationPropertyDao extends GenericDAO<GeolocationProperty, Inte
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<ValueReference> getDistinctPropertyValues(int stdVarId) throws MiddlewareQueryException {
-		List<ValueReference> results = new ArrayList<ValueReference>();
+	public List<ValueReference> getDistinctPropertyValues(final int stdVarId) throws MiddlewareQueryException {
+		final List<ValueReference> results = new ArrayList<ValueReference>();
 		try {
-			String sql = "SELECT DISTINCT value FROM nd_geolocationprop WHERE type_id = :stdVarId ";
-			SQLQuery query = this.getSession().createSQLQuery(sql);
+			final String sql = "SELECT DISTINCT value FROM nd_geolocationprop WHERE type_id = :stdVarId ";
+			final SQLQuery query = this.getSession().createSQLQuery(sql);
 			query.setParameter("stdVarId", stdVarId);
 
-			List<String> list = query.list();
+			final List<String> list = query.list();
 			if (list != null && !list.isEmpty()) {
-				for (String row : list) {
+				for (final String row : list) {
 					results.add(new ValueReference(row, row));
 				}
 			}
 
-		} catch (HibernateException e) {
+		} catch (final HibernateException e) {
 			this.logAndThrowException(
 					"Error at getDistinctPropertyValues=" + stdVarId + " query on GeolocationPropertyDao: " + e.getMessage(), e);
 		}
 		return results;
 	}
 
-	public String getGeolocationPropValue(int stdVarId, int studyId) throws MiddlewareQueryException {
+	public String getGeolocationPropValue(final int stdVarId, final int studyId) throws MiddlewareQueryException {
 		try {
-			StringBuilder sql =
+			final StringBuilder sql =
 					new StringBuilder().append("SELECT value ").append("FROM nd_experiment e ")
 							.append("INNER JOIN nd_geolocationprop gp ON gp.nd_geolocation_id = e.nd_geolocation_id ")
 							.append("WHERE e.project_id = :projectId AND gp.type_id = :stdVarId ORDER BY e.nd_geolocation_id ");
-			SQLQuery query = this.getSession().createSQLQuery(sql.toString());
+			final SQLQuery query = this.getSession().createSQLQuery(sql.toString());
 			query.setParameter("projectId", studyId);
 			query.setParameter("stdVarId", stdVarId);
 			return (String) query.uniqueResult();
-		} catch (HibernateException e) {
+		} catch (final HibernateException e) {
 			this.logAndThrowException(
 					"Error at getGeolocationPropValue=" + stdVarId + " query on GeolocationPropertyDao: " + e.getMessage(), e);
 		}
@@ -96,27 +96,26 @@ public class GeolocationPropertyDao extends GenericDAO<GeolocationProperty, Inte
 	}
 
 	@SuppressWarnings("unchecked")
-	public String getValueOfTrialInstance(int datasetId, int typeId, String trialInstance) throws MiddlewareQueryException {
+	public String getValueOfTrialInstance(final int datasetId, final int typeId, final String trialInstance) throws MiddlewareQueryException {
 		try {
-			StringBuilder sql =
+			final StringBuilder sql =
 					new StringBuilder().append("SELECT gp.value FROM nd_geolocationprop gp ")
 							.append(" INNER JOIN nd_geolocation g ON g.nd_geolocation_id = gp.nd_geolocation_id ")
 							.append("   AND g.description = ").append(trialInstance)
 							.append(" INNER JOIN nd_experiment e ON e.nd_geolocation_id = g.nd_geolocation_id ")
-							.append(" INNER JOIN nd_experiment_project ep ON ep.nd_experiment_id = e.nd_experiment_id ")
-							.append("   AND ep.project_id = ").append(datasetId).append(" WHERE gp.type_id = ").append(typeId);
+							.append(" WHERE gp.type_id = ").append(typeId).append("   AND e.project_id = ").append(datasetId);
 
-			SQLQuery query = this.getSession().createSQLQuery(sql.toString());
+			final SQLQuery query = this.getSession().createSQLQuery(sql.toString());
 			return (String) query.uniqueResult();
 
-		} catch (HibernateException e) {
+		} catch (final HibernateException e) {
 			this.logAndThrowException(
 					"Error at getValueOfTrialInstance=" + datasetId + " query on GeolocationPropertyDao: " + e.getMessage(), e);
 		}
 		return null;
 	}
 
-	public void deleteGeolocationPropertyValueInProject(int studyId, int termId) throws MiddlewareQueryException {
+	public void deleteGeolocationPropertyValueInProject(final int studyId, final int termId) throws MiddlewareQueryException {
 		try {
 			// Please note we are manually flushing because non hibernate based deletes and updates causes the Hibernate session to get out of synch with
 			// underlying database. Thus flushing to force Hibernate to synchronize with the underlying database before the delete
@@ -126,7 +125,7 @@ public class GeolocationPropertyDao extends GenericDAO<GeolocationProperty, Inte
 			executeDeleteStatement(studyId, termId, "pr.object_project_id");
 			executeDeleteStatement(studyId, termId, "pr.subject_project_id");
 
-		} catch (HibernateException e) {
+		} catch (final HibernateException e) {
 			this.logAndThrowException("Error at deleteGeolocationPropertyValueInProject=" + studyId + ", " + termId
 					+ " query on GeolocationPropertyDao: " + e.getMessage(), e);
 		}
@@ -135,9 +134,8 @@ public class GeolocationPropertyDao extends GenericDAO<GeolocationProperty, Inte
 	private void executeDeleteStatement(final int studyId,final int termId,final String joinCriteria) {
 		final StringBuilder sql1 = new StringBuilder().append("Delete ngp.* FROM nd_geolocationprop ngp "
 				+ "INNER JOIN nd_experiment e ON ngp.nd_geolocation_id = e.nd_geolocation_id AND ngp.type_id = :termId "
-				+ "INNER JOIN nd_experiment_project ep ON ep.nd_experiment_id = e.nd_experiment_id "
 				+ "INNER JOIN project_relationship pr ON pr.type_id = :belongsToTypeId "
-				+ "			  AND ").append(joinCriteria).append(" = ep.project_id "
+				+ "			  AND ").append(joinCriteria).append(" = e.project_id "
 				+ "WHERE pr.object_project_id = :studyId");
 
 		final SQLQuery sqlQuery1 = this.getSession().createSQLQuery(sql1.toString());
@@ -150,21 +148,21 @@ public class GeolocationPropertyDao extends GenericDAO<GeolocationProperty, Inte
 
 	public Map<String, String> getGeolocationPropsAndValuesByStudy(final Integer studyId) throws MiddlewareQueryException {
 		Preconditions.checkNotNull(studyId);
-		Map<String, String> geoProperties = new HashMap<>();
-		StringBuilder sql = new StringBuilder().append("SELECT  ").append("    cv.definition as name, geo.value as value ").append("FROM ")
+		final Map<String, String> geoProperties = new HashMap<>();
+		final StringBuilder sql = new StringBuilder().append("SELECT  ").append("    cv.definition as name, geo.value as value ").append("FROM ")
 				.append("    nd_geolocationprop geo ").append("        INNER JOIN ")
 				.append("    cvterm cv ON (cv.cvterm_id = geo.type_id) ").append("WHERE ").append("    geo.nd_geolocation_id = :studyId ")
 				.append("        AND geo.type_id NOT IN (8371, 8190, 8070, 8180) ");
 		try {
-			Query query =
+			final Query query =
 					this.getSession().createSQLQuery(sql.toString()).addScalar("name").addScalar("value").setParameter("studyId", studyId);
-			List<Object> results = query.list();
-			for (Object obj : results) {
-				Object[] row = (Object[]) obj;
+			final List<Object> results = query.list();
+			for (final Object obj : results) {
+				final Object[] row = (Object[]) obj;
 				geoProperties.put((String) row[0], (String) row[1]);
 			}
 			return geoProperties;
-		} catch (MiddlewareQueryException e) {
+		} catch (final MiddlewareQueryException e) {
 			final String message = "Error with getGeolocationPropsAndValuesByStudy() query from studyId: " + studyId;
 			GeolocationPropertyDao.LOG.error(message, e);
 			throw new MiddlewareQueryException(message, e);
