ca72a97dd35a144d25c3a7b55503af23d665e1a9
Clarysabel Tovar <clarysabel@leafnode.io>
2018-10-24 13:13:48 -0300

    removing unused import Issue: IBP-2014 Reviewer: none

--- src/main/java/org/generationcp/middleware/service/api/dataset/DatasetServiceImpl.java
+++ src/main/java/org/generationcp/middleware/service/api/dataset/DatasetServiceImpl.java
@@ -1,30 +1,13 @@
 package org.generationcp.middleware.service.api.dataset;
 
 import org.generationcp.middleware.dao.dms.DmsProjectDao;
-import org.generationcp.middleware.domain.dms.DMSVariableType;
-import org.generationcp.middleware.domain.dms.PhenotypicType;
-import org.generationcp.middleware.domain.dms.Study;
-import org.generationcp.middleware.domain.dms.VariableTypeList;
-import org.generationcp.middleware.domain.oms.TermId;
-import org.generationcp.middleware.domain.ontology.Variable;
-import org.generationcp.middleware.domain.ontology.VariableType;
-import org.generationcp.middleware.exceptions.MiddlewareException;
-import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 import org.generationcp.middleware.manager.DaoFactory;
-import org.generationcp.middleware.manager.OntologyDataManagerImpl;
-import org.generationcp.middleware.manager.api.OntologyDataManager;
 import org.generationcp.middleware.manager.ontology.OntologyVariableDataManagerImpl;
 import org.generationcp.middleware.manager.ontology.api.OntologyVariableDataManager;
-import org.generationcp.middleware.pojos.dms.DmsProject;
-import org.generationcp.middleware.pojos.dms.ProjectProperty;
-import org.generationcp.middleware.pojos.dms.ProjectRelationship;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by clarysabel on 10/22/18.
@@ -52,7 +35,7 @@ public class DatasetServiceImpl implements DatasetService {
 
 	public Integer generateSubObservationDataset (final Integer studyId, final String datasetName, final Integer datasetTypeId, final Integer[] instanceIds,
 			final Integer observationUnitVariableId, final Integer numberOfSubObservationUnits) {
-		
+
 		return null;
 	}
 
