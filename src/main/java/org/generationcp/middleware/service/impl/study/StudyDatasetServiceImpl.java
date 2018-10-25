0b0fbd3900312e4816e9f41c4c9ed90a84d20444
Darla Hermo <darla@leafnode.io>
2018-10-25 10:20:17 +0800

    Use DaoFactory for using PhenotyeDao and DmsProjectDao
    IBP-2002

--- src/main/java/org/generationcp/middleware/service/impl/study/StudyDatasetServiceImpl.java
+++ src/main/java/org/generationcp/middleware/service/impl/study/StudyDatasetServiceImpl.java
@@ -3,26 +3,28 @@ package org.generationcp.middleware.service.impl.study;
 import java.util.List;
 
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
+import org.generationcp.middleware.manager.DaoFactory;
 import org.generationcp.middleware.pojos.dms.DmsProject;
-import org.generationcp.middleware.service.Service;
 import org.generationcp.middleware.service.api.study.StudyDatasetService;
 
 
-public class StudyDatasetServiceImpl extends Service implements StudyDatasetService {
+public class StudyDatasetServiceImpl implements StudyDatasetService {
+	
+	private DaoFactory daoFactory;
 	
 
 	public StudyDatasetServiceImpl(final HibernateSessionProvider sessionProvider) {
-		super(sessionProvider);
+		this.daoFactory = new DaoFactory(sessionProvider);
 	}
 
 	@Override
 	public long countPhenotypesForDataset(final Integer datasetId, final List<Integer> traitIds) {
-		return this.getPhenotypeDao().countPhenotypesForDataset(datasetId, traitIds);
+		return this.daoFactory.getPhenotypeDAO().countPhenotypesForDataset(datasetId, traitIds);
 	}
 
 	@Override
 	public boolean datasetExists(final Integer studyId, final Integer datasetId) {
-		final List<DmsProject> studyDatasets = this.getDmsProjectDao().getDatasetsByStudy(studyId);
+		final List<DmsProject> studyDatasets = this.daoFactory.getDmsProjectDAO().getDatasetsByStudy(studyId);
 		if (studyDatasets != null) {
 			for (final DmsProject dataset : studyDatasets) {
 				if (dataset.getProjectId().equals(datasetId)) {
