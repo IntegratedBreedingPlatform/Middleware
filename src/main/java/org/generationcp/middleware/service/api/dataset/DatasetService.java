458f61f683d50a287a435a5fc24450d64350d582
Aldrin Batac <aldrin.batac@leafnode.io>
2018-11-05 15:03:04 +0800

    Added method to update the value, status and cValueId of Phenotype.
    IBP-2005

--- src/main/java/org/generationcp/middleware/service/api/dataset/DatasetService.java
+++ src/main/java/org/generationcp/middleware/service/api/dataset/DatasetService.java
@@ -3,15 +3,19 @@ package org.generationcp.middleware.service.api.dataset;
 import java.util.List;
 
 import org.generationcp.middleware.domain.ontology.VariableType;
+import org.generationcp.middleware.pojos.dms.Phenotype;
 
 public interface DatasetService {
-	
+
 	long countPhenotypes(Integer datasetId, List<Integer> traitIds);
-	
+
 	void addVariable(Integer datasetId, Integer variableId, VariableType type, String alias);
 	
 	void removeVariables(Integer datasetId, List<Integer> variableIds);
 	
 	boolean isValidObservationUnit(Integer datasetId, Integer observationUnitId);
+
+	Phenotype updatePhenotype(
+		Integer observationUnitId, Integer observationId, Integer categoricalValueId, String value, String valueStatus);
 
 }
--- src/main/java/org/generationcp/middleware/service/impl/dataset/DatasetServiceImpl.java
+++ src/main/java/org/generationcp/middleware/service/impl/dataset/DatasetServiceImpl.java
@@ -2,19 +2,21 @@ package org.generationcp.middleware.service.impl.dataset;
 
 import java.util.List;
 
+
+import org.apache.commons.lang3.EnumUtils;
+import org.generationcp.middleware.dao.dms.PhenotypeDao;
 import org.generationcp.middleware.dao.dms.ProjectPropertyDao;
 import org.generationcp.middleware.domain.ontology.VariableType;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 import org.generationcp.middleware.manager.DaoFactory;
 import org.generationcp.middleware.pojos.dms.DmsProject;
+import org.generationcp.middleware.pojos.dms.Phenotype;
 import org.generationcp.middleware.pojos.dms.ProjectProperty;
 import org.generationcp.middleware.service.api.dataset.DatasetService;
 
-
 public class DatasetServiceImpl implements DatasetService {
-	
+
 	private DaoFactory daoFactory;
-	
 
 	public DatasetServiceImpl(final HibernateSessionProvider sessionProvider) {
 		this.daoFactory = new DaoFactory(sessionProvider);
@@ -23,6 +25,21 @@ public class DatasetServiceImpl implements DatasetService {
 	@Override
 	public long countPhenotypes(final Integer datasetId, final List<Integer> traitIds) {
 		return this.daoFactory.getPhenotypeDAO().countPhenotypesForDataset(datasetId, traitIds);
+	}
+
+	@Override
+	public Phenotype updatePhenotype(
+		final Integer observationUnitId, final Integer observationId, final Integer categoricalValueId, final String value,
+		final String valueStatus) {
+
+		final PhenotypeDao phenotypeDao = this.daoFactory.getPhenotypeDAO();
+		final Phenotype phenotype = phenotypeDao.getByObservationUnitIdAndObservableId(observationUnitId, observationId);
+		phenotype.setValue(value);
+		phenotype.setcValue(categoricalValueId == 0 ? null : categoricalValueId);
+		phenotype.setValueStatus(EnumUtils.getEnum(Phenotype.ValueStatus.class, valueStatus));
+		phenotypeDao.update(phenotype);
+		return phenotype;
+
 	}
 
 	@Override
@@ -49,7 +66,7 @@ public class DatasetServiceImpl implements DatasetService {
 	public boolean isValidObservationUnit(final Integer datasetId, final Integer observationUnitId) {
 		return this.daoFactory.getExperimentDAO().isValidExperiment(datasetId, observationUnitId);
 	}
-	
+
 	protected void setDaoFactory(DaoFactory daoFactory) {
 		this.daoFactory = daoFactory;
 	}
