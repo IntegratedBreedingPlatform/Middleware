42f23f8bc6b20c7e700be15021adabcf26af8b20
Vanina Maletta <vmaletta@droptek.com>
2018-11-22 16:51:37 -0300

    Add import Add getExperimentByObsUnitId

--- src/main/java/org/generationcp/middleware/service/api/dataset/DatasetService.java
+++ src/main/java/org/generationcp/middleware/service/api/dataset/DatasetService.java
@@ -53,6 +53,7 @@ public interface DatasetService {
 		Integer studyId, Integer datasetId,
 		final String programUUID, ObservationUnitImportResult observationUnitImportResult);
 
-	List<String> importDataset(Integer datasetId, ObservationUnitImportResult observationUnitImportResult);
+	ObservationUnitImportResult importDataset(Integer datasetId, ObservationUnitImportResult observationUnitImportResult,
+		final String programUUID);
 
 }
