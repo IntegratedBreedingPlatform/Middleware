eeff5cf332b7b67280766066e5bc9700e3712b5f
Aldrin Batac <aldrin.batac@leafnode.io>
2019-05-29 11:15:11 +0800

    Consolidate Common's DesignType into ExperimentDesignType.
    IBP-2659

--- src/main/java/org/generationcp/middleware/domain/dms/ExperimentDesignType.java
+++ src/main/java/org/generationcp/middleware/domain/dms/ExperimentDesignType.java
@@ -11,30 +11,31 @@ public class ExperimentDesignType {
 	// Constants for well known (e.g. BreedingView) design types.
 	public static final ExperimentDesignType RANDOMIZED_COMPLETE_BLOCK = new ExperimentDesignType(0, "Randomized Complete Block Design",
 		TermId.RANDOMIZED_COMPLETE_BLOCK.getId(), 0,
-		"randomizedCompleteBlockParams.html", 0, 0);
+		"randomizedCompleteBlockParams.html", 0, 0, "Randomized block design");
 
 	public static final ExperimentDesignType RESOLVABLE_INCOMPLETE_BLOCK =
 		new ExperimentDesignType(1, "Resolvable Incomplete Block Design", TermId.RESOLVABLE_INCOMPLETE_BLOCK.getId(),
 			TermId.RESOLVABLE_INCOMPLETE_BLOCK_LATIN.getId(),
-			"incompleteBlockParams.html", 0, 0);
+			"incompleteBlockParams.html", 0, 0, "Resolvable incomplete block design");
 
 	public static final ExperimentDesignType ROW_COL =
 		new ExperimentDesignType(2, "Row-and-Column", TermId.RESOLVABLE_INCOMPLETE_ROW_COL.getId(),
-			TermId.RESOLVABLE_INCOMPLETE_ROW_COL_LATIN.getId(), "rowAndColumnParams.html", 0, 0);
+			TermId.RESOLVABLE_INCOMPLETE_ROW_COL_LATIN.getId(), "rowAndColumnParams.html", 0, 0, "Resolvable row-column design");
 
 	public static final ExperimentDesignType
-		CUSTOM_IMPORT = new ExperimentDesignType(3, "Custom Import Design", TermId.OTHER_DESIGN.getId(), 0, null, 0, 0);
+		CUSTOM_IMPORT = new ExperimentDesignType(3, "Custom Import Design", TermId.OTHER_DESIGN.getId(), 0, null, 0, 0, "");
 
+	// Augmented design is just a variation of the Incomplete Block Design type.
 	public static final ExperimentDesignType AUGMENTED_RANDOMIZED_BLOCK =
 		new ExperimentDesignType(4, "Augmented Randomized Block design", TermId.AUGMENTED_RANDOMIZED_BLOCK.getId(), 0,
-			"augmentedRandomizedBlockParams.html", 0, 0);
+			"augmentedRandomizedBlockParams.html", 0, 0, "Incomplete block design");
 
 	public static final ExperimentDesignType
 		ENTRY_LIST_ORDER = new ExperimentDesignType(5, "Entry list order", TermId.ENTRY_LIST_ORDER.getId(), 0,
-		"entryListOrderParams.html", 0, 0);
+		"entryListOrderParams.html", 0, 0, "");
 
 	public static final ExperimentDesignType P_REP = new ExperimentDesignType(6, "P-Rep Design", TermId.P_REP.getId(), 0,
-		"pRepParams.html", 0, 0);
+		"pRepParams.html", 0, 0, "P-rep design");
 
 	private static final List<ExperimentDesignType> values = ImmutableList
 		.of(RANDOMIZED_COMPLETE_BLOCK, RESOLVABLE_INCOMPLETE_BLOCK, ROW_COL, CUSTOM_IMPORT, AUGMENTED_RANDOMIZED_BLOCK, ENTRY_LIST_ORDER,
@@ -52,6 +53,9 @@ public class ExperimentDesignType {
 	private Integer repNo;
 	private Integer totalNoOfEntries;
 	private String templateName;
+
+	// The name of the design that the system sends to Breeding View. This name is constant and defined by the Breeding View application.
+	private String bvDesignName;
 
 	public ExperimentDesignType() {
 		// do nothing
@@ -80,13 +84,25 @@ public class ExperimentDesignType {
 			TermId.RESOLVABLE_INCOMPLETE_ROW_COL_LATIN.getId() == termId;
 	}
 
+	public static ExperimentDesignType getExperimentDesignTypeByBVDesignName(final String bvDesignName) {
+
+		for (final ExperimentDesignType experimentDesignType : values) {
+			if (experimentDesignType.getBvDesignName().equals(bvDesignName)) {
+				return experimentDesignType;
+			}
+		}
+
+		// if there is no match found in enum constants, throw an exception
+		throw new IllegalArgumentException("No ExperimentDesignType constant found for name: \"" + bvDesignName + "\"");
+	}
+
 	public ExperimentDesignType(final int id) {
 		this.id = id;
 	}
 
 	public ExperimentDesignType(
 		final Integer id, final String name, final Integer termId, final Integer termIdLatinized, final String params,
-		final Integer noOfReps, final Integer noOfEntries) {
+		final Integer noOfReps, final Integer noOfEntries, final String bvDesignName) {
 		this.id = id;
 		this.name = name;
 		this.termId = termId;
@@ -95,6 +111,7 @@ public class ExperimentDesignType {
 		this.repNo = noOfReps;
 		this.totalNoOfEntries = noOfEntries;
 		this.templateName = name.concat(".csv");
+		this.bvDesignName = bvDesignName;
 	}
 
 	public Integer getId() {
@@ -155,6 +172,10 @@ public class ExperimentDesignType {
 
 	public void setTermIdLatinized(final Integer termIdLatinized) {
 		this.termIdLatinized = termIdLatinized;
+	}
+
+	public String getBvDesignName() {
+		return this.bvDesignName;
 	}
 
 }
--- src/test/java/org/generationcp/middleware/domain/dms/ExperimentDesignTypeTest.java
+++ src/test/java/org/generationcp/middleware/domain/dms/ExperimentDesignTypeTest.java
@@ -6,50 +6,79 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(MockitoJUnitRunner.class)
 public class ExperimentDesignTypeTest {
 
 	@Test
 	public void testGetTermIdByDesignTypeId() {
-		Assert.assertEquals(TermId.RANDOMIZED_COMPLETE_BLOCK.getId(), ExperimentDesignType.getTermIdByDesignTypeId(0, false));
-		Assert.assertEquals(TermId.RESOLVABLE_INCOMPLETE_BLOCK.getId(), ExperimentDesignType.getTermIdByDesignTypeId(1, false));
-		Assert.assertEquals(TermId.RESOLVABLE_INCOMPLETE_BLOCK_LATIN.getId(), ExperimentDesignType.getTermIdByDesignTypeId(1, true));
-		Assert.assertEquals(TermId.RESOLVABLE_INCOMPLETE_ROW_COL.getId(), ExperimentDesignType.getTermIdByDesignTypeId(2, false));
-		Assert.assertEquals(TermId.RESOLVABLE_INCOMPLETE_ROW_COL_LATIN.getId(), ExperimentDesignType.getTermIdByDesignTypeId(2, true));
-		Assert.assertEquals(TermId.OTHER_DESIGN.getId(), ExperimentDesignType.getTermIdByDesignTypeId(3, false));
-		Assert.assertEquals(TermId.AUGMENTED_RANDOMIZED_BLOCK.getId(), ExperimentDesignType.getTermIdByDesignTypeId(4, false));
-		Assert.assertEquals(TermId.ENTRY_LIST_ORDER.getId(), ExperimentDesignType.getTermIdByDesignTypeId(5, false));
-		Assert.assertEquals(TermId.P_REP.getId(), ExperimentDesignType.getTermIdByDesignTypeId(6, false));
-		Assert.assertEquals(0, ExperimentDesignType.getTermIdByDesignTypeId(7, false));
+		assertEquals(TermId.RANDOMIZED_COMPLETE_BLOCK.getId(), ExperimentDesignType.getTermIdByDesignTypeId(0, false));
+		assertEquals(TermId.RESOLVABLE_INCOMPLETE_BLOCK.getId(), ExperimentDesignType.getTermIdByDesignTypeId(1, false));
+		assertEquals(TermId.RESOLVABLE_INCOMPLETE_BLOCK_LATIN.getId(), ExperimentDesignType.getTermIdByDesignTypeId(1, true));
+		assertEquals(TermId.RESOLVABLE_INCOMPLETE_ROW_COL.getId(), ExperimentDesignType.getTermIdByDesignTypeId(2, false));
+		assertEquals(TermId.RESOLVABLE_INCOMPLETE_ROW_COL_LATIN.getId(), ExperimentDesignType.getTermIdByDesignTypeId(2, true));
+		assertEquals(TermId.OTHER_DESIGN.getId(), ExperimentDesignType.getTermIdByDesignTypeId(3, false));
+		assertEquals(TermId.AUGMENTED_RANDOMIZED_BLOCK.getId(), ExperimentDesignType.getTermIdByDesignTypeId(4, false));
+		assertEquals(TermId.ENTRY_LIST_ORDER.getId(), ExperimentDesignType.getTermIdByDesignTypeId(5, false));
+		assertEquals(TermId.P_REP.getId(), ExperimentDesignType.getTermIdByDesignTypeId(6, false));
+		assertEquals(0, ExperimentDesignType.getTermIdByDesignTypeId(7, false));
 	}
 
 	@Test
 	public void testGetDesignTypeItemByTermId() {
-		Assert.assertEquals(
+		assertEquals(
 			ExperimentDesignType.RANDOMIZED_COMPLETE_BLOCK,
 			ExperimentDesignType.getDesignTypeItemByTermId(TermId.RANDOMIZED_COMPLETE_BLOCK.getId()));
-		Assert.assertEquals(
+		assertEquals(
 			ExperimentDesignType.RESOLVABLE_INCOMPLETE_BLOCK,
 			ExperimentDesignType.getDesignTypeItemByTermId(TermId.RESOLVABLE_INCOMPLETE_BLOCK.getId()));
-		Assert.assertEquals(
+		assertEquals(
 			ExperimentDesignType.RESOLVABLE_INCOMPLETE_BLOCK,
 			ExperimentDesignType.getDesignTypeItemByTermId(TermId.RESOLVABLE_INCOMPLETE_BLOCK_LATIN.getId()));
-		Assert.assertEquals(
+		assertEquals(
 			ExperimentDesignType.ROW_COL,
 			ExperimentDesignType.getDesignTypeItemByTermId(TermId.RESOLVABLE_INCOMPLETE_ROW_COL.getId()));
-		Assert.assertEquals(
+		assertEquals(
 			ExperimentDesignType.ROW_COL,
 			ExperimentDesignType.getDesignTypeItemByTermId(TermId.RESOLVABLE_INCOMPLETE_ROW_COL_LATIN.getId()));
-		Assert
-			.assertEquals(ExperimentDesignType.CUSTOM_IMPORT, ExperimentDesignType.getDesignTypeItemByTermId(TermId.OTHER_DESIGN.getId()));
-		Assert.assertEquals(
+		assertEquals(ExperimentDesignType.CUSTOM_IMPORT, ExperimentDesignType.getDesignTypeItemByTermId(TermId.OTHER_DESIGN.getId()));
+		assertEquals(
 			ExperimentDesignType.AUGMENTED_RANDOMIZED_BLOCK,
 			ExperimentDesignType.getDesignTypeItemByTermId(TermId.AUGMENTED_RANDOMIZED_BLOCK.getId()));
-		Assert.assertEquals(
+		assertEquals(
 			ExperimentDesignType.ENTRY_LIST_ORDER,
 			ExperimentDesignType.getDesignTypeItemByTermId(TermId.ENTRY_LIST_ORDER.getId()));
-		Assert.assertEquals(ExperimentDesignType.P_REP, ExperimentDesignType.getDesignTypeItemByTermId(TermId.P_REP.getId()));
-		Assert.assertEquals(null, ExperimentDesignType.getDesignTypeItemByTermId(1010101));
+		assertEquals(ExperimentDesignType.P_REP, ExperimentDesignType.getDesignTypeItemByTermId(TermId.P_REP.getId()));
+		assertEquals(null, ExperimentDesignType.getDesignTypeItemByTermId(1010101));
+	}
+
+	@Test
+	public void testDesignTypeFromString() {
+
+		assertEquals(
+			ExperimentDesignType.RANDOMIZED_COMPLETE_BLOCK,
+			ExperimentDesignType.getExperimentDesignTypeByBVDesignName(ExperimentDesignType.RANDOMIZED_COMPLETE_BLOCK.getBvDesignName()));
+		assertEquals(
+			ExperimentDesignType.RESOLVABLE_INCOMPLETE_BLOCK,
+			ExperimentDesignType.getExperimentDesignTypeByBVDesignName(ExperimentDesignType.RESOLVABLE_INCOMPLETE_BLOCK.getBvDesignName()));
+		assertEquals(
+			ExperimentDesignType.ROW_COL,
+			ExperimentDesignType.getExperimentDesignTypeByBVDesignName(ExperimentDesignType.ROW_COL.getBvDesignName()));
+		assertEquals(
+			ExperimentDesignType.P_REP,
+			ExperimentDesignType.getExperimentDesignTypeByBVDesignName(ExperimentDesignType.P_REP.getBvDesignName()));
+		assertEquals(
+			ExperimentDesignType.AUGMENTED_RANDOMIZED_BLOCK,
+			ExperimentDesignType.getExperimentDesignTypeByBVDesignName(ExperimentDesignType.AUGMENTED_RANDOMIZED_BLOCK.getBvDesignName()));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testDesignTypeFromStringException() {
+
+		// This will throw an exception because an empty string doesn't match any of the constants in ExperimentDesignType.
+		ExperimentDesignType.getExperimentDesignTypeByBVDesignName("Any Name");
+
 	}
 
 }
