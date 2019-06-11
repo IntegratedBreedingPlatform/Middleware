c2e507affe7a6d615d61e84de40edf50d0c74037
Aldrin Batac <aldrin.batac@leafnode.io>
2019-06-11 14:02:50 +0800

    Added integration tests for ExperimentPropertyDao.
    IBP-2716

--- src/test/java/org/generationcp/middleware/dao/dms/ExperimentPropertyDaoIntegrationTest.java
+++ src/test/java/org/generationcp/middleware/dao/dms/ExperimentPropertyDaoIntegrationTest.java
@@ -1,22 +1,32 @@
 package org.generationcp.middleware.dao.dms;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.generationcp.middleware.IntegrationTestBase;
 import org.generationcp.middleware.data.initializer.DMSVariableTestDataInitializer;
 import org.generationcp.middleware.domain.dms.ExperimentType;
 import org.generationcp.middleware.domain.dms.ExperimentValues;
 import org.generationcp.middleware.domain.dms.VariableList;
+import org.generationcp.middleware.domain.fieldbook.FieldMapDatasetInfo;
+import org.generationcp.middleware.domain.fieldbook.FieldMapInfo;
+import org.generationcp.middleware.domain.fieldbook.FieldMapLabel;
+import org.generationcp.middleware.domain.fieldbook.FieldMapTrialInstanceInfo;
+import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.domain.ontology.DataType;
 import org.generationcp.middleware.domain.ontology.VariableType;
 import org.generationcp.middleware.enumeration.DatasetTypeEnum;
 import org.generationcp.middleware.operation.saver.ExperimentModelSaver;
 import org.generationcp.middleware.pojos.dms.DmsProject;
+import org.generationcp.middleware.pojos.dms.ExperimentModel;
+import org.generationcp.middleware.pojos.dms.Geolocation;
 import org.generationcp.middleware.pojos.workbench.CropType;
 import org.generationcp.middleware.utils.test.IntegrationTestDataInitializer;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ExperimentPropertyDaoIntegrationTest extends IntegrationTestBase {
 
@@ -58,19 +68,74 @@ public class ExperimentPropertyDaoIntegrationTest extends IntegrationTestBase {
 		//Save the experiment
 		this.experimentModelSaver.addOrUpdateExperiment(new CropType(), 1, ExperimentType.STUDY_INFORMATION, values);
 		final List<String> treatmentFactorValues = this.experimentPropertyDao.getTreatmentFactorValues(1001, 1002, 1);
-		Assert.assertEquals(1, treatmentFactorValues.size());
-		Assert.assertEquals("Value", treatmentFactorValues.get(0));
+		assertEquals(1, treatmentFactorValues.size());
+		assertEquals("Value", treatmentFactorValues.get(0));
 	}
 
 	@Test
 	public void testGetFieldMapLabels() {
 
-		this.experimentPropertyDao.getFieldMapLabels(this.study.getProjectId());
+		final Geolocation geolocation = this.testDataInitializer.createTestGeolocation("1", 101);
+		this.testDataInitializer.addGeolocationProp(geolocation, TermId.SEASON_VAR.getId(), "10101", 1);
+		this.testDataInitializer.addGeolocationProp(geolocation, TermId.TRIAL_LOCATION.getId(), "India", 2);
+
+		final ExperimentModel experimentModel =
+			this.testDataInitializer.createTestExperiment(this.plot, geolocation, TermId.PLOT_EXPERIMENT.getId(), "1", null);
+		this.testDataInitializer.createTestStock(experimentModel);
+		this.testDataInitializer.addExperimentProp(experimentModel, TermId.REP_NO.getId(), RandomStringUtils.randomNumeric(5), 2);
+		this.testDataInitializer.addExperimentProp(experimentModel, TermId.BLOCK_NO.getId(), RandomStringUtils.randomNumeric(5), 3);
+		this.testDataInitializer.addExperimentProp(experimentModel, TermId.RANGE_NO.getId(), RandomStringUtils.randomNumeric(5), 4);
+		this.testDataInitializer.addExperimentProp(experimentModel, TermId.COLUMN_NO.getId(), RandomStringUtils.randomNumeric(5), 5);
+
+		// Need to flush session to sync with underlying database before querying
+		this.sessionProvder.getSession().flush();
+
+		final List<FieldMapDatasetInfo> fieldMapDatasetInfos = this.experimentPropertyDao.getFieldMapLabels(this.study.getProjectId());
+		assertEquals(1, fieldMapDatasetInfos.size());
+		assertEquals(1, fieldMapDatasetInfos.get(0).getTrialInstances().size());
+		final FieldMapTrialInstanceInfo fieldMapTrialInstanceInfo = fieldMapDatasetInfos.get(0).getTrialInstances().get(0);
+		final FieldMapLabel fieldMapLabel = fieldMapTrialInstanceInfo.getFieldMapLabel(experimentModel.getNdExperimentId());
+
+		assertEquals("India", fieldMapTrialInstanceInfo.getSiteName());
+		assertEquals(experimentModel.getNdExperimentId(), fieldMapLabel.getExperimentId());
+		assertNotNull(fieldMapLabel.getObsUnitId());
+		assertEquals(1, fieldMapLabel.getEntryNumber().intValue());
+		assertEquals("Germplasm 1", fieldMapLabel.getGermplasmName());
+		assertNotNull(fieldMapLabel.getRep());
+		assertNotNull(fieldMapLabel.getBlockNo());
+		assertNotNull(fieldMapLabel.getColumn());
+		assertNotNull(fieldMapLabel.getRange());
+		assertNotNull(fieldMapLabel.getGid());
+		assertEquals("General", fieldMapLabel.getSeason().getLabel());
+		assertEquals("Study1", fieldMapLabel.getStudyName());
+		assertEquals(1, fieldMapLabel.getPlotNo().intValue());
 
 	}
 
 	@Test
 	public void testGetAllFieldMapsInBlockByTrialInstanceId() {
+
+		final Geolocation geolocation = this.testDataInitializer.createTestGeolocation("1", 101);
+		this.testDataInitializer.addGeolocationProp(geolocation, TermId.SEASON_VAR.getId(), "10101", 1);
+		this.testDataInitializer.addGeolocationProp(geolocation, TermId.TRIAL_LOCATION.getId(), "India", 2);
+		this.testDataInitializer.addGeolocationProp(geolocation, TermId.BLOCK_ID.getId(), "1234", 3);
+
+		final ExperimentModel experimentModel =
+			this.testDataInitializer.createTestExperiment(this.plot, geolocation, TermId.PLOT_EXPERIMENT.getId(), "1", null);
+		this.testDataInitializer.createTestStock(experimentModel);
+
+		// Need to flush session to sync with underlying database before querying
+		this.sessionProvder.getSession().flush();
+
+		final List<FieldMapInfo> fieldMapInfos1 = this.experimentPropertyDao
+			.getAllFieldMapsInBlockByTrialInstanceId(this.study.getProjectId(), geolocation.getLocationId(), 1234);
+
+		assertEquals(1, fieldMapInfos1.size());
+		assertEquals(1, fieldMapInfos1.get(0).getDatasets().size());
+
+		final List<FieldMapInfo> fieldMapInfos2 = this.experimentPropertyDao
+			.getAllFieldMapsInBlockByTrialInstanceId(this.study.getProjectId(), geolocation.getLocationId(), 9999);
+		assertEquals(0, fieldMapInfos2.size());
 
 	}
 
