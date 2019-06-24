3614474b1334c77d3fd161b671f8cd074169b460
mariebawanan <bawananmarieflor@gmail.com>
2019-06-24 10:47:53 +0800

    Add name in dataset type enum
    IBP-2797

--- src/main/java/org/generationcp/middleware/enumeration/DatasetTypeEnum.java
+++ src/main/java/org/generationcp/middleware/enumeration/DatasetTypeEnum.java
@@ -2,20 +2,21 @@ package org.generationcp.middleware.enumeration;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
 
 public enum DatasetTypeEnum {
 
-	STUDY_CONDITIONS(1),
-	MEANS_DATA(2),
-	SUMMARY_DATA(3),
-	PLOT_DATA(4),
-	PLANT_SUBOBSERVATIONS(5),
-	QUADRAT_SUBOBSERVATIONS(6),
-	TIME_SERIES_SUBOBSERVATIONS(7),
-	CUSTOM_SUBOBSERVATIONS(8),
-	SUB_SAMPLE_DATA(9),
-	WEATHER_DATA(10),
-	MEANS_OVER_TRIAL_INSTANCES(11);
+	STUDY_CONDITIONS(1, "STUDY"),
+	MEANS_DATA(2, "MEANS"),
+	SUMMARY_DATA(3, "SUMMARY"),
+	PLOT_DATA(4, "PLOT"),
+	PLANT_SUBOBSERVATIONS(5, "PLANT"),
+	QUADRAT_SUBOBSERVATIONS(6, "QUADRAT"),
+	TIME_SERIES_SUBOBSERVATIONS(7, "TIMESERIES"),
+	CUSTOM_SUBOBSERVATIONS(8, "CUSTOM"),
+	SUB_SAMPLE_DATA(9, "SS"),
+	WEATHER_DATA(10, "WD"),
+	MEANS_OVER_TRIAL_INSTANCES(11, "OM");
 
 	private static final Map<Integer, DatasetTypeEnum> lookup = new HashMap<>();
 
@@ -30,13 +31,19 @@ public enum DatasetTypeEnum {
 	}
 
 	private final int id;
+	private final String name;
 
-	DatasetTypeEnum(final int id) {
+	DatasetTypeEnum(final int id, final String name) {
 		this.id = id;
+		this.name = name;
 	}
 
 	public int getId() {
 		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
 	}
 
 }
