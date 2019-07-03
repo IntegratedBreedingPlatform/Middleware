95ba395498c3174f8756eed12ab831909f9fbd38
mariebawanan <bawananmarieflor@gmail.com>
2019-07-03 17:28:44 +0800

    Implement queries in getting and counting observationLevels
    IBP-2887

--- src/main/java/org/generationcp/middleware/dao/dms/DatasetTypeDAO.java
+++ src/main/java/org/generationcp/middleware/dao/dms/DatasetTypeDAO.java
@@ -1,10 +1,16 @@
 package org.generationcp.middleware.dao.dms;
 
 import org.generationcp.middleware.dao.GenericDAO;
+import org.generationcp.middleware.domain.dms.DatasetTypeDTO;
 import org.generationcp.middleware.pojos.dms.DatasetType;
+import org.hibernate.Criteria;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 
+import org.hibernate.SQLQuery;
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.hibernate.HibernateException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DatasetTypeDAO extends GenericDAO<DatasetType, Integer> {
@@ -19,5 +25,41 @@ public class DatasetTypeDAO extends GenericDAO<DatasetType, Integer> {
 		return this.getSession().createCriteria(DatasetType.class, "datasetType")
 			.setProjection(Projections.property("datasetType.datasetTypeId"))
 			.add(Restrictions.eq("datasetType.isSubObservationType", true)).list();
+	}
+
+	public List<String> getObservationLevels(final Integer pageSize, final Integer pageNumber) {
+		final StringBuilder sqlString = new StringBuilder();
+		try {
+			sqlString
+				.append(" SELECT name")
+				.append(" FROM dataset_type ").append(" WHERE is_subobs_type = true  ").append(" OR is_obs_type = true  ")
+				.append(" ORDER BY dataset_type_id ");
+			;
+
+			final SQLQuery query =
+				this.getSession().createSQLQuery(sqlString.toString()).addScalar("name");
+
+			if (pageNumber != null && pageSize != null) {
+				query.setFirstResult(pageSize * (pageNumber - 1));
+				query.setMaxResults(pageSize);
+			}
+
+			return query.list();
+		} catch (final HibernateException e) {
+			throw new MiddlewareQueryException("Error in getObservationLevels(): " + e.getMessage(), e);
+		}
+	}
+
+
+	public long countSubObservationLevels() {
+		try {
+			final Criteria criteria = this.getSession().createCriteria(DatasetType.class, "datasetType");
+			criteria.setProjection(Projections.rowCount())
+				.add(Restrictions.eq("datasetType.isSubObservationType", true));
+
+			return ((Long) criteria.uniqueResult()).longValue();
+		} catch (final HibernateException e) {
+			throw new MiddlewareQueryException("Error in countSubObservationLevels(): " + e.getMessage(), e);
+		}
 	}
 }
