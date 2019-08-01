203356cdc327aa965ada0b9378765220777e1c52
Aldrin Batac <aldrin.batac@leafnode.io>
2019-08-01 11:45:35 +0800

    Implement equals and hashCode in CropPersonId.
    IBP-2784

--- src/main/java/org/generationcp/middleware/pojos/workbench/CropPersonId.java
+++ src/main/java/org/generationcp/middleware/pojos/workbench/CropPersonId.java
@@ -1,5 +1,7 @@
 package org.generationcp.middleware.pojos.workbench;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.generationcp.middleware.pojos.Person;
 
 import java.io.Serializable;
@@ -23,6 +25,29 @@ public class CropPersonId implements Serializable {
 
 	public void setPerson(final Person person) {
 		this.person = person;
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(this.cropType.getCropName()).append(this.person.getId()).hashCode();
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (obj == this) {
+			return true;
+		}
+		if (!(obj instanceof CropPerson)) {
+			return false;
+		}
+
+		final CropPerson otherObj = (CropPerson) obj;
+
+		return new EqualsBuilder().append(this.cropType.getCropName(), otherObj.getCropType().getCropName())
+			.append(this.person.getId(), otherObj.getPerson().getId()).isEquals();
 	}
 
 }
