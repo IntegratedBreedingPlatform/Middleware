15753def41a14363f3aa85e158be3ff964b55e0f
Clarysabel Tovar <clarysabel@leafnode.io>
2019-08-27 16:12:48 -0300

    Removed unused variable IBP-2792

--- src/main/java/org/generationcp/middleware/dao/RoleTypeDAO.java
+++ src/main/java/org/generationcp/middleware/dao/RoleTypeDAO.java
@@ -1,16 +1,13 @@
 package org.generationcp.middleware.dao;
 
 import org.generationcp.middleware.pojos.workbench.RoleType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class RoleTypeDAO extends GenericDAO<RoleType,Integer> {
 
-	private static final Logger LOG = LoggerFactory.getLogger(RoleTypeDAO.class);
-
 	public List<RoleType> getRoleTypes(){
 		return this.getAll();
 	}
+
 }
