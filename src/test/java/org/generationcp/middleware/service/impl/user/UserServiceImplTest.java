a6e0ce4451a2a47b44b1d858fc6a3e4142387bc2
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2019-09-06 17:23:13 +0800

    Add Role to user role

--- src/test/java/org/generationcp/middleware/service/impl/user/UserServiceImplTest.java
+++ src/test/java/org/generationcp/middleware/service/impl/user/UserServiceImplTest.java
@@ -13,6 +13,7 @@ import org.generationcp.middleware.pojos.workbench.CropType;
 import org.generationcp.middleware.pojos.workbench.Project;
 import org.generationcp.middleware.pojos.workbench.ProjectUserInfo;
 import org.generationcp.middleware.pojos.workbench.Role;
+import org.generationcp.middleware.pojos.workbench.RoleType;
 import org.generationcp.middleware.pojos.workbench.UserRole;
 import org.generationcp.middleware.pojos.workbench.WorkbenchUser;
 import org.generationcp.middleware.service.api.user.UserDto;
@@ -384,6 +385,12 @@ public class UserServiceImplTest extends IntegrationTestBase {
 		userRole.setCropType(this.workbenchDataManager.getCropTypeByName(CropType.CropEnum.MAIZE.toString()));
 		userRole.setUser(user);
 		userRole.setCreatedDate(new Date());
+		final Role role = new Role();
+		role.setId(1);
+		final RoleType roleType = new RoleType();
+		roleType.setId(1);
+		role.setRoleType(roleType);
+		userRole.setRole(role);
 		this.workbenchDataManager.saveOrUpdateUserRole(userRole);
 
 		this.sessionProvder.getSession().flush();
