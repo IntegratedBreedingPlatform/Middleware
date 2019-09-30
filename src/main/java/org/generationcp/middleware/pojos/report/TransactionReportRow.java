4eee1c50f144a9915fdb7215f79deaec5a13a575
Vanina Maletta <vmaletta@droptek.com>
2019-09-30 11:13:50 -0300

    Change if trndate from int to Date
    Issue: IBP-3104
    Reviewer: Clary

--- src/main/java/org/generationcp/middleware/pojos/report/TransactionReportRow.java
+++ src/main/java/org/generationcp/middleware/pojos/report/TransactionReportRow.java
@@ -16,6 +16,7 @@ import org.generationcp.middleware.pojos.Location;
 import org.generationcp.middleware.pojos.Person;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * This POJO represents a row in reports about Transaction records. Note that different reports require different information and so some
@@ -30,7 +31,7 @@ public class TransactionReportRow implements Serializable {
 
 	private static final long serialVersionUID = 4363149565820886638L;
 
-	private Integer date;
+	private Date date;
 	private Double quantity;
 	private Term scaleOfLot;
 	private Location locationOfLot;
@@ -49,11 +50,11 @@ public class TransactionReportRow implements Serializable {
 
 	}
 
-	public Integer getDate() {
+	public Date getDate() {
 		return this.date;
 	}
 
-	public void setDate(final Integer date) {
+	public void setDate(final Date date) {
 		this.date = date;
 	}
 
@@ -101,7 +102,7 @@ public class TransactionReportRow implements Serializable {
 		return this.person;
 	}
 
-	public void setPerson(Person person) {
+	public void setPerson(final Person person) {
 		this.person = person;
 	}
 
