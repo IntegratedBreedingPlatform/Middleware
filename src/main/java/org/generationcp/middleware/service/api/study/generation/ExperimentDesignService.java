eed03dcf752c4f5e5a88ccfc180aa70677ab763c
Darla Hermo <darla@leafnode.io>
2019-10-07 18:13:16 +0800

    Initial commit of ExperimentDesign service
    IBP-31-7

---
+++ src/main/java/org/generationcp/middleware/service/api/study/generation/ExperimentDesignService.java
@@ -0,0 +1,16 @@
+package org.generationcp.middleware.service.api.study.generation;
+
+import org.generationcp.middleware.domain.etl.MeasurementVariable;
+import org.generationcp.middleware.pojos.workbench.CropType;
+import org.generationcp.middleware.service.api.dataset.ObservationUnitRow;
+
+import java.util.List;
+
+public interface ExperimentDesignService {
+
+	void deleteExperimentDesign(int studyId);
+
+	void saveExperimentDesign(CropType crop, int studyId, List<MeasurementVariable> variables, List<ObservationUnitRow> rows,
+		Boolean isDeletePreviousDesign);
+
+}
