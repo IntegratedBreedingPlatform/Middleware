87df6d8eb21124e756ef3eb97f5a32820293a78e
Aldrin Batac <aldrin.batac@leafnode.io>
2019-10-29 12:58:23 +0800

    Implement creation/saving of Study Instance.
    IBP-3094

--- src/main/java/org/generationcp/middleware/service/impl/study/StudyInstanceServiceImpl.java
+++ src/main/java/org/generationcp/middleware/service/impl/study/StudyInstanceServiceImpl.java
@@ -5,26 +5,23 @@ import org.generationcp.middleware.domain.dms.ExperimentValues;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.domain.ontology.VariableType;
-import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 import org.generationcp.middleware.manager.DaoFactory;
+import org.generationcp.middleware.manager.Operation;
 import org.generationcp.middleware.operation.saver.ExperimentModelSaver;
+import org.generationcp.middleware.pojos.Location;
 import org.generationcp.middleware.pojos.dms.Geolocation;
 import org.generationcp.middleware.pojos.dms.GeolocationProperty;
 import org.generationcp.middleware.pojos.workbench.CropType;
-import org.generationcp.middleware.service.api.dataset.DatasetService;
 import org.generationcp.middleware.service.api.study.StudyInstanceService;
-import org.generationcp.middleware.service.impl.dataset.DatasetServiceImpl;
-import org.generationcp.middleware.util.StringUtil;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Transactional
 public class StudyInstanceServiceImpl implements StudyInstanceService {
-
-	private static final List<Integer> GEOLOCATION_STORAGE_TERMIDS = Arrays
-		.asList(TermId.TRIAL_INSTANCE_FACTOR.getId(), TermId.LATITUDE.getId(), TermId.LONGITUDE.getId(), TermId.GEODETIC_DATUM.getId(),
-			TermId.ALTITUDE.getId());
 
 	public StudyInstanceServiceImpl() {
 		// no-arg constuctor is required by CGLIB proxying used by Spring 3x and older.
@@ -33,20 +30,21 @@ public class StudyInstanceServiceImpl implements StudyInstanceService {
 	public StudyInstanceServiceImpl(final HibernateSessionProvider sessionProvider) {
 		this.daoFactory = new DaoFactory(sessionProvider);
 		this.experimentModelSaver = new ExperimentModelSaver(sessionProvider);
-		this.datasetService = new DatasetServiceImpl(sessionProvider);
 	}
 
-	private DaoFactory daoFactory;
 	private ExperimentModelSaver experimentModelSaver;
-	private DatasetService datasetService;
+	private DaoFactory daoFactory;
 
 	@Override
 	public void createStudyInstance(final CropType crop, final Integer datasetId, final String instanceNumber) {
 
-		final List<MeasurementVariable> measurementVariables = this.datasetService.getObservationSetVariables(datasetId,
+		// Get the existing environment dataset variables.
+		// Since we are creating a new study instance, the values of these variables will be blank.
+		final List<MeasurementVariable> measurementVariables = this.daoFactory.getDmsProjectDAO().getObservationSetVariables(datasetId,
 			Arrays.asList(VariableType.ENVIRONMENT_DETAIL.getId(), VariableType.STUDY_CONDITION.getId()));
 
-		final Geolocation geolocation = this.createGeolocation(measurementVariables);
+		final Geolocation geolocation =
+			this.createGeolocation(measurementVariables, instanceNumber);
 		this.daoFactory.getGeolocationDao().save(geolocation);
 
 		final ExperimentValues experimentValue = new ExperimentValues();
@@ -60,47 +58,38 @@ public class StudyInstanceServiceImpl implements StudyInstanceService {
 
 	}
 
-	protected Geolocation createGeolocation(final List<MeasurementVariable> measurementVariables) {
+	protected Geolocation createGeolocation(final List<MeasurementVariable> measurementVariables, final String instanceNumber) {
 		final Geolocation geolocation = new Geolocation();
+		geolocation.setProperties(new ArrayList<GeolocationProperty>());
 
 		int rank = 1;
 		for (final MeasurementVariable measurementVariable : measurementVariables) {
 			final int variableId = measurementVariable.getTermId();
-			final String value = measurementVariable.getValue();
+			if (TermId.TRIAL_INSTANCE_FACTOR.getId() == variableId) {
+				geolocation.setDescription(instanceNumber);
+			} else if (VariableType.ENVIRONMENT_DETAIL == measurementVariable.getVariableType()) {
+				String value = measurementVariable.getValue();
 
-			if (GEOLOCATION_STORAGE_TERMIDS.contains(variableId)) {
-				this.setGeolocationValue(geolocation, variableId, value);
-			} else if (VariableType.ENVIRONMENT_DETAIL == measurementVariable.getVariableType()
-				|| VariableType.STUDY_CONDITION == measurementVariable.getVariableType()) {
+				// The default value of an instance's location name is "Unspecified Location"
+				if (measurementVariable.getTermId() == TermId.LOCATION_ID.getId()) {
+					value = this.getLocationIdOfUnspecifiedLocation();
+				}
 				final GeolocationProperty geolocationProperty =
-					new GeolocationProperty(geolocation, measurementVariable.getValue(), rank, 1);
-				geolocation.setProperties(Arrays.asList(geolocationProperty));
-			} else {
-				throw new MiddlewareQueryException(
-					"Non-Trial Environment Variable was used in calling create location: " + measurementVariable.getVariableType()
-						.getName());
+					new GeolocationProperty(geolocation, value, rank, measurementVariable.getTermId());
+				geolocation.getProperties().add(geolocationProperty);
+				rank++;
 			}
-			rank++;
+
 		}
 		return geolocation;
 	}
 
-	public void setGeolocationValue(final Geolocation geolocation, final int termId, final String value) {
-		if (TermId.TRIAL_INSTANCE_FACTOR.getId() == termId) {
-			geolocation.setDescription(value);
-
-		} else if (TermId.LATITUDE.getId() == termId) {
-			geolocation.setLatitude(StringUtil.isEmpty(value) ? null : Double.valueOf(value));
-
-		} else if (TermId.LONGITUDE.getId() == termId) {
-			geolocation.setLongitude(StringUtil.isEmpty(value) ? null : Double.valueOf(value));
-
-		} else if (TermId.GEODETIC_DATUM.getId() == termId) {
-			geolocation.setGeodeticDatum(value);
-
-		} else if (TermId.ALTITUDE.getId() == termId) {
-			geolocation.setAltitude(StringUtil.isEmpty(value) ? null : Double.valueOf(value));
+	protected String getLocationIdOfUnspecifiedLocation() {
+		final List<Location> locations = this.daoFactory.getLocationDAO().getByName(Location.UNSPECIFIED_LOCATION, Operation.EQUAL);
+		if (!locations.isEmpty()) {
+			return String.valueOf(locations.get(0).getLocid());
 		}
+		return "";
 	}
 
 }
