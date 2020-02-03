a33ab4bcae381573124e2414d4e65bb7e071ee42
Vanina Maletta <vmaletta@droptek.com>
2020-02-03 12:46:45 -0300

    Change trntype of deposit from 0 to 4
    Issue: IBP-3309
    Reviewer: Clary

--- src/main/java/org/generationcp/middleware/pojos/ims/TransactionType.java
+++ src/main/java/org/generationcp/middleware/pojos/ims/TransactionType.java
@@ -3,8 +3,10 @@ package org.generationcp.middleware.pojos.ims;
 
 public enum TransactionType {
 
-	DEPOSIT("Deposit", 0), WITHDRAWAL("Withdrawal", 1),
-	DISCARD("Discard", 2), ADJUSTMENT("Adjustment", 3);
+	WITHDRAWAL("Withdrawal", 1),
+	DISCARD("Discard", 2),
+	ADJUSTMENT("Adjustment", 3),
+	DEPOSIT("Deposit", 4);
 
 	private final Integer id;
 	private String value;
