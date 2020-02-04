599755c0d10144b6ff6fe6943c766d1f354ad6cf
Vanina Maletta <vmaletta@droptek.com>
2020-02-04 11:50:39 -0300

    Organize imports
    Issue: IBP-3309
    Reviewer: Clary

--- src/main/java/org/generationcp/middleware/dao/AttributeDAO.java
+++ src/main/java/org/generationcp/middleware/dao/AttributeDAO.java
@@ -26,9 +26,6 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * DAO class for {@link Attribute}.
  *
