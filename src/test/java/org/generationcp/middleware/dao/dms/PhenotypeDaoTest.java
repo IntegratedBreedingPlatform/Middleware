6003abae26d5b30706311402873fe13fbc521cc5
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2020-03-19 01:47:59 +0800

    Fixed unit tests
    IBP-3305 / IBP-3490

--- src/test/java/org/generationcp/middleware/dao/dms/PhenotypeDaoTest.java
+++ src/test/java/org/generationcp/middleware/dao/dms/PhenotypeDaoTest.java
@@ -105,21 +105,22 @@ public class PhenotypeDaoTest {
 		final List<Integer> environmentIds = Arrays.asList(1, 2, 3);
 		this.dao.getNumericTraitInfoList(environmentIds, traitIds);
 
-		final String expectedSql = "SELECT p.observable_id, "
-			+ " 	COUNT(DISTINCT plot.parent_id) AS location_count, "
-			+ "		COUNT(DISTINCT s.dbxref_id) AS germplasm_count, "
-			+ "		COUNT(DISTINCT e.nd_experiment_id) AS observation_count , "
-			+ "		IF (MIN(p.value * 1) IS NULL, 0, MIN(p.value * 1))  AS min_value, "
-			+ "		IF (MAX(p.value * 1) IS NULL, 0, MAX(p.value * 1)) AS max_value "
-			+ "		FROM phenotype p "
-			+ "		INNER JOIN nd_experiment e ON e.nd_experiment_id = p.nd_experiment_id "
-			+ "		INNER JOIN project pr ON pr.project_id = e.project_id "
-			+ "		INNER JOIN project plot_ds on plot_ds.study_id = pr.study_id and plot_ds.dataset_type_id = "
-			+ 		DatasetTypeEnum.PLOT_DATA.getId()
-			+ "		INNER JOIN nd_experiment plot ON plot_ds.project_id = plot.project_id "
-			+ "		INNER JOIN stock s ON e.stock_id = s.stock_id "
-			+ "		WHERE plot.parent_id IN (:environmentIds) "
-			+ "		AND p.observable_id IN (:numericVariableIds) GROUP by p.observable_id";
+		final String expectedSql = "SELECT p.observable_id, COUNT(DISTINCT env.nd_experiment_id) AS location_count,"
+			+ " COUNT(DISTINCT s.dbxref_id) AS germplasm_count,"
+			+ " COUNT(DISTINCT e.nd_experiment_id) AS observation_count ,"
+			+ " IF (MIN(p.value * 1) IS NULL, 0, MIN(p.value * 1))  AS min_value,"
+			+ " IF (MAX(p.value * 1) IS NULL, 0, MAX(p.value * 1)) AS max_value  FROM phenotype p"
+			+ "  INNER JOIN nd_experiment e ON e.nd_experiment_id = p.nd_experiment_id"
+			+ "  LEFT JOIN nd_experiment plot ON plot.nd_experiment_id = e.parent_id and plot.type_id = 1155"
+			+ " INNER JOIN project pr ON pr.project_id = e.project_id"
+			+ "  INNER JOIN project env_ds ON env_ds.study_id = pr.study_id"
+			+ " AND env_ds.dataset_type_id = " + DatasetTypeEnum.SUMMARY_DATA.getId() + ""
+			+ " INNER JOIN nd_experiment env ON env_ds.project_id = env.project_id AND env.type_id = 1020"
+			+ "    AND (e.parent_id = env.nd_experiment_id OR plot.parent_id = env.nd_experiment_id)"
+			+ "  INNER JOIN stock s ON e.stock_id = s.stock_id"
+			+ "     WHERE env.nd_experiment_id IN (:environmentIds)"
+			+ "        AND p.observable_id IN (:numericVariableIds)"
+			+ "      GROUP by p.observable_id ";
 		final ArgumentCaptor<String> sqlCaptor = ArgumentCaptor.forClass(String.class);
 		Mockito.verify(this.session).createSQLQuery(sqlCaptor.capture());
 		Assert.assertEquals(this.formatString(expectedSql), this.formatString(sqlCaptor.getValue()));
@@ -133,18 +134,20 @@ public class PhenotypeDaoTest {
 		final List<Integer> environmentIds = Arrays.asList(1, 2, 3);
 		this.dao.getTraitInfoCounts(environmentIds, traitIds);
 
-		final String expectedSql = "SELECT p.observable_id, "
-			+ "		COUNT(DISTINCT plot.parent_id) AS location_count, "
-			+ "		COUNT(DISTINCT s.dbxref_id) AS germplasm_count, "
-			+ "		COUNT(DISTINCT e.nd_experiment_id) AS observation_count "
-			+ "		FROM phenotype p "
-			+ "		INNER JOIN nd_experiment e ON e.nd_experiment_id = p.nd_experiment_id "
-			+ "		INNER JOIN stock s ON e.stock_id = s.stock_id "
-			+ "		INNER JOIN project pr ON pr.project_id = e.project_id "
-			+ "		INNER JOIN project plot_ds on plot_ds.study_id = pr.study_id and plot_ds.dataset_type_id = "
-			+ 		DatasetTypeEnum.PLOT_DATA.getId()
-			+ "		INNER JOIN nd_experiment plot ON plot_ds.project_id = plot.project_id "
-			+ "		WHERE plot.parent_id IN (:environmentIds) AND p.observable_id IN (:variableIds) GROUP by p.observable_id";
+		final String expectedSql = "SELECT p.observable_id,"
+			+ " COUNT(DISTINCT env.nd_experiment_id) AS location_count,"
+			+ " COUNT(DISTINCT s.dbxref_id) AS germplasm_count,"
+			+ " COUNT(DISTINCT e.nd_experiment_id) AS observation_count  FROM phenotype p"
+			+ "  INNER JOIN nd_experiment e ON e.nd_experiment_id = p.nd_experiment_id"
+			+ "  LEFT JOIN nd_experiment plot ON plot.nd_experiment_id = e.parent_id and plot.type_id = 1155"
+			+ " INNER JOIN project pr ON pr.project_id = e.project_id  "
+			+ "INNER JOIN project env_ds ON env_ds.study_id = pr.study_id AND env_ds.dataset_type_id = "
+			+ DatasetTypeEnum.SUMMARY_DATA.getId()
+			+ " INNER JOIN nd_experiment env ON env_ds.project_id = env.project_id AND env.type_id = 1020"
+			+ "    AND (e.parent_id = env.nd_experiment_id OR plot.parent_id = env.nd_experiment_id)"
+			+ "  INNER JOIN stock s ON e.stock_id = s.stock_id"
+			+ "  WHERE env.nd_experiment_id IN (:environmentIds)"
+			+ "     AND p.observable_id IN (:variableIds) GROUP by p.observable_id ";
 		final ArgumentCaptor<String> sqlCaptor = ArgumentCaptor.forClass(String.class);
 		Mockito.verify(this.session).createSQLQuery(sqlCaptor.capture());
 		Assert.assertEquals(this.formatString(expectedSql), this.formatString(sqlCaptor.getValue()));
@@ -229,16 +232,16 @@ public class PhenotypeDaoTest {
 			+ "	phenotype.observable_id  having count(phenotype.observable_id) >= 2 LIMIT 1";
 	}
 	private String getObservationsForTraitMainQuery() {
-		return "SELECT p.observable_id, "
-			+ "		s.dbxref_id, plot.parent_id, "
-			+ "		p.value FROM nd_experiment e "
-			+ "		INNER JOIN stock s ON e.stock_id = s.stock_id "
-			+ "		INNER JOIN phenotype p ON e.nd_experiment_id = p.nd_experiment_id "
-			+ "		INNER JOIN project pr ON pr.project_id = e.project_id "
-			+ "		INNER JOIN project plot_ds on plot_ds.study_id = pr.study_id and plot_ds.dataset_type_id = "
-			+ 		DatasetTypeEnum.PLOT_DATA.getId()
-			+ "		INNER JOIN nd_experiment plot ON plot_ds.project_id = plot.project_id "
-			+ "		WHERE plot.parent_id IN (:environmentIds) AND p.observable_id IN (:traitIds)";
+		return "SELECT p.observable_id, s.dbxref_id, env.nd_experiment_id, p.value  FROM phenotype p"
+			+ "  INNER JOIN nd_experiment e ON e.nd_experiment_id = p.nd_experiment_id"
+			+ "  LEFT JOIN nd_experiment plot ON plot.nd_experiment_id = e.parent_id and plot.type_id = 1155"
+			+ " INNER JOIN project pr ON pr.project_id = e.project_id  INNER JOIN project env_ds ON env_ds.study_id = pr.study_id"
+			+ " AND env_ds.dataset_type_id = " + DatasetTypeEnum.SUMMARY_DATA.getId() + " "
+			+ " INNER JOIN nd_experiment env ON env_ds.project_id = env.project_id AND env.type_id = 1020"
+			+ "    AND (e.parent_id = env.nd_experiment_id OR plot.parent_id = env.nd_experiment_id)"
+			+ "  INNER JOIN stock s ON e.stock_id = s.stock_id"
+			+ "  WHERE env.nd_experiment_id IN (:environmentIds)"
+			+ "    AND p.observable_id IN (:traitIds) ";
 	}
 
 
