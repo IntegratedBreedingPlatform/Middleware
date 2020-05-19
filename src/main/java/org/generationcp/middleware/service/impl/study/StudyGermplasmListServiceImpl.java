465bba62ef3d114037ccae56276cb01f8334f85e
Aldrin Batac <aldrin.batac@leafnode.io>
2020-05-19 08:16:49 +0800

    Modify GermplasmListService to get the entry list from Stock table.
    IBP-3697

--- src/main/java/org/generationcp/middleware/service/impl/study/StudyGermplasmListServiceImpl.java
+++ src/main/java/org/generationcp/middleware/service/impl/study/StudyGermplasmListServiceImpl.java
@@ -1,9 +1,11 @@
 
 package org.generationcp.middleware.service.impl.study;
 
+import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 import org.generationcp.middleware.manager.DaoFactory;
-import org.generationcp.middleware.pojos.ListDataProject;
+import org.generationcp.middleware.pojos.dms.StockModel;
+import org.generationcp.middleware.pojos.dms.StockProperty;
 import org.generationcp.middleware.service.api.study.StudyGermplasmDto;
 import org.generationcp.middleware.service.api.study.StudyGermplasmListService;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +15,7 @@ import java.util.List;
 import java.util.Set;
 
 @Transactional
+// FIXME: Merge this class to StockModelService.
 public class StudyGermplasmListServiceImpl implements StudyGermplasmListService {
 
 	private final DaoFactory daoFactory;
@@ -24,24 +27,24 @@ public class StudyGermplasmListServiceImpl implements StudyGermplasmListService 
 	@Override
 	public List<StudyGermplasmDto> getGermplasmList(final int studyBusinessIdentifier) {
 
-		final List<ListDataProject> listEntries = this.daoFactory.getListDataProjectDAO().getGermplasmList(studyBusinessIdentifier);
+		final List<StockModel> stockModelList = this.daoFactory.getStockDao().getStocksForStudy(studyBusinessIdentifier);
 
 		final List<StudyGermplasmDto> studyGermplasmDtos = new ArrayList<>();
-		Integer index = 0;
-		for (final ListDataProject listDataProject : listEntries) {
+		int index = 0;
+		for (final StockModel stockModel : stockModelList) {
 			final StudyGermplasmDto studyGermplasmDto = new StudyGermplasmDto();
-			studyGermplasmDto.setCross(listDataProject.getGroupName());
-			studyGermplasmDto.setDesignation(listDataProject.getDesignation());
-			studyGermplasmDto.setEntryCode(listDataProject.getEntryCode());
-			studyGermplasmDto.setEntryNumber(listDataProject.getEntryId());
-			studyGermplasmDto.setGermplasmId(listDataProject.getGermplasmId());
+			studyGermplasmDto.setCross(stockModel.getGermplasm().getCrossName()); // TODO: IBP-3697 Verify mapping
+			studyGermplasmDto.setDesignation(stockModel.getName());
+			studyGermplasmDto.setEntryCode(stockModel.getValue());
+			studyGermplasmDto.setEntryNumber(Integer.valueOf(stockModel.getUniqueName()));
+			studyGermplasmDto.setGermplasmId(stockModel.getGermplasm().getGid());
 			++index;
-			studyGermplasmDto.setPosition(index.toString());
-			studyGermplasmDto.setSeedSource(listDataProject.getSeedSource());
-			studyGermplasmDto.setEntryType(listDataProject.getList().getType());
-			studyGermplasmDto.setCheckType(listDataProject.getCheckType());
-			studyGermplasmDto.setStockIds(listDataProject.getStockIDs());
-			studyGermplasmDto.setGroupId(listDataProject.getGroupId());
+			studyGermplasmDto.setPosition(String.valueOf(index));
+			studyGermplasmDto.setSeedSource(stockModel.getGermplasm().getCrossName()); // TODO: IBP-3697 Verify mapping
+			studyGermplasmDto.setEntryType(""); // TODO: IBP-3697 Verify mapping
+			studyGermplasmDto.setCheckType(Integer.valueOf(this.findStockPropValue(TermId.ENTRY_TYPE.getId(), stockModel.getProperties())));
+			studyGermplasmDto.setStockIds(""); // TODO: IBP-3697 Verify mapping
+			studyGermplasmDto.setGroupId(0); // TODO: IBP-3697 Verify mapping
 			studyGermplasmDtos.add(studyGermplasmDto);
 		}
 		return studyGermplasmDtos;
@@ -49,7 +52,18 @@ public class StudyGermplasmListServiceImpl implements StudyGermplasmListService 
 
 	@Override
 	public List<StudyGermplasmDto> getGermplasmListFromPlots(final int studyBusinessIdentifier, final Set<Integer> plotNos) {
-		return daoFactory.getStockDao().getStudyGermplasmDtoList(studyBusinessIdentifier, plotNos);
+		return this.daoFactory.getStockDao().getStudyGermplasmDtoList(studyBusinessIdentifier, plotNos);
+	}
+
+	private String findStockPropValue(final int termId, final Set<StockProperty> properties) {
+		if (properties != null) {
+			for (final StockProperty property : properties) {
+				if (termId == property.getTypeId()) {
+					return property.getValue();
+				}
+			}
+		}
+		return null;
 	}
 
 }
