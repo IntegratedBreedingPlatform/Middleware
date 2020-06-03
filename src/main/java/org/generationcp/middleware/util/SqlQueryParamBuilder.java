b93b9b6c045d2896457f3ad2a771bdd1682fd1f2
Nahuel Soldevilla <nahuel@leafnode.io>
2020-06-03 15:47:40 -0300

    New helper class to set hibernate optional params
    Issue: IBP-3752

---
+++ src/main/java/org/generationcp/middleware/util/SqlQueryParamBuilder.java
@@ -0,0 +1,51 @@
+package org.generationcp.middleware.util;
+
+import org.hibernate.SQLQuery;
+
+import java.util.Collection;
+
+/**
+ * <p>
+ *     Utility object to simplify SQLQuery optional params buiding.
+ *     Allows to reuse the same method for building the query
+ *     and setting the parameters after the query has been built
+ * </p>
+ * <pre>
+ *     if (!paramList.isEmpty()) {
+ *     		builder.append(" and param in (:paramList)");
+ *     		builder.setParameterList("paramList", paramList);
+ *     }
+ * </pre>
+ */
+public class SqlQueryParamBuilder {
+
+	private SQLQuery sqlQuery;
+	private StringBuilder sqlBuilder;
+
+	public SqlQueryParamBuilder(final SQLQuery sqlQuery) {
+		this.sqlQuery = sqlQuery;
+	}
+
+	public SqlQueryParamBuilder(final StringBuilder sqlBuilder) {
+		this.sqlBuilder = sqlBuilder;
+	}
+
+	public StringBuilder append(final String sql) {
+		if (this.sqlBuilder != null) {
+			this.sqlBuilder.append(sql);
+		}
+		return this.sqlBuilder;
+	}
+
+	public void setParameter(final String name, final Object val) {
+		if (this.sqlQuery != null) {
+			this.sqlQuery.setParameter(name, val);
+		}
+	}
+
+	public void setParameterList(final String name, final Collection vals) {
+		if (this.sqlQuery != null) {
+			this.sqlQuery.setParameterList(name, vals);
+		}
+	}
+}
