852febca3d2e517ecf3e29d3d3e4946f61aad7e0
darla-leafnode <darla@leafnode.io>
2020-06-10 08:51:15 +0800

    Add constructors to StudyGermplasmDto
    IBP-3773

--- src/main/java/org/generationcp/middleware/service/api/study/StudyGermplasmDto.java
+++ src/main/java/org/generationcp/middleware/service/api/study/StudyGermplasmDto.java
@@ -37,6 +37,21 @@ public class StudyGermplasmDto implements Serializable {
 
 	public StudyGermplasmDto() { }
 
+	public StudyGermplasmDto(final Integer entryId) {
+		this.entryId = entryId;
+	}
+
+	public StudyGermplasmDto(final Integer entryId, final String entryType, final Integer germplasmId, final String designation, final Integer entryNumber, final String entryCode, final String cross, final String seedSource) {
+		this.entryId = entryId;
+		this.entryType = entryType;
+		this.germplasmId = germplasmId;
+		this.designation = designation;
+		this.entryNumber = entryNumber;
+		this.entryCode = entryCode;
+		this.cross = cross;
+		this.seedSource = seedSource;
+	}
+
 	public StudyGermplasmDto(final StockModel stock) {
 		this.setEntryId(stock.getStockId());
 		this.setDesignation(stock.getName());
