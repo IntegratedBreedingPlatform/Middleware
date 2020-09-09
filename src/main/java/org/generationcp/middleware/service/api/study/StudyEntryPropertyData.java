4e892f080da82fa630fd4dee893e1efa0e8e2b55
Clarysabel Tovar <clarysabel@leafnode.io>
2020-09-09 14:55:41 -0300

    Implemented /entries/table/columns, added some details to /entries/table IBP-3951

--- src/main/java/org/generationcp/middleware/service/api/study/StudyEntryPropertyData.java
+++ src/main/java/org/generationcp/middleware/service/api/study/StudyEntryPropertyData.java
@@ -1,14 +1,29 @@
 package org.generationcp.middleware.service.api.study;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.pojomatic.Pojomatic;
 import org.pojomatic.annotations.AutoProperty;
 
 @AutoProperty
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StudyEntryPropertyData {
 
 	private Integer studyEntryPropertyId;
-	private String value;
 	private Integer variableId;
+	private String value;
+
+	public StudyEntryPropertyData() {
+	}
+
+	public StudyEntryPropertyData(final String value) {
+		this.value = value;
+	}
+
+	public StudyEntryPropertyData(final Integer studyEntryPropertyId, final Integer variableId, final String value) {
+		this.studyEntryPropertyId = studyEntryPropertyId;
+		this.variableId = variableId;
+		this.value = value;
+	}
 
 	public Integer getStudyEntryPropertyId() {
 		return studyEntryPropertyId;
