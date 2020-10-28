21c359736ec15ffd1dc0e66cd4f37f9d2da71f27
cuenyad <cuenya.diego@gmail.com>
2020-10-28 13:11:59 -0300

    Changing fieldType parameter by a Set of fieldTypes and his implementations.
    Issue: IBP-4120
    Review: none

--- src/main/java/org/generationcp/middleware/manager/GermplasmDataManagerImpl.java
+++ src/main/java/org/generationcp/middleware/manager/GermplasmDataManagerImpl.java
@@ -11,6 +11,8 @@
 package org.generationcp.middleware.manager;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 import com.jamonapi.Monitor;
 import com.jamonapi.MonitorFactory;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -950,7 +952,7 @@ public class GermplasmDataManagerImpl extends DataManager implements GermplasmDa
 
 	@Override
 	public List<UserDefinedField> getUserDefinedFieldByFieldTableNameAndType(final String tableName, final String fieldType) {
-		return this.getUserDefinedFieldDao().getByFieldTableNameAndType(tableName, fieldType);
+		return this.getUserDefinedFieldDao().getByFieldTableNameAndType(tableName, ImmutableSet.of(fieldType));
 	}
 
 	@Override
