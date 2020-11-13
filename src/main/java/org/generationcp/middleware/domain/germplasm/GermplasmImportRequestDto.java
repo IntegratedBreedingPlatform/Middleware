078fc908cfca1b87015cd7fd13f11667763e72b8
Clarysabel Tovar <clarysabel@leafnode.io>
2020-11-13 17:23:36 -0300

    Format IBP-4097

--- src/main/java/org/generationcp/middleware/domain/germplasm/GermplasmImportRequestDto.java
+++ src/main/java/org/generationcp/middleware/domain/germplasm/GermplasmImportRequestDto.java
@@ -11,127 +11,127 @@ import java.util.Map;
 	"clientId", "germplasmUUID", "locationAbbr", "breedingMethodAbbr", "reference", "preferredName", "names", "attributes", "creationDate"})
 public class GermplasmImportRequestDto {
 
-		private Integer clientId;
+	private Integer clientId;
 
 	private String germplasmUUID;
 
-		private String locationAbbr;
+	private String locationAbbr;
 
-		private String breedingMethodAbbr;
+	private String breedingMethodAbbr;
 
-		private String reference;
+	private String reference;
 
-		private String preferredName;
+	private String preferredName;
 
-		private Map<String, String> names;
+	private Map<String, String> names;
 
-		private Map<String, String> attributes;
+	private Map<String, String> attributes;
 
 	private String creationDate;
 
 	public GermplasmImportRequestDto() {
-		}
+	}
 
 	public GermplasmImportRequestDto(final Integer clientId, final String germplasmUUID, final String locationAbbr,
 		final String breedingMethodAbbr,
-			final String reference,
+		final String reference,
 		final String preferredName, final Map<String, String> names, final Map<String, String> attributes, final String creationDate) {
-			this.clientId = clientId;
+		this.clientId = clientId;
 		this.germplasmUUID = germplasmUUID;
-			this.locationAbbr = locationAbbr;
-			this.breedingMethodAbbr = breedingMethodAbbr;
-			this.reference = reference;
-			this.preferredName = preferredName;
-			this.names = names;
-			this.attributes = attributes;
-			this.creationDate = creationDate;
-		}
+		this.locationAbbr = locationAbbr;
+		this.breedingMethodAbbr = breedingMethodAbbr;
+		this.reference = reference;
+		this.preferredName = preferredName;
+		this.names = names;
+		this.attributes = attributes;
+		this.creationDate = creationDate;
+	}
 
-		public Integer getClientId() {
-			return clientId;
-		}
+	public Integer getClientId() {
+		return clientId;
+	}
 
-		public void setClientId(final Integer clientId) {
-			this.clientId = clientId;
-		}
+	public void setClientId(final Integer clientId) {
+		this.clientId = clientId;
+	}
 
-		public String getLocationAbbr() {
-			return locationAbbr;
-		}
+	public String getLocationAbbr() {
+		return locationAbbr;
+	}
 
-		public void setLocationAbbr(final String locationAbbr) {
-			this.locationAbbr = locationAbbr;
-		}
+	public void setLocationAbbr(final String locationAbbr) {
+		this.locationAbbr = locationAbbr;
+	}
 
-		public String getBreedingMethodAbbr() {
-			return breedingMethodAbbr;
-		}
+	public String getBreedingMethodAbbr() {
+		return breedingMethodAbbr;
+	}
 
-		public void setBreedingMethodAbbr(final String breedingMethodAbbr) {
-			this.breedingMethodAbbr = breedingMethodAbbr;
-		}
+	public void setBreedingMethodAbbr(final String breedingMethodAbbr) {
+		this.breedingMethodAbbr = breedingMethodAbbr;
+	}
 
 	public String getGermplasmUUID() {
 		return germplasmUUID;
-		}
+	}
 
 	public void setGermplasmUUID(final String germplasmUUID) {
 		this.germplasmUUID = germplasmUUID;
-		}
+	}
 
-		public String getReference() {
-			return reference;
-		}
+	public String getReference() {
+		return reference;
+	}
 
-		public void setReference(final String reference) {
-			this.reference = reference;
-		}
+	public void setReference(final String reference) {
+		this.reference = reference;
+	}
 
-		public String getPreferredName() {
-			return preferredName;
-		}
+	public String getPreferredName() {
+		return preferredName;
+	}
 
-		public void setPreferredName(final String preferredName) {
-			this.preferredName = preferredName;
-		}
+	public void setPreferredName(final String preferredName) {
+		this.preferredName = preferredName;
+	}
 
-		public Map<String, String> getNames() {
-			return names;
-		}
+	public Map<String, String> getNames() {
+		return names;
+	}
 
-		public void setNames(final Map<String, String> names) {
-			this.names = names;
-		}
+	public void setNames(final Map<String, String> names) {
+		this.names = names;
+	}
 
-		public Map<String, String> getAttributes() {
-			return attributes;
-		}
+	public Map<String, String> getAttributes() {
+		return attributes;
+	}
 
-		public void setAttributes(final Map<String, String> attributes) {
-			this.attributes = attributes;
-		}
+	public void setAttributes(final Map<String, String> attributes) {
+		this.attributes = attributes;
+	}
 
 	public String getCreationDate() {
-			return creationDate;
-		}
+		return creationDate;
+	}
 
 	public void setCreationDate(final String creationDate) {
-			this.creationDate = creationDate;
-		}
+		this.creationDate = creationDate;
+	}
 
-		@Override
-		public int hashCode() {
-			return Pojomatic.hashCode(this);
-		}
+	@Override
+	public int hashCode() {
+		return Pojomatic.hashCode(this);
+	}
 
-		@Override
-		public String toString() {
-			return Pojomatic.toString(this);
-		}
+	@Override
+	public String toString() {
+		return Pojomatic.toString(this);
+	}
 
-		@Override
-		public boolean equals(final Object o) {
-			return Pojomatic.equals(this, o);
-		}
+	@Override
+	public boolean equals(final Object o) {
+		return Pojomatic.equals(this, o);
+	}
 
 }
