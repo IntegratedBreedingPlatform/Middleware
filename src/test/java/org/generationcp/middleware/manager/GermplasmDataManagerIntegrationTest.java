2d46bbb1cdc4fcce97bb432d6ef73d9f403d4df4
Leonel Kovacic <leonel@leafnode.io>
2020-12-21 16:12:03 -0300

    IBP-4217: added method to return a plot code values by the given gids. Delete deprecated method GermplasmDataManager#getPlotCodeValue

--- src/test/java/org/generationcp/middleware/manager/GermplasmDataManagerIntegrationTest.java
+++ src/test/java/org/generationcp/middleware/manager/GermplasmDataManagerIntegrationTest.java
@@ -731,42 +731,6 @@ public class GermplasmDataManagerIntegrationTest extends IntegrationTestBase {
 	}
 
 	@Test
-	public void testGetPlotCodeValue() {
-		final GermplasmDataManagerImpl unitToTest = new GermplasmDataManagerImpl();
-
-		// We want to mock away calls to other methods in same unit.
-		final GermplasmDataManagerImpl partiallyMockedUnit = Mockito.spy(unitToTest);
-		final Integer testGid = 1;
-
-		// First set up data such that no plot code attribute is associated.
-		Mockito.doReturn(null).when(partiallyMockedUnit).getPlotCodeField();
-		final List<Attribute> attributes = new ArrayList<Attribute>();
-		Mockito.doReturn(attributes).when(partiallyMockedUnit).getAttributesByGID(Matchers.anyInt());
-
-		final String plotCode1 = partiallyMockedUnit.getPlotCodeValue(testGid);
-		assertThat("getPlotCodeValue() should never return null.", plotCode1, is(notNullValue()));
-		assertThat("Expected `Unknown` returned when there is no plot code attribute present.", "Unknown", is(equalTo(plotCode1)));
-		// Now setup data so that gid has plot code attribute associated with it.
-		final UserDefinedField udfld = new UserDefinedField();
-		udfld.setFldno(1152);
-		udfld.setFtable("ATRIBUTS");
-		udfld.setFtype("PASSPORT");
-		udfld.setFcode("PLOTCODE");
-
-		Mockito.when(partiallyMockedUnit.getPlotCodeField()).thenReturn(udfld);
-		final Attribute plotCodeAttr = new Attribute();
-		plotCodeAttr.setTypeId(udfld.getFldno());
-		plotCodeAttr.setAval("The PlotCode Value");
-		attributes.add(plotCodeAttr);
-		Mockito.when(partiallyMockedUnit.getAttributesByGID(testGid)).thenReturn(attributes);
-
-		final String plotCode2 = partiallyMockedUnit.getPlotCodeValue(testGid);
-		assertThat("getPlotCodeValue() should never return null.", plotCode2, is(notNullValue()));
-		assertThat("Expected value of plot code attribute returned when plot code attribute is present.", plotCodeAttr.getAval(),
-			is(equalTo(plotCode2)));
-	}
-
-	@Test
 	public void testGetPreferredIdsByGIDs() throws MiddlewareQueryException {
 		final List<Integer> gids = Arrays.asList(1, 2, 3, 4, 5);
 		final Map<Integer, String> results = this.germplasmDataManager.getPreferredIdsByGIDs(gids);
