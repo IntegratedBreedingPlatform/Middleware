9c9eaed4d068868a6fe0636076fd7ff511542fd1
Leonel Kovacic <leonel@leafnode.io>
2021-03-02 18:45:28 -0300

    IBP-4324: code review changes

--- src/main/java/org/generationcp/middleware/service/api/workbench/WorkbenchService.java
+++ src/main/java/org/generationcp/middleware/service/api/workbench/WorkbenchService.java
@@ -2,14 +2,8 @@ package org.generationcp.middleware.service.api.workbench;
 
 import org.generationcp.middleware.pojos.workbench.WorkbenchSidebarCategoryLink;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 public interface WorkbenchService {
 
 	WorkbenchSidebarCategoryLink getWorkbenchSidebarLinksByCategoryId(Integer workbenchSidebarCategoryLink);
-
-	void close();
 
 }
--- src/main/java/org/generationcp/middleware/service/impl/workbench/WorkbenchServiceImpl.java
+++ src/main/java/org/generationcp/middleware/service/impl/workbench/WorkbenchServiceImpl.java
@@ -7,10 +7,6 @@ import org.generationcp.middleware.service.api.workbench.WorkbenchService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 @Service
 @Transactional
 public class WorkbenchServiceImpl implements WorkbenchService {
@@ -25,13 +21,6 @@ public class WorkbenchServiceImpl implements WorkbenchService {
 	@Override
 	public WorkbenchSidebarCategoryLink getWorkbenchSidebarLinksByCategoryId(final Integer workbenchSidebarCategoryLink) {
 		return this.workbenchDaoFactory.getWorkbenchSidebarCategoryLinkDao().getById(workbenchSidebarCategoryLink);
-	}
-
-	@Override
-	public void close() {
-		if (this.sessionProvider != null) {
-			this.sessionProvider.close();
-		}
 	}
 
 }
