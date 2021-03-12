bda91752a089710fbe17664127ac039cc63ea9d4
cuenyad <cuenya.diego@gmail.com>
2021-03-12 18:22:58 -0300

    create a new DTO to been used in GermplasmNameResource.
    Issue: IBP-4434
    Review: none

---
+++ src/main/java/org/generationcp/middleware/domain/germplasm/GermplasmNameRequestDto.java
@@ -0,0 +1,101 @@
+package org.generationcp.middleware.domain.germplasm;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.pojomatic.Pojomatic;
+import org.pojomatic.annotations.AutoProperty;
+
+@AutoProperty
+public class GermplasmNameRequestDto {
+
+	@JsonIgnore
+	private Integer id;
+
+	@JsonIgnore
+	private Integer gid;
+
+	private String name;
+
+	private Integer date;
+
+	private Integer locationId;
+
+	private Integer typeId;
+
+	private Integer status;
+
+	public GermplasmNameRequestDto(final Integer id, final Integer gid){
+		this.setId(id);
+		this.setGid(gid);
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(final Integer id) {
+		this.id = id;
+	}
+
+	public Integer getGid() {
+		return this.gid;
+	}
+
+	public void setGid(final Integer gid) {
+		this.gid = gid;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public Integer getDate() {
+		return this.date;
+	}
+
+	public void setDate(final Integer date) {
+		this.date = date;
+	}
+
+	public Integer getLocationId() {
+		return this.locationId;
+	}
+
+	public void setLocationId(final Integer locationId) {
+		this.locationId = locationId;
+	}
+
+	public Integer getTypeId() {
+		return this.typeId;
+	}
+
+	public void setTypeId(final Integer typeId) {
+		this.typeId = typeId;
+	}
+
+	public Integer getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(final Integer status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		return Pojomatic.hashCode(this);
+	}
+
+	@Override
+	public String toString() {
+		return Pojomatic.toString(this);
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		return Pojomatic.equals(this, o);
+	}
+}
