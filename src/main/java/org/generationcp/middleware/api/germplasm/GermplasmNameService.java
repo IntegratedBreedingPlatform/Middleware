e74822d534668b4bfdf5fc6d54e5b363c87e3d53
cuenyad <cuenya.diego@gmail.com>
2021-03-12 18:24:21 -0300

    Implement new Germplasm Name service.
    Issue: IBP-4434
    Review: none

---
+++ src/main/java/org/generationcp/middleware/api/germplasm/GermplasmNameService.java
@@ -0,0 +1,19 @@
+package org.generationcp.middleware.api.germplasm;
+
+import org.generationcp.middleware.domain.germplasm.GermplasmNameDto;
+import org.generationcp.middleware.domain.germplasm.GermplasmNameRequestDto;
+import org.generationcp.middleware.pojos.Name;
+import org.generationcp.middleware.pojos.UserDefinedField;
+
+public interface GermplasmNameService {
+
+	Name getNameByNameId(Integer nameId);
+
+	UserDefinedField getNameType(final Integer nameTypeId);
+
+	void deleteName(Integer nameId);
+
+	void updateName(final GermplasmNameRequestDto germplasmNameRequestDto);
+
+	Integer createName(final GermplasmNameRequestDto germplasmNameRequestDto);
+}
---
+++ src/main/java/org/generationcp/middleware/api/germplasm/GermplasmNameServiceImpl.java
@@ -0,0 +1,80 @@
+package org.generationcp.middleware.api.germplasm;
+
+import org.apache.commons.lang.StringUtils;
+import org.generationcp.middleware.api.nametype.GermplasmNameTypeDTO;
+import org.generationcp.middleware.domain.germplasm.GermplasmNameDto;
+import org.generationcp.middleware.domain.germplasm.GermplasmNameRequestDto;
+import org.generationcp.middleware.hibernate.HibernateSessionProvider;
+import org.generationcp.middleware.manager.DaoFactory;
+import org.generationcp.middleware.pojos.Name;
+import org.generationcp.middleware.pojos.UserDefinedField;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class GermplasmNameServiceImpl implements GermplasmNameService {
+
+	private final DaoFactory daoFactory;
+
+	public GermplasmNameServiceImpl(final HibernateSessionProvider sessionProvider) {
+		this.daoFactory = new DaoFactory(sessionProvider);
+	}
+
+	@Override
+	public Name getNameByNameId(final Integer nameId) {
+		return daoFactory.getNameDao().getById(nameId);
+	}
+
+	@Override
+	public UserDefinedField getNameType(final Integer nameTypeId) {
+		return this.daoFactory.getUserDefinedFieldDAO().getById(nameTypeId);
+	}
+
+	@Override
+	public void deleteName(final Integer nameId) {
+		final Name name = daoFactory.getNameDao().getById(nameId);
+		daoFactory.getNameDao().makeTransient(name);
+	}
+
+	@Override
+	public void updateName(final GermplasmNameRequestDto germplasmNameRequestDto){
+		final Name name = daoFactory.getNameDao().getById(germplasmNameRequestDto.getId());
+
+		if (germplasmNameRequestDto.getTypeId() != null) {
+			name.setTypeId(germplasmNameRequestDto.getTypeId());
+		}
+
+		if(!StringUtils.isBlank(germplasmNameRequestDto.getName())){
+			name.setNval(germplasmNameRequestDto.getName());
+		}
+
+		if (germplasmNameRequestDto.getDate() != null) {
+			name.setNdate(germplasmNameRequestDto.getDate());
+		}
+
+		if (germplasmNameRequestDto.getLocationId() != null) {
+			name.setLocationId(germplasmNameRequestDto.getLocationId());
+		}
+
+		if (germplasmNameRequestDto.getStatus() != null) {
+			name.setNstat(germplasmNameRequestDto.getStatus());
+		}
+		daoFactory.getNameDao().save(name);
+	}
+
+	@Override
+	public Integer createName(final GermplasmNameRequestDto germplasmNameRequestDto) {
+		final Name name = new Name();
+		name.setGermplasmId(germplasmNameRequestDto.getGid());
+		name.setTypeId(germplasmNameRequestDto.getTypeId());
+		name.setNval(germplasmNameRequestDto.getName());
+		name.setNdate(germplasmNameRequestDto.getDate());
+		name.setLocationId(germplasmNameRequestDto.getLocationId());
+		name.setNstat(germplasmNameRequestDto.getStatus());
+		daoFactory.getNameDao().save(name);
+		return name.getNid();
+	}
+}
