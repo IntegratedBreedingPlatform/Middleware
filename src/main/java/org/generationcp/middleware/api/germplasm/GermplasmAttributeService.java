b20ff3f6fbd6f8c14e5b8c5eb4c381b81b088161
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2021-03-12 17:41:53 +0800

    Added method for creating GermplasmAttributeDto
    IBP-4415

--- src/main/java/org/generationcp/middleware/api/germplasm/GermplasmAttributeService.java
+++ src/main/java/org/generationcp/middleware/api/germplasm/GermplasmAttributeService.java
@@ -1,11 +1,14 @@
 package org.generationcp.middleware.api.germplasm;
 
 import org.generationcp.middleware.domain.germplasm.GermplasmAttributeDto;
+import org.generationcp.middleware.domain.germplasm.GermplasmAttributeRequestDto;
 
 import java.util.List;
 
 public interface GermplasmAttributeService {
 
 	List<GermplasmAttributeDto> getGermplasmAttributeDtos(Integer gid, String attributeType);
+
+	Integer createGermplasmAttributeDto(Integer gid, GermplasmAttributeRequestDto dto, Integer userId);
 
 }
--- src/main/java/org/generationcp/middleware/api/germplasm/GermplasmAttributeServiceImpl.java
+++ src/main/java/org/generationcp/middleware/api/germplasm/GermplasmAttributeServiceImpl.java
@@ -1,8 +1,11 @@
 package org.generationcp.middleware.api.germplasm;
 
 import org.generationcp.middleware.domain.germplasm.GermplasmAttributeDto;
+import org.generationcp.middleware.domain.germplasm.GermplasmAttributeRequestDto;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 import org.generationcp.middleware.manager.DaoFactory;
+import org.generationcp.middleware.pojos.Attribute;
+import org.generationcp.middleware.pojos.UDTableType;
 
 import java.util.List;
 
@@ -17,6 +20,20 @@ public class GermplasmAttributeServiceImpl implements GermplasmAttributeService 
 	@Override
 	public List<GermplasmAttributeDto> getGermplasmAttributeDtos(final Integer gid, final String attributeType) {
 		return this.daoFactory.getAttributeDAO().getGermplasmAttributeDtos(gid, attributeType);
+	}
+
+	@Override
+	public Integer createGermplasmAttributeDto(Integer gid, GermplasmAttributeRequestDto dto, final Integer userId) {
+		final Attribute newAttribute = new Attribute();
+		newAttribute.setGermplasmId(gid);
+		newAttribute.setTypeId(this.daoFactory.getUserDefinedFieldDAO().getByTableTypeAndCode(UDTableType.ATRIBUTS_ATTRIBUTE.getTable(),
+			dto.getAttributeType(), dto.getAttributeCode()).getFldno());
+		newAttribute.setUserId(userId);
+		newAttribute.setAval(dto.getValue());
+		newAttribute.setLocationId(dto.getLocationId());
+		newAttribute.setReferenceId(0);
+		newAttribute.setAdate(Integer.valueOf(dto.getDate()));
+		return this.daoFactory.getAttributeDAO().save(newAttribute).getAid();
 	}
 
 }
