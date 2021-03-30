1524457bfb7319bed9c326fa3a9044ffb429bf03
cuenyad <cuenya.diego@gmail.com>
2021-03-30 18:03:33 -0300

    Adding GermplasmSearchLabelPrinting
    Issue: IBP-4205
    Review: none

--- src/main/java/org/generationcp/middleware/domain/labelprinting/LabelPrintingType.java
+++ src/main/java/org/generationcp/middleware/domain/labelprinting/LabelPrintingType.java
@@ -3,7 +3,7 @@ package org.generationcp.middleware.domain.labelprinting;
 public enum LabelPrintingType {
 
 	OBSERVATION_DATASET("ObservationDataset"), SUBOBSERVATION_DATASET("SubObservationDataset"),
-	LOT("Lot");
+	LOT("Lot"), GERMPLASM_SEARCH("GermplasmSearch");
 
 	private String code;
 
