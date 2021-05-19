ea11b55adf37c31d46b428b28d63ad73e4b12069
cuenyad <cuenya.diego@gmail.com>
2021-05-19 15:23:56 -0300

    Fixing test.
    IBP-4397
    Review none

--- src/test/java/org/generationcp/middleware/api/program/ProgramServiceImplTest.java
+++ src/test/java/org/generationcp/middleware/api/program/ProgramServiceImplTest.java
@@ -2,50 +2,85 @@ package org.generationcp.middleware.api.program;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.generationcp.middleware.IntegrationTestBase;
+import org.generationcp.middleware.WorkbenchTestDataUtil;
 import org.generationcp.middleware.manager.WorkbenchDaoFactory;
 import org.generationcp.middleware.pojos.workbench.CropType;
 import org.generationcp.middleware.pojos.workbench.Project;
 import org.generationcp.middleware.pojos.workbench.WorkbenchUser;
+import org.generationcp.middleware.service.api.user.UserService;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class ProgramServiceImplTest extends IntegrationTestBase {
 
 	private ProgramServiceImpl programService;
 
-	private Project testProject;
+	private Project testProject1;
 
-	private WorkbenchUser workbenchUser;
+	private Project testProject2;
+
+	private WorkbenchUser workbenchUserTest;
+
+	@Autowired
+	private WorkbenchTestDataUtil workbenchTestDataUtil;
+
+	@Autowired
+	private UserService userService;
+
+	private WorkbenchDaoFactory daoFactory;
 
 	@Before
 	public void setUp() {
 		this.programService = new ProgramServiceImpl(this.workbenchSessionProvider);
-		final WorkbenchDaoFactory daoFactory = new WorkbenchDaoFactory(this.workbenchSessionProvider);
+		this.daoFactory = new WorkbenchDaoFactory(this.workbenchSessionProvider);
 
-		if (this.testProject == null) {
-			this.testProject = new Project();
-			this.testProject.setProjectName("Project " + RandomStringUtils.randomAlphanumeric(10));
-			this.testProject.setStartDate(new Date());
-			this.testProject.setUniqueID(UUID.randomUUID().toString());
-			this.testProject.setLastOpenDate(new Date());
-			this.testProject.setCropType(new CropType("maize"));
-			daoFactory.getProjectDAO().save(this.testProject);
+		if (this.testProject1 == null) {
+			this.testProject1 = this.buildProject("Project1 ");
+		}
+		if (this.testProject2 == null) {
+			this.testProject2 = this.buildProject("Project2 ");
 		}
 
-		final Integer userId = this.findAdminUser();
-		this.workbenchUser = daoFactory.getWorkbenchUserDAO().getById(userId);
+		final WorkbenchUser user = this.workbenchTestDataUtil.createTestUserData();
+		this.workbenchUserTest = this.userService.addUser(user);
 	}
 
 	@Test
-	public void saveOrUpdateProjectUserInfo() {
-		this.programService.saveOrUpdateProjectUserInfo(this.workbenchUser.getUserid(), this.testProject.getUniqueID());
-		final ProgramDTO programDTO = this.programService.getLastOpenedProject(this.workbenchUser.getUserid());
-		assertEquals(programDTO.getUniqueID(), this.testProject.getUniqueID());
+	public void saveProjectUserInfo() {
+		this.programService.saveOrUpdateProjectUserInfo(this.workbenchUserTest.getUserid(), this.testProject1.getUniqueID());
+		final ProgramDTO programDTO = this.programService.getLastOpenedProject(this.workbenchUserTest.getUserid());
+		assertEquals(programDTO.getUniqueID(), this.testProject1.getUniqueID());
 	}
 
+	@Test
+	public void updateProjectUserInfo() {
+		final ProgramDTO programDTO = this.programService.getLastOpenedProject(this.workbenchUserTest.getUserid());
+		assertNull(programDTO);
+
+		this.programService.saveOrUpdateProjectUserInfo(this.workbenchUserTest.getUserid(), this.testProject1.getUniqueID());
+		final ProgramDTO programDTO1 = this.programService.getLastOpenedProject(this.workbenchUserTest.getUserid());
+		assertEquals(programDTO1.getUniqueID(), this.testProject1.getUniqueID());
+
+		this.programService.saveOrUpdateProjectUserInfo(this.workbenchUserTest.getUserid(), this.testProject2.getUniqueID());
+		final ProgramDTO programDTO2 = this.programService.getLastOpenedProject(this.workbenchUserTest.getUserid());
+		assertEquals(programDTO2.getUniqueID(), this.testProject2.getUniqueID());
+	}
+
+	private Project buildProject(final String projectName){
+		final Project project = new Project();
+		project.setProjectName(projectName + RandomStringUtils.randomAlphanumeric(10));
+		project.setStartDate(new Date());
+		project.setUniqueID(UUID.randomUUID().toString());
+		project.setLastOpenDate(new Date());
+		project.setCropType(new CropType("maize"));
+		this.daoFactory.getProjectDAO().save(project);
+		return project;
+	}
 }
