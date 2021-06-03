8ccee2474ba7632d856dbd3ce4849e79fe8e2f08
cuenyad <cuenya.diego@gmail.com>
2021-06-03 17:43:05 -0300

    return Variable instead AttributeDTO and adding programUUID to recover the alias from the variables if exists.
    After the migration of attribute to cvterm the AttributeDTO is not need more.
    
    Issue: IBP-4659
    Review: none

--- src/main/java/org/generationcp/middleware/api/attribute/AttributeService.java
+++ src/main/java/org/generationcp/middleware/api/attribute/AttributeService.java
@@ -1,7 +1,9 @@
 package org.generationcp.middleware.api.attribute;
 
+import org.generationcp.middleware.domain.ontology.Variable;
+
 import java.util.List;
 
 public interface AttributeService {
-	List<AttributeDTO> searchAttributes(String name);
+	List<Variable> searchAttributes(String name, String programUUID);
 }
