7981cb09b8e2add909666e7d83545403cdaf9c62
cuenyad <cuenya.diego@gmail.com>
2021-06-04 15:24:07 -0300

    modifying the getTypesFromUserDefinedFieldTable query to get Names and create getAttributeFromOntology to get attributes from cvterm.
    Issue: IBP-4659
    Review: none

--- src/main/java/org/generationcp/middleware/dao/GermplasmSearchDAO.java
+++ src/main/java/org/generationcp/middleware/dao/GermplasmSearchDAO.java
@@ -208,7 +208,7 @@ public class GermplasmSearchDAO extends GenericDAO<Germplasm, Integer> {
 				return new ArrayList<>(germplasmSearchResult);
 			}
 
-			final Map<String, Integer> attributeTypesMap = this.getAttributeTypesMap(germplasmSearchParameter.getAddedColumnsPropertyIds());
+			final Map<String, Integer> attributeTypesMap = this.getAttributeTypesMap(germplasmSearchParameter.getAddedColumnsPropertyIds(), null);
 			final Map<String, Integer> nameTypesMap = this.getNameTypesMap(germplasmSearchParameter.getAddedColumnsPropertyIds());
 
 			// Query for values for added columns
@@ -672,7 +672,7 @@ public class GermplasmSearchDAO extends GenericDAO<Germplasm, Integer> {
 
 	}
 
-	protected Map<String, Integer> getAttributeTypesMap(final List<String> addedColumnsPropertyIds) {
+	protected Map<String, Integer> getAttributeTypesMap(final List<String> addedColumnsPropertyIds, final String programUUID) {
 
 		final List<String> nonStandardColumns = new ArrayList<>();
 		for (final String propertyId : addedColumnsPropertyIds) {
@@ -681,7 +681,7 @@ public class GermplasmSearchDAO extends GenericDAO<Germplasm, Integer> {
 			}
 		}
 
-		return this.getTypesFromUserDefinedFieldTable(addedColumnsPropertyIds, nonStandardColumns, GermplasmSearchDAO.ATRIBUTS);
+		return this.getAttributeFromOntology(addedColumnsPropertyIds, nonStandardColumns, programUUID);
 	}
 
 	protected Map<String, Integer> getNameTypesMap(final List<String> addedColumnsPropertyIds) {
@@ -693,17 +693,41 @@ public class GermplasmSearchDAO extends GenericDAO<Germplasm, Integer> {
 			}
 		}
 
-		return this.getTypesFromUserDefinedFieldTable(addedColumnsPropertyIds, nonStandardColumns, "NAMES");
+		return this.getNameTypesFromUserDefinedFieldTable(addedColumnsPropertyIds, nonStandardColumns);
 	}
 
-	private Map<String, Integer> getTypesFromUserDefinedFieldTable(final List<String> addedColumnsPropertyIds,
-		final List<String> nonStandardColumns, final String ftable) {
+	private Map<String, Integer> getNameTypesFromUserDefinedFieldTable(final List<String> addedColumnsPropertyIds,
+		final List<String> nonStandardColumns) {
 		final Map<String, Integer> typesMap = new HashMap<>();
-		final String field = GermplasmSearchDAO.ATRIBUTS.equals(ftable) ? "fcode" : "fname";
 		if (!nonStandardColumns.isEmpty()) {
 			final SQLQuery query = this.getSession()
-				.createSQLQuery("SELECT " + field + ", fldno from udflds where ftable = :ftable and " + field + " IN (:fieldList)");
-			query.setParameter("ftable", ftable);
+				.createSQLQuery("SELECT fname , fldno from udflds where ftable = 'NAMES' and fname IN (:fieldList)");
+			query.setParameterList("fieldList", addedColumnsPropertyIds);
+			final List<Object[]> results = query.list();
+
+			for (final Object[] row : results) {
+				typesMap.put(String.valueOf(row[0]).toUpperCase(), (Integer) row[1]);
+			}
+
+		}
+
+		return typesMap;
+	}
+
+	private Map<String, Integer> getAttributeFromOntology(final List<String> addedColumnsPropertyIds,
+		final List<String> nonStandardColumns, final String programUUID) {
+		final Map<String, Integer> typesMap = new HashMap<>();
+		if (!nonStandardColumns.isEmpty()) {
+			final SQLQuery query = this.getSession()
+				.createSQLQuery("SELECT if(vo.alias is null,cv.name ,vo.alias) AS name, cv.cvterm_id "
+					+ " FROM cvterm cv INNER JOIN cvtermprop cp ON cp.type_id = " + TermId.VARIABLE_TYPE.getId() + " and cv.cvterm_id = cp.cvterm_id " //
+					+ " LEFT JOIN variable_overrides vo ON vo.cvterm_id = cv.cvterm_id AND vo.program_uuid = :programUUID " //
+					+ " WHERE cp.value in (select name from cvterm where cvterm_id in ("
+					+ VariableType.GERMPLASM_PASSPORT.getId() + ","
+					+ VariableType.GERMPLASM_ATTRIBUTE.getId() + ")) " //
+					+ "and cv.name IN (:fieldList) or vo.alias IN (:fieldList)");
+			query.setParameter("programUUID", programUUID);
+			query.setParameterList("fieldList", addedColumnsPropertyIds);
 			query.setParameterList("fieldList", addedColumnsPropertyIds);
 			final List<Object[]> results = query.list();
 
@@ -740,7 +764,7 @@ public class GermplasmSearchDAO extends GenericDAO<Germplasm, Integer> {
 			germplasmSearchRequest.getAddedColumnsPropertyIds().replaceAll(String::toUpperCase);
 
 			final List<String> addedColumnsPropertyIds = germplasmSearchRequest.getAddedColumnsPropertyIds();
-			final Map<String, Integer> attributeTypesMap = this.getAttributeTypesMap(addedColumnsPropertyIds);
+			final Map<String, Integer> attributeTypesMap = this.getAttributeTypesMap(addedColumnsPropertyIds, programUUID);
 			final Map<String, Integer> nameTypesMap = this.getNameTypesMap(addedColumnsPropertyIds);
 
 			// main query
@@ -944,7 +968,7 @@ public class GermplasmSearchDAO extends GenericDAO<Germplasm, Integer> {
 		final Pageable pageable, final String programUUID) {
 
 		final List<String> addedColumnsPropertyIds = germplasmSearchRequest.getAddedColumnsPropertyIds();
-		final Map<String, Integer> attributeTypesMap = this.getAttributeTypesMap(addedColumnsPropertyIds);
+		final Map<String, Integer> attributeTypesMap = this.getAttributeTypesMap(addedColumnsPropertyIds, programUUID);
 		final Map<String, Integer> nameTypesMap = this.getNameTypesMap(addedColumnsPropertyIds);
 
 		// main query
@@ -1008,7 +1032,7 @@ public class GermplasmSearchDAO extends GenericDAO<Germplasm, Integer> {
 			}
 
 			final List<String> addedColumnsPropertyIds = germplasmSearchRequest.getAddedColumnsPropertyIds();
-			final Map<String, Integer> attributeTypesMap = this.getAttributeTypesMap(addedColumnsPropertyIds);
+			final Map<String, Integer> attributeTypesMap = this.getAttributeTypesMap(addedColumnsPropertyIds, programUUID);
 			final Map<String, Integer> nameTypesMap = this.getNameTypesMap(addedColumnsPropertyIds);
 
 			// main query
