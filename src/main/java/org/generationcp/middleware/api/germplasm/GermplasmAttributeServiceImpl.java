f21f54414a4cfa54e763fb7e1976676b9ca9be9c
Clarysabel Tovar <clarysabel@leafnode.io>
2021-06-07 19:23:10 -0300

    Fixing tests, adding logic to resolve categorical value id for categorical variables IBP-4440

--- src/main/java/org/generationcp/middleware/api/germplasm/GermplasmAttributeServiceImpl.java
+++ src/main/java/org/generationcp/middleware/api/germplasm/GermplasmAttributeServiceImpl.java
@@ -2,15 +2,23 @@ package org.generationcp.middleware.api.germplasm;
 
 import org.generationcp.middleware.domain.germplasm.GermplasmAttributeDto;
 import org.generationcp.middleware.domain.germplasm.GermplasmAttributeRequestDto;
+import org.generationcp.middleware.domain.oms.TermId;
+import org.generationcp.middleware.domain.ontology.Variable;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 import org.generationcp.middleware.manager.DaoFactory;
+import org.generationcp.middleware.manager.ontology.api.OntologyVariableDataManager;
 import org.generationcp.middleware.pojos.Attribute;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigInteger;
 import java.util.List;
 
 public class GermplasmAttributeServiceImpl implements GermplasmAttributeService {
 
 	private final DaoFactory daoFactory;
+
+	@Autowired
+	private OntologyVariableDataManager ontologyVariableDataManager;
 
 	public GermplasmAttributeServiceImpl(final HibernateSessionProvider sessionProvider) {
 		this.daoFactory = new DaoFactory(sessionProvider);
@@ -23,6 +31,7 @@ public class GermplasmAttributeServiceImpl implements GermplasmAttributeService 
 
 	@Override
 	public Integer createGermplasmAttribute(final Integer gid, final GermplasmAttributeRequestDto dto, final Integer userId) {
+		final Variable variable = this.ontologyVariableDataManager.getVariable(null, dto.getVariableId(), false);
 		final Attribute newAttribute = new Attribute();
 		newAttribute.setGermplasmId(gid);
 		newAttribute.setTypeId(dto.getVariableId());
@@ -30,15 +39,18 @@ public class GermplasmAttributeServiceImpl implements GermplasmAttributeService 
 		newAttribute.setLocationId(dto.getLocationId());
 		newAttribute.setReferenceId(0);
 		newAttribute.setAdate(Integer.valueOf(dto.getDate()));
+		newAttribute.setcValueId(this.resolveCategoricalValueId(variable, dto.getValue()));
 		return this.daoFactory.getAttributeDAO().save(newAttribute).getAid();
 	}
 
 	@Override
 	public void updateGermplasmAttribute(final Integer attributeId, final GermplasmAttributeRequestDto dto) {
+		final Variable variable = this.ontologyVariableDataManager.getVariable(null, dto.getVariableId(), false);
 		final Attribute attribute = this.daoFactory.getAttributeDAO().getById(attributeId);
 		attribute.setAval(dto.getValue());
 		attribute.setLocationId(dto.getLocationId());
 		attribute.setAdate(Integer.valueOf(dto.getDate()));
+		attribute.setcValueId(this.resolveCategoricalValueId(variable, dto.getValue()));
 		this.daoFactory.getAttributeDAO().update(attribute).getAid();
 	}
 
@@ -46,6 +58,16 @@ public class GermplasmAttributeServiceImpl implements GermplasmAttributeService 
 	public void deleteGermplasmAttribute(final Integer attributeId) {
 		final Attribute attribute = this.daoFactory.getAttributeDAO().getById(attributeId);
 		this.daoFactory.getAttributeDAO().makeTransient(attribute);
+	}
+
+	private Integer resolveCategoricalValueId(final Variable variable, final String value) {
+		BigInteger categoricalValueId = null;
+		if (variable.getScale().getDataType().getId().equals(TermId.CATEGORICAL_VARIABLE.getId())) {
+			categoricalValueId =
+				variable.getScale().getCategories().stream().filter(category -> value.equalsIgnoreCase(category.getName())).findFirst()
+					.map(category -> BigInteger.valueOf(category.getId())).orElse(null);
+		}
+		return categoricalValueId != null ? categoricalValueId.intValue() : null;
 	}
 
 }
