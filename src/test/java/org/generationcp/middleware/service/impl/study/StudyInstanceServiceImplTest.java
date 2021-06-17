e456aa854c60040e6f0b75013a6fd0a2c1265f61
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2021-06-17 09:17:20 +0800

    Added missing library
    IBP-4531

--- src/test/java/org/generationcp/middleware/service/impl/study/StudyInstanceServiceImplTest.java
+++ src/test/java/org/generationcp/middleware/service/impl/study/StudyInstanceServiceImplTest.java
@@ -49,6 +49,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 import java.util.UUID;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
