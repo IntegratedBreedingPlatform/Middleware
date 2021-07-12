cca4fcdb668d6f4881fe90fa156f6c832d730563
cuenyad <cuenya.diego@gmail.com>
2021-07-12 10:53:02 -0300

    Modifying tests by PR comments.
    Issue: IBP-4607
    Review: none

--- src/test/java/org/generationcp/middleware/api/nametype/GermplasmNameTypeServiceImplIntegrationTest.java
+++ src/test/java/org/generationcp/middleware/api/nametype/GermplasmNameTypeServiceImplIntegrationTest.java
@@ -29,20 +29,27 @@ public class GermplasmNameTypeServiceImplIntegrationTest extends IntegrationTest
 	}
 
 	@Test
-	public void testCreateNameType(){
+	public void testCreateNameType_Ok(){
+		final String name = "TEST" + RandomStringUtils.randomAlphabetic(10);
+		final String code = "TEST" + RandomStringUtils.randomAlphabetic(10).toUpperCase();
+		final String description = "Test Description" + RandomStringUtils.randomAlphabetic(10);
+
 		final GermplasmNameTypeRequestDTO germplasmNameTypeRequestDTO = new GermplasmNameTypeRequestDTO();
-		germplasmNameTypeRequestDTO.setName("TEST" + RandomStringUtils.randomAlphabetic(10));
-		germplasmNameTypeRequestDTO.setCode("TEST" + RandomStringUtils.randomAlphabetic(10));
-		germplasmNameTypeRequestDTO.setDescription("Test Description" + RandomStringUtils.randomAlphabetic(10));
+		germplasmNameTypeRequestDTO.setName(name);
+		germplasmNameTypeRequestDTO.setCode(code);
+		germplasmNameTypeRequestDTO.setDescription(description);
 		final Integer nameTypeId = this.germplasmNameTypeService.createNameType(germplasmNameTypeRequestDTO);
 		final Set<String> codes = new HashSet<>(Arrays.asList(germplasmNameTypeRequestDTO.getCode()));
 		final List<GermplasmNameTypeDTO> nameTypeRequestDTOs = this.germplasmNameTypeService.filterGermplasmNameTypes(codes);
 		Assert.assertThat(1, equalTo(nameTypeRequestDTOs.size()));
 		Assert.assertThat(nameTypeId, equalTo(nameTypeRequestDTOs.get(0).getId()));
+		Assert.assertThat(name, equalTo(nameTypeRequestDTOs.get(0).getName()));
+		Assert.assertThat(code, equalTo(nameTypeRequestDTOs.get(0).getCode()));
+		Assert.assertThat(description, equalTo(nameTypeRequestDTOs.get(0).getDescription()));
 	}
 
 	@Test
-	public void testGetNameTypes() {
+	public void testGetNameTypes_Pagination() {
 		final List<GermplasmNameTypeDTO> germplasmNameTypeDTOs = this.germplasmNameTypeService.getNameTypes(new PageRequest(0, 10));
 		Assert.assertThat(10, equalTo(germplasmNameTypeDTOs.size()));
 	}
