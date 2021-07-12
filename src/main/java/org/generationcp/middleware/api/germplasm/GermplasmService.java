48763dfadd1e6104af3d6fe32041a64fef8ffcf5
Clarysabel Tovar <clarysabel@leafnode.io>
2021-07-12 12:51:55 -0300

    Fix typo IBP-4766

--- src/main/java/org/generationcp/middleware/api/germplasm/GermplasmService.java
+++ src/main/java/org/generationcp/middleware/api/germplasm/GermplasmService.java
@@ -108,6 +108,6 @@ public interface GermplasmService {
 
 	void updateGermplasmBasicDetails(Integer gid, GermplasmBasicDetailsDto germplasmBasicDetailsDto);
 
-	void updateGermplasmPedigree(Integer gid, ProgenitorsUpdateRequestDto progenitorsUpdateRequstDto);
+	void updateGermplasmPedigree(Integer gid, ProgenitorsUpdateRequestDto progenitorsUpdateRequestDto);
 
 }
