90f95d17e573dd48b759a9f1d9ccde23b593caef
Nahuel Soldevilla <nahuel@leafnode.io>
2021-07-14 11:52:13 -0300

    IBP-4732: Fix merge from master / Adapt ObservationUnitIDGenerator
    Apply changes of 4a51febf8b8f645e304c93b81063ba1a4f51fb65 to renamed
    class ObservationUnitIDGenerator

--- src/main/java/org/generationcp/middleware/service/api/ObservationUnitIDGenerator.java
+++ src/main/java/org/generationcp/middleware/service/api/ObservationUnitIDGenerator.java
@@ -5,6 +5,7 @@ import com.google.common.base.Preconditions;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.generationcp.middleware.pojos.dms.ExperimentModel;
 import org.generationcp.middleware.pojos.workbench.CropType;
+import org.generationcp.middleware.util.uid.UIDGenerator;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
@@ -12,26 +13,23 @@ import java.util.UUID;
 
 public class ObservationUnitIDGenerator {
 
-	public static final String MID_STRING = "P";
-
+	public static final UIDGenerator.UID_ROOT UID_ROOT = UIDGenerator.UID_ROOT.OBSERVATION_UNIT;
 	public static final int SUFFIX_LENGTH = 8;
 
 	public static void generateObservationUnitIds(final CropType crop, final List<ExperimentModel> experiments) {
-		Preconditions.checkNotNull(crop);
-		Preconditions.checkState(!CollectionUtils.isEmpty(experiments));
+		UIDGenerator.<ExperimentModel>generate(crop, experiments, UID_ROOT, SUFFIX_LENGTH,
+			new UIDGenerator.UIDAdapter<ExperimentModel>() {
 
-		final boolean doUseUUID = crop.isUseUUID();
-		for (final ExperimentModel experiment : experiments) {
-			if (experiment.getObsUnitId() == null) {
-				if (doUseUUID) {
-					experiment.setObsUnitId(UUID.randomUUID().toString());
-				} else {
-					final String cropPrefix = crop.getPlotCodePrefix();
-					experiment.setObsUnitId(cropPrefix + MID_STRING
-						+ RandomStringUtils.randomAlphanumeric(SUFFIX_LENGTH));
+				@Override
+				public String getUID(final ExperimentModel entry) {
+					return entry.getObsUnitId();
 				}
-			}
-		}
-	}
 
+				@Override
+				public void setUID(final ExperimentModel entry, final String uid) {
+					entry.setObsUnitId(uid);
+				}
+			});
+
+	}
 }
