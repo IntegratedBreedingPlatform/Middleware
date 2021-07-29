3f410eabf1834469c3ee0ae5981e828f1fbb0df1
Darla Grace Hermo <darla@leafnode.io>
2021-07-29 11:22:16 +0800

    Register validators for Person, Breeding Method, Location, Numeric Db ID data types
    IBP-4667 / IBP-4680

--- src/main/java/org/generationcp/middleware/service/api/ontology/VariableDataValidatorFactoryImpl.java
+++ src/main/java/org/generationcp/middleware/service/api/ontology/VariableDataValidatorFactoryImpl.java
@@ -1,27 +1,48 @@
 package org.generationcp.middleware.service.api.ontology;
 
 import org.generationcp.middleware.domain.ontology.DataType;
-import org.generationcp.middleware.reports.Reporter;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.EnumMap;
 import java.util.Optional;
 
 public class VariableDataValidatorFactoryImpl implements VariableDataValidatorFactory {
 
-	private Map<DataType, VariableValueValidator> dataTypeValidatorMap = new HashMap<>();
+	private final EnumMap<DataType, VariableValueValidator> dataTypeValidatorMap = new EnumMap<>(DataType.class);
+
+	@Autowired
+	private PersonValidator personValidator;
+
+	@Autowired
+	private BreedingMethodValidator breedingMethodValidator;
+
+	@Autowired
+	private LocationValidator locationValidator;
 
 	@Override
 	public Optional<VariableValueValidator> getValidator(final DataType dataType) {
 		// Look for validator in custom or prioritized registered validators
 		if (this.dataTypeValidatorMap.containsKey(dataType)) {
 			return Optional.of(this.dataTypeValidatorMap.get(dataType));
-		} else if (DataType.NUMERIC_VARIABLE.getId().equals(dataType.getId())) {
+
+		} else if (DataType.PERSON.getId().equals(dataType.getId())) {
+			return Optional.of(this.personValidator);
+
+		} else if (DataType.BREEDING_METHOD.getId().equals(dataType.getId()) ) {
+			return Optional.of(this.breedingMethodValidator);
+
+		} else if (DataType.LOCATION.getId().equals(dataType.getId()) ) {
+			return Optional.of(this.locationValidator);
+
+		} else if (NumericValueValidator.NUMERIC_DATATYPES.contains(dataType.getId())) {
 			return Optional.of(new NumericValueValidator());
+
 		} else if (DataType.CHARACTER_VARIABLE.getId().equals(dataType.getId())) {
 			return Optional.of(new CharacterValueValidator());
+
 		} else if (DataType.DATE_TIME_VARIABLE.getId().equals(dataType.getId())) {
 			return Optional.of(new DateValueValidator());
+
 		} else if (DataType.CATEGORICAL_VARIABLE.getId().equals(dataType.getId())) {
 			return Optional.of(new CategoricalValueDescriptionValidator());
 		}
