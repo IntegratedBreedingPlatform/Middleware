5ee5c972e5fb41023e887000725639d49d0fa702
Darla Grace Hermo <darla@leafnode.io>
2021-07-29 10:28:52 +0800

    Implemented BreedingMethod validator
    IBP-4667 / IBP-4680

---
+++ src/main/java/org/generationcp/middleware/service/api/ontology/BreedingMethodValidator.java
@@ -0,0 +1,54 @@
+package org.generationcp.middleware.service.api.ontology;
+
+import liquibase.util.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.generationcp.middleware.api.breedingmethod.BreedingMethodSearchRequest;
+import org.generationcp.middleware.api.breedingmethod.BreedingMethodService;
+import org.generationcp.middleware.domain.etl.MeasurementVariable;
+import org.generationcp.middleware.domain.oms.TermId;
+import org.generationcp.middleware.domain.ontology.DataType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import java.util.Collections;
+
+@Configurable
+public class BreedingMethodValidator implements VariableValueValidator {
+
+	public static final Integer SCALE_BM_ID = 1909;
+	public static final Integer SCALE_BM_NAME = 1910;
+
+	@Autowired
+	private BreedingMethodService breedingMethodService;
+
+	@Override
+	public boolean isValid(final MeasurementVariable variable) {
+		this.ensureBreedingMethodDataType(variable.getDataTypeId());
+		final String value = variable.getValue();
+		if (!StringUtils.isEmpty(value)) {
+			final BreedingMethodSearchRequest request = new BreedingMethodSearchRequest();
+
+			final Integer scaleId = variable.getScaleId();
+			if (TermId.BREEDING_METHOD_SCALE.getId() == scaleId) {
+				request.setMethodAbbreviations(Collections.singletonList(value));
+			} else if (SCALE_BM_ID.equals(scaleId)) {
+				if (NumberUtils.isDigits(value)) {
+					request.setMethodIds(Collections.singletonList(Integer.parseInt(value)));
+				} else {
+					return false;
+				}
+			} else if (SCALE_BM_NAME.equals(scaleId)) {
+				request.setMethodNames(Collections.singletonList(value));
+			}
+			return this.breedingMethodService.countBreedingMethods(request) > 0;
+		}
+		return true;
+	}
+
+	private void ensureBreedingMethodDataType(final Integer dataTypeId) {
+		if (!DataType.BREEDING_METHOD.getId().equals(dataTypeId)) {
+			throw new IllegalStateException("The ensureBreedingMethodDataType method must never be called for non Breeding Method variables. "
+				+ "Please report this error to your administrator.");
+		}
+	}
+}
---
+++ src/test/java/org/generationcp/middleware/service/impl/ontology/BreedingMethodValidatorTest.java
@@ -0,0 +1,139 @@
+package org.generationcp.middleware.service.impl.ontology;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.generationcp.middleware.api.breedingmethod.BreedingMethodSearchRequest;
+import org.generationcp.middleware.api.breedingmethod.BreedingMethodService;
+import org.generationcp.middleware.api.location.search.LocationSearchRequest;
+import org.generationcp.middleware.domain.etl.MeasurementVariable;
+import org.generationcp.middleware.domain.oms.TermId;
+import org.generationcp.middleware.domain.ontology.DataType;
+import org.generationcp.middleware.service.api.ontology.BreedingMethodValidator;
+import org.generationcp.middleware.service.api.ontology.LocationValidator;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Collections;
+import java.util.Random;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BreedingMethodValidatorTest {
+
+	@Mock
+	private BreedingMethodService breedingMethodService;
+
+	@InjectMocks
+	private BreedingMethodValidator breedingMethodValidator;
+
+	@Test(expected = IllegalStateException.class)
+	public void test_isValid_NonBreedingMethodDataType_ShouldThrowException(){
+		final MeasurementVariable variable = new MeasurementVariable();
+		variable.setDataTypeId(DataType.CHARACTER_VARIABLE.getId());
+		this.breedingMethodValidator.isValid(variable);
+	}
+
+	@Test
+	public void test_isValid_EmptyValue(){
+		final MeasurementVariable variable = new MeasurementVariable();
+		variable.setDataTypeId(DataType.BREEDING_METHOD.getId());
+		Assert.assertTrue(this.breedingMethodValidator.isValid(variable));
+	}
+
+	@Test
+	public void test_isValid_BreedingMethodIDScale_ValidValue(){
+		final Integer id = new Random().nextInt(100);
+		final BreedingMethodSearchRequest request = new BreedingMethodSearchRequest();
+		request.setMethodIds(Collections.singletonList(id));
+		Mockito.doReturn(1L).when(this.breedingMethodService).countBreedingMethods(request);
+
+		final MeasurementVariable variable = new MeasurementVariable();
+		variable.setDataTypeId(DataType.BREEDING_METHOD.getId());
+		variable.setScaleId(BreedingMethodValidator.SCALE_BM_ID);
+		variable.setValue(id.toString());
+		Assert.assertTrue(this.breedingMethodValidator.isValid(variable));
+	}
+
+	@Test
+	public void test_isValid_BreedingMethodIDScale_NonExistingValue(){
+		final Integer id = new Random().nextInt(100);
+		final BreedingMethodSearchRequest request = new BreedingMethodSearchRequest();
+		request.setMethodIds(Collections.singletonList(id));
+		Mockito.doReturn(0L).when(this.breedingMethodService).countBreedingMethods(request);
+
+		final MeasurementVariable variable = new MeasurementVariable();
+		variable.setDataTypeId(DataType.BREEDING_METHOD.getId());
+		variable.setScaleId(BreedingMethodValidator.SCALE_BM_ID);
+		variable.setValue(id.toString());
+		Assert.assertFalse(this.breedingMethodValidator.isValid(variable));
+	}
+
+	@Test
+	public void test_isValid_BreedingMethodIDScale_NonDigitsValue(){
+		final MeasurementVariable variable = new MeasurementVariable();
+		variable.setDataTypeId(DataType.BREEDING_METHOD.getId());
+		variable.setScaleId(BreedingMethodValidator.SCALE_BM_ID);
+		variable.setValue("1.5");
+		Assert.assertFalse(this.breedingMethodValidator.isValid(variable));
+	}
+
+	@Test
+	public void test_isValid_BreedingMethodAbbrScale_ValidValue(){
+		final String abbr = RandomStringUtils.randomAlphabetic(20);
+		final BreedingMethodSearchRequest request = new BreedingMethodSearchRequest();
+		request.setMethodAbbreviations(Collections.singletonList(abbr));
+		Mockito.doReturn(1L).when(this.breedingMethodService).countBreedingMethods(request);
+
+		final MeasurementVariable variable = new MeasurementVariable();
+		variable.setDataTypeId(DataType.BREEDING_METHOD.getId());
+		variable.setScaleId(TermId.BREEDING_METHOD_SCALE.getId());
+		variable.setValue(abbr);
+		Assert.assertTrue(this.breedingMethodValidator.isValid(variable));
+	}
+
+	@Test
+	public void test_isValid_BreedingMethodAbbrScale_NonExistingAbbr(){
+		final String abbr = RandomStringUtils.randomAlphabetic(20);
+		final BreedingMethodSearchRequest request = new BreedingMethodSearchRequest();
+		request.setMethodAbbreviations(Collections.singletonList(abbr));
+		Mockito.doReturn(0L).when(this.breedingMethodService).countBreedingMethods(request);
+
+		final MeasurementVariable variable = new MeasurementVariable();
+		variable.setDataTypeId(DataType.BREEDING_METHOD.getId());
+		variable.setScaleId(TermId.BREEDING_METHOD_SCALE.getId());
+		variable.setValue(abbr);
+		Assert.assertFalse(this.breedingMethodValidator.isValid(variable));
+	}
+
+	@Test
+	public void test_isValid_BreedingMethodNameScale_ValidValue(){
+		final String name = RandomStringUtils.randomAlphabetic(20);
+		final BreedingMethodSearchRequest request = new BreedingMethodSearchRequest();
+		request.setMethodNames(Collections.singletonList(name));
+		Mockito.doReturn(1L).when(this.breedingMethodService).countBreedingMethods(request);
+
+		final MeasurementVariable variable = new MeasurementVariable();
+		variable.setDataTypeId(DataType.BREEDING_METHOD.getId());
+		variable.setScaleId(BreedingMethodValidator.SCALE_BM_NAME);
+		variable.setValue(name);
+		Assert.assertTrue(this.breedingMethodValidator.isValid(variable));
+	}
+
+	@Test
+	public void test_isValid_BreedingMethodNameScale_NonExistingName(){
+		final String name = RandomStringUtils.randomAlphabetic(20);
+		final BreedingMethodSearchRequest request = new BreedingMethodSearchRequest();
+		request.setMethodNames(Collections.singletonList(name));
+		Mockito.doReturn(0L).when(this.breedingMethodService).countBreedingMethods(request);
+
+		final MeasurementVariable variable = new MeasurementVariable();
+		variable.setDataTypeId(DataType.BREEDING_METHOD.getId());
+		variable.setScaleId(BreedingMethodValidator.SCALE_BM_NAME);
+		variable.setValue(name);
+		Assert.assertFalse(this.breedingMethodValidator.isValid(variable));
+	}
+
+}
