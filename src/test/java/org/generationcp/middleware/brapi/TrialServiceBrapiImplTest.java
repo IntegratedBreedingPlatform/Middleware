72d3b20138a4568373ad5f579d919f57b7c31b2f
Darla Grace Hermo <darla@leafnode.io>
2021-08-03 10:07:37 +0800

    Remove extra character (inadvertently committed)
    IBP-4667

--- src/test/java/org/generationcp/middleware/brapi/TrialServiceBrapiImplTest.java
+++ src/test/java/org/generationcp/middleware/brapi/TrialServiceBrapiImplTest.java
@@ -492,7 +492,7 @@ public class TrialServiceBrapiImplTest extends IntegrationTestBase {
 			.createStudy("StudyDeleted", "StudyDeleted-Description", 6, this.commonTestProject.getUniqueID(),
 				this.testUser.getUserid().toString(), null, null);
 		deletedStudy.setDeleted(true);
-		final DmsProject environmentDatasetDeleted =`
+		final DmsProject environmentDatasetDeleted =
 			this.testDataInitializer
 				.createDmsProject("Environment Dataset Deleted", "Environment Dataset-Description Deleted", deletedStudy, deletedStudy,
 					DatasetTypeEnum.SUMMARY_DATA);
