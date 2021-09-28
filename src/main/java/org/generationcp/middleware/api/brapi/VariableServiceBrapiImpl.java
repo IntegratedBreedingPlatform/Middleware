9a8a1a4d03d94e87a922e7948d81cc25a54f6bcd
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2021-09-28 14:38:17 +0800

    Initial commit for VariableServiceBrapi methods
    IBP-5014 / IBP-5053

---
+++ src/main/java/org/generationcp/middleware/api/brapi/VariableServiceBrapiImpl.java
@@ -0,0 +1,32 @@
+package org.generationcp.middleware.api.brapi;
+
+import org.generationcp.middleware.domain.search_request.brapi.v2.VariableSearchRequestDTO;
+import org.generationcp.middleware.hibernate.HibernateSessionProvider;
+import org.generationcp.middleware.manager.DaoFactory;
+import org.generationcp.middleware.service.api.study.VariableDTO;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VariableServiceBrapiImpl implements VariableServiceBrapi{
+
+    private HibernateSessionProvider sessionProvider;
+    private DaoFactory daoFactory;
+
+    public VariableServiceBrapiImpl() {
+        // no-arg constuctor is required by CGLIB proxying used by Spring 3x and older.
+    }
+    public VariableServiceBrapiImpl(final HibernateSessionProvider sessionProvider) {
+        this.daoFactory = new DaoFactory(sessionProvider);
+        this.sessionProvider = sessionProvider;
+    }
+
+    public List<VariableDTO> getObservationVariables(VariableSearchRequestDTO requestDTO, Pageable pageable) {
+        return new ArrayList<>();
+    }
+
+    public long countObservationVariables(VariableSearchRequestDTO requestDTO){
+        return 1;
+    }
+}
