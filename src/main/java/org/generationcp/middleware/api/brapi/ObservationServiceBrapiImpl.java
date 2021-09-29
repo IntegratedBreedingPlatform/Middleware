f60d94eb0df216ec3c4797a46ee2f46efd47976b
Aldrin Batac <aldrin.batac@leafnode.io>
2021-09-29 11:06:47 +0800

    Resolve the categorical id of observation's value if variable is categorical.
    IBP-5015

--- src/main/java/org/generationcp/middleware/api/brapi/ObservationServiceBrapiImpl.java
+++ src/main/java/org/generationcp/middleware/api/brapi/ObservationServiceBrapiImpl.java
@@ -3,6 +3,7 @@ package org.generationcp.middleware.api.brapi;
 import org.generationcp.middleware.api.brapi.v2.observation.ObservationDto;
 import org.generationcp.middleware.api.brapi.v2.observation.ObservationSearchRequestDto;
 import org.generationcp.middleware.dao.dms.PhenotypeDao;
+import org.generationcp.middleware.domain.dms.ValueReference;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 import org.generationcp.middleware.manager.DaoFactory;
 import org.generationcp.middleware.pojos.dms.ExperimentModel;
@@ -14,6 +15,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -45,6 +48,8 @@ public class ObservationServiceBrapiImpl implements ObservationServiceBrapi {
 		final Map<String, ExperimentModel> experimentModelMap = this.daoFactory.getExperimentDao()
 			.getByObsUnitIds(observations.stream().map(ObservationDto::getObservationUnitDbId).collect(Collectors.toList())).stream()
 			.collect(Collectors.toMap(ExperimentModel::getObsUnitId, Function.identity()));
+		final Map<Integer, List<ValueReference>> validValuesForCategoricalVariables =
+			this.getCategoriesForCategoricalVariables(observations);
 
 		final PhenotypeDao dao = this.daoFactory.getPhenotypeDAO();
 		for (final ObservationDto observation : observations) {
@@ -53,11 +58,30 @@ public class ObservationServiceBrapiImpl implements ObservationServiceBrapi {
 			phenotype.setUpdatedDate(new Date());
 			phenotype.setObservableId(Integer.valueOf(observation.getObservationVariableDbId()));
 			phenotype.setValue(observation.getValue());
-			phenotype.setcValue(0); // TODO:
+			phenotype.setcValue(
+				this.resolveCategoricalIdValue(Integer.valueOf(observation.getObservationVariableDbId()), observation.getValue(),
+					validValuesForCategoricalVariables));
 			phenotype.setExperiment(experimentModelMap.get(observation.getObservationUnitDbId()));
 			final Phenotype saved = dao.save(phenotype);
 			observationDbIds.add(saved.getPhenotypeId());
 		}
 		return observationDbIds;
+	}
+
+	private Map<Integer, List<ValueReference>> getCategoriesForCategoricalVariables(final List<ObservationDto> observations) {
+		final Set<Integer> variableIds =
+			observations.stream().map((o) -> Integer.valueOf(o.getObservationVariableDbId())).collect(Collectors.toSet());
+		return this.daoFactory.getCvTermRelationshipDao().getCategoriesForCategoricalVariables(new ArrayList<>(variableIds));
+
+	}
+
+	private Integer resolveCategoricalIdValue(final Integer variableId, final String value,
+		final Map<Integer, List<ValueReference>> validValuesForCategoricalVariables) {
+		if (validValuesForCategoricalVariables.containsKey(variableId)) {
+			final Optional<ValueReference> match =
+				validValuesForCategoricalVariables.get(variableId).stream().filter(v -> v.getName().equals(value)).findAny();
+			return match.isPresent() ? Integer.valueOf(match.get().getKey()) : null;
+		}
+		return null;
 	}
 }
