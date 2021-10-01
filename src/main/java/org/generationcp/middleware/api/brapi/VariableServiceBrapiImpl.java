d7666db75a071b6fd3d23e074a6f896d26f06666
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2021-10-01 10:02:30 +0800

    Added logic to retrieve external references
    IBP-5014

--- src/main/java/org/generationcp/middleware/api/brapi/VariableServiceBrapiImpl.java
+++ src/main/java/org/generationcp/middleware/api/brapi/VariableServiceBrapiImpl.java
@@ -1,13 +1,20 @@
 package org.generationcp.middleware.api.brapi;
 
+import org.generationcp.middleware.api.brapi.v2.germplasm.ExternalReferenceDTO;
 import org.generationcp.middleware.domain.search_request.brapi.v2.VariableSearchRequestDTO;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 import org.generationcp.middleware.manager.DaoFactory;
+import org.generationcp.middleware.service.api.sample.SampleObservationDto;
 import org.generationcp.middleware.service.api.study.VariableDTO;
 import org.springframework.data.domain.Pageable;
+import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class VariableServiceBrapiImpl implements VariableServiceBrapi{
 
@@ -22,9 +29,31 @@ public class VariableServiceBrapiImpl implements VariableServiceBrapi{
         this.sessionProvider = sessionProvider;
     }
 
-    public List<VariableDTO> getObservationVariables(VariableSearchRequestDTO requestDTO, Pageable pageable) {
+    public List<VariableDTO> getObservationVariables(final String crop, VariableSearchRequestDTO requestDTO, Pageable pageable) {
         final List<VariableDTO> variableDTOS = this.daoFactory.getCvTermDao().getObservationVariables(requestDTO, pageable);
+        if(!CollectionUtils.isEmpty(variableDTOS)) {
+            final List<String> variableIds = new ArrayList<>(variableDTOS.stream().map(VariableDTO::getObservationVariableDbId)
+                    .collect(Collectors.toSet()));
+            variableIds.addAll(variableDTOS.stream().map(variableDTO -> variableDTO.getTrait().getTraitDbId())
+                    .collect(Collectors.toSet()));
+            variableIds.addAll(variableDTOS.stream().map(variableDTO -> variableDTO.getScale().getScaleDbId())
+                    .collect(Collectors.toSet()));
+            variableIds.addAll(variableDTOS.stream().map(variableDTO -> variableDTO.getMethod().getMethodDbId())
+                    .collect(Collectors.toSet()));
 
+            final Map<String, List<ExternalReferenceDTO>> externalReferencesMap =
+                    this.daoFactory.getVariableExternalReferenceDAO().getExternalReferences(variableIds).stream()
+                            .collect(groupingBy(
+                                    ExternalReferenceDTO::getEntityId));
+            for(final VariableDTO dto: variableDTOS) {
+                dto.setCommonCropName(crop);
+                dto.setCrop(crop);
+                dto.setExternalReferences(externalReferencesMap.get(dto.getObservationVariableDbId()));
+                dto.getScale().setExternalReferences(externalReferencesMap.get(dto.getScale().getScaleDbId()));
+                dto.getMethod().setExternalReferences(externalReferencesMap.get(dto.getMethod().getMethodDbId()));
+                dto.getTrait().setExternalReferences(externalReferencesMap.get(dto.getTrait().getTraitDbId()));
+            }
+        }
         return variableDTOS;
     }
 
--- src/main/java/org/generationcp/middleware/dao/VariableExternalReferenceDAO.java
+++ src/main/java/org/generationcp/middleware/dao/VariableExternalReferenceDAO.java
@@ -3,6 +3,7 @@ package org.generationcp.middleware.dao;
 import org.generationcp.middleware.api.brapi.v2.germplasm.ExternalReferenceDTO;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.pojos.ExperimentExternalReference;
+import org.generationcp.middleware.pojos.VariableExternalReference;
 import org.hibernate.HibernateException;
 import org.hibernate.SQLQuery;
 import org.hibernate.transform.AliasToBeanResultTransformer;
@@ -11,15 +12,15 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-public class VariableExternalReferenceDAO extends GenericDAO<ExperimentExternalReference, Integer> {
+public class VariableExternalReferenceDAO extends GenericDAO<VariableExternalReference, Integer> {
 
     private static final Logger LOG = LoggerFactory.getLogger(VariableExternalReferenceDAO.class);
 
-    public List<ExternalReferenceDTO> getExternalReferences(final List<Integer> cvtermIds) {
+    public List<ExternalReferenceDTO> getExternalReferences(final List<String> cvtermIds) {
 
         try {
             final SQLQuery sqlQuery = this.getSession().createSQLQuery(
-                    "SELECT CAST(nd_experiment_id AS CHAR(255)) as entityId, reference_id as referenceID, reference_source as referenceSource "
+                    "SELECT CAST(cvterm_id AS CHAR(255)) as entityId, reference_id as referenceID, reference_source as referenceSource "
                             + "FROM external_reference_variable WHERE cvterm_id IN (:cvtermIds)");
 
             sqlQuery.addScalar("entityId").addScalar("referenceID").addScalar("referenceSource")
