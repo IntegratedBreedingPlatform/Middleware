4db91457c265ed56723bfef9796c7492796ebe6f
Nahuel Soldevilla <nahuel@leafnode.io>
2021-10-15 16:44:08 -0300

    IBP-5029: Basic COP api services: get matrix and csv (Incomplete)

--- src/main/java/org/generationcp/middleware/api/germplasm/pedigree/cop/CopService.java
+++ src/main/java/org/generationcp/middleware/api/germplasm/pedigree/cop/CopService.java
@@ -1,8 +1,14 @@
 package org.generationcp.middleware.api.germplasm.pedigree.cop;
 
+import com.google.common.collect.Table;
+
+import java.util.List;
+
 public interface CopService {
 
 	double coefficientOfParentage(int gid1, int gid2);
 
 	double coefficientOfInbreeding(int gid);
+
+	Table<Integer, Integer, Double> coefficientOfParentage(List<Integer> gids);
 }
--- src/main/java/org/generationcp/middleware/api/germplasm/pedigree/cop/CopServiceImpl.java
+++ src/main/java/org/generationcp/middleware/api/germplasm/pedigree/cop/CopServiceImpl.java
@@ -1,5 +1,7 @@
 package org.generationcp.middleware.api.germplasm.pedigree.cop;
 
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
 import org.generationcp.middleware.api.germplasm.pedigree.GermplasmPedigreeService;
 import org.generationcp.middleware.api.germplasm.pedigree.GermplasmTreeNode;
 import org.generationcp.middleware.hibernate.HibernateSessionProvider;
@@ -7,6 +9,15 @@ import org.generationcp.middleware.manager.DaoFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.time.Duration.between;
+import static org.apache.commons.lang3.time.DurationFormatUtils.formatDurationHMS;
+import static org.generationcp.middleware.util.Debug.debug;
 
 @Service
 @Transactional
@@ -34,5 +45,63 @@ public class CopServiceImpl implements CopService {
 		final GermplasmTreeNode gidTree = this.germplasmPedigreeService.getGermplasmPedigreeTree(gid, null, true);
 		final CopCalculation copCalculation = new CopCalculation();
 		return copCalculation.coefficientOfInbreeding(gidTree);
+	}
+
+	@Override
+	public Table<Integer, Integer, Double> coefficientOfParentage(final List<Integer> gids) {
+		final Table<Integer, Integer, Double> matrix = HashBasedTable.create();
+		final CopCalculation copCalculation = new CopCalculation(matrix);
+		final Map<Integer, GermplasmTreeNode> nodes = new HashMap<>();
+
+		// TODO verify/improve perf
+
+		for (final Integer gid1 : gids) {
+			for (final Integer gid2 : gids) {
+				if (!(matrix.contains(gid1, gid2) || matrix.contains(gid2, gid1))) {
+
+					final GermplasmTreeNode gid1Tree;
+					if (!nodes.containsKey(gid1)) {
+						debug("retrieving pedigree: gid=%d", gid1);
+						final Instant start = Instant.now();
+						gid1Tree = this.germplasmPedigreeService.getGermplasmPedigreeTree(gid1, null, true);
+						final Instant end = Instant.now();
+						debug("pedigree retrieved: gid=%d, Duration: %s", gid1, formatDurationHMS(between(start, end).toMillis()));
+						this.trackNodes(gid1Tree, nodes);
+					} else {
+						gid1Tree = nodes.get(gid1);
+					}
+
+					final GermplasmTreeNode gid2Tree;
+					if (!nodes.containsKey(gid2)) {
+						debug("retrieving pedigree: gid=%d", gid2);
+						final Instant start = Instant.now();
+						gid2Tree = this.germplasmPedigreeService.getGermplasmPedigreeTree(gid2, null, true);
+						final Instant end = Instant.now();
+						debug("pedigree retrieved: gid=%d, Duration: %s", gid2, formatDurationHMS(between(start, end).toMillis()));
+						this.trackNodes(gid2Tree, nodes);
+					} else {
+						gid2Tree = nodes.get(gid2);
+					}
+
+					copCalculation.coefficientOfParentage(gid1Tree, gid2Tree);
+				}
+			}
+		}
+		return matrix;
+	}
+
+	private void trackNodes(final GermplasmTreeNode gid1Tree, final Map<Integer, GermplasmTreeNode> nodes) {
+		// TODO stream.iterate() ... limit()
+		nodes.put(gid1Tree.getGid(), gid1Tree);
+		GermplasmTreeNode femaleParentNode = gid1Tree.getFemaleParentNode();
+		while (femaleParentNode != null) {
+			nodes.put(femaleParentNode.getGid(), femaleParentNode);
+			femaleParentNode = femaleParentNode.getFemaleParentNode();
+		}
+		GermplasmTreeNode maleParentNode = gid1Tree.getMaleParentNode();
+		while (maleParentNode != null) {
+			nodes.put(maleParentNode.getGid(), maleParentNode);
+			maleParentNode = maleParentNode.getMaleParentNode();
+		}
 	}
 }
