f5ecabc51e481582afd5dd50fd10be005cb0dd77
mderamos <marc.ramos@leafnode.io>
2022-03-07 09:31:07 +0800

    Added unit test
    IBP-5471

--- src/test/java/org/generationcp/middleware/brapi/VariableServiceBrapiImplTest.java
+++ src/test/java/org/generationcp/middleware/brapi/VariableServiceBrapiImplTest.java
@@ -6,6 +6,7 @@ import org.generationcp.middleware.WorkbenchTestDataUtil;
 import org.generationcp.middleware.api.brapi.StudyServiceBrapi;
 import org.generationcp.middleware.api.brapi.TrialServiceBrapi;
 import org.generationcp.middleware.api.brapi.VariableServiceBrapi;
+import org.generationcp.middleware.api.brapi.v2.attribute.AttributeDTO;
 import org.generationcp.middleware.api.brapi.v2.study.StudyImportRequestDTO;
 import org.generationcp.middleware.api.brapi.v2.trial.TrialImportRequestDTO;
 import org.generationcp.middleware.domain.dms.StudySummary;
@@ -13,6 +14,7 @@ import org.generationcp.middleware.domain.oms.CvId;
 import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.domain.ontology.DataType;
 import org.generationcp.middleware.domain.ontology.VariableType;
+import org.generationcp.middleware.domain.search_request.brapi.v2.AttributeSearchRequestDTO;
 import org.generationcp.middleware.domain.search_request.brapi.v2.VariableSearchRequestDTO;
 import org.generationcp.middleware.manager.DaoFactory;
 import org.generationcp.middleware.manager.api.WorkbenchDataManager;
@@ -135,6 +137,70 @@ public class VariableServiceBrapiImplTest extends IntegrationTestBase {
 		Assert.assertEquals(1, dto.getExternalReferences().size());
 		Assert.assertEquals(varExRef.getSource(), dto.getExternalReferences().get(0).getReferenceSource());
 		Assert.assertEquals(varExRef.getReferenceId(), dto.getExternalReferences().get(0).getReferenceID());
+	}
+
+	@Test
+	public void testGetGermplasmAttributes() {
+		final List<String> possibleValues = new ArrayList<>();
+		possibleValues.add(RandomStringUtils.randomAlphabetic(5));
+		possibleValues.add(RandomStringUtils.randomAlphabetic(5));
+		possibleValues.add(RandomStringUtils.randomAlphabetic(5));
+		final CVTerm categoricalVariable = this.testDataInitializer.createCategoricalVariable(VariableType.GERMPLASM_PASSPORT, possibleValues);
+		final VariableOverrides vo = new VariableOverrides();
+		vo.setAlias(RandomStringUtils.randomAlphabetic(10));
+		vo.setVariableId(categoricalVariable.getCvTermId());
+		this.daoFactory.getVariableProgramOverridesDao().save(vo);
+
+		final CvTermExternalReference varExRef = new CvTermExternalReference();
+		varExRef.setCvTerm(categoricalVariable);
+		varExRef.setSource(RandomStringUtils.randomAlphabetic(5));
+		varExRef.setReferenceId(RandomStringUtils.randomAlphabetic(5));
+		this.daoFactory.getCvTermExternalReferenceDAO().save(varExRef);
+
+		final CVTermSynonym synonym = new CVTermSynonym();
+		synonym.setSynonym(RandomStringUtils.randomAlphabetic(10));
+		synonym.setCvTermId(categoricalVariable.getCvTermId());
+		synonym.setTypeId(1230);
+		synonym.setCvTermSynonymId(categoricalVariable.getCvTermId());
+		this.daoFactory.getCvTermSynonymDao().save(synonym);
+
+		this.sessionProvder.getSession().flush();
+
+		final AttributeSearchRequestDTO searchRequestDTO = new AttributeSearchRequestDTO();
+		searchRequestDTO.setAttributeDbIds(Collections.singletonList(categoricalVariable.getCvTermId().toString()));
+		final String cropName = "MAIZE";
+		final List<AttributeDTO> attributeDTOS = this.variableServiceBrapi.getGermplasmAttributes(searchRequestDTO, null);
+		Assert.assertEquals(1, attributeDTOS.size());
+		final AttributeDTO dto = attributeDTOS.get(0);
+		Assert.assertEquals(categoricalVariable.getCvTermId().toString(), dto.getAttributeDbId());
+		Assert.assertEquals(categoricalVariable.getDefinition(), dto.getAttributeDescription());
+		Assert.assertEquals(categoricalVariable.getCvTermId().toString(), dto.getOntologyReference().getOntologyDbId());
+		Assert.assertEquals(vo.getAlias(), dto.getAttributeName());
+		Assert.assertEquals(categoricalVariable.getName(), dto.getOntologyReference().getOntologyName());
+		Assert.assertNotNull(dto.getMethod());
+		Assert.assertNotNull(dto.getTrait());
+		Assert.assertNotNull(dto.getScale());
+		Assert.assertEquals(3, dto.getScale().getValidValues().getCategories().size());
+		Assert.assertEquals(1, dto.getSynonyms().size());
+		Assert.assertEquals(synonym.getSynonym(), dto.getSynonyms().get(0));
+		Assert.assertEquals(1, dto.getExternalReferences().size());
+		Assert.assertEquals(varExRef.getSource(), dto.getExternalReferences().get(0).getReferenceSource());
+		Assert.assertEquals(varExRef.getReferenceId(), dto.getExternalReferences().get(0).getReferenceID());
+	}
+
+	@Test
+	public void testCountGermplasmAttributes() {
+		final List<String> possibleValues = new ArrayList<>();
+		possibleValues.add(RandomStringUtils.randomAlphabetic(5));
+		possibleValues.add(RandomStringUtils.randomAlphabetic(5));
+		possibleValues.add(RandomStringUtils.randomAlphabetic(5));
+		final CVTerm categoricalVariable = this.testDataInitializer.createCategoricalVariable(VariableType.GERMPLASM_ATTRIBUTE, possibleValues);
+
+		final AttributeSearchRequestDTO searchRequestDTO = new AttributeSearchRequestDTO();
+		searchRequestDTO.setAttributeDbIds(Collections.singletonList(categoricalVariable.getCvTermId().toString()));
+
+		final long count = this.variableServiceBrapi.countGermplasmAttributes(searchRequestDTO);
+		Assert.assertEquals((long) 1, count);
 	}
 
 	@Test
