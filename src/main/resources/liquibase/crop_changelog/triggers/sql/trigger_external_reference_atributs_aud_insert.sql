08003777c8d32a0079532689be7ef466d41a364f
cheryleigh <chery@leafnode.io>
2022-03-22 10:58:45 +0800

    IBP-5516: add triggers for external_reference_atributs

---
+++ src/main/resources/liquibase/crop_changelog/triggers/sql/trigger_external_reference_atributs_aud_insert.sql
@@ -0,0 +1,15 @@
+GO
+
+CREATE TRIGGER trigger_external_reference_atributs_aud_insert
+    AFTER INSERT
+    ON external_reference_atributs
+    FOR EACH ROW
+BEGIN
+    IF (SELECT checkTableIsAudited('EXTERNAL_REFERENCE_ATRIBUTS') = 1) THEN
+        INSERT INTO external_reference_atributs_aud(rev_type, id, gid, reference_id, reference_source, created_by, modified_by, created_date, modified_date)
+        VALUES (0, NEW.id, NEW.gid, NEW.reference_id, NEW.reference_source, NEW.created_by, NEW.modified_by, NEW.created_date, NEW.modified_date);
+    END IF;
+
+END;
+
+GO
