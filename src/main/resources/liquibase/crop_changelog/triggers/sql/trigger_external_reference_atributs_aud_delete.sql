c22f9f24b7ff414bbb25d4085752bca46f196a53
cheryleigh <chery@leafnode.io>
2022-03-22 18:18:06 +0800

    correct foreign key ID
    IBP-5516

--- src/main/resources/liquibase/crop_changelog/triggers/sql/trigger_external_reference_atributs_aud_delete.sql
+++ src/main/resources/liquibase/crop_changelog/triggers/sql/trigger_external_reference_atributs_aud_delete.sql
@@ -9,8 +9,8 @@ BEGIN
         SET @modifiedBy = getAuditModifiedByValue(OLD.modified_by);
         SET @modifiedDate = (SELECT COALESCE(OLD.modified_date, CURRENT_TIMESTAMP));
     
-        INSERT INTO external_reference_atributs_aud(rev_type, id, gid, reference_id, reference_source, created_by, modified_by, created_date, modified_date)
-        VALUES (2, OLD.id, OLD.gid, OLD.reference_id, OLD.reference_source, OLD.created_by, @modifiedBy, OLD.created_date, @modifiedDate);
+        INSERT INTO external_reference_atributs_aud(rev_type, id, aid, reference_id, reference_source, created_by, modified_by, created_date, modified_date)
+        VALUES (2, OLD.id, OLD.aid, OLD.reference_id, OLD.reference_source, OLD.created_by, @modifiedBy, OLD.created_date, @modifiedDate);
     END IF;
 
 END;
--- src/main/resources/liquibase/crop_changelog/triggers/sql/trigger_external_reference_atributs_aud_update.sql
+++ src/main/resources/liquibase/crop_changelog/triggers/sql/trigger_external_reference_atributs_aud_update.sql
@@ -6,8 +6,8 @@ CREATE TRIGGER trigger_external_reference_atributs_aud_update
     FOR EACH ROW
 BEGIN
     IF (SELECT checkTableIsAudited('EXTERNAL_REFERENCE_ATRIBUTS') = 1) THEN
-        INSERT INTO external_reference_atributs_aud(rev_type, id, gid, reference_id, reference_source, created_by, modified_by, created_date, modified_date)
-        VALUES (1, NEW.id, NEW.gid, NEW.reference_id, NEW.reference_source, NEW.created_by, NEW.modified_by, NEW.created_date, NEW.modified_date);
+        INSERT INTO external_reference_atributs_aud(rev_type, id, aid, reference_id, reference_source, created_by, modified_by, created_date, modified_date)
+        VALUES (1, NEW.id, NEW.aid, NEW.reference_id, NEW.reference_source, NEW.created_by, NEW.modified_by, NEW.created_date, NEW.modified_date);
     END IF;
 
 END;
