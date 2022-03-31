14ec8ef721e8f5210fc84380b98a1ed60086728e
Nahuel Soldevilla <nahuel@leafnode.io>
2022-03-31 11:15:16 -0300

    IBP-5527: save cop results using file storage (s3/sftp)
    - change FileStorageService scope to prototype to use it outside of
    request (async context)
    - return byte[] for csv download (compatible with file storage
    interface)
    - if file storage is not configured, still save to temp folder (TODO
    review)

--- src/main/java/org/generationcp/middleware/api/germplasm/pedigree/cop/CopUtils.java
+++ src/main/java/org/generationcp/middleware/api/germplasm/pedigree/cop/CopUtils.java
@@ -7,8 +7,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public abstract class CopUtils {
+
+	/**
+	 * AWS S3 uses forward slash to identify folders
+	 */
+	public static final String FILE_STORAGE_PATH_SLASH = "/";
+	public static final String FILE_STORAGE_PATH_ROOT = "cop";
 
 	public static File generateFile(final CopResponse results, final String fileNameFullPath) throws IOException {
 		try (final CSVWriter csvWriter = new CSVWriter(
@@ -21,8 +28,25 @@ public abstract class CopUtils {
 
 	}
 
-	public static String getFileFullPath(final Integer listId) {
+	public static String getFileTempFullPath(final Integer listId) {
 		final String tmpdir = System.getProperty("java.io.tmpdir");
-		return tmpdir + File.separator + "COP-listId-" + listId + ".csv";
+		return tmpdir + File.separator + getFileName(listId);
+	}
+
+	public static String getStorageFilePath(final Integer listId) {
+		return FILE_STORAGE_PATH_ROOT + FILE_STORAGE_PATH_SLASH + getFileName(listId);
+	}
+
+	public static String getFileName(final Integer listId) {
+		return "COP-listId-" + listId + ".csv";
+	}
+
+	public static byte[] matrixToCsvBytes(final List<String[]> csvLines) {
+		final StringBuilder sb = new StringBuilder();
+		for (final String[] csvLine : csvLines) {
+			sb.append(String.join(";", csvLine));
+			sb.append("\n");
+		}
+		return sb.toString().getBytes(StandardCharsets.UTF_8);
 	}
 }
