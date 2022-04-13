dc54e9102b0674a3cc7b26be5ef1bce79a89e40c
Nahuel Soldevilla <nahuel@leafnode.io>
2022-04-13 17:27:19 -0300

    IBP-5575: BTypeEnum: distinguish between id and value
    Use btype value = 1 in calculation for special case btype=2
    TODO confirm with graham

--- src/main/java/org/generationcp/middleware/api/germplasm/pedigree/cop/BTypeEnum.java
+++ src/main/java/org/generationcp/middleware/api/germplasm/pedigree/cop/BTypeEnum.java
@@ -11,31 +11,31 @@ public enum BTypeEnum {
 	 * homozygous, Fully inbreed, self pollinating
 	 * From fortran code: FOR SF CROPS ASSUMING F4 FOR LINES WITH UNKNOWN SOURCE BUT KNOWN PARENTS
 	 */
-	SELF_FERTILIZING_F4(2, "Self Fertilizing with unknown source but known parents"),
+	SELF_FERTILIZING_F4(2, 1, "Self Fertilizing with unknown source but known parents"),
 	/**
 	 * homozygous, Fully inbreed, self pollinating
 	 */
-	SELF_FERTILIZING(1, "Self Fertilizing"),
+	SELF_FERTILIZING(1, 1, "Self Fertilizing"),
 	/**
 	 * heterozygous, No inbreeding
 	 */
-	CROSS_FERTILIZING(0, "Cross Fertilizing");
+	CROSS_FERTILIZING(0, 0, "Cross Fertilizing");
 
+	/*
+	 * btype=2 not meant to be used as value? we separate then between id and value, so that value can be used
+	 */
+	private final double id;
 	private final double value;
 	private final String description;
 
-	BTypeEnum(final int value, final String description) {
+	BTypeEnum(final double id, final double value, final String description) {
+		this.id = id;
 		this.value = value;
 		this.description = description;
 	}
 
-	public static BTypeEnum fromValue(final int bType) {
-		for (BTypeEnum bTypeEnum : BTypeEnum.values()) {
-			if (bTypeEnum.value == bType) {
-				return bTypeEnum;
-			}
-		}
-		return null;
+	public double getId() {
+		return id;
 	}
 
 	public double getValue() {
