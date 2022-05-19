dfd2431dcab6107198e6ea3597b7a351e5d8a712
cuenyad <cuenya.diego@gmail.com>
2022-05-19 13:16:06 -0300

    Replace Export Germplasm List by Export Study Entries Label printing.
    Issue: IBP-5395/IBP-5649
    Review: none

--- src/main/java/org/generationcp/middleware/domain/labelprinting/LabelPrintingType.java
+++ src/main/java/org/generationcp/middleware/domain/labelprinting/LabelPrintingType.java
@@ -3,7 +3,8 @@ package org.generationcp.middleware.domain.labelprinting;
 public enum LabelPrintingType {
 
 	OBSERVATION_DATASET("ObservationDataset"), SUBOBSERVATION_DATASET("SubObservationDataset"),
-	LOT("Lot"), GERMPLASM("Germplasm"), GERMPLASM_LIST("Germplasm List");
+	LOT("Lot"), GERMPLASM("Germplasm"), GERMPLASM_LIST("Germplasm List"),
+	STUDY_ENTRIES("Study Entries");
 
 	private String code;
 
