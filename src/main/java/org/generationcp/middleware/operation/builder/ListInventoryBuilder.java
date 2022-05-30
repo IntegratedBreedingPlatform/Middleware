10d79929466d381db83faeb2c0f7b4888bf64ba8
Clarysabel Tovar <clarysabel@leafnode.io>
2022-05-30 16:44:42 -0300

    Removed ListInventoryBuilder unused code IBP-5717

--- src/main/java/org/generationcp/middleware/operation/builder/ListInventoryBuilder.java
+++ src/main/java/org/generationcp/middleware/operation/builder/ListInventoryBuilder.java
@@ -5,9 +5,6 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.generationcp.middleware.domain.inventory.GermplasmInventory;
 import org.generationcp.middleware.domain.inventory.ListDataInventory;
-import org.generationcp.middleware.domain.inventory.ListEntryLotDetails;
-import org.generationcp.middleware.domain.inventory.LotDetails;
-import org.generationcp.middleware.domain.inventory.util.LotTransformer;
 import org.generationcp.middleware.domain.oms.Term;
 import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
@@ -15,8 +12,6 @@ import org.generationcp.middleware.hibernate.HibernateSessionProvider;
 import org.generationcp.middleware.manager.DaoFactory;
 import org.generationcp.middleware.pojos.Germplasm;
 import org.generationcp.middleware.pojos.GermplasmListData;
-import org.generationcp.middleware.pojos.Location;
-import org.generationcp.middleware.pojos.ims.Lot;
 import org.generationcp.middleware.pojos.oms.CVTerm;
 import org.generationcp.middleware.pojos.oms.CVTermRelationship;
 
@@ -169,27 +164,6 @@ public class ListInventoryBuilder extends Builder {
 		}
 	}
 
-	public List<LotDetails> retrieveInventoryLotsForGermplasm(final Integer gid) throws MiddlewareQueryException {
-		List<LotDetails> lotDetails = null;
-		final List<Lot> lots = daoFactory.getLotDao().getLotAggregateDataForGermplasm(gid);
-		final Map<Integer, Object[]> lotStatusDataForGermplasm = daoFactory.getLotDao().getLotStatusDataForGermplasm(gid);
-		this.setLotStatusForGermplasm(lots, lotStatusDataForGermplasm);
-		lotDetails = LotTransformer.extraLotDetails(lots);
-		this.setLocationsAndScales(lotDetails);
-		return lotDetails;
-	}
-
-	public List<ListEntryLotDetails> retrieveInventoryLotsForListEntry(final Integer listId, final Integer recordId, final Integer gid)
-			throws MiddlewareQueryException {
-		List<ListEntryLotDetails> lotRows = new ArrayList<ListEntryLotDetails>();
-
-		final List<Lot> lots = daoFactory.getLotDao().getLotAggregateDataForListEntry(listId, gid);
-		lotRows = LotTransformer.extractLotDetailsForListEntry(lots, recordId);
-		this.setLocationsAndScales(lotRows);
-
-		return lotRows;
-	}
-
 	/*
 	 * Retrieve the number of lots with available balance per germplasm and available seed balance per germplsm along with overall scaleId
 	 */
@@ -232,156 +206,4 @@ public class ListInventoryBuilder extends Builder {
 		}
 	}
 
-	/*
-	 * Perform one-retrieval for central/local scales and central/local locations for list of lots
-	 */
-	private void setLocationsAndScales(final List<? extends LotDetails> lots) throws MiddlewareQueryException {
-
-		final List<Integer> locationIds = new ArrayList<Integer>();
-		final List<Integer> scaleIds = new ArrayList<Integer>();
-		final Map<Integer, List<LotDetails>> scaleLotMap = new HashMap<Integer, List<LotDetails>>();
-		final Map<Integer, List<LotDetails>> locationLotMap = new HashMap<Integer, List<LotDetails>>();
-
-		this.createScaleAndLocationMaps(lots, locationIds, scaleIds, scaleLotMap, locationLotMap);
-
-		final List<Location> allLocations = daoFactory.getLocationDAO().getByIds(locationIds);
-		final List<Term> allScales = new ArrayList<Term>();
-		final List<CVTerm> cvTerms = daoFactory.getCvTermDao().getByIds(scaleIds);
-		for (final CVTerm cvTerm : cvTerms) {
-			allScales.add(TermBuilder.mapCVTermToTerm(cvTerm));
-		}
-
-		Map<Integer, String> mapScaleAbbrMap = new HashMap<>();
-		Map<Integer, String> mapScaleMethodMap = new HashMap<>();
-		Map<Integer, Integer> scaleIdWiseTermId = new HashMap<>();
-		Map<Integer, Integer> scaleIdWiseMethodTermId = new HashMap<>();
-		if (!scaleIds.isEmpty()) {
-
-			final List<CVTermRelationship> relationshipsOfScales =
-					daoFactory.getCvTermRelationshipDao().getBySubjectIdsAndTypeId(scaleIds, TermId.HAS_SCALE.getId());
-
-			List<Integer> listObjectIds = new ArrayList<>();
-			if (relationshipsOfScales != null) {
-				for (CVTermRelationship relationship : relationshipsOfScales) {
-					scaleIdWiseTermId.put(relationship.getSubjectId(), relationship.getObjectId());
-					listObjectIds.add(relationship.getObjectId());
-				}
-
-				List<CVTerm> listScaleCvTerm = daoFactory.getCvTermDao().getByIds(listObjectIds);
-
-				for (final CVTerm cvTerm : listScaleCvTerm) {
-					Term term = TermBuilder.mapCVTermToTerm(cvTerm);
-					mapScaleAbbrMap.put(term.getId(), term.getName());
-				}
-
-			}
-
-			final List<CVTermRelationship> scaleMethodRelationship =
-					daoFactory.getCvTermRelationshipDao().getBySubjectIdsAndTypeId(scaleIds, TermId.HAS_METHOD.getId());
-
-			List<Integer> listMethodObjectIds = new ArrayList<>();
-			if (scaleMethodRelationship != null) {
-				for (CVTermRelationship relationship : scaleMethodRelationship) {
-					scaleIdWiseMethodTermId.put(relationship.getSubjectId(), relationship.getObjectId());
-					listMethodObjectIds.add(relationship.getObjectId());
-				}
-
-				List<CVTerm> listScaleMethodCvTerm = daoFactory.getCvTermDao().getByIds(listMethodObjectIds);
-
-				for (final CVTerm cvTerm : listScaleMethodCvTerm) {
-					Term term = TermBuilder.mapCVTermToTerm(cvTerm);
-					mapScaleMethodMap.put(term.getId(), term.getName());
-				}
-
-			}
-		}
-
-		for (final Location location : allLocations) {
-			final List<LotDetails> lotList = locationLotMap.get(location.getLocid());
-			for (final LotDetails lot : lotList) {
-				lot.setLocationOfLot(location);
-			}
-		}
-
-		for (final Term scale : allScales) {
-			final List<LotDetails> lotList = scaleLotMap.get(scale.getId());
-			for (final LotDetails lot : lotList) {
-				lot.setScaleOfLot(scale);
-				String scaleName = "";
-
-				if (scaleIdWiseTermId.containsKey(lot.getScaleId())) {
-					Integer hasScaleCvTermId = scaleIdWiseTermId.get(lot.getScaleId());
-
-					if (mapScaleAbbrMap.containsKey(hasScaleCvTermId)) {
-						scaleName = mapScaleAbbrMap.get(hasScaleCvTermId);
-					}
-
-				}
-
-				lot.setLotScaleNameAbbr(scaleName);
-
-				String scaleMethodName = "";
-				if (scaleIdWiseMethodTermId.containsKey(lot.getScaleId())) {
-					Integer hasMethodCvTermId = scaleIdWiseMethodTermId.get(lot.getScaleId());
-
-					if (mapScaleMethodMap.containsKey(hasMethodCvTermId)) {
-						scaleMethodName = mapScaleMethodMap.get(hasMethodCvTermId);
-					}
-				}
-				lot.setLotScaleMethodName(scaleMethodName);
-
-			}
-		}
-	}
-
-	// create maps of scale/location IDs to lots for easier setting of Terms and Locations
-	private void createScaleAndLocationMaps(final List<? extends LotDetails> lots, final List<Integer> locationIds,
-			final List<Integer> scaleIds, final Map<Integer, List<LotDetails>> scaleLotMap,
-			final Map<Integer, List<LotDetails>> locationLotMap) {
-
-		for (final LotDetails lot : lots) {
-			final Integer locationId = lot.getLocId();
-			final List<LotDetails> lotList = locationLotMap.get(locationId);
-			if (lotList != null) {
-				lotList.add(lot);
-			} else {
-				final List<LotDetails> listLot = new ArrayList<LotDetails>();
-				listLot.add(lot);
-				locationLotMap.put(locationId, listLot);
-			}
-			locationIds.add(locationId);
-
-			final Integer scaleId = lot.getScaleId();
-			if (scaleLotMap.get(scaleId) != null) {
-				scaleLotMap.get(scaleId).add(lot);
-			} else {
-				final List<LotDetails> listLot = new ArrayList<LotDetails>();
-				listLot.add(lot);
-				scaleLotMap.put(scaleId, listLot);
-			}
-			scaleIds.add(scaleId);
-		}
-	}
-
-	private void setLotStatusForGermplasm(final List<Lot> lots, final Map<Integer, Object[]> lotStatusMap) {
-		String lotStatus = null;
-		for (Lot lot : lots) {
-			lotStatus = "";
-			if (lotStatusMap.containsKey(lot.getId())) {
-				Object[] row = lotStatusMap.get(lot.getId());
-				BigInteger distinctStatusCount = (BigInteger) row[0];
-				if (distinctStatusCount.intValue() > 1) {
-					lotStatus = ListDataInventory.RESERVED;
-				} else if (distinctStatusCount.intValue() == 1) {
-					Integer status = (Integer) row[1];
-					if (status == 0) {
-						lotStatus = ListDataInventory.RESERVED;
-					} else if (status == 1) {
-						lotStatus = ListDataInventory.WITHDRAWN;
-					}
-				}
-			}
-			lot.getAggregateData().setLotStatus(lotStatus);
-		}
-	}
 }
