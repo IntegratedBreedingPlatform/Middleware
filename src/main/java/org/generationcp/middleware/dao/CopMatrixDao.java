9eb2cd8e1c83fceb5575f433c5015beab4acd883
Nahuel Soldevilla <nahuel@leafnode.io>
2022-08-31 12:24:11 -0300

    IBP-5826: COP sort: sort COP results in gids list param order

--- src/main/java/org/generationcp/middleware/dao/CopMatrixDao.java
+++ src/main/java/org/generationcp/middleware/dao/CopMatrixDao.java
@@ -1,13 +1,15 @@
 package org.generationcp.middleware.dao;
 
-import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.ArrayTable;
 import com.google.common.collect.Table;
 import org.generationcp.middleware.pojos.CopMatrix;
 import org.hibernate.Session;
-import org.hibernate.criterion.Restrictions;
 
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+
+import static java.util.stream.Collectors.toCollection;
 
 public class CopMatrixDao {
 
@@ -19,14 +21,16 @@ public class CopMatrixDao {
 
 	public Table<Integer, Integer, Double> getByGids(Set<Integer> gids) {
 		//noinspection unchecked
-		final List<CopMatrix> results = this.session.createCriteria(CopMatrix.class)
-			.add(Restrictions.conjunction()
-				.add(Restrictions.in("gid1", gids))
-				.add(Restrictions.in("gid2", gids)
-				))
+		final List<CopMatrix> results = this.session.createSQLQuery("select * from cop_matrix as c "
+			+ " where c.gid1 in (:gids) and c.gid2 in (:gids) "
+			+ " order by field(gid1, :gids), field(gid2, :gids) ")
+			.addEntity(CopMatrix.class)
+			.setParameterList("gids", gids)
 			.list();
 
-		final Table<Integer, Integer, Double> matrix = HashBasedTable.create();
+		final Set<Integer> rows = results.stream().map(CopMatrix::getGid1).collect(toCollection(LinkedHashSet::new));
+		final Set<Integer> cols = results.stream().map(CopMatrix::getGid2).collect(toCollection(LinkedHashSet::new));
+		final Table<Integer, Integer, Double> matrix = ArrayTable.create(rows, cols);
 		for (final CopMatrix result : results) {
 			matrix.put(
 				result.getGermplasm1().getGid(),
