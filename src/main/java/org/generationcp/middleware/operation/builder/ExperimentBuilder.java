01ba8005ee215e401e971319bb4553fa19d3cc48
cuenyad <cuenya.diego@gmail.com>
2022-09-06 12:31:30 -0300

    Fixing tests.
    Issue: IBP-5894
    Review: none

--- src/main/java/org/generationcp/middleware/operation/builder/ExperimentBuilder.java
+++ src/main/java/org/generationcp/middleware/operation/builder/ExperimentBuilder.java
@@ -485,7 +485,9 @@ public class ExperimentBuilder extends Builder {
 			return new Variable(variableType, stockModel.getGermplasm().getGid());
 		}
 		if (standardVariable.getId() == TermId.DESIG.getId()) {
-			return new Variable(variableType, stockModel.getGermplasm().getPreferredName().getNval());
+			// Fix to test cases where the names are not getting from the germplasm
+			final Name preferredName = stockModel.getGermplasm().getPreferredName();
+			return new Variable(variableType, preferredName != null ? preferredName.getNval() : "");
 		}
 		final String val = this.findStockValue(variableType.getId(), stockModel.getProperties());
 
