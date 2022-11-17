783eeee9a89bbbbb182e05ad52366bb65b301292
cuenyad <cuenya.diego@gmail.com>
2022-11-17 15:23:24 -0300

    Moving DTOs to been used in middleware.
    IBP-6056
    Review none

---
+++ src/main/java/org/generationcp/middleware/domain/labelprinting/PresetType.java
@@ -0,0 +1,29 @@
+package org.generationcp.middleware.domain.labelprinting;
+
+public enum PresetType {
+
+	LABEL_PRINTING_PRESET ("LabelPrintingPreset"),
+	CROSSING_PRESET("CrossingPreset");
+
+	private String name;
+
+	PresetType(final String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public static PresetType getEnum(final String name) {
+		for (PresetType e : PresetType.values()) {
+			if (e.getName().equals(name))
+				return e;
+		}
+		return null;
+	}
+}
