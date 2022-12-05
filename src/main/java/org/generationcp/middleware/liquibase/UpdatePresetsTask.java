a52480dc522b7415ac3b84c0c19f4ba611e62ebe
cuenyad <cuenya.diego@gmail.com>
2022-12-05 15:48:43 -0300

    Adding Changes by PR comments.
    IBP-6056
    Review none

--- src/main/java/org/generationcp/middleware/liquibase/UpdatePresetsTask.java
+++ src/main/java/org/generationcp/middleware/liquibase/UpdatePresetsTask.java
@@ -29,29 +29,35 @@ public class UpdatePresetsTask implements liquibase.change.custom.CustomTaskChan
 		"GERMPLASM_LABEL_PRINTING_PRESET", "GERMPLASM_LIST_LABEL_PRINTING_PRESET",
 		"STUDY_ENTRIES_LABEL_PRINTING_PRESET");
 
+	private static final String SELECT_PRESETS = "SELECT * FROM program_preset where (configuration  not like %s and configuration not like %s) and tool_section in (%s) ";
+
+	private static final String SELECT_VARIABLE = "SELECT * FROM udflds WHERE ftable='NAMES' AND ftype='NAME' and fldno = %s";
+
+	private static final String SELECT_NAME = "SELECT * FROM CVTERM WHERE CVTERM_ID = %s";
+
 	private static final String UPDATE_PRESET = "update program_preset set configuration = '%s' where program_preset_id = %s";
+
 	private static final Integer MAX_FIELD_STATIC_ID = 63;
+
 	private static final Integer MAX_FIXED_TYPE_INDEX = 10000;
-	private ObjectMapper jacksonMapper;
+
 	private JdbcConnection dbConn;
 
 	@Override
 	public void execute(final Database database) throws CustomChangeException {
 
-		this.jacksonMapper = new ObjectMapper();
-		this.jacksonMapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
+		final ObjectMapper jacksonMapper = new ObjectMapper();
+		jacksonMapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
 		Statement selectStatement = null;
 		Statement updateStatement = null;
 
 		try {
 			this.dbConn = (JdbcConnection) database.getConnection();
-			selectStatement = dbConn.createStatement();
-			updateStatement = dbConn.createStatement();
+			selectStatement = this.dbConn.createStatement();
+			updateStatement = this.dbConn.createStatement();
 
-			final ResultSet rs = selectStatement.executeQuery(
-				"SELECT * FROM program_preset where (configuration  not like '%VARIABLE%' and configuration not like '%STATIC%') "
-					+ "and tool_section in (" + UpdatePresetsTask.TOOL_SECTIONS.stream().collect(Collectors.joining("','", "'", "'"))
-					+ ")");
+			final String toolSetions = UpdatePresetsTask.TOOL_SECTIONS.stream().collect(Collectors.joining("','", "'", "'"));
+			final ResultSet rs = selectStatement.executeQuery(String.format(UpdatePresetsTask.SELECT_PRESETS,"'%VARIABLE%'","'%STATIC%'", toolSetions));
 
 			while (rs.next()) {
 				final String programPresetId = rs.getString("program_preset_id");
@@ -59,14 +65,13 @@ public class UpdatePresetsTask implements liquibase.change.custom.CustomTaskChan
 
 				try {
 					final LabelPrintingPresetDTO labelPrintingPresetDTO =
-						this.jacksonMapper.readValue(configuration, LabelPrintingPresetDTO.class);
+						jacksonMapper.readValue(configuration, LabelPrintingPresetDTO.class);
 					final List<List<String>> selectedFields = new ArrayList<>();
-					final Statement finalSelectStatement = selectStatement;
 					labelPrintingPresetDTO.getSelectedFields().stream().forEach(list -> {
 						selectedFields.add(list.stream().map((fieldId) -> {
 							try {
 								return this.concatenateFieldTypeName(fieldId);
-							} catch (Exception e) {
+							} catch (final Exception e) {
 								throw new RuntimeException(e);
 							}
 						}).filter(Objects::nonNull).collect(Collectors.toList()));
@@ -78,7 +83,7 @@ public class UpdatePresetsTask implements liquibase.change.custom.CustomTaskChan
 							.map((fieldId) -> {
 								try {
 									return this.concatenateFieldTypeName(fieldId);
-								} catch (Exception e) {
+								} catch (final Exception e) {
 									throw new RuntimeException(e);
 								}
 							}).filter(Objects::nonNull).collect(Collectors.toList());
@@ -86,16 +91,16 @@ public class UpdatePresetsTask implements liquibase.change.custom.CustomTaskChan
 					}
 
 					final String updatedConfiguration =
-						this.jacksonMapper.writerWithView(PresetDTO.View.Configuration.class).writeValueAsString(labelPrintingPresetDTO);
+						jacksonMapper.writerWithView(PresetDTO.View.Configuration.class).writeValueAsString(labelPrintingPresetDTO);
 
 					final String updateSql = String.format(UpdatePresetsTask.UPDATE_PRESET, updatedConfiguration, programPresetId);
 					updateStatement.execute(updateSql);
-				} catch (IOException e1) {
-					dbConn.rollback();
+				} catch (final IOException e1) {
+					this.dbConn.rollback();
 					throw new CustomChangeException(e1);
 				}
 			}
-			dbConn.commit();
+			this.dbConn.commit();
 		} catch (final SQLException | DatabaseException e) {
 			throw new CustomChangeException(e);
 		} finally {
@@ -113,11 +118,11 @@ public class UpdatePresetsTask implements liquibase.change.custom.CustomTaskChan
 		final Integer id = Integer.valueOf(fieldId);
 		if (id <= UpdatePresetsTask.MAX_FIELD_STATIC_ID) {
 			return "STATIC_" + id;
-		} else if (isVariable(id)) {
+		} else if (this.isVariable(id)) {
 			return "VARIABLE_" + id;
-		} else if (isVariable(id - UpdatePresetsTask.MAX_FIXED_TYPE_INDEX)) {
+		} else if (this.isVariable(id - UpdatePresetsTask.MAX_FIXED_TYPE_INDEX)) {
 			return "VARIABLE_" + (id - UpdatePresetsTask.MAX_FIXED_TYPE_INDEX);
-		} else if (isName(id - UpdatePresetsTask.MAX_FIXED_TYPE_INDEX)) {
+		} else if (this.isName(id - UpdatePresetsTask.MAX_FIXED_TYPE_INDEX)) {
 			return "NAME_" + (id - UpdatePresetsTask.MAX_FIXED_TYPE_INDEX);
 		}
 		return null;
@@ -125,7 +130,7 @@ public class UpdatePresetsTask implements liquibase.change.custom.CustomTaskChan
 
 	private boolean isVariable(final Integer fieldId) throws SQLException, DatabaseException {
 		final Statement selectStatement = this.dbConn.createStatement();
-		final ResultSet rs = selectStatement.executeQuery("SELECT * FROM CVTERM WHERE CVTERM_ID = " + fieldId);
+		final ResultSet rs = selectStatement.executeQuery(String.format(UpdatePresetsTask.SELECT_VARIABLE, fieldId));
 		final boolean isVariable = rs.next();
 		selectStatement.close();
 		return isVariable;
@@ -133,7 +138,7 @@ public class UpdatePresetsTask implements liquibase.change.custom.CustomTaskChan
 
 	private boolean isName(final Integer fieldId) throws SQLException, DatabaseException {
 		final Statement selectStatement = this.dbConn.createStatement();
-		final ResultSet rs = selectStatement.executeQuery("SELECT * FROM udflds WHERE ftable='NAMES' AND ftype='NAME' and fldno = " + fieldId);
+		final ResultSet rs = selectStatement.executeQuery(String.format(UpdatePresetsTask.SELECT_NAME, fieldId));
 		final boolean isName = rs.next();
 		selectStatement.close();
 		return isName;
