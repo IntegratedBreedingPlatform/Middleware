4b824208ccbeab093474e0c01a646b258fd72205
Clarysabel Tovar <clarysabel@leafnode.io>
2022-12-20 14:35:11 -0300

    Removed finals IBP-6078

--- src/main/java/org/generationcp/middleware/service/api/permission/PermissionService.java
+++ src/main/java/org/generationcp/middleware/service/api/permission/PermissionService.java
@@ -7,9 +7,9 @@ import java.util.Set;
 
 public interface PermissionService {
 
-	List<PermissionDto> getPermissions(final Integer userId, final String cropName, final Integer programId);
+	List<PermissionDto> getPermissions(Integer userId, String cropName, Integer programId);
 
-	List<PermissionDto> getPermissionLinks(final Integer userId, final String cropName, final Integer programId);
+	List<PermissionDto> getPermissionLinks(Integer userId, String cropName, Integer programId);
 
 	PermissionDto getPermissionTree(Integer roleTypeId);
 
