4edec1542b1dfa1844ff60cdadfd9dc5b33c8a71
cuenyad <cuenya.diego@gmail.com>
2023-03-28 14:32:54 -0300

    Implement rank column in PERMISSION table and reorder Manage Studies granular permissions.
    Issue: IBP-5924
    Review: none

--- src/main/java/org/generationcp/middleware/pojos/workbench/Permission.java
+++ src/main/java/org/generationcp/middleware/pojos/workbench/Permission.java
@@ -44,6 +44,9 @@ public class Permission {
 	@OneToMany(mappedBy = "permission", fetch = FetchType.EAGER)
 	private final List<RoleTypePermission> roleTypePermissions = new ArrayList<>();
 
+	@Column(name = "rank", nullable = false)
+	private Integer rank;
+
 	public List<RoleTypePermission> getRoleTypePermissions() {
 		return this.roleTypePermissions;
 	}
