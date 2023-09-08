e0ddeb1f0ea51cbed3aadacb3a7ad19718c4c4e5
mderamos <marc.ramos@leafnode.io>
2023-09-08 10:58:06 +0800

    Added     VIEW_PEDIGREE_INFORMATION permission
    IBP-6388

--- src/main/java/org/generationcp/middleware/pojos/workbench/PermissionsEnum.java
+++ src/main/java/org/generationcp/middleware/pojos/workbench/PermissionsEnum.java
@@ -59,7 +59,9 @@ public enum PermissionsEnum {
 	MS_MANAGE_CONFIRMED_OBSERVATIONS,
 	MS_ACCEPT_PENDING_OBSERVATION,
 
-	MS_VIEW_GERMPLASM_DETAILS;
+	MS_VIEW_GERMPLASM_DETAILS,
+
+	VIEW_PEDIGREE_INFORMATION;
 
 	public static final String HAS_CREATE_LOTS_BATCH = " or hasAnyAuthority('ADMIN'"
 		+ ", 'STUDIES'"
@@ -96,4 +98,7 @@ public enum PermissionsEnum {
 
 	public static final List<String> VIEW_GERMPLASM_DETAILS_PERMISSIONS =
 			Arrays.asList(ADMIN.name(), STUDIES.name(), MANAGE_STUDIES.name(), MS_VIEW_GERMPLASM_DETAILS.name());
+
+	public static final List<String> VIEW_PEDIGREE_INFORMATION_PERMISSIONS =
+			Arrays.asList(ADMIN.name(), GERMPLASM.name(), VIEW_PEDIGREE_INFORMATION.name());
 }
